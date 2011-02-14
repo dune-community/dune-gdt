154851f377442180358cfa51f3090380ef5640ff
felix.albrecht <felix.albrecht@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-02-14 15:38:12 +0000

    moved example to examples/, added tests/, added cmake files
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@20 4028485c-44d9-4cde-a312-5a4635ee2db9

--- dune/fem/fem_functionals.hh
+++
@@ -1,6 +0,0 @@
-#ifndef DUNE_fem_functionals.hh
-#define DUNE_fem_functionals .hh
-
-// add your classes here
-
-#endif // DUNE_fem_functionals.hh
---
+++ tests/l2functional/l2functional_test.cc
@@ -0,0 +1,24 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <iostream>
+#include <dune/fem/misc/mpimanager.hh>
+
+int main(int argc, char** argv)
+{
+  try {
+    // Maybe initialize Mpi
+    Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
+    std::cout << "Hello World! This is l2functional_test." << std::endl;
+    if (Dune::MPIHelper::isFake)
+      std::cout << "This is a sequential program." << std::endl;
+    else
+      std::cout << "I am rank " << helper.rank() << " of " << helper.size() << " processes!" << std::endl;
+    return 0;
+  } catch (Dune::Exception& e) {
+    std::cerr << "Dune reported error: " << e << std::endl;
+  } catch (...) {
+    std::cerr << "Unknown exception thrown!" << std::endl;
+  }
+}
