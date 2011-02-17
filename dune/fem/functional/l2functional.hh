ae9f64cdda3c640ea6b9d2100c2ab4121055f73c
felix.albrecht <felix.albrecht@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-02-17 10:41:29 +0000

    first use of applyLocal() in L2Functional
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@33 4028485c-44d9-4cde-a312-5a4635ee2db9

--- dune/fem/functional/l2functional.hh
+++ dune/fem/functional/l2functional.hh
@@ -19,6 +19,8 @@ namespace Functionals {
   * \brief      This class represents a local DoF vector.
   *
   *             It is based upon std::vector and should be replaced by something clever in the future!
+  *
+  * \todo       Doc me, please!
   **/
 template <class ElementType>
 class LocalDoFVector
@@ -110,11 +112,19 @@ public:
 
   typedef typename InducingFunctionType::RangeFieldType RangeFieldType;
 
+  typedef LocalDoFVector<RangeFieldType> LocalDoFVectorType;
+
   L2Functional(const InducingFunctionType& inducingFunction)
     : inducingFunction_(inducingFunction)
   {
   }
 
+  /**
+    * \brief      This function represents the application of the functional to a
+    *             discrete function.
+    *
+    * \todo       Doc me, please!
+    **/
   template <class DiscreteFunctionType>
   RangeFieldType operator()(const DiscreteFunctionType& discreteFunction) const
   {
@@ -126,7 +136,6 @@ public:
     typedef typename DiscreteFunctionSpaceType::GridPartType GridPartType;
     typedef typename DiscreteFunctionSpaceType::IteratorType EntityIteratorType;
     typedef typename EntityIteratorType::Entity EntityType;
-    typedef typename EntityType::Geometry EntityGeometryType;
     typedef typename DiscreteFunctionType::RangeType RangeType;
     typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
 
@@ -138,55 +147,15 @@ public:
     for (EntityIteratorType entityIterator = discreteFunctionSpace.begin(); entityIterator != BehindLastEntityIterator;
          ++entityIterator) {
       // entity and geometry
-      const EntityType& entity                 = *entityIterator;
-      const EntityGeometryType& entityGeometry = entity.geometry();
-
-      // quadrature
-      typedef CachingQuadrature<GridPartType, 0> EntityQuadratureType;
-      typedef typename EntityQuadratureType::CoordinateType EntityCoordinateType;
-      const int quadratureOrder = (2 * discreteFunctionSpace.order() + 1);
-      const EntityQuadratureType entityQuadrature(entity, quadratureOrder);
-      const unsigned int numberOfQuadraturePoints = entityQuadrature.nop();
+      const EntityType& entity = *entityIterator;
 
       // local function and basefunction set
       const LocalFunctionType& localFunction    = discreteFunction.localFunction(entity);
       const BaseFunctionSetType baseFunctionSet = discreteFunctionSpace.baseFunctionSet(entity);
-      const unsigned numberOfLocalDoFs          = baseFunctionSet.numBaseFunctions();
 
       // local DoF and functional vector
-      typedef LocalDoFVector<RangeFieldType> LocalDoFVectorType;
       const LocalDoFVectorType localDoFVector(localFunction);
-      LocalDoFVectorType localFunctionalVector(numberOfLocalDoFs);
-
-      // do loop over all local DoFs
-      for (unsigned int localDoF = 0; localDoF < numberOfLocalDoFs; ++localDoF) {
-        // value of the L2 functional, applied to the local basefunction, associated with the local DoF
-        RangeFieldType localFunctionalValue = 0.0;
-        // do walk over quadrature points
-        for (unsigned int quadraturePoint = 0; quadraturePoint < numberOfQuadraturePoints; ++quadraturePoint) {
-          // coordinates
-          const EntityCoordinateType xReferenceElement = entityQuadrature.point(quadraturePoint);
-          const EntityCoordinateType xWorld            = entityGeometry.global(xReferenceElement);
-
-          // integration factors
-          const double integrationFactor = entityGeometry.integrationElement(xReferenceElement);
-          const double quadratureWeight  = entityQuadrature.weight(quadraturePoint);
-
-          // evaluate function and basefunction
-          RangeType functionValue = 0.0;
-          inducingFunction_.evaluate(xWorld, functionValue);
-          RangeType baseFunctionValue = 0.0;
-          baseFunctionSet.evaluate(localDoF, xReferenceElement, baseFunctionValue);
-
-          // compute integral
-          localFunctionalValue += integrationFactor * quadratureWeight * functionValue * baseFunctionValue;
-
-        } // done walk over quadrature points
-
-        // set local vector
-        localFunctionalVector[localDoF] = localFunctionalValue;
-
-      } // done loop over all local DoFs
+      const LocalDoFVectorType localFunctionalVector = applyLocal(discreteFunctionSpace, entity, baseFunctionSet);
 
       // compute product
       ret += localDoFVector * localFunctionalVector;
@@ -194,7 +163,73 @@ public:
     } // done gridwalk
 
     return ret;
-  }
+
+  } // end operator()
+
+  /**
+    * \brief      This function respresents the local application of the functional
+    *             to a local basefunctionset.
+    *
+    * \todo       Doc me, please!
+    **/
+  template <class DiscreteFunctionSpaceType, class EntityType, class BaseFunctionSetType>
+  LocalDoFVectorType applyLocal(const DiscreteFunctionSpaceType& discreteFunctionSpace, const EntityType& entity,
+                                const BaseFunctionSetType& baseFunctionSet) const
+  {
+    // some types we will need
+    typedef typename DiscreteFunctionSpaceType::GridPartType GridPartType;
+    typedef typename DiscreteFunctionSpaceType::IteratorType EntityIteratorType;
+    typedef typename EntityIteratorType::Entity EntityType;
+    typedef typename EntityType::Geometry EntityGeometryType;
+    typedef typename BaseFunctionSetType::RangeType RangeType;
+
+    // init return vector
+    const unsigned numberOfLocalDoFs = baseFunctionSet.numBaseFunctions();
+    LocalDoFVectorType ret(numberOfLocalDoFs);
+
+    // geometry
+    const EntityGeometryType& entityGeometry = entity.geometry();
+
+    // quadrature
+    typedef CachingQuadrature<GridPartType, 0> EntityQuadratureType;
+    typedef typename EntityQuadratureType::CoordinateType EntityCoordinateType;
+    const unsigned quadratureOrder = (2 * discreteFunctionSpace.order() + 1);
+    const EntityQuadratureType entityQuadrature(entity, quadratureOrder);
+    const unsigned numberOfQuadraturePoints = entityQuadrature.nop();
+
+    // do loop over all local DoFs
+    for (unsigned int localDoF = 0; localDoF < numberOfLocalDoFs; ++localDoF) {
+      // value of the L2 functional, applied to the local basefunction, associated with the local DoF
+      RangeFieldType localFunctionalValue = 0.0;
+      // do walk over quadrature points
+      for (unsigned int quadraturePoint = 0; quadraturePoint < numberOfQuadraturePoints; ++quadraturePoint) {
+        // coordinates
+        const EntityCoordinateType xReferenceElement = entityQuadrature.point(quadraturePoint);
+        const EntityCoordinateType xWorld            = entityGeometry.global(xReferenceElement);
+
+        // integration factors
+        const double integrationFactor = entityGeometry.integrationElement(xReferenceElement);
+        const double quadratureWeight  = entityQuadrature.weight(quadraturePoint);
+
+        // evaluate function and basefunction
+        RangeType functionValue = 0.0;
+        inducingFunction_.evaluate(xWorld, functionValue);
+        RangeType baseFunctionValue = 0.0;
+        baseFunctionSet.evaluate(localDoF, xReferenceElement, baseFunctionValue);
+
+        // compute integral
+        localFunctionalValue += integrationFactor * quadratureWeight * functionValue * baseFunctionValue;
+
+      } // done walk over quadrature points
+
+      // set local vector
+      ret[localDoF] = localFunctionalValue;
+
+    } // done loop over all local DoFs
+
+    return ret;
+
+  } // end applyLocal()
 
 private:
   const InducingFunctionType& inducingFunction_;
