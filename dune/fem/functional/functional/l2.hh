98927c8517f6c00b0c2e36fa12005508be7bbe7e
m_droh01 <m_droh01@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-03-21 14:12:14 +0000

    fem-functionals idea by Stefan, Felix, Martin
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@59 4028485c-44d9-4cde-a312-5a4635ee2db9

---
+++ dune/fem/functional/functional/l2.hh
@@ -0,0 +1,19 @@
+#ifndef L2_PB9LO59Y
+#define L2_PB9LO59Y
+
+namespace Dune {
+namespace Fem {
+namespace Functional {
+namespace Functional {
+
+class L2
+{
+};
+
+} // end of namespace Constraints
+} // end of namespace Functional
+} // end of namespace Fem
+} // end of namespace Dune
+
+
+#endif /* end of include guard: L2_PB9LO59Y */
---
+++ dune/fem/functional/subspace/subspaces.hh
@@ -0,0 +1,24 @@
+#ifndef SUBSPACES_WBM7R4K5
+#define SUBSPACES_WBM7R4K5
+
+
+namespace Dune {
+namespace Fem {
+namespace Functional {
+namespace Subspace {
+
+class Linear
+{
+};
+
+class Affine
+{
+};
+
+} // end of namespace Constraints
+} // end of namespace Functional
+} // end of namespace Fem
+} // end of namespace Dune
+
+
+#endif /* end of include guard: SUBSPACES_WBM7R4K5 */
