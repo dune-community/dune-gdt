919811e95f85ec3a9aa8177346977aa0b51b770d
m_droh01 <m_droh01@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-03-22 15:12:55 +0000

    temporary commit documentation in dirichlet constraints
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@76 4028485c-44d9-4cde-a312-5a4635ee2db9

--- dune/fem/constraints/dirichlet.hh
+++ dune/fem/constraints/dirichlet.hh
@@ -10,29 +10,54 @@ namespace Constraints {
  * This class implements constraints on the degrees of freedom on a @ref
  * Subspace::Linear "linear subspace" @endref.
  *
- * @tparam DiscFuncSpace
+ * Constraints efficiently implement functionals @f$ f_i:\cal X_H \to
+ * \mathbb{R}@f$ for each degree of freedom @f$i\in C \subset
+ * \{1,\dots,H\}@f$, where $H$ is the number of degree of freedoms in the
+ * underlying discrete function space @f$\cal X_H\@f$. In case of this Dirichlet constraints class, the set
+ * @f$\{1,\dots,C\}@f$ includes all
+ *
+ * @note The Dirichlet constraints only make sense on a finite element space,
+ * not on a discontinuous discrete function space.
+ *
+ * @tparam DiscFuncSpace discrete function space on which constraints shall
+ * be applied.
  */
 template <class DiscFuncSpace>
 class Dirichlet
 {
 public:
+  //! Discrete function space on which the Dirichlet constraints are applied
   typedef DiscFuncSpace DiscreteFunctionSpace;
 
+  //! Underlying grid part
   typedef typename DiscreteFunctionSpace::GridPartType GridPartType;
 
+  //! Dimension of the grid part
   static const int griddim = GridPartType::GridType::dimension;
 
+  //! Return type of local() method, implementing the LocalConstraints
+  //! interface
   typedef Constraints::LocalDefault<double, griddim, griddim> LocalConstraintsType;
 
 public:
-  template <class DomainConstraint>
-  Dirichlet(DiscFuncSpace& space, DomainConstraint& domainConstraint)
+  /** Constructor for the Dirichlet constraints
+   *
+   *  @param space    discrete function space object on which the Dirichlet
+   *                  constraints are applied
+   */
+  Dirichlet(DiscFuncSpace& space)
     : space_(space)
     , gridPart_(space.gridPart())
   {
   }
 
 
+  /** return a local constraint object for the entity \a en
+   *
+   * @param en Entity for which the local constraints shall be compted
+   *
+   * @returns local constraints object (copyable).
+   */
   template <class Entity>
   const LocalConstraintsType local(const Entity& en)
   {
