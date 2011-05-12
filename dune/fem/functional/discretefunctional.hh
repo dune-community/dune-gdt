2e2d02d3018bce437181824acf752effaf4e06c8
nolte <nolte@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-05-12 13:02:03 +0000

    add trivial functionality
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@129 4028485c-44d9-4cde-a312-5a4635ee2db9

--- dune/fem/functional/discretefunctional.hh
+++ dune/fem/functional/discretefunctional.hh
@@ -10,6 +10,8 @@ namespace Dune
     template< class DiscreteFunction >
     class DiscreteFunctional
     {
+      typedef DiscreteFunctional< DiscreteFunction > ThisType;
+
     public:
       typedef DiscreteFunction DiscreteFunctionType;
 
@@ -18,6 +20,9 @@ namespace Dune
       DiscreteFunctional ( const std::string &name, const DiscreteFunctionSpace &dfSpace );
 
       FieldType operator () ( const DiscreteFunction &u ) const;
+
+      const std::string &name () const;
+      const DiscreteFunctionSpace &space () const;
 
     };
 
