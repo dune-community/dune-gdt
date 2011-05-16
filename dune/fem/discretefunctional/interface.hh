39fcac5f7a11207fce930128ccdbc15bdb3470c7
felix.albrecht <felix.albrecht@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-05-16 14:36:33 +0000

    moved and renamed discretefucntional.hh which Martin N. began to match the naming scheme of the module
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@131 4028485c-44d9-4cde-a312-5a4635ee2db9

---
+++ dune/fem/discretefunctional/interface.hh
@@ -0,0 +1,38 @@
+#ifndef DUNE_FEM_FUNCTIONALS_DISCRETEFUNCTIONAL_INTERFACE_HH
+#define DUNE_FEM_FUNCTIONALS_DISCRETEFUNCTIONAL_INTERFACE_HH
+
+namespace Dune
+{
+
+namespace Functionals
+{
+
+namespace DiscreteFunctional
+{
+
+template< class DiscreteFunction >
+class Interface
+{
+  typedef Interface< DiscreteFunction > ThisType;
+
+public:
+  typedef DiscreteFunction DiscreteFunctionType;
+
+  typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
+
+  Interface ( const std::string &name, const DiscreteFunctionSpace &dfSpace );
+
+  FieldType operator () ( const DiscreteFunction &u ) const;
+
+  const std::string &name () const;
+  const DiscreteFunctionSpace &space () const;
+
+};
+
+} // end namespace DiscreteFunctional
+
+} // end namespace Functionals
+
+} // end namespace Dune
+
+#endif // #ifndef DUNE_FEM_FUNCTIONALS_DISCRETEFUNCTIONAL_INTERFACE_HH
