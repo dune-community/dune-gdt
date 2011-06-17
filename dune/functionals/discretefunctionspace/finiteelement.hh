452fe12f094abb0fbd443ba9f498c6cdc007277c
felix.albrecht <felix.albrecht@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-06-17 14:30:40 +0000

    added LocalBasefucntion to DiscreteFunctionSPace::ContinuousFiniteElement
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@148 4028485c-44d9-4cde-a312-5a4635ee2db9

--- dune/functionals/discretefunctionspace/finiteelement.hh
+++ dune/functionals/discretefunctionspace/finiteelement.hh
@@ -4,8 +4,8 @@
 // dune-fem includes
 #include <dune/fem/space/lagrangespace.hh>
 
-// dune-functionals includes
-#include <dune/functionals/common/localbasefunctionprovider.hh>
+//dune-functionals includes
+#include <dune/functionals/common/localbasefunction.hh>
 
 namespace Dune
 {
@@ -30,19 +30,15 @@ public:
   typedef Dune::LagrangeDiscreteFunctionSpace< FunctionSpaceType, GridPartType, polOrder >
     HostSpaceType;
 
-  typedef typename HostSpaceType::RangeFieldType
-    RangeFieldType;
+  typedef Dune::Functionals::Common::LocalBaseFunction< HostSpaceType >
+    LocalBaseFunctionType;
 
   typedef typename HostSpaceType::EntityType
     EntityType;
 
-  typedef Dune::Functionals::Common::LocalBaseFunctionProvider< HostSpaceType >
-    LocalBaseFunctionProviderType;
-
   ContinuousFiniteElement( GridPartType& gridPart )
     : gridPart_( gridPart ),
-      hostSpace_( gridPart ),
-      localBaseFunctionProvider_( hostSpace_ )
+      hostSpace_( gridPart )
   {
   }
 
@@ -56,16 +52,15 @@ public:
     return hostSpace_;
   }
 
-  const LocalBaseFunctionProviderType& localBaseFunctionProvider() const
+  const LocalBaseFunctionType localBaseFunction( const EntityType& entity, const unsigned int localDoFNumber ) const
   {
-    return localBaseFunctionProvider_;
+    return LocalBaseFunctionType( entity, hostSpace_.baseFunctionSet( entity ), localDoFNumber );
   }
 
 private:
 
   const GridPartType& gridPart_;
   const HostSpaceType hostSpace_;
-  const LocalBaseFunctionProviderType localBaseFunctionProvider_;
 
 }; // end class ContinuousFiniteElement
 
