0dd2867475c3ad354b97096af6991c7eb2759956
felix.albrecht <felix.albrecht@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-06-17 14:30:42 +0000

    moved DiscreteOperator::Local from dune/fem/operator/ to dune/functionals/discreteoperator/local/
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@149 4028485c-44d9-4cde-a312-5a4635ee2db9

--- dune/functionals/discreteoperator/local/integration.hh
+++ dune/functionals/discreteoperator/local/integration.hh
@@ -7,10 +7,9 @@
 #define DUNE_FEM_FUNCTIONALS_OPERATOR_ELLIPTICFINITEELEMENT_HH
 
 // dune fem includes
-#include <dune/fem/function/common/function.hh>
 #include <dune/fem/quadrature/cachingquadrature.hh>
 
-// dune-fem-functionals includes
+// dune-functionals includes
 #include <dune/fem/common/localmatrix.hh>
 #include <dune/fem/common/localvector.hh>
 
@@ -25,6 +24,7 @@ namespace DiscreteOperator {
 namespace Local {
 
 /**
+  \todo   correct doc
   \brief  Linear operator.
 
           This class represents a linear operator \f$A: V_{h} \rightarrow W_{h}^{-1}\f$ which arises from a weak
@@ -70,11 +70,13 @@ namespace Local {
           Dune::DiscreteFunctionSpaceInterface. If the test and the ansatz space are identicall, this template
           argument may be omitted.
   **/
-template <class LocalEvaluationType, class DiscreteAnsatzFunctionSpaceImp,
+template <class LocalEvaluationImp, class DiscreteAnsatzFunctionSpaceImp,
           class DiscreteTestFunctionSpaceImp = DiscreteAnsatzFunctionSpaceImp>
 class Codim0Integration
 {
 public:
+  typedef LocalEvaluationImp LocalEvaluationType;
+
   typedef DiscreteAnsatzFunctionSpaceImp DiscreteAnsatzFunctionSpaceType;
 
   typedef DiscreteTestFunctionSpaceImp DiscreteTestFunctionSpaceType;
@@ -84,14 +86,6 @@ public:
   typedef Dune::Functionals::Common::LocalMatrix<RangeFieldType> LocalMatrixType;
 
   typedef typename DiscreteAnsatzFunctionSpaceType::EntityType EntityType;
-
-  typedef typename DiscreteAnsatzFunctionSpaceType::LocalBaseFunctionProviderType LocalAnsatzBaseFunctionProviderType;
-
-  typedef typename DiscreteAnsatzFunctionSpaceType::LocalBaseFunctionProviderType LocalTestBaseFunctionProviderType;
-
-  typedef typename LocalAnsatzBaseFunctionProviderType::LocalBaseFunctionType LocalAnsatzBaseFunctionType;
-
-  typedef typename LocalTestBaseFunctionProviderType::LocalBaseFunctionType LocalTestBaseFunctionType;
 
   /**
     \brief      Constructor storing the local operation and the ansatz and test space.
@@ -109,8 +103,6 @@ public:
     : localEvaluation_(localEvaluation)
     , ansatzSpace_(ansatzSpace)
     , testSpace_(testSpace)
-    , localAnsatzBaseFunctionProvider_(ansatzSpace.localBaseFunctionProvider())
-    , localTestBaseFunctionProvider_(testSpace.localBaseFunctionProvider())
   {
   }
 
@@ -127,11 +119,17 @@ public:
     : localEvaluation_(localEvaluation)
     , ansatzSpace_(space)
     , testSpace_(space)
-    , localAnsatzBaseFunctionProvider_(space.localBaseFunctionProvider())
-    , localTestBaseFunctionProvider_(space.localBaseFunctionProvider())
   {
   }
 
+  /**
+    \brief  Returns the local operation \f$a\f$.
+    \return \f$a\f$.
+    **/
+  const LocalEvaluationType localEvaluation() const
+  {
+    return localEvaluation_;
+  }
 
   /**
     \brief  Returns the ansatz space \f$V_h\f$.
@@ -149,15 +147,6 @@ public:
   const DiscreteTestFunctionSpaceType& testSpace() const
   {
     return testSpace_;
-  }
-
-  /**
-    \brief  Returns the local operation \f$a\f$.
-    \return \f$a\f$.
-    **/
-  const LocalEvaluationType& localEvaluation() const
-  {
-    return localEvaluation_;
   }
 
   /**
@@ -202,17 +191,14 @@ public:
   //    } // done loop over all local ansatz DoFs
 
   //    return ret;
-
   //  }
 
 private:
-  const LocalEvaluationType& localEvaluation_;
+  const LocalEvaluationType localEvaluation_;
   const DiscreteAnsatzFunctionSpaceType& ansatzSpace_;
   const DiscreteTestFunctionSpaceType& testSpace_;
-  const LocalAnsatzBaseFunctionProviderType& localAnsatzBaseFunctionProvider_;
-  const LocalTestBaseFunctionProviderType& localTestBaseFunctionProvider_;
 
-}; // end class Linear
+}; // end class Codim0Integration
 
 } // end namespace Local
 
