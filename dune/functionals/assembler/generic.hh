ebed1151dcb6012d3ca2935efefc2508ca478ce0
felix.albrecht <felix.albrecht@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-06-17 14:31:12 +0000

    finished matrix assembling and matrix constraints in systemassembler
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@161 4028485c-44d9-4cde-a312-5a4635ee2db9

--- dune/functionals/assembler/generic.hh
+++ dune/functionals/assembler/generic.hh
@@ -29,7 +29,7 @@ public:
   }
 
   template <class LocalMatrixAssemblerType, class MatrixPtrType>
-  void assemble(const LocalMatrixAssemblerType localMatrixAssembler, MatrixPtrType matrix)
+  void assemble(const LocalMatrixAssemblerType localMatrixAssembler, MatrixPtrType matrixPtr)
   {
     // some types
     typedef typename AnsatzFunctionSpaceType::IteratorType EntityIteratorType;
@@ -40,27 +40,67 @@ public:
 
     typedef Dune::Functionals::Common::LocalMatrix<RangeFieldType> LocalMatrixType;
 
-    // storage
+    typedef typename AnsatzFunctionSpaceType::ConstraintsType ConstraintsType;
+
+    typedef typename ConstraintsType::LocalConstraintsType LocalConstraintsType;
+
+    // common storage for all entities
     LocalMatrixType localMatrix(ansatzSpace_.numMaxDoFs(), testSpace_.numMaxDoFs());
 
-    // do gridwalk
+    // do first gridwalk to assemble
     const EntityIteratorType behindLastEntity = ansatzSpace_.end();
     for (EntityIteratorType entityIterator = ansatzSpace_.begin(); entityIterator != behindLastEntity;
          ++entityIterator) {
-      // stuff
       const EntityType& entity = *entityIterator;
 
-      // assemble local
       localMatrixAssembler.assembleLocal(entity, localMatrix);
 
-      // add to matrix
+      addToMatrix(entity, localMatrix, matrixPtr);
 
-    } // done gridwalk
+    } // done first gridwalk to assemble
 
+    const ConstraintsType constraints = ansatzSpace_.constraints();
+
+    // do second gridwalk, to apply constraints
+    for (EntityIteratorType entityIterator = ansatzSpace_.begin(); entityIterator != behindLastEntity;
+         ++entityIterator) {
+      const EntityType& entity = *entityIterator;
+
+      const LocalConstraintsType& localConstraints = constraints.local(entity);
+
+      applyLocalMatrixConstraints(localConstraints, matrixPtr);
+
+    } // done second gridwalk, to apply constraints
+
+    // apply constraints
 
   } // end method assemble
 
 private:
+  template <class EntityType, class LocalMatrixType, class MatrixPtrType>
+  void addToMatrix(const EntityType& entity, const LocalMatrixType& localMatrix, MatrixPtrType& matrixPtr)
+  {
+    for (int i = 0; i < ansatzSpace_.baseFunctionSet(entity).numBaseFunctions(); ++i) {
+      for (int j = 0; j < ansatzSpace_.baseFunctionSet(entity).numBaseFunctions(); ++j) {
+        const int globalI = ansatzSpace_.mapToGlobal(entity, i);
+        const int globalJ = testSpace_.mapToGlobal(entity, j);
+
+        matrixPtr->operator[](globalI)[globalJ] += localMatrix[i][j];
+      }
+    }
+  } // end method addToMatrix
+
+  template <class LocalConstraintsType, class MatrixPtrType>
+  void applyLocalMatrixConstraints(const LocalConstraintsType& localConstraints, MatrixPtrType& matrixPtr)
+  {
+    for (unsigned int i = 0; i < localConstraints.rowDofsSize(); ++i) {
+      for (unsigned int j = 0; j < localConstraints.columnDofsSize(); ++j) {
+        matrixPtr->operator[](localConstraints.rowDofs(i))[localConstraints.columnDofs(j)] =
+            localConstraints.localMatrix(i, j);
+      }
+    }
+  } // end applyLocalMatrixConstraints
+
   const AnsatzFunctionSpaceType& ansatzSpace_;
   const TestFunctionSpaceType& testSpace_;
 
