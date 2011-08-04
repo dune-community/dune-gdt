990f85e65c1626718b533587d82e864ed7924109
felix.albrecht <felix.albrecht@4028485c-44d9-4cde-a312-5a4635ee2db9>
2011-08-04 10:16:59 +0000

    began use of pdelab and localfucntions, 2.1 snapshot
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem-functionals/trunk@203 4028485c-44d9-4cde-a312-5a4635ee2db9

--- dune/functionals/discretefunctionspace/continuous/lagrange.hh
+++ dune/functionals/discretefunctionspace/continuous/lagrange.hh
@@ -4,6 +4,10 @@
 // dune-fem includes
 #include <dune/fem/space/lagrangespace.hh>
 
+// dune-pdelab includes
+#include <dune/pdelab/finiteelementmap/p1fem.hh>
+#include <dune/pdelab/gridfunctionspace/gridfunctionspace.hh>
+
 //dune-functionals includes
 #include <dune/functionals/common/localbasefunctionset.hh>
 
@@ -19,7 +23,7 @@ namespace DiscreteFunctionSpace
 namespace Continuous
 {
 
-template< class FunctionSpaceImp, class GridPartImp, int polOrder >
+template< class FunctionSpaceImp, class GridViewImp, int polOrder >
 class Lagrange
 {
 public:
@@ -27,125 +31,142 @@ public:
   typedef FunctionSpaceImp
     FunctionSpaceType;
 
-  typedef GridPartImp
-    GridPartType;
+  typedef GridViewImp
+    GridViewType;
 
-  typedef Lagrange< FunctionSpaceType, GridPartType, polOrder >
+  typedef Lagrange< FunctionSpaceType, GridViewType, polOrder >
     ThisType;
 
-  typedef Dune::LagrangeDiscreteFunctionSpace< FunctionSpaceType, GridPartType, polOrder >
+  typedef typename FunctionSpaceType::DomainType
+    DomainType;
+
+  typedef typename FunctionSpaceType::DomainFieldType
+    DomainFieldType;
+
+  typedef typename FunctionSpaceType::RangeFieldType
+    RangeFieldType;
+
+  typedef typename FunctionSpaceType::RangeType
+    RangeType;
+
+  typedef typename FunctionSpaceType::JacobianRangeType
+    JacobianRangeType;
+
+  typedef typename FunctionSpaceType::HessianRangeType
+    HessianRangeType;
+
+  static const unsigned int dimDomain = FunctionSpaceType::dimDomain;
+
+  static const unsigned int dimRange = FunctionSpaceType::dimRange;
+
+private:
+
+  typedef Dune::PDELab::P1LocalFiniteElementMap< DomainFieldType, RangeFieldType, dimDomain >
+    LocalFiniteElementMapType;
+
+public:
+
+//  typedef Dune::LagrangeDiscreteFunctionSpace< FunctionSpaceType, GridPartType, polOrder >
+//    HostSpaceType;
+
+  typedef Dune::PDELab::GridFunctionSpace< GridViewType, LocalFiniteElementMapType >
     HostSpaceType;
 
   typedef Dune::Functionals::Common::LocalBaseFunctionSet< ThisType >
     LocalBaseFunctionSetType;
 
-  typedef typename HostSpaceType::EntityType
+  typedef typename GridViewType::template Codim< 0 >::Entity
     EntityType;
 
-  typedef typename HostSpaceType::DomainType
-    DomainType;
+//  /**
+//    \defgroup dune-fem related
+//    \{
+//    **/
+//  typedef typename HostSpaceType::BaseFunctionSetType
+//    BaseFunctionSetType;
 
-  typedef typename HostSpaceType::DomainFieldType
-    DomainFieldType;
+//  typedef typename HostSpaceType::IteratorType
+//    IteratorType;
+//  /**
+//    \}
+//    **/
 
-  typedef typename HostSpaceType::RangeFieldType
-    RangeFieldType;
 
-  typedef typename HostSpaceType::RangeType
-    RangeType;
-
-  typedef typename HostSpaceType::JacobianRangeType
-    JacobianRangeType;
-
-  typedef typename HostSpaceType::HessianRangeType
-    HessianRangeType;
-
-  /**
-    \defgroup dune-fem related
-    \{
-    **/
-  typedef typename HostSpaceType::BaseFunctionSetType
-    BaseFunctionSetType;
-
-  typedef typename HostSpaceType::IteratorType
-    IteratorType;
-  /**
-    \}
-    **/
-
-  Lagrange( GridPartType& gridPart )
-    : gridPart_( gridPart ),
-      hostSpace_( gridPart ),
-      numMaxLocalDoFs_( -1 )
+  Lagrange( GridViewType& gridView )
+    : gridView_( gridView ),
+      localFiniteElementMap_(),
+      hostSpace_( gridView_, localFiniteElementMap_ )/*,
+      numMaxLocalDoFs_( -1 )*/
   {
-    // in the simple case, there should be the same number of dofs on each entity
-    const IteratorType entityIterator = begin();
-    const EntityType& entity = *entityIterator;
-    numMaxLocalDoFs_ = hostSpace_.baseFunctionSet( entity ).numBaseFunctions();
+//    // in the simple case, there should be the same number of dofs on each entity
+//    const IteratorType entityIterator = begin();
+//    const EntityType& entity = *entityIterator;
+//    numMaxLocalDoFs_ = hostSpace_.baseFunctionSet( entity ).numBaseFunctions();
   }
 
-  const GridPartType& gridPart() const
-  {
-    return gridPart_;
-  }
+//  const GridPartType& gridPart() const
+//  {
+//    return gridPart_;
+//  }
 
-  const HostSpaceType& hostSpace() const
-  {
-    return hostSpace_;
-  }
+//  const HostSpaceType& hostSpace() const
+//  {
+//    return hostSpace_;
+//  }
 
-  const LocalBaseFunctionSetType localBaseFunctionSet( const EntityType& entity ) const
-  {
-    return LocalBaseFunctionSetType( *this, entity );
-  }
+//  const LocalBaseFunctionSetType localBaseFunctionSet( const EntityType& entity ) const
+//  {
+//    return LocalBaseFunctionSetType( *this, entity );
+//  }
 
   const unsigned int size() const
   {
     return hostSpace_.size();
   }
 
-  const int numMaxLocalDoFs() const
-  {
-    return numMaxLocalDoFs_;
-  }
+//  const int numMaxLocalDoFs() const
+//  {
+//    return numMaxLocalDoFs_;
+//  }
 
-  const int order() const
-  {
-    return hostSpace_.order();
-  }
+//  const int order() const
+//  {
+//    return hostSpace_.order();
+//  }
 
-  /**
-    \defgroup dune-fem related
-    \{
-    **/
-  IteratorType begin() const
-  {
-    return hostSpace_.begin();
-  }
+//  /**
+//    \defgroup dune-fem related
+//    \{
+//    **/
+//  IteratorType begin() const
+//  {
+//    return hostSpace_.begin();
+//  }
 
-  const IteratorType end() const
-  {
-    return hostSpace_.end();
-  }
+//  const IteratorType end() const
+//  {
+//    return hostSpace_.end();
+//  }
 
-  const BaseFunctionSetType baseFunctionSet( const EntityType& entity ) const
-  {
-    return hostSpace_.baseFunctionSet( entity );
-  }
+//  const BaseFunctionSetType baseFunctionSet( const EntityType& entity ) const
+//  {
+//    return hostSpace_.baseFunctionSet( entity );
+//  }
 
-  int mapToGlobal( const EntityType& entity, const int localDof) const
-  {
-    return hostSpace_.mapToGlobal( entity, localDof);
-  }
-  /**
-    \}
-    **/
+//  int mapToGlobal( const EntityType& entity, const int localDof) const
+//  {
+//    return hostSpace_.mapToGlobal( entity, localDof);
+//  }
+//  /**
+//    \}
+//    **/
 
 private:
 
-  const GridPartType& gridPart_;
+  const GridViewType& gridView_;
+  const LocalFiniteElementMapType localFiniteElementMap_;
   const HostSpaceType hostSpace_;
-  unsigned int numMaxLocalDoFs_;
+//  unsigned int numMaxLocalDoFs_;
 
 }; // end class Lagrange
 
