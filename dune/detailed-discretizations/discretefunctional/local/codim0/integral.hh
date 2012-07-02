474c5535426334e24e5e312de29dfc4799d539a6
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-07-02 19:43:50 +0200

    added further use of shared_ptr

--- dune/detailed-discretizations/discretefunctional/local/codim0/integral.hh
+++ dune/detailed-discretizations/discretefunctional/local/codim0/integral.hh
@@ -41,18 +41,12 @@ public:
 
   typedef typename FunctionSpaceType::DomainType DomainType;
 
-  Integral(const LocalEvaluationType localEvaluation)
+  Integral(const LocalEvaluationType& localEvaluation)
     : localEvaluation_(localEvaluation)
   {
   }
 
-  //! copy constructor
-  Integral(const ThisType& other)
-    : localEvaluation_(other.localEvaluation())
-  {
-  }
-
-  LocalEvaluationType localEvaluation() const
+  const LocalEvaluationType& localEvaluation() const
   {
     return localEvaluation_;
   }
@@ -118,9 +112,12 @@ public:
 
 private:
   //! assignment operator
-  ThisType& operator=(const ThisType& other);
+  ThisType& operator=(const ThisType&);
 
-  const LocalEvaluationType localEvaluation_;
+  //! copy constructor
+  Integral(const ThisType&);
+
+  const LocalEvaluationType& localEvaluation_;
 
 }; // end class Integral
 
@@ -145,19 +142,12 @@ public:
   {
   }
 
-  //! copy constructor
-  IntegralInduced(const ThisType& other)
-    : inducingOperator_(other.inducingOperator())
-    , inducingDiscreteFunction_(other.inducingDiscreteFunction())
-  {
-  }
-
-  InducingOperatorType inducingOperator() const
+  const InducingOperatorType& inducingOperator() const
   {
     return inducingOperator_;
   }
 
-  InducingDiscreteFunctionType inducingDiscreteFunction() const
+  const InducingDiscreteFunctionType& inducingDiscreteFunction() const
   {
     return inducingDiscreteFunction_;
   }
@@ -236,8 +226,11 @@ private:
   //! assignment operator
   ThisType& operator=(const ThisType&);
 
-  const InducingOperatorType inducingOperator_;
-  const InducingDiscreteFunctionType inducingDiscreteFunction_;
+  //! copy constructor
+  IntegralInduced(const ThisType&);
+
+  const InducingOperatorType& inducingOperator_;
+  const InducingDiscreteFunctionType& inducingDiscreteFunction_;
 
 }; // end class IntegralInduced
 
