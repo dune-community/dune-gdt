5b249027c72ebcda3c53ee9f99665ad701c985ab
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-07-03 20:51:55 +0200

    [examples.elliptic.continuous_galerkin] make use the changed stuff

--- examples/elliptic/continuous_galerkin.cc
+++ examples/elliptic/continuous_galerkin.cc
@@ -12,6 +12,7 @@
 #include <dune/common/parametertree.hh>
 #include <dune/common/exceptions.hh>
 #include <dune/common/shared_ptr.hh>
+#include <dune/common/timer.hh>
 
 // dune-fem
 #include <dune/fem/space/common/functionspace.hh>
@@ -29,6 +30,11 @@
 #include <dune/detailed-discretizations/discreteoperator/local/codim0/integral.hh>
 #include <dune/detailed-discretizations/evaluation/local/unary/scale.hh>
 #include <dune/detailed-discretizations/discretefunctional/local/codim0/integral.hh>
+#include <dune/detailed-discretizations/la/factory/eigen.hh>
+#include <dune/detailed-discretizations/assembler/local/codim0/matrix.hh>
+#include <dune/detailed-discretizations/assembler/local/codim0/vector.hh>
+#include <dune/detailed-discretizations/assembler/system/constrained.hh>
+#include <dune/detailed-discretizations/la/backend/solver/eigen.hh>
 
 /**
   \brief      Creates a parameter file if it does not exist.
@@ -45,19 +51,22 @@ void ensureParamFile(std::string filename)
     std::ofstream file;
     file.open(filename);
     file << "[helper-tools.grid.provider.cube]" << std::endl;
-    file << "level = 2" << std::endl;
-    file << "[data.a]" << std::endl;
+    file << "level = 4" << std::endl;
+    file << "[diffusion]" << std::endl;
     file << "variable = x" << std::endl;
     file << "expression.0 = 1.0" << std::endl;
     file << "expression.1 = 1.0" << std::endl;
     file << "expression.2 = 1.0" << std::endl;
     file << "order = 0" << std::endl;
-    file << "[data.f]" << std::endl;
+    file << "[force]" << std::endl;
     file << "variable = x" << std::endl;
     file << "expression.0 = 1.0" << std::endl;
     file << "expression.1 = 1.0" << std::endl;
     file << "expression.2 = 1.0" << std::endl;
     file << "order = 0" << std::endl;
+    file << "[solver]" << std::endl;
+    file << "maxIter = 5000" << std::endl;
+    file << "precision = 1e-12" << std::endl;
     file.close();
   } // only write param file if there is none
 } // void ensureParamFile()
@@ -73,18 +82,29 @@ int main(int argc, char** argv)
   try {
     // mpi
     Dune::MPIHelper::instance(argc, argv);
+
     // parameter
     const std::string filename = "continuous_galerkin.param";
     ensureParamFile(filename);
     Dune::ParameterTree paramTree = Dune::HelperTools::Common::ParameterTree::init(argc, argv, filename);
+
+    // timer
+    Dune::Timer timer;
+
     // grid
+    std::cout << "setting up grid:" << std::endl;
     typedef Dune::HelperTools::Grid::Provider::UnitCube<Dune::GridSelector::GridType> GridProviderType;
-    GridProviderType gridProvider(paramTree);
+    Dune::HelperTools::Common::ParameterTree::assertSub(paramTree, GridProviderType::id, filename);
+    GridProviderType gridProvider(paramTree.sub(GridProviderType::id));
     typedef GridProviderType::GridType GridType;
     GridType& grid = gridProvider.grid();
     typedef Dune::LeafGridPart<GridType> GridPartType;
     GridPartType gridPart(grid);
+    std::cout << "took " << timer.elapsed() << " sec" << std::endl;
+
     // function spaces
+    std::cout << "setting up function spaces... " << std::flush;
+    timer.reset();
     const int dimDomain = GridProviderType::dim;
     const int dimRange  = 1;
     typedef double DomainFieldType;
@@ -99,22 +119,70 @@ int main(int argc, char** argv)
     const AnsatzSpaceType ansatzSpace(discreteH1);
     typedef AnsatzSpaceType TestSpaceType;
     const TestSpaceType testSpace(discreteH1);
+    std::cout << "done (took " << timer.elapsed() << " sec)" << std::endl;
+
     // left hand side (operator)
+    std::cout << "setting up operator and functional... " << std::flush;
+    timer.reset();
     typedef Dune::DetailedDiscretizations::Evaluation::Local::Binary::Elliptic<FunctionSpaceType>
         EllipticEvaluationType;
-    Dune::HelperTools::Common::ParameterTree::assertSub(paramTree, "data.a", filename);
-    const EllipticEvaluationType ellipticEvaluation(paramTree.sub("data.a"));
+    Dune::HelperTools::Common::ParameterTree::assertSub(paramTree, "diffusion", filename);
+    const EllipticEvaluationType ellipticEvaluation(paramTree.sub("diffusion"));
     typedef Dune::DetailedDiscretizations::DiscreteOperator::Local::Codim0::Integral<EllipticEvaluationType>
         EllipticOperatorType;
     const EllipticOperatorType ellipticOperator(ellipticEvaluation);
+
     // right hand side (functional)
     typedef Dune::DetailedDiscretizations::Evaluation::Local::Unary::Scale<FunctionSpaceType> ProductEvaluationType;
-    Dune::HelperTools::Common::ParameterTree::assertSub(paramTree, "data.f", filename);
-    const ProductEvaluationType productEvaluation(paramTree.sub("data.f"));
+    Dune::HelperTools::Common::ParameterTree::assertSub(paramTree, "force", filename);
+    const ProductEvaluationType productEvaluation(paramTree.sub("force"));
     typedef Dune::DetailedDiscretizations::DiscreteFunctional::Local::Codim0::Integral<ProductEvaluationType>
         L2FunctionalType;
     const L2FunctionalType l2Functional(productEvaluation);
+    std::cout << "done (took " << timer.elapsed() << " sec)" << std::endl;
 
+    // system matrix and right hand side
+    std::cout << "setting up matrix and vector container... " << std::flush;
+    timer.reset();
+    typedef Dune::DetailedDiscretizations::LA::Factory::Eigen<RangeFieldType> ContainerFactory;
+    typedef typename ContainerFactory::SparseMatrixType MatrixType;
+    MatrixType systemMatrix = ContainerFactory::createSparseMatrix(ansatzSpace, testSpace);
+    typedef typename ContainerFactory::DenseVectorType VectorType;
+    VectorType rhs      = ContainerFactory::createDenseVector(testSpace);
+    VectorType solution = ContainerFactory::createDenseVector(testSpace);
+    std::cout << "done (took " << timer.elapsed() << " sec)" << std::endl;
+
+    // assembler
+    std::cout << "setting up assembler... " << std::flush;
+    timer.reset();
+    typedef Dune::DetailedDiscretizations::Assembler::Local::Codim0::Matrix<EllipticOperatorType>
+        LocalMatrixAssemblerType;
+    const LocalMatrixAssemblerType localmatrixAssembler(ellipticOperator);
+    typedef Dune::DetailedDiscretizations::Assembler::Local::Codim0::Vector<L2FunctionalType> LocalVectorAssemblerType;
+    const LocalVectorAssemblerType localVectorAssembler(l2Functional);
+    typedef Dune::DetailedDiscretizations::Assembler::System::Constrained<AnsatzSpaceType, TestSpaceType>
+        SystemAssemblerType;
+    const SystemAssemblerType systemAssembler(ansatzSpace, testSpace);
+    std::cout << "done (took " << timer.elapsed() << " sec)" << std::endl;
+
+    // assemble system
+    std::cout << "assembling system... " << std::flush;
+    timer.reset();
+    systemAssembler.assembleSystem(localmatrixAssembler, systemMatrix, localVectorAssembler, rhs);
+    std::cout << "done (took " << timer.elapsed() << " sec)" << std::endl;
+
+    // solve system
+    // typedef Dune::DetailedDiscretizations::LA::Solver::Eigen::BicgstabIlut Solver;
+    typedef Dune::DetailedDiscretizations::LA::Solver::Eigen::CgDiagonalUp Solver;
+    std::cout << "solving linear system using " << Solver::id << "... " << std::flush;
+    timer.reset();
+    Dune::HelperTools::Common::ParameterTree::assertSub(paramTree, "solver", filename);
+    Solver::apply(systemMatrix,
+                  solution,
+                  rhs,
+                  paramTree.sub("solver").get("maxIter", 5000),
+                  paramTree.sub("solver").get("precision", 1e-12));
+    std::cout << "done (took " << timer.elapsed() << " sec)" << std::endl;
 
     // done
     return 0;
