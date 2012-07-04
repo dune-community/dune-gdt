ded2b67c0cfe176fc63650c34eac7fd69a3ba368
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-07-04 10:51:42 +0200

    [la.backend.solver.eigen] added additional solvers

--- dune/detailed-discretizations/la/backend/solver/eigen.hh
+++ dune/detailed-discretizations/la/backend/solver/eigen.hh
@@ -21,7 +21,8 @@ public:
   static const std::string id;
 
   template <class MatrixType, class SolutionType, class RhsType>
-  static void apply(MatrixType& matrix, SolutionType& solution, RhsType& rhs, unsigned int maxIter, double precision)
+  static void apply(MatrixType& matrix, SolutionType& solution, RhsType& rhs, unsigned int maxIter = 5000,
+                    double precision = 1e-12)
   {
     typedef typename MatrixType::EntryType EntryType;
     typedef typename MatrixType::StorageType EigenMatrixType;
@@ -37,13 +38,37 @@ public:
 
 const std::string BicgstabIlut::id = "eigen.bicgstab.incompletelut";
 
-struct CgDiagonalUp
+struct BicgstabDiagonal
 {
 public:
   static const std::string id;
 
   template <class MatrixType, class SolutionType, class RhsType>
-  static void apply(MatrixType& matrix, SolutionType& solution, RhsType& rhs, unsigned int maxIter, double precision)
+  static void apply(MatrixType& matrix, SolutionType& solution, RhsType& rhs, unsigned int maxIter = 5000,
+                    double precision = 1e-12)
+  {
+    typedef typename MatrixType::EntryType EntryType;
+    typedef typename MatrixType::StorageType EigenMatrixType;
+    typedef ::Eigen::DiagonalPreconditioner<EntryType> PreconditionerType;
+    typedef ::Eigen::BiCGSTAB<EigenMatrixType, PreconditionerType> SolverType;
+    SolverType solver;
+    solver.setMaxIterations(maxIter);
+    solver.setTolerance(precision);
+    solver.compute(*(matrix.storage()));
+    *(solution.storage()) = solver.solve(*(rhs.storage()));
+  }
+}; // struct BicgstabDiagonal
+
+const std::string BicgstabDiagonal::id = "eigen.bicgstab.diagonal";
+
+struct CgDiagonalUpper
+{
+public:
+  static const std::string id;
+
+  template <class MatrixType, class SolutionType, class RhsType>
+  static void apply(MatrixType& matrix, SolutionType& solution, RhsType& rhs, unsigned int maxIter = 5000,
+                    double precision = 1e-12)
   {
     typedef typename MatrixType::EntryType EntryType;
     typedef typename MatrixType::StorageType EigenMatrixType;
@@ -55,9 +80,72 @@ public:
     solver.compute(*(matrix.storage()));
     *(solution.storage()) = solver.solve(*(rhs.storage()));
   }
-}; // struct CgDiagonalUp
+}; // struct CgDiagonalUpper
 
-const std::string CgDiagonalUp::id = "eigen.cg.diagonal.up";
+const std::string CgDiagonalUpper::id = "eigen.cg.diagonal.upper";
+
+struct CgDiagonalLower
+{
+public:
+  static const std::string id;
+
+  template <class MatrixType, class SolutionType, class RhsType>
+  static void apply(MatrixType& matrix, SolutionType& solution, RhsType& rhs, unsigned int maxIter = 5000,
+                    double precision = 1e-12)
+  {
+    typedef typename MatrixType::EntryType EntryType;
+    typedef typename MatrixType::StorageType EigenMatrixType;
+    typedef ::Eigen::DiagonalPreconditioner<EntryType> PreconditionerType;
+    typedef ::Eigen::ConjugateGradient<EigenMatrixType, ::Eigen::Lower, PreconditionerType> SolverType;
+    SolverType solver;
+    solver.setMaxIterations(maxIter);
+    solver.setTolerance(precision);
+    solver.compute(*(matrix.storage()));
+    *(solution.storage()) = solver.solve(*(rhs.storage()));
+  }
+}; // struct CgDiagonalLower
+
+const std::string CgDiagonalLower::id = "eigen.cg.diagonal.lower";
+
+struct SimplicialcholeskyUpper
+{
+public:
+  static const std::string id;
+
+  template <class MatrixType, class SolutionType, class RhsType>
+  static void apply(MatrixType& matrix, SolutionType& solution, RhsType& rhs, unsigned int maxIter = 5000,
+                    double precision = 1e-12)
+  {
+    typedef typename MatrixType::EntryType EntryType;
+    typedef typename MatrixType::StorageType EigenMatrixType;
+    typedef ::Eigen::SimplicialCholesky<EigenMatrixType, ::Eigen::Upper> SolverType;
+    SolverType solver;
+    solver.compute(*(matrix.storage()));
+    *(solution.storage()) = solver.solve(*(rhs.storage()));
+  }
+}; // struct SimplicialcholeskyUpper
+
+const std::string SimplicialcholeskyUpper::id = "eigen.simplicialcholesky.upper";
+
+struct SimplicialcholeskyLower
+{
+public:
+  static const std::string id;
+
+  template <class MatrixType, class SolutionType, class RhsType>
+  static void apply(MatrixType& matrix, SolutionType& solution, RhsType& rhs, unsigned int maxIter = 5000,
+                    double precision = 1e-12)
+  {
+    typedef typename MatrixType::EntryType EntryType;
+    typedef typename MatrixType::StorageType EigenMatrixType;
+    typedef ::Eigen::SimplicialCholesky<EigenMatrixType, ::Eigen::Lower> SolverType;
+    SolverType solver;
+    solver.compute(*(matrix.storage()));
+    *(solution.storage()) = solver.solve(*(rhs.storage()));
+  }
+}; // struct SimplicialcholeskyLower
+
+const std::string SimplicialcholeskyLower::id = "eigen.simplicialcholesky.lower";
 
 } // namespace Eigen
 
