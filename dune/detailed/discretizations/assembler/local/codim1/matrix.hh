ef28c9f3501b6b89037a7ff81435e16f66c55e12
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-10-08 10:26:47 +0200

    [assembler.local.codim1.matrix] added local boundary assembler

--- dune/detailed/discretizations/assembler/local/codim1/matrix.hh
+++ dune/detailed/discretizations/assembler/local/codim1/matrix.hh
@@ -134,6 +134,97 @@ private:
   const LocalOperatorType& localOperator_;
 }; // end class Inner
 
+template <class LocalOperatorImp>
+class Boundary
+{
+public:
+  typedef LocalOperatorImp LocalOperatorType;
+
+  typedef Boundary<LocalOperatorType> ThisType;
+
+  typedef typename LocalOperatorType::RangeFieldType RangeFieldType;
+
+  Boundary(const LocalOperatorType& localOperator)
+    : localOperator_(localOperator)
+  {
+  }
+
+  const LocalOperatorType& localOperator() const
+  {
+    return localOperator_;
+  }
+
+private:
+  static const unsigned int numTmpObjectsRequired_ = 4;
+
+public:
+  std::vector<unsigned int> numTmpObjectsRequired() const
+  {
+    std::vector<unsigned int> ret(2, 0);
+    ret[0] = numTmpObjectsRequired_;
+    ret[1] = localOperator_.numTmpObjectsRequired();
+    return ret;
+  } // std::vector< unsigned int > numTmpObjectsRequired() const
+
+  template <class IntersectionType, class AnsatzSpaceType, class TestSpaceType, class MatrixBackendType,
+            class LocalMatrixType>
+  void assembleLocal(const IntersectionType& intersection, const AnsatzSpaceType& ansatzSpace,
+                     const TestSpaceType& testSpace, MatrixBackendType& matrix,
+                     std::vector<std::vector<LocalMatrixType>>& tmpLocalMatricesContainer) const
+  {
+    // preparations
+    assert(intersection.boundary());
+    typedef typename IntersectionType::EntityPointer EntityPointerType;
+    typedef typename IntersectionType::Entity EntityType;
+    typedef typename AnsatzSpaceType::BaseFunctionSetType::LocalBaseFunctionSetType LocalAnsatzBaseFunctionSetType;
+    typedef typename TestSpaceType::BaseFunctionSetType::LocalBaseFunctionSetType LocalTestBaseFunctionSetType;
+    // get inside entity and basefunctionsets
+    const EntityPointerType entityPtr                               = intersection.inside();
+    const EntityType& entity                                        = *entityPtr;
+    const LocalAnsatzBaseFunctionSetType localAnsatzBaseFunctionSet = ansatzSpace.baseFunctionSet().local(entity);
+    const LocalTestBaseFunctionSetType localTestBaseFunctionSet     = testSpace.baseFunctionSet().local(entity);
+    // ensure enough tmp local matrices
+    assert(tmpLocalMatricesContainer.size() > 1);
+    std::vector<LocalMatrixType>& tmpLocalMatrices = tmpLocalMatricesContainer[0];
+    if (tmpLocalMatrices.size() < numTmpObjectsRequired_) {
+      tmpLocalMatrices.resize(
+          numTmpObjectsRequired_,
+          LocalMatrixType(ansatzSpace.map().maxLocalSize(), testSpace.map().maxLocalSize(), RangeFieldType(0.0)));
+    } // ensure enough tmp local matrices
+    // apply local operator
+    localOperator_.applyLocal(localAnsatzBaseFunctionSet,
+                              localTestBaseFunctionSet,
+                              intersection,
+                              tmpLocalMatrices[0],
+                              tmpLocalMatricesContainer[1]);
+    // write local matrices to global (see below)
+    addToMatrix(ansatzSpace, testSpace, entity, entity, tmpLocalMatrices[0], matrix);
+  } // void assembleLocal() const
+
+private:
+  //! assignment operator
+  ThisType& operator=(const ThisType&);
+
+  template <class AnsatzSpaceType, class TestSpaceType, class EntityType, class LocalMatrixType, class SystemMatrixType>
+  void addToMatrix(const AnsatzSpaceType& ansatzSpace, const TestSpaceType& testSpace, const EntityType& ansatzEntity,
+                   const EntityType& testEntity, const LocalMatrixType& localMatrix,
+                   SystemMatrixType& systemMatrix) const
+  {
+    unsigned int rows = ansatzSpace.baseFunctionSet().local(ansatzEntity).size();
+    unsigned int cols = testSpace.baseFunctionSet().local(testEntity).size();
+    for (unsigned int i = 0; i < rows; ++i) {
+      for (unsigned int j = 0; j < cols; ++j) {
+        const unsigned int globalI = ansatzSpace.map().toGlobal(ansatzEntity, i);
+        const unsigned int globalJ = testSpace.map().toGlobal(testEntity, j);
+
+        systemMatrix.add(globalI, globalJ, localMatrix[i][j]);
+      }
+    }
+  } // end method addToMatrix
+
+  const LocalOperatorType& localOperator_;
+}; // end class Boundary
+
 } // end namespace Codim1
 
 } // end namespace Local
