b318bce86b9665ea103816f612140eb27667c4f7
Felix Albrecht <mail@felixalbrecht.de>
2012-12-12 17:55:41 +0100

    [discretefunction.local] update

--- dune/detailed/discretizations/discretefunction/local.hh
+++ dune/detailed/discretizations/discretefunction/local.hh
@@ -1,71 +1,57 @@
 #ifndef DUNE_DETAILED_DISCRETIZATIONS_DISCRETEFUNCTION_LOCAL_HH
 #define DUNE_DETAILED_DISCRETIZATIONS_DISCRETEFUNCTION_LOCAL_HH
 
-// system
 #include <vector>
 
-namespace Dune
-{
-
+namespace Dune {
 namespace Detailed {
-
-namespace Discretizations
-{
-
-namespace DiscreteFunction
-{
+namespace Discretizations {
+namespace DiscreteFunction {
 
 template< class DiscreteFunctionImp >
 class LocalConst
 {
 public:
+  typedef DiscreteFunctionImp DiscreteFunctionType;
 
-  typedef DiscreteFunctionImp
-    DiscreteFunctionType;
+  typedef LocalConst< DiscreteFunctionType > ThisType;
 
-  typedef LocalConst< DiscreteFunctionType >
-    ThisType;
-
-  typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType
-    DiscreteFunctionSpaceType;
+  typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
 
   typedef typename DiscreteFunctionSpaceType::GridPartType::template Codim< 0 >::EntityType EntityType;
 
-  typedef typename DiscreteFunctionType::DomainType
-    DomainType;
+  typedef typename DiscreteFunctionSpaceType::FunctionSpaceType FunctionSpaceType;
 
-  typedef typename DiscreteFunctionType::RangeFieldType
-    RangeFieldType;
+  typedef typename FunctionSpaceType::DomainType DomainType;
 
-  typedef typename DiscreteFunctionType::RangeType
-    RangeType;
+  typedef typename FunctionSpaceType::RangeFieldType RangeFieldType;
 
-  typedef typename DiscreteFunctionType::JacobianRangeType
-    JacobianRangeType;
+  typedef typename FunctionSpaceType::RangeType RangeType;
+
+  typedef typename FunctionSpaceType::JacobianRangeType JacobianRangeType;
+
+  typedef typename DiscreteFunctionType::size_type size_type;
 
 private:
   typedef typename DiscreteFunctionSpaceType::BaseFunctionSetType::LocalBaseFunctionSetType
     LocalBaseFunctionSetType;
 
 public:
-  LocalConst( const DiscreteFunctionType& discreteFunction, const EntityType& entity )
-    : discreteFunction_( discreteFunction ),
-      entity_( entity ),
-      localBaseFunctionSet_( discreteFunction_.space().baseFunctionSet().local( entity_ ) ),
-      size_( localBaseFunctionSet_.size() ),
-      order_( localBaseFunctionSet_.order() )
-  {
-  }
+  LocalConst(const DiscreteFunctionType& discreteFunction, const EntityType& entity)
+    : discreteFunction_(discreteFunction)
+    , entity_(entity)
+    , localBaseFunctionSet_(discreteFunction_.space().baseFunctionSet().local(entity_))
+    , size_(localBaseFunctionSet_.size())
+    , order_(localBaseFunctionSet_.order())
+  {}
 
-  //! copy constructor
-  LocalConst( const ThisType& other )
-    : discreteFunction_( other.discreteFunction() ),
-      entity_( other.entity() ),
-      localBaseFunctionSet_( discreteFunction_.space().baseFunctionSet().local( entity_ ) ),
-      size_( localBaseFunctionSet_.size() ),
-      order_( localBaseFunctionSet_.order() )
-  {
-  }
+  LocalConst(const ThisType& other)
+    : discreteFunction_(other.discreteFunction_)
+    , entity_( other.entity_)
+    , localBaseFunctionSet_(other.localBaseFunctionSet_)
+    , size_(other.size_)
+    , order_(other.order_)
+  {}
 
   const DiscreteFunctionType& discreteFunction() const
   {
@@ -77,10 +63,11 @@ public:
     return entity_;
   }
 
-  const RangeFieldType& operator[]( const unsigned int localDofNumber ) const
+  const RangeFieldType& operator[](const size_type localDofNumber) const
   {
-    const unsigned int globalDofNumber = discreteFunction_.space().map().toGlobal( entity_, localDofNumber );
-    return discreteFunction_[globalDofNumber];
+    const size_type globalDofNumber = discreteFunction_.space().map().toGlobal(entity_, localDofNumber);
+    assert(discreteFunction_.vector()->size() == discreteFunction_.space().map().size());
+    return discreteFunction_.vector()->operator[](globalDofNumber);
   }
 
   int order() const
@@ -88,7 +75,7 @@ public:
     return order_;
   }
 
-  unsigned int size() const
+  size_type size() const
   {
     return size_;
   }
@@ -98,7 +85,7 @@ public:
     std::vector< RangeType > baseFunctionValues( size(), RangeType( 0.0 ) );
     localBaseFunctionSet_.evaluate( x, baseFunctionValues );
     ret = 0.0;
-    for( unsigned int i = 0; i < size(); ++i )
+    for( size_type i = 0; i < size(); ++i )
     {
       baseFunctionValues[i] *= operator[](i);
       ret += baseFunctionValues[i];
@@ -110,7 +97,7 @@ public:
     std::vector< JacobianRangeType > baseFunctionJacobianValues( size(), JacobianRangeType( 0.0 ) );
     localBaseFunctionSet_.jacobian( x, baseFunctionJacobianValues );
     ret = 0.0;
-    for( unsigned int i = 0; i < size(); ++i )
+    for( size_type i = 0; i < size(); ++i )
     {
       baseFunctionJacobianValues[i] *= operator[](i);
       ret += baseFunctionJacobianValues[i];
@@ -118,16 +105,13 @@ public:
   }
 
 private:
-
-  //! assignment operator
-  ThisType& operator=( const ThisType& );
+  ThisType& operator=(const ThisType&);
 
   const DiscreteFunctionType& discreteFunction_;
   const EntityType& entity_;
   const LocalBaseFunctionSetType localBaseFunctionSet_;
-  const unsigned int size_;
+  const size_type size_;
   const int order_;
-
 }; // end class LocalConst
 
 template< class DiscreteFunctionImp >
@@ -135,50 +119,33 @@ class Local
   : public LocalConst< DiscreteFunctionImp >
 {
 public:
+  typedef DiscreteFunctionImp DiscreteFunctionType;
 
-  typedef DiscreteFunctionImp
-    DiscreteFunctionType;
+  typedef Local< DiscreteFunctionType > ThisType;
 
-  typedef Local< DiscreteFunctionType >
-    ThisType;
+  typedef LocalConst< DiscreteFunctionType > BaseType;
 
-  typedef LocalConst< DiscreteFunctionType >
-    BaseType;
+  typedef typename BaseType::EntityType EntityType;
 
-  typedef typename BaseType::DiscreteFunctionSpaceType
-    DiscreteFunctionSpaceType;
+  typedef typename BaseType::RangeFieldType RangeFieldType;
 
-  typedef typename BaseType::EntityType
-    EntityType;
+  typedef typename DiscreteFunctionType::size_type size_type;
 
-  typedef typename BaseType::DomainType
-    DomainType;
-
-  typedef typename BaseType::RangeFieldType
-    RangeFieldType;
-
-  typedef typename BaseType::RangeType
-    RangeType;
-
-  typedef typename BaseType::JacobianRangeType
-    JacobianRangeType;
-
-  Local( DiscreteFunctionType& discreteFunction, const EntityType& entity )
-    : BaseType( const_cast< const DiscreteFunctionType& >( discreteFunction ), entity ),
-      discreteFunction_( discreteFunction )
+  Local(DiscreteFunctionType& discreteFunction, const EntityType& entity)
+    : BaseType(const_cast< const DiscreteFunctionType& >(discreteFunction), entity)
+    , discreteFunction_(discreteFunction)
   {}
 
   using BaseType::entity;
 
   using BaseType::operator[];
 
-  RangeFieldType& operator[]( const unsigned int localDofNumber )
+  RangeFieldType& operator[]( const size_type localDofNumber )
   {
-    const unsigned int globalDofNumber = discreteFunction_.space().map().toGlobal( entity(), localDofNumber );
-    return discreteFunction_[globalDofNumber];
+    const size_type globalDofNumber = discreteFunction_.space().map().toGlobal(BaseType::entity(), localDofNumber);
+    assert(discreteFunction_.vector()->size() == discreteFunction_.space().map().size());
+    return discreteFunction_.vector()->operator[](globalDofNumber);
   }
-
-  using BaseType::order;
 
   using BaseType::size;
 
@@ -187,16 +154,12 @@ public:
   using BaseType::jacobian;
 
 private:
-
   DiscreteFunctionType& discreteFunction_;
 }; // end class Local
 
-} // end namespace DiscreteFunction
-
+} // namespace DiscreteFunction
 } // namespace Discretizations
-
 } // namespace Detailed
-
-} // end namespace Dune
+} // namespace Dune
 
 #endif // DUNE_DETAILED_DISCRETIZATIONS_DISCRETEFUNCTION_LOCAL_HH
