a70b3063ca6857e1f73093e8f965a8820e2e1ba5
Felix Albrecht <mail@felixalbrecht.de>
2012-12-13 18:10:49 +0100

    [basefunctionset.local.lagrange] disabled deprecation warnings

--- dune/detailed/discretizations/basefunctionset/local/lagrange.hh
+++ dune/detailed/discretizations/basefunctionset/local/lagrange.hh
@@ -62,7 +62,10 @@ public:
     typedef typename BaseFunctionSetType::BaseFunctionSetType HostBaseFunctionSetType;
     const HostBaseFunctionSetType tmpBaseFunctionSet = baseFunctionSet_.baseFunctionSet(entity_);
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
     size_ = tmpBaseFunctionSet.numBaseFunctions();
+#pragma GCC diagnostic pop
     // this is still fishy, i.e. p-adaptivity
     order_ = baseFunctionSet_.space().order();
   }
@@ -104,7 +107,10 @@ public:
 
     // and evaluate
     for (unsigned int i = 0; i < size_; ++i) {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
       baseFunctionSet.evaluate(i, x, ret[i]);
+#pragma GCC diagnostic pop
     }
   }
 
@@ -135,8 +141,11 @@ public:
 
     // evaluate
     for (unsigned int i = 0; i < size_; ++i) {
-      // get untransposed jacobian
+// get untransposed jacobian
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
       baseFunctionSet.jacobian(i, x, jacobianUntransposed);
+#pragma GCC diagnostic pop
 
       // transpose for each dim of range
       const unsigned int dimRange = DiscreteFunctionSpaceType::dimRange;
