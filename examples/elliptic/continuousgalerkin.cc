9cf208f463dd15a4fc9e849a39888c784a22348d
Felix Albrecht <mail@felixalbrecht.de>
2012-12-14 14:54:54 +0100

    [examples.elliptic.continuousgalerkin] update

--- examples/elliptic/continuousgalerkin.cc
+++ examples/elliptic/continuousgalerkin.cc
@@ -28,7 +28,7 @@
 #include <dune/stuff/grid/boundaryinfo.hh>
 #include <dune/stuff/function/expression.hh>
 #include <dune/stuff/discretefunction/projection/dirichlet.hh>
-#include <dune/stuff/la/solver/eigen/sparse.hh>
+#include <dune/stuff/la/solver/eigen.hh>
 
 #include <dune/detailed/discretizations/discretefunctionspace/continuous/lagrange.hh>
 #include <dune/detailed/discretizations/discretefunctionspace/sub/linear.hh>
@@ -95,9 +95,9 @@ void ensureParamFile(const std::string& filename)
     file << "[neumann]" << std::endl;
     file << "order = 0"  << std::endl;
     file << "variable = x" << std::endl;
-    file << "expression = [0.1; 0.0; 0.0]" << std::endl;
+    file << "expression = [1.0; 0.0; 0.0]" << std::endl;
     file << "[solver]" << std::endl;
-    file << "type = eigen.bicgstab.incompletelut"  << std::endl;
+    file << "type = eigen.iterative.bicgstab.diagonal"  << std::endl;
     file << "maxIter = 5000"  << std::endl;
     file << "precision = 1e-12"  << std::endl;
     file.close();
@@ -281,6 +281,7 @@ int main(int argc, char** argv)
     systemAssembler.addLocalVectorAssembler(localforceVectorAssembler, forceVector);
     systemAssembler.addLocalVectorAssembler(localNeumannVectorAssembler, neumannVector);
     systemAssembler.assemble();
+    info << "done (took " << timer.elapsed() << " sec)" << std::endl;
 
     info << "applying constraints... " << std::flush;
     timer.reset();
@@ -291,22 +292,21 @@ int main(int argc, char** argv)
     info << "done (took " << timer.elapsed() << " sec)" << std::endl;
 
     info << "solving linear system (of size " << systemMatrix->rows() << "x" << systemMatrix->cols() << ")" << std::endl;
-    const std::string solverType = paramTree.get("solver.type", "eigen.bicgstab.incompletelut");
+    const std::string solverType = paramTree.get("solver.type", "eigen.iterative.bicgstab.diagonal");
     const unsigned int solverMaxIter = paramTree.get("solver.maxIter", 5000);
     const double solverPrecision = paramTree.get("solver.precision", 1e-12);
     info << "  using '" << solverType << "'... " << std::flush;
     timer.reset();
-    typedef typename Dune::Stuff::LA::Solver::Eigen::Sparse::Interface< RangeFieldType > SolverType;
-    const SolverType* solver = Dune::Stuff::LA::Solver::Eigen::Sparse::create(solverType);
-    const bool success = solver->apply(*systemMatrix,
-                                       *rhsVector,
+    typedef typename Dune::Stuff::LA::Solver::Eigen::Interface< MatrixType > SolverType;
+    SolverType* solver = Dune::Stuff::LA::Solver::Eigen::create< MatrixType >(solverType);
+    solver->init(*systemMatrix);
+    const bool success = solver->apply(*rhsVector,
                                        *solutionVector,
                                        solverMaxIter,
                                        solverPrecision);
     if (!success)
       DUNE_THROW(Dune::MathError,
-                 "\nERROR: linear solver '" << solverType << "' did not converge in " << solverMaxIter << " iterations"
-                 << " with a tolerance of " << solverPrecision << "!");
+                 "\nERROR: linear solver '" << solverType << "' reported a problem!");
     if (solutionVector->size() != ansatzSpace.map().size())
       DUNE_THROW(Dune::MathError,
                  "\nERROR: linear solver '" << solverType << "' produced a solution of wrong size (is "
