bfed579406a2e9af8c87f46470b04ae8bd851555
Felix Albrecht <felix.albrecht@uni-muenster.de>
2013-01-18 18:09:16 +0100

    Revert "disabled some warning from the core modules"
    This reverts commit 315c5d200ea7038fd90c32c44f0959de212a04a8.

--- dune/detailed/discretizations/discretefunctional/local/codim0/integral.hh
+++ dune/detailed/discretizations/discretefunctional/local/codim0/integral.hh
@@ -5,14 +5,16 @@
 #ifndef DUNE_DETAILED_DISCRETIZATIONS_DISCRETEFUNCTIONAL_LOCAL_CODIM0_INTEGRAL_HH
 #define DUNE_DETAILED_DISCRETIZATIONS_DISCRETEFUNCTIONAL_LOCAL_CODIM0_INTEGRAL_HH
 
-#include <dune/stuff/common/header/disable_warnings.hh>
-  #include <dune/common/dynmatrix.hh>
+// dune-common
+#include <dune/common/dynmatrix.hh>
 
-  #include <dune/geometry/quadraturerules.hh>
-#include <dune/stuff/common/header/reenable_warnings.hh>
+// dune-geometry
+#include <dune/geometry/quadraturerules.hh>
 
+// dune-stuff
 #include <dune/stuff/common/vector.hh>
 
+// dune-detailed-discretizations
 #include <dune/detailed/discretizations/basefunctionset/local/wrapper.hh>
 
 namespace Dune
--- examples/elliptic/continuousgalerkin.cc
+++ examples/elliptic/continuousgalerkin.cc
@@ -1,28 +1,26 @@
-#include <dune/stuff/common/header/disable_warnings.hh>
-  #ifdef HAVE_CMAKE_CONFIG
-    #include "cmake_config.h"
-  #elif defined (HAVE_CONFIG_H)
-    #include "config.h"
-  #endif // ifdef HAVE_CMAKE_CONFIG
+#ifdef HAVE_CMAKE_CONFIG
+  #include "cmake_config.h"
+#elif defined (HAVE_CONFIG_H)
+  #include "config.h"
+#endif // ifdef HAVE_CMAKE_CONFIG
 
-  #define HAVE_DUNE_DETAILED_DISCRETIZATIONS 1
+#define HAVE_DUNE_DETAILED_DISCRETIZATIONS 1
 
-  #include <iostream>
-  #include <sstream>
+#include <iostream>
+#include <sstream>
 
-  #include <boost/filesystem.hpp>
+#include <boost/filesystem.hpp>
 
-  #include <dune/common/exceptions.hh>
-  #include <dune/common/shared_ptr.hh>
-  #include <dune/common/timer.hh>
+#include <dune/common/exceptions.hh>
+#include <dune/common/shared_ptr.hh>
+#include <dune/common/timer.hh>
 
-  #include <dune/grid/io/file/vtk/vtkwriter.hh>
+#include <dune/grid/io/file/vtk/vtkwriter.hh>
 
-  #include <dune/grid/part/leaf.hh>
+#include <dune/grid/part/leaf.hh>
 
-  #include <dune/fem/misc/mpimanager.hh>
-  #include <dune/fem/space/common/functionspace.hh>
-#include <dune/stuff/common/header/reenable_warnings.hh>
+#include <dune/fem/misc/mpimanager.hh>
+#include <dune/fem/space/common/functionspace.hh>
 
 #include <dune/stuff/common/parameter/tree.hh>
 #include <dune/stuff/common/logging.hh>
