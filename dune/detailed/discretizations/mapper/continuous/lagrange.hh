f1c050c65a8bd01f9f2ba21d5a9dd878b53d8522
Rene Milk <rene.milk@uni-muenster.de>
2013-01-24 17:47:12 +0100

    pragma cleanup

--- dune/detailed/discretizations/mapper/continuous/lagrange.hh
+++ dune/detailed/discretizations/mapper/continuous/lagrange.hh
@@ -88,9 +88,7 @@ public:
     typedef typename LagrangePointSetContainerType::const_iterator IteratorType;
     const IteratorType end = lagrangePointSetContainer_.end();
     for (IteratorType it = lagrangePointSetContainer_.begin(); it != end; ++it) {
-      const LagrangePointSetType* lagrangePointSet = *it;
-      if (lagrangePointSet)
-        delete lagrangePointSet;
+      delete *it;
     }
   } // ~Lagrange()
 
