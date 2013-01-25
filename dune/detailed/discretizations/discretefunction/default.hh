5972abe3374fb9cc11c5531ad5cf670293dd7f3d
Felix Albrecht <mail@felixalbrecht.de>
2013-01-25 22:36:43 +0100

    [discretefunction.default] added DefaultConst

--- dune/detailed/discretizations/discretefunction/default.hh
+++ dune/detailed/discretizations/discretefunction/default.hh
@@ -15,6 +15,9 @@ namespace Discretizations {
 namespace DiscreteFunction {
 
 template< class DiscreteFunctionSpaceImp, class VectorImp >
+class DefaultConst;
+
+template< class DiscreteFunctionSpaceImp, class VectorImp >
 class Default
   : public Dune::VTKFunction< typename DiscreteFunctionSpaceImp::GridViewType >
 {
@@ -28,13 +31,15 @@ public:
 
   typedef Default< DiscreteFunctionSpaceType, VectorType > ThisType;
 
+  typedef DefaultConst< DiscreteFunctionSpaceType, VectorType > ConstType;
+
   typedef typename DiscreteFunctionSpaceType::GridViewType::template Codim< 0 >::Iterator::Entity EntityType;
 
   static const int polOrder = DiscreteFunctionSpaceType::polynomialOrder;
 
-  typedef Dune::Detailed::Discretizations::DiscreteFunction::Local< ThisType > LocalFunctionType;
-
   typedef Dune::Detailed::Discretizations::DiscreteFunction::LocalConst< ThisType > ConstLocalFunctionType;
+
+  typedef Dune::Detailed::Discretizations::DiscreteFunction::Local< ThisType > LocalFunctionType;
 
   typedef typename DiscreteFunctionSpaceType::FunctionSpaceType FunctionSpaceType;
 
@@ -51,22 +56,29 @@ public:
   typedef typename VectorType::size_type size_type;
 
   Default(const DiscreteFunctionSpaceType& _space,
-          const std::string _name = "discrete_function" )
-    : BaseType()
-    , space_(_space)
-    , name_(_name)
-    , vector_(new VectorType(space_.map().size()))
-  {}
-
-  Default(const DiscreteFunctionSpaceType& _space,
           Dune::shared_ptr< VectorType > _vector,
           const std::string _name = "discrete_function")
-    : BaseType()
-    , space_(_space)
-    , name_(_name)
+    : BaseType(),
+      space_(_space)
     , vector_(_vector)
+    , name_(_name)
   {
     assert(vector_->size() == space_.map().size() && "Given vector has wrong size!");
+  }
+
+  Default(const DiscreteFunctionSpaceType& _space,
+          const std::string _name = "discrete_function")
+    : BaseType(),
+      space_(_space)
+    , vector_(new VectorType(space_.map().size()))
+    , name_(_name)
+  {
+    assert(vector_->size() == space_.map().size() && "Given vector has wrong size!");
+  }
+
+  operator ConstType() const
+  {
+    return ConstType(space_, vector_, name_);
   }
 
 private:
@@ -84,45 +96,144 @@ public:
     return name_;
   }
 
-  void setName(const std::string& _name)
+  ConstLocalFunctionType localFunction(const EntityType& entity) const
   {
-    name_ = _name;
+    return ConstLocalFunctionType(*this, entity);
   }
-
-  void clear()
-  {
-    const RangeFieldType zero(0);
-    for (size_type ii = 0; ii < size(); ++ii)
-      vector_->operator[](ii) = zero;
-  } // void clear()
-
-//  RangeFieldType& operator[](const size_type globalDofNumber)
-//  {
-//    assert(vector_->size() == space_.map().size() && "Given vector has wrong size!");
-//    assert(globalDofNumber < size());
-//    return vector_->operator[](globalDofNumber);
-//  }
-
-//  const RangeFieldType& operator[](const size_type globalDofNumber) const
-//  {
-//    assert(vector_->size() == space_.map().size() && "Given vector has wrong size!");
-//    assert(globalDofNumber < size());
-//    return vector_->operator[](globalDofNumber);
-//  }
 
   LocalFunctionType localFunction(const EntityType& entity)
   {
     return LocalFunctionType(*this, entity);
   }
 
-  ConstLocalFunctionType localFunction(const EntityType& entity) const
-  {
-    return ConstLocalFunctionType(*this, entity);
-  }
-
   Dune::shared_ptr< VectorType > vector()
   {
     return vector_;
+  }
+
+  const Dune::shared_ptr< const VectorType > vector() const
+  {
+    return vector_;
+  }
+
+  /**
+      @name Convenience methods
+      @{
+   **/
+  size_type size() const
+  {
+    return vector_->size();
+  }
+  /**
+      @}
+   **/
+
+  void clear()
+  {
+    const RangeFieldType zero(0);
+    for (size_type ii = 0; ii < vector_->size(); ++ii)
+      vector_->set(ii, zero);
+  }
+
+  /**
+      @name Methods to comply with the Dune::VTKFunction interface
+      @{
+   **/
+  virtual int ncomps() const
+  {
+    return dimRange;
+  }
+
+  virtual RangeFieldType evaluate(int component, const EntityType& entity, const DomainType& x) const
+  {
+    assert(vector_->size() == space_.map().size() && "Given vector has wrong size!");
+    RangeType ret(0.0);
+    localFunction(entity).evaluate(x, ret);
+    return ret[component];
+  }
+  /**
+      @}
+     **/
+
+private:
+  const DiscreteFunctionSpaceType& space_;
+  Dune::shared_ptr< VectorType > vector_;
+  const std::string name_;
+}; // class Defaul
+
+
+template< class DiscreteFunctionSpaceImp, class VectorImp >
+class DefaultConst
+  : public Dune::VTKFunction< typename DiscreteFunctionSpaceImp::GridViewType >
+{
+private:
+  typedef Dune::VTKFunction< typename DiscreteFunctionSpaceImp::GridViewType > BaseType;
+
+public:
+  typedef DiscreteFunctionSpaceImp DiscreteFunctionSpaceType;
+
+  typedef VectorImp VectorType;
+
+  typedef DefaultConst< DiscreteFunctionSpaceType, VectorType > ThisType;
+
+  typedef Default< DiscreteFunctionSpaceType, VectorType > NonConstType;
+
+  typedef typename DiscreteFunctionSpaceType::GridViewType::template Codim< 0 >::Iterator::Entity EntityType;
+
+  static const int polOrder = DiscreteFunctionSpaceType::polynomialOrder;
+
+  typedef Dune::Detailed::Discretizations::DiscreteFunction::LocalConst< ThisType > ConstLocalFunctionType;
+
+  typedef typename DiscreteFunctionSpaceType::FunctionSpaceType FunctionSpaceType;
+
+  typedef typename FunctionSpaceType::DomainType DomainType;
+
+  typedef typename FunctionSpaceType::RangeFieldType RangeFieldType;
+
+  typedef typename FunctionSpaceType::RangeType RangeType;
+
+  static const int dimDomain = DiscreteFunctionSpaceType::dimDomain;
+
+  static const int dimRange = DiscreteFunctionSpaceType::dimRange;
+
+  typedef typename VectorType::size_type size_type;
+
+  DefaultConst(const DiscreteFunctionSpaceType& _space,
+               const Dune::shared_ptr< const VectorType > _vector,
+               const std::string _name = "discrete_function")
+    : BaseType()
+    , space_(_space)
+    , name_(_name)
+    , vector_(_vector)
+  {
+    assert(vector_->size() == space_.map().size() && "Given vector has wrong size!");
+  }
+
+  DefaultConst(const NonConstType& _nonConstOther)
+    : BaseType()
+    , space_(_nonConstOther.space())
+    , name_(_nonConstOther.name())
+    , vector_(_nonConstOther.vector())
+  {}
+
+private:
+  DefaultConst(const ThisType&);
+  ThisType& operator=(const ThisType&);
+
+public:
+  const DiscreteFunctionSpaceType& space() const
+  {
+    return space_;
+  }
+
+  virtual std::string name() const
+  {
+    return name_;
+  }
+
+  ConstLocalFunctionType localFunction(const EntityType& entity) const
+  {
+    return ConstLocalFunctionType(*this, entity);
   }
 
   const Dune::shared_ptr< const VectorType > vector() const
@@ -164,9 +275,9 @@ public:
 
 private:
   const DiscreteFunctionSpaceType& space_;
-  std::string name_;
-  Dune::shared_ptr< VectorType > vector_;
-}; // class Default
+  const std::string name_;
+  const Dune::shared_ptr< const VectorType > vector_;
+}; // class DefaulConst
 
 } // namespace DiscreteFunction
 } // namespace Discretizations
