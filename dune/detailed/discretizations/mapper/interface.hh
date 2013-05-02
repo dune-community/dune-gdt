014358f4605d71d4527c4487bf821bae46245b3c
Felix Albrecht <mail@felixalbrecht.de>
2013-05-02 20:44:24 +0200

    [mapper] small simplification and redesign

--- dune/detailed/discretizations/mapper/interface.hh
+++ dune/detailed/discretizations/mapper/interface.hh
@@ -7,25 +7,15 @@
 namespace Dune {
 namespace Detailed {
 namespace Discretizations {
-namespace Mapper {
 
 
 template <class Traits>
-class Interface
+class MapperInterface
 {
 public:
-  typedef Interface<Traits> ThisType;
   typedef typename Traits::derived_type derived_type;
 
   typedef typename Traits::BackendType BackendType;
-  typedef typename Traits::SpaceType SpaceType;
-  typedef typename Traits::IndexType IndexType;
-
-  const SpaceType& space() const
-  {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().space());
-    return asImp().space();
-  }
 
   const BackendType& backend() const
   {
@@ -33,28 +23,28 @@ public:
     return asImp().backend();
   }
 
-  IndexType maxNumDofs() const
+  size_t maxNumDofs() const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().maxNumDofs());
     return asImp().maxNumDofs();
   }
 
   template <class EntityType>
-  IndexType numDofs(const EntityType& entity) const
+  size_t numDofs(const EntityType& entity) const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().numDofs(entity));
     return asImp().numDofs(entity);
   }
 
   template <class EntityType>
-  void mapToGlobal(const EntityType& entity, Dune::DynamicVector<IndexType>& globalIndices) const
+  void mapToGlobal(const EntityType& entity, Dune::DynamicVector<size_t>& globalIndices) const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().mapToGlobal(entity, globalIndices));
     asImp().mapToGlobal(entity, globalIndices);
   }
 
   template <class EntityType>
-  IndexType mapToGlobal(const EntityType& entity, const IndexType& localIndex) const
+  size_t mapToGlobal(const EntityType& entity, const size_t& localIndex) const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().mapToGlobal(entity, localIndex));
     return asImp().mapToGlobal(entity, localIndex);
@@ -69,10 +59,9 @@ public:
   {
     return static_cast<const derived_type&>(*this);
   }
-}; // class Interface
+}; // class MapperInterface
 
 
-} // namespace Mapper
 } // namespace Discretizations
 } // namespace Detailed
 } // namespace Dune
