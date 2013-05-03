15ed3edb30eaa4f8273bd4ae0cde23b6dd69bcdf
Felix Albrecht <mail@felixalbrecht.de>
2013-05-03 14:54:23 +0200

    [space.interface] added methods to compute pattern

--- dune/detailed/discretizations/space/interface.hh
+++ dune/detailed/discretizations/space/interface.hh
@@ -2,6 +2,9 @@
 #define DUNE_DETAILED_DISCRETIZATIONS_SPACE_INTERFACE_HH
 
 #include <dune/common/bartonnackmanifcheck.hh>
+#include <dune/common/dynvector.hh>
+
+#include <dune/stuff/la/container/pattern.hh>
 
 namespace Dune {
 namespace Detailed {
@@ -12,7 +15,6 @@ template< class Traits >
 class SpaceInterface
 {
 public:
-  typedef SpaceInterface< Traits >      ThisType;
   typedef typename Traits::derived_type derived_type;
 
   typedef typename Traits::GridPartType   GridPartType;
@@ -27,6 +29,8 @@ public:
   typedef typename Traits::MapperType           MapperType;
   typedef typename Traits::BaseFunctionSetType  BaseFunctionSetType;
   typedef typename Traits::EntityType           EntityType;
+
+  typedef Dune::Stuff::LA::Container::SparsityPatternDefault PatternType;
 
   const GridPartType& gridPart() const
   {
@@ -57,6 +61,51 @@ public:
     CHECK_INTERFACE_IMPLEMENTATION(asImp().baseFunctionSet(entity));
     return asImp().baseFunctionSet(entity);
   }
+
+  PatternType* computePattern() const
+  {
+    return computePattern(*this);
+  }
+
+  template< class OtherSpaceType >
+  PatternType* computePattern(const OtherSpaceType& other) const
+  {
+    // check type of the other space
+    return computePattern(gridPart(), other);
+  }
+
+  /**
+   *  \brief  computes a sparsity pattern, where this space is the test space (rows/outer) and the other space is the
+   *          ansatz space (cols/inner)
+   */
+  template< class LocalGridPartType, class O >
+  PatternType* computePattern(const LocalGridPartType& localGridPart,
+                              const SpaceInterface< O >& otherSpace) const
+  {
+    typedef typename PatternType::size_type size_type;
+    PatternType* ret = new PatternType(mapper().size());
+    PatternType& pattern = *ret;
+    // walk the grid part
+    for (typename LocalGridPartType::template Codim< 0 >::IteratorType entityIt = localGridPart.template begin< 0 >();
+         entityIt != localGridPart.template end< 0 >();
+         ++entityIt) {
+      const typename LocalGridPartType::template Codim< 0 >::EntityType& entity = *entityIt;
+      // get basefunctionsets
+      const auto testBase = baseFunctionSet(entity);
+      const auto ansatzBase = otherSpace.baseFunctionSet(entity);
+      Dune::DynamicVector< size_t > globalRows(testBase.size(), 0);
+      mapper().mapToGlobal(entity, globalRows);
+      Dune::DynamicVector< size_t > globalCols(ansatzBase.size(), 0);
+      otherSpace.mapper().mapToGlobal(entity, globalCols);
+      for (size_t ii = 0; ii < testBase.size(); ++ii) {
+        auto& columns = pattern.inner(globalRows[ii]);
+        for (size_t jj = 0; jj < ansatzBase.size(); ++jj) {
+          columns.insert(globalCols[jj]);
+        }
+      }
+    } // walk the grid part
+    return ret;
+  } // ... computePattern(...)
 
   derived_type& asImp()
   {
