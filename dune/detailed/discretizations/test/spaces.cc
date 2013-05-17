f6d25862e6a4c2a578612c412aab53c31b2b42bd
Felix Albrecht <mail@felixalbrecht.de>
2013-05-17 17:51:42 +0200

    [test.spaces] update

--- dune/detailed/discretizations/test/spaces.cc
+++ dune/detailed/discretizations/test/spaces.cc
@@ -1,8 +1,9 @@
 #include <dune/stuff/test/test_common.hh>
 
-#if HAVE_ALUGRID
-
+#include <memory>
 #include <vector>
+
+#include <dune/common/typetraits.hh>
 
 #include <dune/grid/alugrid.hh>
 
@@ -19,16 +20,16 @@
 namespace Stuff = Dune::Stuff;
 namespace DD    = Dune::Detailed::Discretizations;
 
-
-static const unsigned int dimDomain = 2;
-typedef Dune::ALUGrid<dimDomain, dimDomain, Dune::simplex, Dune::conforming> GridType;
-typedef typename GridType::ctype DomainFieldType;
+typedef Stuff::GridProviderInterface<> GridProviderType;
+typedef typename GridProviderType::GridType GridType;
+typedef Dune::Stuff::GridProviders<GridType> GridProviders;
+typedef Dune::grid::Part::Leaf::Const<GridType> GridPartType;
 typedef double RangeFieldType;
 static const unsigned int dimRange = 1;
-typedef Dune::grid::Part::Leaf::Const<GridType> GridPartType;
 
-typedef testing::Types<DD::ContinuousLagrangeSpace::FemLocalfunctionsWrapper<GridPartType, 1, RangeFieldType, dimRange>,
-                       DD::ContinuousLagrangeSpace::FemWrapper<GridPartType, 1, RangeFieldType, dimRange>> SpaceTypes;
+typedef testing::Types<DD::ContinuousLagrangeSpace::FemWrapper<GridPartType, 1, RangeFieldType, dimRange>,
+                       DD::ContinuousLagrangeSpace::FemLocalfunctionsWrapper<GridPartType, 1, RangeFieldType, dimRange>>
+    SpaceTypes;
 
 template <class T>
 struct SpaceCRTPtest : public ::testing::Test
@@ -45,7 +46,7 @@ struct SpaceCRTPtest : public ::testing::Test
     typedef typename SpaceType::DomainFieldType S_DomainFieldType;
     static const unsigned int DUNE_UNUSED(s_dimDomain) = SpaceType::dimDomain;
     typedef typename SpaceType::RangeFieldType S_RangeFieldType;
-    static const unsigned int DUNE_UNUSED(s_dimRangeRows) = SpaceType::dimRangeRows;
+    static const unsigned int DUNE_UNUSED(s_dimRange) = SpaceType::dimRange;
     static const unsigned int DUNE_UNUSED(s_dimRangeCols) = SpaceType::dimRangeCols;
     static const int DUNE_UNUSED(s_polOrder) = SpaceType::polOrder;
     typedef typename SpaceType::BackendType S_BackendType;
@@ -80,11 +81,13 @@ struct SpaceCRTPtest : public ::testing::Test
     typedef typename BaseFunctionSetType::Traits B_Traits;
     typedef typename B_Traits::BackendType B_BackendType;
     typedef typename B_Traits::EntityType B_EntityType;
-    typedef typename B_Traits::DomainType B_DomainType;
-    typedef typename B_Traits::RangeType B_RangeType;
-    typedef typename B_Traits::JacobianRangeType B_JacobianRangeType;
+    typedef typename BaseFunctionSetType::DomainType B_DomainType;
+    typedef typename BaseFunctionSetType::RangeType B_RangeType;
+    typedef typename BaseFunctionSetType::JacobianRangeType B_JacobianRangeType;
     // check the basefunctionset for functionality
-    typedef DD::BaseFunctionSetInterface<B_Traits> BaseFunctionSetInterfaceType;
+    typedef DD::
+        BaseFunctionSetInterface<B_Traits, S_DomainFieldType, s_dimDomain, S_RangeFieldType, s_dimRange, s_dimRangeCols>
+            BaseFunctionSetInterfaceType;
     const BaseFunctionSetInterfaceType& baseFunctionSetAsInterface =
         static_cast<const BaseFunctionSetInterfaceType&>(baseFunctionSet);
     const B_EntityType& DUNE_UNUSED(b_entity) = baseFunctionSetAsInterface.entity();
@@ -103,11 +106,9 @@ struct SpaceCRTPtest : public ::testing::Test
 TYPED_TEST_CASE(SpaceCRTPtest, SpaceTypes);
 TYPED_TEST(SpaceCRTPtest, SpaceCRTP)
 {
-  const Stuff::GridProviderInterface<GridType>* gridProvider =
-      Dune::Stuff::GridProviders<GridType>::create("gridprovider.cube");
-  const Dune::grid::Part::Leaf::Const<GridType> gridPart(*(gridProvider->grid()));
+  const std::shared_ptr<const GridProviderType> gridProvider(GridProviders::create("gridprovider.cube"));
+  const GridPartType gridPart(*(gridProvider->grid()));
   this->check(gridPart);
-  delete gridProvider;
 }
 
 
@@ -116,11 +117,3 @@ int main(int argc, char** argv)
   test_init(argc, argv);
   return RUN_ALL_TESTS();
 }
-
-#else
-int main(int /*argc*/, char** /*argv*/)
-{
-  std::cerr << "'HAVE_ALUGRID' not set!";
-  return 1;
-}
-#endif // HAVE_ALUGRID
