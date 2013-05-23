ec80fbd9063eb93ea1cc1e901fb9b6f55d1af858
Felix Albrecht <mail@felixalbrecht.de>
2013-05-23 19:43:31 +0200

    [evaluation] major interface change the inducing functions are given to the evaluation, and they have to provide a tuple of local functions for a given entity

--- dune/detailed/discretizations/localevaluation/elliptic.hh
+++ dune/detailed/discretizations/localevaluation/elliptic.hh
@@ -1,6 +1,8 @@
 #ifndef DUNE_DETAILED_DISCRETIZATIONS_EVALUATION_ELLIPTIC_HH
 #define DUNE_DETAILED_DISCRETIZATIONS_EVALUATION_ELLIPTIC_HH
 
+#include <tuple>
+
 #include <dune/common/dynmatrix.hh>
 #include <dune/common/typetraits.hh>
 #include <dune/common/static_assert.hh>
@@ -19,27 +21,62 @@ namespace LocalEvaluation {
 
 
 // forward, to be used in the traits
+template< class LocalizableFunctionImp >
 class Elliptic;
 
 
 /**
  *  \brief  Traits for the Elliptic evaluation.
  */
+template< class LocalizableFunctionImp >
 class EllipticTraits
 {
 public:
-  typedef Elliptic derived_type;
+  typedef Elliptic< LocalizableFunctionImp >  derived_type;
+  typedef LocalizableFunctionImp              LocalizableFunctionType;
+  dune_static_assert((Dune::IsBaseOf< Dune::Stuff::LocalizableFunction, LocalizableFunctionImp >::value),
+                     "ERROR: LocalizableFunctionImp is not a Dune::Stuff::LocalizableFunction.");
 };
 
 
 /**
  *  \brief  Computes an elliptic evaluation.
  */
+template< class LocalizableFunctionImp >
 class Elliptic
-  : public LocalEvaluation::Codim0Interface< EllipticTraits, 2 >
+  : public LocalEvaluation::Codim0Interface< EllipticTraits< LocalizableFunctionImp >, 2 >
 {
 public:
-  typedef EllipticTraits Traits;
+  typedef EllipticTraits< LocalizableFunctionImp > Traits;
+  typedef typename Traits::LocalizableFunctionType LocalizableFunctionType;
+
+  Elliptic(const LocalizableFunctionType& inducingFunction)
+    : inducingFunction_(inducingFunction)
+  {}
+
+//  template< class EntityType >
+//  auto localFunctions(const EntityType& entity) const -> decltype(LocalizableFunctionType::template localFunction< EntityType >(entity))
+//  {
+//    return inducingFunction_.localFunction(entity);
+//  }
+
+  template< class EntityType >
+  std::tuple< typename LocalizableFunctionType::template LocalFunction< EntityType >::Type > localFunctions(const EntityType& entity) const
+  {
+    return std::make_tuple(inducingFunction_.localFunction(entity));
+  }
+
+  /**
+   * \brief extracts the local functions and calls the correct order() method
+   */
+  template< class L, class T, class A, class D, int d, class R, int rT, int rCT, int rA, int rCA >
+  int order(const std::tuple< L >& localFunctions,
+            const BaseFunctionSetInterface< T, D, d, R, rT, rCT >& testBase,
+            const BaseFunctionSetInterface< A, D, d, R, rA, rCA >& ansatzBase) const
+  {
+    const auto& localFunction = std::get< 0 >(localFunctions);
+    return order(localFunction, testBase, ansatzBase);
+  }
 
   /**
    *  \todo add copydoc
@@ -56,6 +93,20 @@ public:
       return std::max(int(localFunction.order() + testBase.order() + ansatzBase.order() - 2), 0);
   } // int order(...)
 
+  /**
+   * \brief extracts the local functions and calls the correct evaluate() method
+   */
+  template< class L, class T, class A, class D, int d, class R, int rT, int rCT, int rA, int rCA >
+  static void evaluate(const std::tuple< L >& localFunctions,
+                       const BaseFunctionSetInterface< T, D, d, R, rT, rCT >& testBase,
+                       const BaseFunctionSetInterface< A, D, d, R, rA, rCA >& ansatzBase,
+                       const Dune::FieldVector< D, d >& localPoint,
+                       Dune::DynamicMatrix< R >& ret)
+  {
+    const auto& localFunction = std::get< 0 >(localFunctions);
+    evaluate(localFunction, testBase, ansatzBase, localPoint, ret);
+  }
+
   template< class L, class T, class A, class D, int d, class R, int rL, int rCL, int rT, int rCT, int rA, int rCA >
   static void evaluate(const Dune::Stuff::LocalFunctionInterface< L, D, d, R, rL, rCL >& /*localFunction*/,
                        const BaseFunctionSetInterface< T, D, d, R, rT, rCT >& /*testBase*/,
@@ -66,7 +117,6 @@ public:
     dune_static_assert((Dune::AlwaysFalse< R >::value),
                        "ERROR: not implemented for this combination of dimensions!");
   }
-
 
   /**
    *  \brief  Computes an elliptic evaluationf for a scalar local function and scalar basefunctionsets.
@@ -107,6 +157,9 @@ public:
       }
     }
   } // ... evaluate< ..., 1, 1 >(...)
+
+private:
+  const LocalizableFunctionType& inducingFunction_;
 }; // class LocalElliptic
 
 
