cf645bf8268b73e6327ededb69fb10e25abffd18
Felix Albrecht <mail@felixalbrecht.de>
2013-05-23 19:47:01 +0200

    [localfunctional.interface] added Codim1Interface

--- dune/detailed/discretizations/localfunctional/interface.hh
+++ dune/detailed/discretizations/localfunctional/interface.hh
@@ -54,6 +54,47 @@ public:
   }
 }; // class Codim0Interface
 
+template <class Traits>
+class Codim1Interface
+{
+public:
+  typedef typename Traits::derived_type derived_type;
+
+  size_t numTmpObjectsRequired() const
+  {
+    CHECK_INTERFACE_IMPLEMENTATION(asImp().numTmpObjectsRequired());
+    return asImp().numTmpObjectsRequired();
+  }
+
+  /**
+   *  \brief Applies the local functional.
+   *  \tparam T                 Traits of the test BaseFunctionSetInterface implementation
+   *  \tparam IntersectionType  A model of Dune::Intersection< ... >
+   *  \tparam D                 DomainFieldType
+   *  \tparam d                 dimDomain
+   *  \tparam R                 RangeFieldType
+   *  \tparam r                 dimRange of the of the testBase
+   *  \tparam rC                dimRangeCols of the testBase
+   *  \attention ret is assumed to be zero!
+   */
+  template <class T, class IntersectionType, class D, int d, class R, int r, int rC>
+  void apply(const BaseFunctionSetInterface<T, D, d, R, r, rC>& testBase, const IntersectionType& intersection,
+             Dune::DynamicVector<R>& ret, std::vector<Dune::DynamicVector<R>>& tmpLocalVectors) const
+  {
+    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().apply(testBase, intersection, ret, tmpLocalVectors));
+  }
+
+  derived_type& asImp()
+  {
+    return static_cast<derived_type&>(*this);
+  }
+
+  const derived_type& asImp() const
+  {
+    return static_cast<const derived_type&>(*this);
+  }
+}; // class Codim1Interface
+
 
 } // namespace LocalFunctional
 } // namespace Discretizations
