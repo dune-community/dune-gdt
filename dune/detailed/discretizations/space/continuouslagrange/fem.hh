8334a7d9ab10d2073b161e99143e49e2d599505f
Felix Albrecht <mail@felixalbrecht.de>
2013-06-19 16:37:47 +0200

    [space.continuouslagrange.fem] added another localConstraints()

--- dune/detailed/discretizations/space/continuouslagrange/fem.hh
+++ dune/detailed/discretizations/space/continuouslagrange/fem.hh
@@ -122,7 +122,7 @@ public:
   }
 
   void localConstraints(const EntityType& entity,
-                        Constraints::Dirichlet< typename GridPartType::GridViewType, RangeFieldType >& ret) const
+                        Constraints::Dirichlet< typename GridPartType::GridViewType, RangeFieldType, true >& ret) const
   {
     const auto& lagrangePointSet = backend_.lagrangePointSet(entity);
     std::set< size_t > localDirichletDofs;
@@ -172,7 +172,57 @@ public:
     } else {
       ret.setSize(0, 0);
     }
-  } // ... localConstraints(..., Dirichlet)
+  } // ... localConstraints(..., Dirichlet< ..., true >)
+
+  void localConstraints(const EntityType& entity,
+                        Constraints::Dirichlet< typename GridPartType::GridViewType, RangeFieldType, false >& ret) const
+  {
+    const auto& lagrangePointSet = backend_.lagrangePointSet(entity);
+    std::set< size_t > localDirichletDofs;
+    // loop over all intersections
+    const auto& gridBoundary = ret.gridBoundary();
+    const auto intersectionEndIt = gridPart_.iend(entity);
+    for (auto intersectionIt = gridPart_.ibegin(entity); intersectionIt !=intersectionEndIt; ++intersectionIt) {
+      const auto& intersection = *intersectionIt;
+      // only work on dirichlet intersections
+      if (gridBoundary.dirichlet(intersection)) {
+        // get local face number of boundary intersection
+        const int intersectionIndex = intersection.indexInInside();
+        // iterate over face dofs and set unit row
+        const auto faceDofEndIt = lagrangePointSet.template endSubEntity< 1 >(intersectionIndex);
+        for (auto faceDofIt = lagrangePointSet.template beginSubEntity< 1 >(intersectionIndex);
+             faceDofIt != faceDofEndIt;
+             ++faceDofIt) {
+          const size_t localDofIndex = *faceDofIt;
+          localDirichletDofs.insert(localDofIndex);
+        } // iterate over face dofs and set unit row
+      } // only work on dirichlet intersections
+    } // loop over all intersections
+    const BaseFunctionSetType basis = baseFunctionSet(entity);
+    const size_t numRows = localDirichletDofs.size();
+    if (numRows > 0) {
+      const size_t numCols = basis.size();
+      ret.setSize(numRows, numCols);
+      mapper_.globalIndices(entity, tmpMappedRows_);
+      mapper_.globalIndices(entity, tmpMappedCols_);
+      size_t localRow = 0;
+      const RangeFieldType zero(0);
+      const RangeFieldType one(1);
+      for (auto localDirichletDofIt = localDirichletDofs.begin();
+           localDirichletDofIt != localDirichletDofs.end();
+           ++localDirichletDofIt) {
+        const size_t& localDirichletDofIndex = * localDirichletDofIt;
+        ret.globalRow(localRow) = tmpMappedRows_[localDirichletDofIndex];
+        for (size_t jj = 0; jj < ret.cols(); ++jj) {
+          ret.globalCol(jj) = tmpMappedCols_[jj];
+          ret.value(localRow, jj) = zero;
+        }
+        ++localRow;
+      }
+    } else {
+      ret.setSize(0, 0);
+    }
+  } // ... localConstraints(..., Dirichlet< ..., false >)
 
 private:
   const GridPartType& gridPart_;
