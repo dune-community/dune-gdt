ec90b76fba7b7f9d1b4c3e498452b03fde787b87
Felix Albrecht <mail@felixalbrecht.de>
2013-06-19 16:38:11 +0200

    [discretefunction.default] removed unnecessary const

--- dune/detailed/discretizations/discretefunction/default.hh
+++ dune/detailed/discretizations/discretefunction/default.hh
@@ -214,7 +214,7 @@ public:
     return ConstLocalFunctionType(*this, entity);
   }
 
-  const std::shared_ptr<const VectorType> vector() const
+  std::shared_ptr<const VectorType> vector() const
   {
     return vector_;
   }
