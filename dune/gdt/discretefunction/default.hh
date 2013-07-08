e33f047518ac707bd84a714b8ddd65de1c62296b
Felix Albrecht <mail@felixalbrecht.de>
2013-07-08 11:11:25 +0200

    [discretefunction.default] do not enforce VectorInterface for the moment

--- dune/gdt/discretefunction/default.hh
+++ dune/gdt/discretefunction/default.hh
@@ -37,7 +37,7 @@ class DiscreteFunctionDefaultConst
 {
 public:
   typedef typename SpaceInterface< typename SpaceImp::Traits >::derived_type SpaceType;
-  typedef typename Dune::Stuff::LA::VectorInterface< typename VectorImp::Traits >::derived_type VectorType;
+  typedef /*typename Dune::Stuff::LA::VectorInterface< typename*/ VectorImp/*::Traits >::derived_type*/ VectorType;
 
   typedef typename SpaceType::EntityType EntityType;
 
@@ -104,7 +104,7 @@ class DiscreteFunctionDefault
   typedef DiscreteFunctionDefaultConst< SpaceImp, VectorImp > BaseType;
 public:
   typedef typename SpaceInterface< typename SpaceImp::Traits >::derived_type SpaceType;
-  typedef typename Dune::Stuff::LA::VectorInterface< typename VectorImp::Traits >::derived_type VectorType;
+  typedef /*typename Dune::Stuff::LA::VectorInterface< typename*/ VectorImp/*::Traits >::derived_type*/ VectorType;
 
   typedef typename SpaceType::EntityType EntityType;
 
