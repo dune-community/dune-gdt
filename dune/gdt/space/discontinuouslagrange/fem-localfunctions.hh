d6983aeada486a4cc4a6600ae9f6f5ad7cb2a9b2
Felix Albrecht <mail@felixalbrecht.de>
2013-08-28 20:20:55 +0200

    [space.discontinuousgalerkin...] choose correct pattern

--- dune/gdt/space/discontinuouslagrange/fem-localfunctions.hh
+++ dune/gdt/space/discontinuouslagrange/fem-localfunctions.hh
@@ -99,6 +99,7 @@ template< class GridPartImp, int polynomialOrder, class RangeFieldImp >
 class FemLocalfunctionsWrapper< GridPartImp, polynomialOrder, RangeFieldImp, 1, 1 >
   : public SpaceInterface< FemLocalfunctionsWrapperTraits< GridPartImp, polynomialOrder, RangeFieldImp, 1, 1 > >
 {
+  typedef SpaceInterface< FemLocalfunctionsWrapperTraits< GridPartImp, polynomialOrder, RangeFieldImp, 1, 1 > > BaseType;
 public:
   typedef FemLocalfunctionsWrapperTraits< GridPartImp, polynomialOrder, RangeFieldImp, 1, 1 > Traits;
 
@@ -114,6 +115,8 @@ public:
   typedef typename Traits::MapperType           MapperType;
   typedef typename Traits::BaseFunctionSetType  BaseFunctionSetType;
   typedef typename Traits::EntityType           EntityType;
+
+  typedef Dune::Stuff::LA::SparsityPatternDefault PatternType;
 
 private:
   typedef typename Traits::BaseFunctionSetMapType BaseFunctionSetMapType;
@@ -156,6 +159,15 @@ public:
                         Constraints::LocalDefault< R >& /*ret*/) const
   {
     dune_static_assert(Dune::AlwaysFalse< R >::value, "ERROR: not implemented for arbitrary constraints!");
+  }
+
+  using BaseType::computePattern;
+
+  template< class LocalGridPartType, class OtherSpaceType >
+  PatternType* computePattern(const LocalGridPartType& localGridPart,
+                              const OtherSpaceType& otherSpace) const
+  {
+    return BaseType::computeVolumeAndCouplingPattern(localGridPart, otherSpace);
   }
 
 private:
