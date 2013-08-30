0fe8697101567fc43b55e72480dbc6def176ab0f
Felix Albrecht <mail@felixalbrecht.de>
2013-08-30 09:19:17 +0200

    [discretefunction.difference] is now usable

--- dune/gdt/discretefunction/difference.hh
+++ dune/gdt/discretefunction/difference.hh
@@ -15,62 +15,80 @@ namespace GDT {
 
 namespace LocalFunction {
 
-template< class MinuendType, class SubrahendImp, class D, int d, class R, int rR, int rC = 1 >
+template< class EntityType, class MinuendType, class SubtrahendType  >
 class Difference;
 
-template< class MinuendType, class SubrahendImp, class D, int d, class R, int rR, int rC = 1 >
-class DifferenceTraits;
 
-
-template< class MinuendType, class SubtrahendType, class D, int d, class R, int r >
-class DifferenceTraits< MinuendType, SubtrahendType, D, d, R, r, 1 >
+template< class EntityImp, class MinuendType, class SubtrahendType >
+class DifferenceTraits
 {
-  static_assert(std::is_base_of< Stuff::LocalFunctionInterface< typename MinuendType::Traits, D, d, R, r, 1 >,
-                                                                MinuendType >::value,
-                "MinuendType has to be derived from Stuff::LocalFunctionInterface< ..., D, d, R, r, 1 >");
-  static_assert(std::is_base_of< Stuff::LocalFunctionInterface< typename SubtrahendType::Traits, D, d, R, r, 1 >,
-                                                                SubtrahendType >::value,
-                "SubtrahendType has to be derived from Stuff::LocalFunctionInterface< ..., D, d, R, r, 1 >");
-  static_assert(std::is_same< typename MinuendType::EntityType, typename SubtrahendType::EntityType >::value,
-                "EntityType of MinuendType and SubtrahendType do not match!");
 public:
-  typedef Difference< MinuendType, SubtrahendType, D, d, R, r, 1 > derived_type;
-  typedef typename MinuendType::EntityType EntityType;
+  typedef typename MinuendType::template LocalFunction< EntityImp >::Type  LocalMinuendType;
+  typedef typename SubtrahendType::template LocalFunction< EntityImp >::Type LocalSubtrahendType;
+  typedef typename LocalMinuendType::DomainFieldType DomainFieldType;
+  static_assert(std::is_same< DomainFieldType, typename LocalSubtrahendType::DomainFieldType >::value,
+                "DomainFieldType of LocalMinuendType and LocalSubtrahendType do not match!");
+  static const unsigned int dimDomain = LocalMinuendType::dimDomain;
+  static_assert(dimDomain == LocalSubtrahendType::dimDomain,
+                "dimDomain of LocalMinuendType and LocalSubtrahendType do not match!");
+  typedef typename LocalMinuendType::RangeFieldType RangeFieldType;
+  static_assert(std::is_same< RangeFieldType, typename LocalSubtrahendType::RangeFieldType >::value,
+                "RangeFieldType of LocalMinuendType and LocalSubtrahendType do not match!");
+  static const unsigned int dimRangeRows = LocalMinuendType::dimRangeRows;
+  static_assert(dimRangeRows == LocalSubtrahendType::dimRangeRows,
+                "dimRangeRows of LocalMinuendType and LocalSubtrahendType do not match!");
+  static const unsigned int dimRangeCols = LocalMinuendType::dimRangeCols;
+  static_assert(dimRangeCols == LocalSubtrahendType::dimRangeCols,
+                "dimRangeCols of LocalMinuendType and LocalSubtrahendType do not match!");
+  typedef EntityImp EntityType;
+  typedef Difference< EntityType, MinuendType, SubtrahendType > derived_type;
+  static_assert(std::is_base_of<  Stuff::LocalFunctionInterface<  typename LocalMinuendType::Traits,
+                                                                  DomainFieldType, dimDomain,
+                                                                  RangeFieldType, dimRangeRows, dimRangeCols >,
+                                  LocalMinuendType >::value,
+                "LocalMinuendType has to be derived from Stuff::LocalFunctionInterface!");
+  static_assert(std::is_base_of<  Stuff::LocalFunctionInterface<  typename LocalSubtrahendType::Traits,
+                                                                  DomainFieldType, dimDomain,
+                                                                  RangeFieldType, dimRangeRows, dimRangeCols >,
+                                  LocalSubtrahendType >::value,
+                "LocalSubtrahendType has to be derived from Stuff::LocalFunctionInterface!");
 };
 
 
-template< class MinuendType, class SubtrahendType, class D, int d, class R, int r >
-class Difference< MinuendType, SubtrahendType, D, d, R, r, 1 >
-  : public Stuff::LocalFunctionInterface< DifferenceTraits< MinuendType, SubtrahendType, D, d, R, r, 1 >, D, d, R, r, 1 >
+template< class EntityImp, class MinuendType, class SubtrahendType  >
+class Difference
+  : public Stuff::LocalFunctionInterface< DifferenceTraits< EntityImp, MinuendType, SubtrahendType >,
+                                          typename DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::DomainFieldType,
+                                          DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::dimDomain,
+                                          typename DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::RangeFieldType,
+                                          DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::dimRangeRows,
+                                          DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::dimRangeCols >
 {
+  typedef Stuff::LocalFunctionInterface<  DifferenceTraits< EntityImp, MinuendType, SubtrahendType >,
+                                          typename DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::DomainFieldType,
+                                          DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::dimDomain,
+                                          typename DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::RangeFieldType,
+                                          DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::dimRangeRows,
+                                          DifferenceTraits< EntityImp, MinuendType, SubtrahendType >::dimRangeCols > BaseType;
 public:
-  typedef DifferenceTraits< MinuendType, SubtrahendType, D, d, R, r, 1 > Traits;
-  typedef typename Traits::EntityType EntityType;
+  typedef DifferenceTraits< EntityImp, MinuendType, SubtrahendType >  Traits;
+  typedef typename Traits::EntityType                                 EntityType;
 
-  typedef D                                               DomainFieldType;
-  static const unsigned int                               dimDomain = d;
-  typedef Dune::FieldVector< DomainFieldType, dimDomain > DomainType;
+  typedef typename BaseType::DomainType         DomainType;
+  typedef typename BaseType::RangeType          RangeType;
+  typedef typename BaseType::JacobianRangeType  JacobianRangeType;
 
-  typedef R                                             RangeFieldType;
-  static const unsigned int                             dimRange = r;
-  static const unsigned int                             dimRangeRows = dimRange;
-  static const unsigned int                             dimRangeCols = 1;
-  typedef Dune::FieldVector< RangeFieldType, dimRange > RangeType;
-
-  typedef Dune::FieldMatrix< RangeFieldType, dimRange, dimDomain > JacobianRangeType;
-
-  Difference(const MinuendType& minuend, const SubtrahendType& subtrahend)
-    : minuend_(minuend)
-    , subtrahend_(subtrahend)
+  Difference(const EntityType& entity, const MinuendType& minuend, const SubtrahendType& subtrahend)
+    : entity_(entity)
+    , minuend_(minuend.localFunction(entity))
+    , subtrahend_(subtrahend.localFunction(entity))
     , tmp_value_(0)
     , tmp_jacobian_value_(0)
-  {
-    assert(minuend_.entity() == subtrahend_.entity());
-  }
+  {}
 
   const EntityType& entity() const
   {
-    return minuend_.entity();
+    return entity_;
   }
 
   virtual int order() const
@@ -96,8 +114,9 @@ public:
   }
 
 private:
-  const MinuendType& minuend_;
-  const SubtrahendType& subtrahend_;
+  const EntityType& entity_;
+  const typename Traits::LocalMinuendType& minuend_;
+  const typename Traits::LocalSubtrahendType& subtrahend_;
   mutable RangeType tmp_value_;
   mutable JacobianRangeType tmp_jacobian_value_;
 };
@@ -112,32 +131,16 @@ template< class MinuendType, class SubtrahendType >
 class Difference
   : public Stuff::LocalizableFunction
 {
+  static_assert(std::is_base_of< Stuff::LocalizableFunction, MinuendType >::value,
+                "MinuendType has to be derived from Stuff::LocalizableFunction!");
+  static_assert(std::is_base_of< Stuff::LocalizableFunction, SubtrahendType >::value,
+                "SubtrahendType has to be derived from Stuff::LocalizableFunction!");
 public:
   template< class EntityType >
   class LocalFunction
   {
-    typedef typename MinuendType::template LocalFunction< EntityType >     LocalMinuendType;
-    typedef typename SubtrahendType::template LocalFunction< EntityType >  LocalSubtrahendType;
-    typedef typename LocalMinuendType::DomainFieldType DomainFieldType;
-    static_assert(std::is_same< DomainFieldType, typename LocalSubtrahendType::DomainFieldType >::value,
-                  "DomainFieldType of LocalMinuendType and LocalSubtrahendType do not match!");
-    static const unsigned int dimDomain = LocalMinuendType::dimDomain;
-    static_assert(dimDomain == LocalSubtrahendType::dimDomain,
-                  "dimDomain of LocalMinuendType and LocalSubtrahendType do not match!");
-    typedef typename LocalMinuendType::RangeFieldType RangeFieldType;
-    static_assert(std::is_same< RangeFieldType, typename LocalSubtrahendType::RangeFieldType >::value,
-                  "RangeFieldType of LocalMinuendType and LocalSubtrahendType do not match!");
-    static const unsigned int dimRangeRows = LocalMinuendType::dimRangeRows;
-    static_assert(dimRangeRows == LocalSubtrahendType::dimRangeRows,
-                  "dimRangeRows of LocalMinuendType and LocalSubtrahendType do not match!");
-    static const unsigned int dimRangeCols = LocalMinuendType::dimRangeCols;
-    static_assert(dimRangeCols == LocalSubtrahendType::dimRangeCols,
-                  "dimRangeCols of LocalMinuendType and LocalSubtrahendType do not match!");
   public:
-    typedef GDT::LocalFunction::Difference< LocalMinuendType,
-                                            LocalSubtrahendType,
-                                            DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRangeRows, dimRangeCols > Type;
+    typedef GDT::LocalFunction::Difference< EntityType, MinuendType, SubtrahendType > Type;
   };
 
   Difference(const MinuendType& minuend, const SubtrahendType& subtrahend)
@@ -148,8 +151,7 @@ public:
   template< class EntityType >
   typename LocalFunction< EntityType >::Type localFunction(const EntityType& entity) const
   {
-    return typename LocalFunction< EntityType >::Type(minuend_.localFunction(entity),
-                                                      subtrahend_.localFunction(entity));
+    return typename LocalFunction< EntityType >::Type(entity, minuend_, subtrahend_);
   }
 
 private:
