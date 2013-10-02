44a43e331cb00e58d761e789e1d0831dfe5febb2
Felix Albrecht <mail@felixalbrecht.de>
2013-10-02 22:40:09 +0200

    [operator.products] updated L2 and H1

--- dune/gdt/operator/products.hh
+++ dune/gdt/operator/products.hh
@@ -11,10 +11,9 @@
 #include <dune/stuff/functions/interfaces.hh>
 #include <dune/stuff/functions/constant.hh>
 
-#include "../basefunctionset/wrapper.hh"
+#include "../localoperator/codim0.hh"
 #include "../localfunctional/codim0.hh"
 #include "../localevaluation/product.hh"
-#include "../localoperator/codim0.hh"
 #include "../localevaluation/elliptic.hh"
 
 namespace Dune {
@@ -26,40 +25,34 @@ template< class GridPartType >
 class L2
 {
 public:
+  typedef typename GridPartType::template Codim< 0 >::EntityType EntityType;
+  typedef typename GridPartType::ctype DomainFieldType;
+  static const unsigned int dimDomain = GridPartType::dimension;
+
   L2(const GridPartType& grid_part)
     : grid_part_(grid_part)
   {}
 
-  template< class RangeType, class SourceType >
-  typename RangeType::template LocalFunction< typename GridPartType::template Codim< 0 >::EntityType >::Type::RangeFieldType
-    apply2(const RangeType& range, const SourceType& source) const
+  template< class RR, int rRR, int rCR, class RS, int rRS, int rCS >
+  void apply2(const Stuff::LocalizableFunctionInterface< EntityType, DomainFieldType, dimDomain, RR, rRR, rCR >& /*range*/,
+              const Stuff::LocalizableFunctionInterface< EntityType, DomainFieldType, dimDomain, RS, rRS, rCS >& /*source*/) const
   {
-    // checks
-    static_assert(std::is_base_of< Stuff::LocalizableFunction, SourceType >::value,
-                  "SourceType has to be derived from Stuff::LocalizableFunction!");
-    static_assert(std::is_base_of< Stuff::LocalizableFunction, RangeType >::value,
-                  "RangeType has to be derived from Stuff::LocalizableFunction!");
-    typedef typename GridPartType::template Codim< 0 >::EntityType EntityType;
-    typedef typename SourceType::template LocalFunction< EntityType >::Type SourceLocalFunctionType;
-    typedef typename RangeType::template LocalFunction< EntityType >::Type RangeLocalFunctionType;
-    typedef typename SourceLocalFunctionType::DomainFieldType DomainFieldType;
-    static_assert(std::is_same< DomainFieldType, typename RangeLocalFunctionType::DomainFieldType >::value,
-                  "DomainFieldType of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
-    static const unsigned int dimDomain = SourceLocalFunctionType::dimDomain;
-    static_assert(dimDomain == RangeLocalFunctionType::dimDomain,
-                  "dimDomain of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
-    typedef typename SourceLocalFunctionType::RangeFieldType RangeFieldType;
-    static_assert(std::is_same< RangeFieldType, typename RangeLocalFunctionType::RangeFieldType >::value,
-                  "RangeFieldType of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
-    static const unsigned int dimRangeRows = SourceLocalFunctionType::dimRangeRows;
-    static_assert(dimRangeRows == RangeLocalFunctionType::dimRangeRows,
-                  "dimRangeRows of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
-    static const unsigned int dimRangeCols = SourceLocalFunctionType::dimRangeCols;
-    static_assert(dimRangeCols == RangeLocalFunctionType::dimRangeCols,
-                  "dimRangeCols of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
+    static_assert((Dune::AlwaysFalse< RR >::value), "Not implemented for this combination!");
+  }
+
+  template< class RangeFieldType, int dimRangeRows, int dimRangeCols >
+  RangeFieldType apply2(const Stuff::LocalizableFunctionInterface
+                            < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRangeRows, dimRangeCols >&
+                          range,
+                        const Stuff::LocalizableFunctionInterface
+                            < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRangeRows, dimRangeCols >&
+                          source) const
+  {
+    typedef Stuff::LocalizableFunctionInterface
+        < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRangeRows, dimRangeCols >
+      RangeType;
     // some preparations
-    typedef LocalFunctional::Codim0Integral< LocalEvaluation::Product< RangeType > > LocalFunctionalType;
-    const LocalFunctionalType local_functional(range);
+    const LocalFunctional::Codim0Integral< LocalEvaluation::Product< RangeType > > local_functional(range);
     RangeFieldType ret = 0;
     DynamicVector< RangeFieldType > local_functional_result(1, 0);
     std::vector< DynamicVector< RangeFieldType > > tmp_vectors(local_functional.numTmpObjectsRequired(),
@@ -70,17 +63,14 @@ public:
     for (auto entity_it = grid_part_.template begin< 0 >(); entity_it != entity_it_end; ++entity_it) {
       const auto& entity = *entity_it;
       // get the local function
-      const auto source_local_function = source.localFunction(entity);
-      typedef BaseFunctionSet::LocalFunctionWrapper<  SourceLocalFunctionType, DomainFieldType, dimDomain,
-                                                      RangeFieldType, dimRangeRows, dimRangeCols > LocalSourceWrapperType;
-      const LocalSourceWrapperType local_source_wrapper(source_local_function);
+      const auto source_local_function = source.local_function(entity);
       // apply local functional
-      local_functional.apply(local_source_wrapper, local_functional_result, tmp_vectors);
+      local_functional.apply(*source_local_function, local_functional_result, tmp_vectors);
       assert(local_functional_result.size() == 1);
       ret += local_functional_result[0];
     } // walk the grid
     return ret;
-  }
+  } // ... apply2(...)
 
 private:
   const GridPartType& grid_part_;
@@ -91,43 +81,33 @@ template< class GridPartType >
 class H1
 {
 public:
+  typedef typename GridPartType::template Codim< 0 >::EntityType EntityType;
+  typedef typename GridPartType::ctype DomainFieldType;
+  static const unsigned int dimDomain = GridPartType::dimension;
+
   H1(const GridPartType& grid_part)
     : grid_part_(grid_part)
   {}
 
-  template< class RangeType, class SourceType >
-  typename RangeType::template LocalFunction< typename GridPartType::template Codim< 0 >::EntityType >::Type::RangeFieldType
-    apply2(const RangeType& range, const SourceType& source) const
+  template< class RR, int rRR, int rCR, class RS, int rRS, int rCS >
+  void apply2(const Stuff::LocalizableFunctionInterface< EntityType, DomainFieldType, dimDomain, RR, rRR, rCR >& /*range*/,
+              const Stuff::LocalizableFunctionInterface< EntityType, DomainFieldType, dimDomain, RS, rRS, rCS >& /*source*/) const
   {
-    // checks
-    static_assert(std::is_base_of< Stuff::LocalizableFunction, SourceType >::value,
-                  "SourceType has to be derived from Stuff::LocalizableFunction!");
-    static_assert(std::is_base_of< Stuff::LocalizableFunction, RangeType >::value,
-                  "RangeType has to be derived from Stuff::LocalizableFunction!");
-    typedef typename GridPartType::template Codim< 0 >::EntityType EntityType;
-    typedef typename SourceType::template LocalFunction< EntityType >::Type SourceLocalFunctionType;
-    typedef typename RangeType::template LocalFunction< EntityType >::Type RangeLocalFunctionType;
-    typedef typename SourceLocalFunctionType::DomainFieldType DomainFieldType;
-    static_assert(std::is_same< DomainFieldType, typename RangeLocalFunctionType::DomainFieldType >::value,
-                  "DomainFieldType of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
-    static const unsigned int dimDomain = SourceLocalFunctionType::dimDomain;
-    static_assert(dimDomain == RangeLocalFunctionType::dimDomain,
-                  "dimDomain of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
-    typedef typename SourceLocalFunctionType::RangeFieldType RangeFieldType;
-    static_assert(std::is_same< RangeFieldType, typename RangeLocalFunctionType::RangeFieldType >::value,
-                  "RangeFieldType of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
-    static const unsigned int dimRangeRows = SourceLocalFunctionType::dimRangeRows;
-    static_assert(dimRangeRows == RangeLocalFunctionType::dimRangeRows,
-                  "dimRangeRows of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
-    static const unsigned int dimRangeCols = SourceLocalFunctionType::dimRangeCols;
-    static_assert(dimRangeCols == RangeLocalFunctionType::dimRangeCols,
-                  "dimRangeCols of SourceLocalFunctionType and RangeLocalFunctionType do not match!");
+    static_assert((Dune::AlwaysFalse< RR >::value), "Not implemented for this combination!");
+  }
+
+  template< class RangeFieldType, int dimRangeRows, int dimRangeCols >
+  RangeFieldType apply2(const Stuff::LocalizableFunctionInterface
+                            < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRangeRows, dimRangeCols >&
+                          range,
+                        const Stuff::LocalizableFunctionInterface
+                            < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRangeRows, dimRangeCols >&
+                          source) const
+  {
     // some preparations
-    typedef Stuff::FunctionConstant< DomainFieldType, dimDomain, RangeFieldType, dimRangeRows, dimRangeCols >
-        ConstantFunctionType;
+    typedef Stuff::Function::Constant< EntityType, DomainFieldType, dimDomain, RangeFieldType, 1 > ConstantFunctionType;
     const ConstantFunctionType one(1);
-    typedef LocalOperator::Codim0Integral< LocalEvaluation::Elliptic< ConstantFunctionType > > LocalOperatorType;
-    const LocalOperatorType local_operator(one);
+    const LocalOperator::Codim0Integral< LocalEvaluation::Elliptic< ConstantFunctionType > > local_operator(one);
     RangeFieldType ret = 0;
     DynamicMatrix< RangeFieldType > local_operator_result(1, 1, 0);
     std::vector< DynamicMatrix< RangeFieldType > > tmp_matrices(local_operator.numTmpObjectsRequired(),
@@ -138,22 +118,16 @@ public:
     for (auto entity_it = grid_part_.template begin< 0 >(); entity_it != entity_it_end; ++entity_it) {
       const auto& entity = *entity_it;
       // get the local functions
-      const auto source_local_function = source.localFunction(entity);
-      typedef BaseFunctionSet::LocalFunctionWrapper<  SourceLocalFunctionType, DomainFieldType, dimDomain,
-                                                      RangeFieldType, dimRangeRows, dimRangeCols > LocalSourceWrapperType;
-      const LocalSourceWrapperType local_source_wrapper(source_local_function);
-      const auto range_local_function = range.localFunction(entity);
-      typedef BaseFunctionSet::LocalFunctionWrapper<  RangeLocalFunctionType, DomainFieldType, dimDomain,
-                                                      RangeFieldType, dimRangeRows, dimRangeCols > LocalRangeWrapperType;
-      const LocalRangeWrapperType local_range_wrapper(range_local_function);
+      const auto source_local_function = source.local_function(entity);
+      const auto range_local_function = range.local_function(entity);
       // apply local operator
-      local_operator.apply(local_source_wrapper, local_range_wrapper, local_operator_result, tmp_matrices);
+      local_operator.apply(*source_local_function, *range_local_function, local_operator_result, tmp_matrices);
       assert(local_operator_result.rows() == 1);
       assert(local_operator_result.cols() == 1);
       ret += local_operator_result[0][0];
     } // walk the grid
     return ret;
-  }
+  } // ... apply2(...)
 
 private:
   const GridPartType& grid_part_;
