1585d96270a9833c7498e0d1763249aceb0712bf
Felix Albrecht <mail@felixalbrecht.de>
2013-10-02 20:24:34 +0200

    [localevaluation.interfaces] updated to new functions

--- dune/gdt/localevaluation/interface.hh
+++ dune/gdt/localevaluation/interface.hh
@@ -14,8 +14,6 @@
 
 #include <dune/stuff/functions/interfaces.hh>
 
-#include <dune/gdt/basefunctionset/interface.hh>
-
 namespace Dune {
 namespace GDT {
 namespace LocalEvaluation {
@@ -29,13 +27,23 @@ namespace LocalEvaluation {
  *  A derived class has to provide a method
 \code
 template< class EntityType >
-std::tuple< ... > localFunctions(const EntityType& entity) const
+typename LocalfunctionTuple< EntityType >::Type localFunctions(const EntityType& entity) const
 {
   ...
 }
 \endcode
- *  which returns the inducing local functions which are needed for one entity. Whatever this method returns will be
- *  given to order() and evaluate()
+ *  which returns the inducing local functions which are needed for one entity
+ * and a class
+\code
+  template< class EntityType >
+  class LocalfunctionTuple
+  {
+  public:
+    typedef ... Type;
+  };
+\endcode
+ * which defines the return type of this method. Whatever localFunctions() returns will be given to order() and
+ * evaluate().
  */
 template <class Traits, int numArguments>
 class Codim0Interface
@@ -56,17 +64,17 @@ public:
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTuple Type of the localFunction container, that is returned by localFunctions(), i.e.
-   * std::tuple< ... >
-   *  \tparam T   Traits of the test BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
+   *  \tparam E   EntityType
    *  \tparam D   DomainFieldType
    *  \tparam d   dimDomain
    *  \tparam R   RangeFieldType
    *  \tparam r   dimRange of the testBase
    *  \tparam rC  dimRangeRows of the testBase
    */
-  template <class LocalFunctionTuple, class T, class D, int d, class R, int r, int rC>
-  int order(const LocalFunctionTuple& localFunctions, const BaseFunctionSetInterface<T, D, d, R, r, rC>& testBase) const
+  template <class LocalFunctionTuple, class E, class D, int d, class R, int r, int rC>
+  size_t order(const LocalFunctionTuple& localFunctions,
+               const Stuff::LocalfunctionSetInterface<E, D, d, R, r, rC>& testBase) const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctions, testBase));
     return asImp().order(localFunctions, testBase);
@@ -74,9 +82,8 @@ public:
 
   /**
    *  \brief  Computes a unary codim 0 evaluation.
-   *  \tparam LocalFunctionTuple Type of the localFunction container, that is returned by localFunctions(), i.e.
-   * std::tuple< ... >
-   *  \tparam T   Traits of the test BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
+   *  \tparam E   EntityType
    *  \tparam D   DomainFieldType
    *  \tparam d   dimDomain
    *  \tparam R   RangeFieldType
@@ -84,8 +91,9 @@ public:
    *  \tparam rC  dimRangeRows of the testBase
    *  \attention ret is assumed to be zero!
    */
-  template <class LocalFunctionTuple, class T, class D, int d, class R, int r, int rC>
-  void evaluate(const LocalFunctionTuple& localFunctions, const BaseFunctionSetInterface<T, D, d, R, r, rC>& testBase,
+  template <class LocalFunctionTuple, class E, class D, int d, class R, int r, int rC>
+  void evaluate(const LocalFunctionTuple& localFunctions,
+                const Stuff::LocalfunctionSetInterface<E, D, d, R, r, rC>& testBase,
                 const Dune::FieldVector<D, d>& localPoint, Dune::DynamicVector<R>& ret) const
   {
     CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().evaluate(localFunctions, testBase, localPoint, ret));
@@ -109,18 +117,18 @@ public:
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTuple Type of the localFunction container, that is returned by localFunctions(), i.e.
-   * std::tuple< ... >
-   *  \tparam T       Traits of the test BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
+   *  \tparam E       EntityType
    *  \tparam D       DomainFieldType
    *  \tparam d       dimDomain
    *  \tparam R       RangeFieldType
    *  \tparam r{T,A}  dimRange of the {testBase,ansatzBase}
    *  \tparam rC{T,A} dimRangeRows of the {testBase,ansatzBase}
    */
-  template <class LocalFunctionTuple, class T, class A, class D, int d, class R, int rT, int rCT, int rA, int rCA>
-  int order(const LocalFunctionTuple& localFunctions, const BaseFunctionSetInterface<T, D, d, R, rT, rCT>& testBase,
-            const BaseFunctionSetInterface<A, D, d, R, rA, rCA>& ansatzBase) const
+  template <class LocalFunctionTuple, class E, class D, int d, class R, int rT, int rCT, int rA, int rCA>
+  size_t order(const LocalFunctionTuple& localFunctions,
+               const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBase,
+               const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBase) const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctions, testBase, ansatzBase));
     return asImp().order(localFunctions, testBase, ansatzBase);
@@ -128,10 +136,8 @@ public:
 
   /**
    *  \brief  Computes a binary codim 0 evaluation.
-   *  \tparam LocalFunctionTuple Type of the localFunction container, that is returned by localFunctions(), i.e.
-   * std::tuple< ... >
-   *  \tparam T         Traits of the test BaseFunctionSetInterface implementation
-   *  \tparam A         Traits of the ansatz BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
+   *  \tparam E         EntityType
    *  \tparam D         DomainFieldType
    *  \tparam d         dimDomain
    *  \tparam R         RangeFieldType
@@ -139,9 +145,10 @@ public:
    *  \tparam rC{L,T,A} dimRangeRows of the {localFunction,testBase,ansatzBase}
    *  \attention ret is assumed to be zero!
    */
-  template <class LocalFunctionTuple, class T, class A, class D, int d, class R, int rT, int rCT, int rA, int rCA>
-  void evaluate(const LocalFunctionTuple& localFunctions, const BaseFunctionSetInterface<T, D, d, R, rT, rCT>& testBase,
-                const BaseFunctionSetInterface<A, D, d, R, rA, rCA>& ansatzBase,
+  template <class LocalFunctionTuple, class E, class D, int d, class R, int rT, int rCT, int rA, int rCA>
+  void evaluate(const LocalFunctionTuple& localFunctions,
+                const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBase,
+                const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBase,
                 const Dune::FieldVector<D, d>& localPoint, Dune::DynamicMatrix<R>& ret) const
   {
     CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().evaluate(localFunctions, testBase, ansatzBase, localPoint, ret));
@@ -189,17 +196,17 @@ public:
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTuple  Type of the localFunction container, that is returned by localFunctions(), i.e.
-   * std::tuple< ... >
-   *  \tparam T                   Traits of the test BaseFunctionSetInterface implementation
-   *  \tparam D                   DomainFieldType
-   *  \tparam d                   dimDomain
-   *  \tparam R                   RangeFieldType
-   *  \tparam r                   dimRange of the testBase
-   *  \tparam rC                  dimRangeRows of the testBase
+   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
+   *  \tparam E   EntityType
+   *  \tparam D   DomainFieldType
+   *  \tparam d   dimDomain
+   *  \tparam R   RangeFieldType
+   *  \tparam r   dimRange of the testBase
+   *  \tparam rC  dimRangeRows of the testBase
    */
-  template <class LocalFunctionTuple, class T, class D, int d, class R, int r, int rC>
-  int order(const LocalFunctionTuple& localFunctions, const BaseFunctionSetInterface<T, D, d, R, r, rC>& testBase) const
+  template <class LocalFunctionTuple, class E, class D, int d, class R, int r, int rC>
+  size_t order(const LocalFunctionTuple& localFunctions,
+               const Stuff::LocalfunctionSetInterface<E, D, d, R, r, rC>& testBase) const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctions, testBase));
     return asImp().order(localFunctions, testBase);
@@ -207,9 +214,8 @@ public:
 
   /**
    *  \brief  Computes a binary codim 1 evaluation.
-   *  \tparam LocalFunctionTuple  Type of the localFunction container, that is returned by localFunctions(), i.e.
-   * std::tuple< ... >
-   *  \tparam T                   Traits of the entity test BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
+   *  \tparam E                   EntityType
    *  \tparam IntersectionType    A model of Dune::Intersection< ... >
    *  \tparam D                   DomainFieldType
    *  \tparam d                   dimDomain
@@ -218,8 +224,9 @@ public:
    *  \tparam rC                  dimRangeRows of the testBase
    *  \attention ret is assumed to be zero!
    */
-  template <class LocalFunctionTuple, class T, class IntersectionType, class D, int d, class R, int r, int rC>
-  void evaluate(const LocalFunctionTuple& localFunctions, const BaseFunctionSetInterface<T, D, d, R, r, rC>& testBase,
+  template <class LocalFunctionTuple, class E, class IntersectionType, class D, int d, class R, int r, int rC>
+  void evaluate(const LocalFunctionTuple& localFunctions,
+                const Stuff::LocalfunctionSetInterface<E, D, d, R, r, rC>& testBase,
                 const IntersectionType& intersection, const Dune::FieldVector<D, d - 1>& localPoint,
                 Dune::DynamicVector<R>& ret) const
   {
@@ -244,19 +251,18 @@ public:
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTuple  Type of the localFunction container, that is returned by localFunctions(), i.e.
-   * std::tuple< ... >
-   *  \tparam T                   Traits of the test BaseFunctionSetInterface implementation
-   *  \tparam A                   Traits of the ansatz BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTuple  Type of the container that is returned by localFunctions()
+   *  \tparam E                   EntityType
    *  \tparam D                   DomainFieldType
    *  \tparam d                   dimDomain
    *  \tparam R                   RangeFieldType
    *  \tparam r{T,A}              dimRange of the {testBase,ansatzBase}
    *  \tparam rC{T,A}             dimRangeRows of the {testBase,ansatzBase}
    */
-  template <class LocalFunctionTuple, class T, class A, class D, int d, class R, int rT, int rCT, int rA, int rCA>
-  int order(const LocalFunctionTuple& localFunctions, const BaseFunctionSetInterface<T, D, d, R, rT, rCT>& testBase,
-            const BaseFunctionSetInterface<A, D, d, R, rA, rCA>& ansatzBase) const
+  template <class LocalFunctionTuple, class E, class D, int d, class R, int rT, int rCT, int rA, int rCA>
+  size_t order(const LocalFunctionTuple& localFunctions,
+               const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBase,
+               const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBase) const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctions, testBase, ansatzBase));
     return asImp().order(localFunctions, testBase, ansatzBase);
@@ -264,10 +270,8 @@ public:
 
   /**
    *  \brief  Computes a binary codim 1 evaluation.
-   *  \tparam LocalFunctionTuple  Type of the localFunction container, that is returned by localFunctions(), i.e.
-   * std::tuple< ... >
-   *  \tparam T                   Traits of the entity test BaseFunctionSetInterface implementation
-   *  \tparam A                   Traits of the entity ansatz BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTuple  Type of the container that is returned by localFunctions()
+   *  \tparam E                   EntityType
    *  \tparam IntersectionType    A model of Dune::Intersection< ... >
    *  \tparam D                   DomainFieldType
    *  \tparam d                   dimDomain
@@ -276,11 +280,13 @@ public:
    *  \tparam rC{T,A}             dimRangeRows of the {testBase*,ansatzBase*}
    *  \attention ret is assumed to be zero!
    */
-  template <class LocalFunctionTuple, class T, class A, class IntersectionType, class D, int d, class R, int rT,
-            int rCT, int rA, int rCA>
-  void evaluate(const LocalFunctionTuple& localFunctions, const BaseFunctionSetInterface<T, D, d, R, rT, rCT>& testBase,
-                const BaseFunctionSetInterface<A, D, d, R, rA, rCA>& ansatzBase, const IntersectionType& intersection,
-                const Dune::FieldVector<D, d - 1>& localPoint, Dune::DynamicMatrix<R>& ret) const
+  template <class LocalFunctionTuple, class E, class IntersectionType, class D, int d, class R, int rT, int rCT, int rA,
+            int rCA>
+  void evaluate(const LocalFunctionTuple& localFunctions,
+                const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBase,
+                const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBase,
+                const IntersectionType& intersection, const Dune::FieldVector<D, d - 1>& localPoint,
+                Dune::DynamicMatrix<R>& ret) const
   {
     CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(
         asImp().evaluate(localFunctions, testBase, ansatzBase, intersection, localPoint, ret));
@@ -304,27 +310,23 @@ public:
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTupleEn  Type of the entity localFunction container, that is returned by localFunctions(),
-   * i.e. std::tuple< ... >
-   *  \tparam LocalFunctionTupleNe  Type of the neighbor localFunction container, that is returned by localFunctions(),
-   * i.e. std::tuple< ... >
-   *  \tparam TE                    Traits of the entity test BaseFunctionSetInterface implementation
-   *  \tparam AE                    Traits of the entity ansatz BaseFunctionSetInterface implementation
-   *  \tparam TN                    Traits of the neighbor test BaseFunctionSetInterface implementation
-   *  \tparam AN                    Traits of the neighbor ansatz BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTupleEn  Type of the container that is returned by localFunctions(entity)
+   *  \tparam LocalFunctionTupleNe  Type of the container that is returned by localFunctions(neighbour)
+   *  \tparam E                     EntityType
+   *  \tparam N                     NeighbourEntityType
    *  \tparam D                     DomainFieldType
    *  \tparam d                     dimDomain
    *  \tparam R                     RangeFieldType
    *  \tparam r{T,A}                dimRange of the {testBase*,ansatzBase*}
    *  \tparam rC{T,A}               dimRangeRows of the {testBase*,ansatzBase*}
    */
-  template <class LocalFunctionTupleEn, class LocalFunctionTupleNe, class TE, class AE, class TN, class AN, class D,
-            int d, class R, int rT, int rCT, int rA, int rCA>
-  int order(const LocalFunctionTupleEn& localFunctionsEntity, const LocalFunctionTupleNe& localFunctionsNeighbor,
-            const BaseFunctionSetInterface<TE, D, d, R, rT, rCT>& testBaseEntity,
-            const BaseFunctionSetInterface<AE, D, d, R, rA, rCA>& ansatzBaseEntity,
-            const BaseFunctionSetInterface<TN, D, d, R, rT, rCT>& testBaseNeighbor,
-            const BaseFunctionSetInterface<AN, D, d, R, rA, rCA>& ansatzBaseNeighbor) const
+  template <class LocalFunctionTupleEn, class LocalFunctionTupleNe, class E, class N, class D, int d, class R, int rT,
+            int rCT, int rA, int rCA>
+  size_t order(const LocalFunctionTupleEn& localFunctionsEntity, const LocalFunctionTupleNe& localFunctionsNeighbor,
+               const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBaseEntity,
+               const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBaseEntity,
+               const Stuff::LocalfunctionSetInterface<N, D, d, R, rT, rCT>& testBaseNeighbor,
+               const Stuff::LocalfunctionSetInterface<N, D, d, R, rA, rCA>& ansatzBaseNeighbor) const
   {
     CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctionsEntity,
                                                  localFunctionsNeighbor,
@@ -342,14 +344,10 @@ public:
 
   /**
    *  \brief  Computes a quaternary codim 1 evaluation.
-   *  \tparam LocalFunctionTupleEn  Type of the entity localFunction container, that is returned by localFunctions(),
-   * i.e. std::tuple< ... >
-   *  \tparam LocalFunctionTupleNe  Type of the neighbor localFunction container, that is returned by localFunctions(),
-   * i.e. std::tuple< ... >
-   *  \tparam TE                    Traits of the entity test BaseFunctionSetInterface implementation
-   *  \tparam AE                    Traits of the entity ansatz BaseFunctionSetInterface implementation
-   *  \tparam TN                    Traits of the neighbor test BaseFunctionSetInterface implementation
-   *  \tparam AN                    Traits of the neighbor ansatz BaseFunctionSetInterface implementation
+   *  \tparam LocalFunctionTupleEn  Type of the container that is returned by localFunctions(entity)
+   *  \tparam LocalFunctionTupleNe  Type of the container that is returned by localFunctions(neighbour)
+   *  \tparam E                     EntityType
+   *  \tparam N                     NeighbourEntityType
    *  \tparam IntersectionType      A model of Dune::Intersection< ... >
    *  \tparam D                     DomainFieldType
    *  \tparam d                     dimDomain
@@ -358,13 +356,13 @@ public:
    *  \tparam rC{T,A}               dimRangeRows of the {testBase*,ansatzBase*}
    *  \attention entityEntityRet, entityEntityRet, entityEntityRet and neighborEntityRet are assumed to be zero!
    */
-  template <class LocalFunctionTupleEn, class LocalFunctionTupleNe, class TE, class AE, class TN, class AN,
-            class IntersectionType, class D, int d, class R, int rT, int rCT, int rA, int rCA>
+  template <class LocalFunctionTupleEn, class LocalFunctionTupleNe, class E, class N, class IntersectionType, class D,
+            int d, class R, int rT, int rCT, int rA, int rCA>
   void evaluate(const LocalFunctionTupleEn& localFunctionsEntity, const LocalFunctionTupleNe& localFunctionsNeighbor,
-                const BaseFunctionSetInterface<TE, D, d, R, rT, rCT>& testBaseEntity,
-                const BaseFunctionSetInterface<AE, D, d, R, rA, rCA>& ansatzBaseEntity,
-                const BaseFunctionSetInterface<TN, D, d, R, rT, rCT>& testBaseNeighbor,
-                const BaseFunctionSetInterface<AN, D, d, R, rA, rCA>& ansatzBaseNeighbor,
+                const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBaseEntity,
+                const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBaseEntity,
+                const Stuff::LocalfunctionSetInterface<N, D, d, R, rT, rCT>& testBaseNeighbor,
+                const Stuff::LocalfunctionSetInterface<N, D, d, R, rA, rCA>& ansatzBaseNeighbor,
                 const IntersectionType& intersection, const Dune::FieldVector<D, d - 1>& localPoint,
                 Dune::DynamicMatrix<R>& entityEntityRet, Dune::DynamicMatrix<R>& neighborNeighborRet,
                 Dune::DynamicMatrix<R>& entityNeighborRet, Dune::DynamicMatrix<R>& neighborEntityRet) const
