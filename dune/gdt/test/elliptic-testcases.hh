ea2ed7c61c36a765a2d4d1d1f8c694b9dbaa6f66
Felix Albrecht <mail@felixalbrecht.de>
2013-10-06 00:00:57 +0200

    [test.elliptic-testcases] added ER07

--- dune/gdt/test/elliptic-testcases.hh
+++ dune/gdt/test/elliptic-testcases.hh
@@ -70,6 +70,117 @@ private:
 
 
 template< class GridType >
+class ER07
+  : public Base< GridType >
+{
+  typedef Base< GridType > BaseType;
+public:
+  typedef typename BaseType::GridPartType GridPartType;
+  typedef typename BaseType::EntityType   EntityType;
+  typedef typename BaseType::DomainFieldType  DomainFieldType;
+  static const unsigned int                   dimDomain = BaseType::dimDomain;
+  typedef double            RangeFieldType;
+  static const unsigned int dimRange = 1;
+  typedef Dune::Stuff::GridboundaryAllDirichlet< typename GridPartType::IntersectionType > BoundaryInfoType;
+
+  typedef Dune::Stuff::Function::Constant
+      < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange >
+    ConstantFunctionType;
+  typedef Dune::Stuff::Function::Expression
+      < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange >
+    ExpressionFunctionType;
+  typedef ConstantFunctionType    DiffusionType;
+  typedef ExpressionFunctionType  ForceType;
+  typedef ExpressionFunctionType  DirichletType;
+  typedef ConstantFunctionType    NeumannType;
+  typedef ExpressionFunctionType  ExactSolutionType;
+
+  ER07(const size_t num_refinements = 2)
+    : BaseType(create_initial_grid(), num_refinements)
+    , boundary_info_()
+    , diffusion_(1)
+    , force_("x", "64.0 * pi * pi * (cos(8.0 * pi * x[0]) + cos(8.0 * pi * x[1]))", 3)
+    , dirichlet_("x", "cos(8.0 * pi * x[0]) + cos(8.0 * pi * x[1])", 3)
+    , neumann_(0)
+    , exact_solution_("x",
+                      "cos(8.0 * pi * x[0]) + cos(8.0 * pi * x[1])",
+                      3,
+                      "exact solution",
+                      {{"-8.0 * pi * sin(8.0 * pi * x[0])",
+                        "-8.0 * pi * sin(8.0 * pi * x[1])"}})
+  {}
+
+  void print_header(std::ostream& out = std::cout) const
+  {
+    out << "+==========================================================+\n"
+        << "|+========================================================+|\n"
+        << "||  Testcase ER07: smooth data, nonhomogeneous dirichlet  ||\n"
+        << "||  (see page 858 in Epshteyn, Riviere, 2007)             ||\n"
+        << "|+--------------------------------------------------------+|\n"
+        << "||  domain = [0, 1] x [0 , 1]                             ||\n"
+        << "||  diffusion = 1                                         ||\n"
+        << "||  force     = 64 pi^2 (cos(8 pi x) + cos(8 pi y))       ||\n"
+        << "||  dirichlet = cos(8 pi x) + cos(8 pi y)                 ||\n"
+        << "||  exact solution = cos(8 pi x) + cos(8 pi y)            ||\n"
+        << "|+========================================================+|\n"
+        << "+==========================================================+" << std::endl;
+  } // ... print_header(...)
+
+  const BoundaryInfoType& boundary_info() const
+  {
+    return boundary_info_;
+  }
+
+  const DiffusionType& diffusion() const
+  {
+    return diffusion_;
+  }
+
+  const ForceType& force() const
+  {
+    return force_;
+  }
+
+  const DirichletType& dirichlet() const
+  {
+    return dirichlet_;
+  }
+
+  const NeumannType& neumann() const
+  {
+    return neumann_;
+  }
+
+  bool provides_exact_solution() const
+  {
+    return true;
+  }
+
+  const ExactSolutionType& exact_solution() const
+  {
+    return exact_solution_;
+  }
+
+private:
+  static std::shared_ptr< GridType > create_initial_grid()
+  {
+    typedef Dune::Stuff::GridProviderCube< GridType > GridProviderType;
+    auto grid_provider = std::unique_ptr< GridProviderType >(new GridProviderType(0, 1, 16));
+    auto grid = grid_provider->grid();
+    grid->globalRefine(1);
+    return grid;
+  } // ... create_initial_grid(...)
+
+  const BoundaryInfoType boundary_info_;
+  const DiffusionType diffusion_;
+  const ForceType force_;
+  const DirichletType dirichlet_;
+  const NeumannType neumann_;
+  const ExactSolutionType exact_solution_;
+}; // class ER07
+
+
+template< class GridType >
 class ESV07
   : public Base< GridType >
 {
