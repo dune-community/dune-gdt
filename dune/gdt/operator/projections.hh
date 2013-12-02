e152a9178f7b58f2fa4cd995563769b381182315
Felix Albrecht <mail@felixalbrecht.de>
2013-12-02 15:10:17 +0100

    [operator.projections] rewrote Dirichlet

--- dune/gdt/operator/projections.hh
+++ dune/gdt/operator/projections.hh
@@ -164,6 +164,9 @@ private:
 }; // class L2
 
 
+/**
+ *  \note If you add other dimension/polorder/space combinations, do not forget to add a testcase in tests/operators.cc
+ */
 template <class GridPartImp>
 class Dirichlet
 {
@@ -185,136 +188,91 @@ public:
   {
   }
 
+  /**
+   *  \brief  Does a dirichlet projection in the sense that the lagrange point set on each entity is matched against
+   *          those vertices of the entity, which lie on the dirichlet boundary.
+   */
   template <class R, class GP, int p, class V>
   void apply(const Stuff::LocalizableFunctionInterface<EntityType, DomainFieldType, dimDomain, R, 1, 1>& source,
              DiscreteFunction<ContinuousLagrangeSpace::FemWrapper<GP, p, R, 1, 1>, V>& range) const
   {
-    typedef DiscreteFunction<ContinuousLagrangeSpace::FemWrapper<GP, p, R, 1, 1>, V> RangeType;
+    static_assert(p == 1, "This is untested for other polynomial orders! If this works just remove this assert!");
     // clear range
-    range.vector().backend() *= 0.0;
+    Stuff::Common::clear(range.vector());
     // walk the grid
     const auto entity_it_end = grid_part_.template end<0>();
     for (auto entity_it = grid_part_.template begin<0>(); entity_it != entity_it_end; ++entity_it) {
-      const auto& entity = *entity_it;
-      // only consider entities with boundary intersections
-      if (entity.hasBoundaryIntersections()) {
-        const auto source_local_function = source.local_function(entity);
-        auto range_local_function        = range.local_discrete_function(entity);
-        auto range_local_DoF_vector      = range_local_function.vector();
-        const auto lagrangePointSet      = range.space().backend().lagrangePointSet(entity);
-        // get the lagrange points' coordinates
-        typedef typename RangeType::SpaceType::BackendType::LagrangePointSetType::CoordinateType
-            LagrangePointCoordinateType;
-        std::vector<LagrangePointCoordinateType> lagrangePoints(lagrangePointSet.nop(), LagrangePointCoordinateType(0));
-        for (size_t ii = 0; ii < lagrangePointSet.nop(); ++ii)
-          lagrangePoints[ii] = entity.geometry().global(lagrangePointSet.point(ii));
-        // walk the intersections
-        const auto intersection_it_end = grid_part_.iend(entity);
-        for (auto intersection_it = grid_part_.ibegin(entity); intersection_it != intersection_it_end;
-             ++intersection_it) {
-          const auto& intersection = *intersection_it;
-          // only consider dirichlet boundary intersection
-          if (boundary_info_.dirichlet(intersection)) {
-            // loop over all lagrange points
-            for (size_t ii = 0; ii < lagrangePointSet.nop(); ++ii) {
-              // if dof lies on the boundary intersection
-              if (Dune::Stuff::Grid::intersectionContains(intersection, lagrangePoints[ii])) {
-                // set the corresponding target dof
-                range_local_DoF_vector.set(ii, source_local_function->evaluate(lagrangePointSet.point(ii)));
-              } // if dof lies on the boundary intersection
-            } // loop over all lagrange points
-          } // only consider dirichlet boundary intersection
-        } // walk the intersections
-      } // only consider entities with boundary intersection
+      const auto& entity           = *entity_it;
+      const auto local_source      = source.local_function(entity);
+      auto local_range             = range.local_discrete_function(entity);
+      auto& local_range_DoF_vector = local_range.vector();
+      const auto lagrange_points = range.space().backend().lagrangePointSet(entity);
+      std::vector<DomainType> points(lagrange_points.nop(), DomainType(0));
+      for (size_t ii = 0; ii < lagrange_points.nop(); ++ii)
+        points[ii] = lagrange_points.point(ii);
+      // and do the work (see below)
+      apply_local(entity, points, local_source, local_range_DoF_vector);
     } // walk the grid
   } // ... apply(...) const
 
-  template <class R, class GP, int p, class V>
+  /**
+   *  \brief  Does a dirichlet projection in the sense that the lagrange point set on each entity is matched against
+   *          those vertices of the entity, which lie on the dirichlet boundary.
+   */
+  template <class R, class GP, class V>
   void apply(const Stuff::LocalizableFunctionInterface<EntityType, DomainFieldType, dimDomain, R, 1, 1>& source,
-             DiscreteFunction<ContinuousLagrangeSpace::FemLocalfunctionsWrapper<GP, p, R, 1, 1>, V>& range) const
+             DiscreteFunction<ContinuousLagrangeSpace::FemLocalfunctionsWrapper<GP, 1, R, 1, 1>, V>& range) const
   {
-    typedef ContinuousLagrangeSpace::FemLocalfunctionsWrapper<GP, p, R, 1, 1> RangeSpaceType;
     // checks
-    static_assert(dimDomain == 2, "This does not work for other dimensions!");
-    static_assert(p == 1, "Not tested for higher polynomial orders!");
-    FieldVector<R, 1> tmp_source_value(R(0));
+    typedef ContinuousLagrangeSpace::FemLocalfunctionsWrapper<GP, 1, R, 1, 1> SpaceType;
+    static_assert(SpaceType::dimDomain == dimDomain, "Dimensions do not match!");
+    static_assert(dimDomain == 2, "This is untested for other dimensions! If this works just remove this assert!");
     // clear range
-    range.vector().backend() *= 0.0;
+    Stuff::Common::clear(range.vector());
     // walk the grid
     const auto entity_it_end = grid_part_.template end<0>();
     for (auto entity_it = grid_part_.template begin<0>(); entity_it != entity_it_end; ++entity_it) {
-      const auto& entity = *entity_it;
-      // only work if we are at the boundary
-      if (entity.hasBoundaryIntersections()) {
-        // get the local quantities
-        const auto local_source      = source.local_function(entity);
-        auto local_range             = range.local_discrete_function(entity);
-        auto& local_range_DoF_vector = local_range.vector();
-        // get local finite elements
-        // * we are a CG space
-        const auto cg_finite_element      = range.space().backend().finiteElement(entity);
-        const auto& cg_local_coefficients = cg_finite_element.localCoefficients();
-        // * but we also need a local DG finite element
-        typedef Dune::DGLocalFiniteElement<typename RangeSpaceType::Traits::FiniteElementType> DgFiniteElementType;
-        const DgFiniteElementType dg_finite_element(entity.type(), p);
-        const auto& dg_local_coefficients = dg_finite_element.localCoefficients();
-        assert(dg_local_coefficients.size() == cg_local_coefficients.size() && "Wrong finite element given!");
-
-        // first we loop over all vertices of the entity
-        std::vector<DomainType> global_vertices(entity.template count<dimDomain>(), DomainType(0));
-        std::vector<size_t> local_DoF_ids(global_vertices.size(), 0);
-        for (size_t local_vertex_id = 0; local_vertex_id < global_vertices.size(); ++local_vertex_id) {
-          // get the vertex
-          const auto vertexPtr             = entity.template subEntity<dimDomain>(local_vertex_id);
-          const auto& vertex               = *vertexPtr;
-          global_vertices[local_vertex_id] = vertex.geometry().center();
-          // find the global DoF id to this vertex, therefore
-          // loop over all local DoFs
-          for (size_t ii = 0; ii < dg_local_coefficients.size(); ++ii) {
-            const auto& entity_cg_local_key = cg_local_coefficients.localKey(ii);
-            if (entity_cg_local_key.subEntity() == local_vertex_id) {
-              // get the local DoF to this vertex
-              const auto& entity_dg_local_key = dg_local_coefficients.localKey(ii);
-              assert(entity_cg_local_key.codim() == dimDomain && "Wrong finite element given!");
-              local_DoF_ids[local_vertex_id] = entity_dg_local_key.index();
-              // there must be one and only one for a polorder 1 lagrange basis
-              break;
-            }
-          } // loop over all local DoFs
-        } // loop over all vertices of the entity
-
-        // then we walk the intersections
-        const auto intersection_it_end = grid_part_.iend(entity);
-        for (auto intersection_it = grid_part_.ibegin(entity); intersection_it != intersection_it_end;
-             ++intersection_it) {
-          const auto& intersection = *intersection_it;
-          if (boundary_info_.dirichlet(intersection)) {
-            const auto& intersection_geometry = intersection.geometry();
-            // and walk its corners (i.e. the vertices in 2d)
-            for (size_t local_intersection_corner_id = 0;
-                 int(local_intersection_corner_id) < intersection_geometry.corners();
-                 ++local_intersection_corner_id) {
-              const auto global_intersection_corner = intersection_geometry.corner(local_intersection_corner_id);
-              // to check which vertex this corner is
-              // loop over all vertices of the entity again
-              for (size_t local_vertex_id = 0; local_vertex_id < global_vertices.size(); ++local_vertex_id) {
-                // and check for equality
-                if (Stuff::Common::FloatCmp::eq(global_intersection_corner, global_vertices[local_vertex_id])) {
-                  // this vertex is on the dirichlet boundary
-                  // * so we evaluate the source
-                  local_source->evaluate(global_intersection_corner, tmp_source_value);
-                  // * and set the corresponding local DoF
-                  local_range_DoF_vector.set(local_DoF_ids[local_vertex_id], tmp_source_value[0]);
-                }
-              } // loop over all vertices of the entity
-            } // walk its corners
-          }
-        } // walk the intersections
-      }
+      const auto& entity           = *entity_it;
+      const auto local_source      = source.local_function(entity);
+      auto local_range             = range.local_discrete_function(entity);
+      auto& local_range_DoF_vector = local_range.vector();
+      const auto lagrange_points   = range.space().lagrange_points(entity);
+      // and do the work (see below)
+      apply_local(entity, lagrange_points, local_source, local_range_DoF_vector);
     } // walk the grid
   } // ... apply(...) const
 
 private:
+  template <class LagrangePointsType, class LocalSourceType, class LocalRangeVectorType>
+  void apply_local(const EntityType& entity, const LagrangePointsType& lagrange_points,
+                   const LocalSourceType& local_source, LocalRangeVectorType& local_range_DoF_vector) const
+  {
+    // walk the intersections
+    const auto intersection_it_end = grid_part_.iend(entity);
+    for (auto intersection_it = grid_part_.ibegin(entity); intersection_it != intersection_it_end; ++intersection_it) {
+      const auto& intersection = *intersection_it;
+      // only work on boundary intersections
+      if (boundary_info_.dirichlet(intersection)) {
+        const auto& intersection_geometry = intersection.geometry();
+        // and walk its corners (i.e. the vertices in 2d)
+        for (int local_intersection_corner_id = 0; local_intersection_corner_id < intersection_geometry.corners();
+             ++local_intersection_corner_id) {
+          const auto local_vertex = entity.geometry().local(intersection_geometry.corner(local_intersection_corner_id));
+          // loop over all local lagrange points
+          for (size_t lagrange_point_id = 0; lagrange_point_id < lagrange_points.size(); ++lagrange_point_id) {
+            const auto& lagrange_point = lagrange_points[lagrange_point_id];
+            // and check for equality
+            if (Stuff::Common::FloatCmp::eq(local_vertex, lagrange_point)) {
+              // this lagrange point is on the dirichlet boundary
+              // so we evaluate the source and set the corresponding local DoF
+              local_range_DoF_vector.set(lagrange_point_id, local_source->evaluate(lagrange_point));
+            }
+          } // loop over all local lagrange points
+        } // walk its corners
+      } // only work on boundary intersections
+    } // walk the intersections
+  } // ... apply_local(...)
+
   const GridPartType& grid_part_;
   const BoundaryInfoType& boundary_info_;
 }; // class Dirichlet
