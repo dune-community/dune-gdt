29175d0b59a009f218ded7576b214166f4498233
Felix Schindler <felix@schindlerfamily.de>
2014-03-21 00:34:52 +0100

    [basefunctionset.pdelab] update * replaced override -> final in virtual functions * specialize for range 1x1 and give a static error otherwise * some notes and clean up

--- dune/gdt/basefunctionset/pdelab.hh
+++ dune/gdt/basefunctionset/pdelab.hh
@@ -6,8 +6,12 @@
 #ifndef DUNE_GDT_BASEFUNCTIONSET_PDELAB_HH
 #define DUNE_GDT_BASEFUNCTIONSET_PDELAB_HH
 
+#include <type_traits>
+
 #include <dune/common/fvector.hh>
 #include <dune/common/fmatrix.hh>
+
+#include <dune/common/typetraits.hh>
 
 #include <dune/pdelab/gridfunctionspace/localfunctionspace.hh>
 
@@ -23,16 +27,27 @@ namespace BaseFunctionSet {
 // forward, to be used in the traits and to allow for specialization
 template <class PdelabSpaceImp, class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim,
           int rangeDimCols = 1>
-class PdelabWrapper;
+class PdelabWrapper
+{
+  static_assert(Dune::AlwaysFalse<PdelabSpaceImp>::value, "Untested for arbitrary dimension!");
+};
 
 
+// forward, to allow for specialization
 template <class PdelabSpaceImp, class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim,
           int rangeDimCols = 1>
 class PdelabWrapperTraits
 {
+  static_assert(Dune::AlwaysFalse<PdelabSpaceImp>::value, "Untested for arbitrary dimension!");
+};
+
+
+//! Specialization for dimRange = 1, dimRangeRows = 1
+template <class PdelabSpaceImp, class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp>
+class PdelabWrapperTraits<PdelabSpaceImp, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1, 1>
+{
 public:
-  typedef PdelabWrapper<PdelabSpaceImp, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>
-      derived_type;
+  typedef PdelabWrapper<PdelabSpaceImp, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1, 1> derived_type;
 
 private:
   typedef PDELab::LocalFunctionSpace<PdelabSpaceImp, PDELab::TrialSpaceTag> PdelabLFSType;
@@ -43,25 +58,24 @@ public:
   typedef EntityImp EntityType;
 
 private:
-  friend class PdelabWrapper<PdelabSpaceImp, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim,
-                             rangeDimCols>;
+  friend class PdelabWrapper<PdelabSpaceImp, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1, 1>;
 };
 
 
-template <class PdelabSpaceType, class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp,
-          int rangeDim>
-class PdelabWrapper<PdelabSpaceType, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
+//! Specialization for dimRange = 1, dimRangeRows = 1
+template <class PdelabSpaceType, class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp>
+class PdelabWrapper<PdelabSpaceType, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1, 1>
     : public BaseFunctionSetInterface<PdelabWrapperTraits<PdelabSpaceType, EntityImp, DomainFieldImp, domainDim,
-                                                          RangeFieldImp, rangeDim, 1>,
-                                      DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
+                                                          RangeFieldImp, 1, 1>,
+                                      DomainFieldImp, domainDim, RangeFieldImp, 1, 1>
 {
-  typedef PdelabWrapper<PdelabSpaceType, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1> ThisType;
+  typedef PdelabWrapper<PdelabSpaceType, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1, 1> ThisType;
   typedef BaseFunctionSetInterface<PdelabWrapperTraits<PdelabSpaceType, EntityImp, DomainFieldImp, domainDim,
-                                                       RangeFieldImp, rangeDim, 1>,
-                                   DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1> BaseType;
+                                                       RangeFieldImp, 1, 1>,
+                                   DomainFieldImp, domainDim, RangeFieldImp, 1, 1> BaseType;
 
 public:
-  typedef PdelabWrapperTraits<PdelabSpaceType, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1> Traits;
+  typedef PdelabWrapperTraits<PdelabSpaceType, EntityImp, DomainFieldImp, domainDim, RangeFieldImp, 1, 1> Traits;
   typedef typename Traits::BackendType BackendType;
   typedef typename Traits::EntityType EntityType;
 
@@ -74,25 +88,27 @@ public:
   static const unsigned int dimDomain = domainDim;
   typedef Dune::FieldVector<DomainFieldType, dimDomain> DomainType;
   typedef RangeFieldImp RangeFieldType;
-  static const unsigned int dimRange     = rangeDim;
+  static const unsigned int dimRange     = 1;
   static const unsigned int dimRangeCols = 1;
   typedef Dune::FieldVector<RangeFieldType, dimRange> RangeType;
   typedef Dune::FieldMatrix<RangeFieldType, dimRange, dimDomain> JacobianRangeType;
 
-  PdelabWrapper(const PdelabSpaceType& space, const EntityType& ent)
+  PdelabWrapper(const PdelabSpaceType& space, const EntityType& ent, const size_t oo)
     : BaseType(ent)
-    , order_(0)
+    , order_(oo)
+    , tmp_domain_(RangeFieldType(0))
   {
     PdelabLFSType* lfs_ptr = new PdelabLFSType(space);
     lfs_ptr->bind(this->entity());
     lfs_     = std::unique_ptr<PdelabLFSType>(lfs_ptr);
     backend_ = std::unique_ptr<BackendType>(new BackendType(FESwitchType::basis(lfs_->finiteElement())));
     order_   = backend_->order();
-  }
+  } // PdelabWrapper(...)
 
   PdelabWrapper(ThisType&& source)
     : BaseType(source.entity())
     , order_(std::move(source.order_))
+    , tmp_domain_(RangeFieldType(0))
     , lfs_(std::move(source.lfs_))
     , backend_(std::move(source.backend_))
   {
@@ -107,33 +123,32 @@ public:
     return *backend_;
   }
 
-  virtual size_t size() const DS_OVERRIDE
+  virtual size_t size() const DS_FINAL
   {
     return backend_->size();
   }
 
-  virtual size_t order() const DS_OVERRIDE
+  virtual size_t order() const DS_FINAL
   {
     return order_;
   }
 
-  virtual void evaluate(const DomainType& xx, std::vector<RangeType>& ret) const DS_OVERRIDE
+  virtual void evaluate(const DomainType& xx, std::vector<RangeType>& ret) const DS_FINAL
   {
     assert(ret.size() >= backend_->size());
     backend_->evaluateFunction(xx, ret);
-  }
+  } // ... evaluate(...)
 
   using BaseType::evaluate;
 
-  virtual void jacobian(const DomainType& xx, std::vector<JacobianRangeType>& ret) const DS_OVERRIDE
+  virtual void jacobian(const DomainType& xx, std::vector<JacobianRangeType>& ret) const DS_FINAL
   {
     assert(ret.size() >= backend_->size());
     backend_->evaluateJacobian(xx, ret);
-    DomainType tmp(0.0);
-    const auto jocobian_inverse_transposed = this->entity().geometry().jacobianInverseTransposed(xx);
+    const auto jacobian_inverse_transposed = this->entity().geometry().jacobianInverseTransposed(xx);
     for (size_t ii = 0; ii < ret.size(); ++ii) {
-      jocobian_inverse_transposed.mv(ret[ii][0], tmp);
-      ret[ii][0] = tmp;
+      jacobian_inverse_transposed.mv(ret[ii][0], tmp_domain_);
+      ret[ii][0] = tmp_domain_;
     }
   } // ... jacobian(...)
 
@@ -141,6 +156,7 @@ public:
 
 private:
   size_t order_;
+  mutable DomainType tmp_domain_;
   std::unique_ptr<const PdelabLFSType> lfs_;
   std::unique_ptr<const BackendType> backend_;
 }; // class PdelabWrapper
