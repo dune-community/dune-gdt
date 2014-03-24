9e5f19a8d820e4c8dcb5535bb21c209846ca0029
Felix Schindler <felix@schindlerfamily.de>
2014-03-24 08:40:56 +0100

    [functional.base] added first draft * AssemblableVolumeBase * AssemblableFaceBase

---
+++ dune/gdt/functional/base.hh
@@ -0,0 +1,291 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Albrecht
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_GDT_FUNCTIONAL_BASE_HH
+#define DUNE_GDT_FUNCTIONAL_BASE_HH
+
+#include <dune/common/dynvector.hh>
+
+#include <dune/gdt/assembler/gridwalker.hh>
+#include <dune/gdt/assembler/local/codim0.hh>
+#include <dune/gdt/assembler/local/codim1.hh>
+
+#include "interfaces.hh"
+
+namespace Dune {
+namespace GDT {
+namespace Functional {
+
+
+// forward, to be used in the traits
+template< class ImpTraits >
+class AssemblableVolumeBase;
+
+
+template< class ImpTraits >
+class AssemblableVolumeBaseTraits
+{
+public:
+  typedef typename ImpTraits::derived_type  derived_type;
+  typedef typename ImpTraits::GridViewType  GridViewType;
+  typedef typename ImpTraits::SpaceType     SpaceType;
+  typedef typename ImpTraits::VectorType    VectorType;
+  typedef typename ImpTraits::ScalarType    ScalarType;
+}; // class AssemblableVolumeBaseTraits
+
+
+template< class ImpTraits >
+class AssemblableVolumeBase
+    : public AssemblableFunctionalInterface< ImpTraits >
+    , public Functor::Codim0< typename ImpTraits::GridViewType >
+{
+  typedef AssemblableFunctionalInterface< ImpTraits > InterfaceType;
+  typedef AssemblableVolumeBaseTraits< ImpTraits > Traits;
+public:
+  typedef typename Traits::GridViewType GridViewType;
+  typedef typename Traits::SpaceType    SpaceType;
+  typedef typename Traits::VectorType   VectorType;
+  typedef typename Traits::ScalarType   ScalarType;
+
+  typedef typename GridViewType::template Codim< 0 >::Entity EntityType;
+
+private:
+  typedef typename ImpTraits::LocalFunctionalType LocalFunctionalType;
+  typedef LocalAssembler::Codim0Vector< LocalFunctionalType > LocalAssemblerType;
+
+public:
+  AssemblableVolumeBase(VectorType& vector, const SpaceType& space, const GridViewType& grid_view)
+    : vector_(vector)
+    , space_(space)
+    , grid_view_(grid_view)
+    , local_assembler_(nullptr)
+    , prepared_(false)
+    , assembled_(false)
+  {}
+
+  AssemblableVolumeBase(VectorType& vector, const SpaceType& space)
+    : vector_(vector)
+    , space_(space)
+    , grid_view_(*(space.grid_view()))
+    , local_assembler_(nullptr)
+    , prepared_(false)
+    , assembled_(false)
+  {}
+
+  const GridViewType& grid_view() const
+  {
+    return grid_view_;
+  }
+
+  const SpaceType& space() const
+  {
+    return space_;
+  }
+
+  VectorType& vector()
+  {
+    return vector_;
+  }
+
+  const VectorType& vector() const
+  {
+    return vector_;
+  }
+
+private:
+  virtual const LocalFunctionalType& local_functional() const = 0;
+
+public:
+  virtual void prepare()
+  {
+    if (!assembled_ && !prepared_) {
+      local_assembler_ = std::unique_ptr< LocalAssemblerType >(new LocalAssemblerType(local_functional()));
+      const auto num_tmp_objects_required = local_assembler_->numTmpObjectsRequired();
+      const size_t max_local_size = space_.mapper().maxNumDofs();
+      tmp_local_vectors_
+          = { std::vector< DynamicVector< ScalarType > >(num_tmp_objects_required[0],
+                                                         DynamicVector< ScalarType >(max_local_size))
+            , std::vector< DynamicVector< ScalarType > >(num_tmp_objects_required[1],
+                                                         DynamicVector< ScalarType >(max_local_size))
+            };
+      tmp_indices_ = DynamicVector< size_t >(max_local_size, 0);
+      prepared_ = true;
+    }
+  } // ... prepare()
+
+  virtual void apply_local(const EntityType& entity)
+  {
+    assert(prepared_);
+    local_assembler_->assembleLocal(space_, entity, vector_, tmp_local_vectors_, tmp_indices_);
+  } // ... apply_local(...)
+
+  void assemble()
+  {
+    if (!assembled_) {
+      GridWalker< GridViewType > grid_walker(grid_view_);
+      grid_walker.add(*this);
+      grid_walker.walk();
+      assembled_ = true;
+    }
+  } // ... assemble()
+
+  template< class S >
+  ScalarType apply(const Stuff::LA::VectorInterface< S >& source) const
+  {
+    assemble();
+    return vector_.dot(source);
+  } // ... apply(...)
+
+private:
+  VectorType& vector_;
+  const SpaceType& space_;
+  const GridViewType& grid_view_;
+  std::unique_ptr< LocalAssemblerType > local_assembler_;
+  bool prepared_;
+  bool assembled_;
+  std::vector< std::vector< DynamicVector< ScalarType > > > tmp_local_vectors_;
+  DynamicVector< size_t > tmp_indices_;
+}; // class AssemblableVolumeBase
+
+
+// forward, to be used in the traits
+template< class ImpTraits >
+class AssemblableFaceBase;
+
+
+template< class ImpTraits >
+class AssemblableFaceBaseTraits
+{
+public:
+  typedef typename ImpTraits::derived_type  derived_type;
+  typedef typename ImpTraits::GridViewType  GridViewType;
+  typedef typename ImpTraits::SpaceType     SpaceType;
+  typedef typename ImpTraits::VectorType    VectorType;
+  typedef typename ImpTraits::ScalarType    ScalarType;
+}; // class AssemblableFaceBaseTraits
+
+
+template< class ImpTraits >
+class AssemblableFaceBase
+    : public AssemblableFunctionalInterface< ImpTraits >
+    , public Functor::Codim1< typename ImpTraits::GridViewType >
+{
+  typedef AssemblableFunctionalInterface< AssemblableFaceBaseTraits< ImpTraits > > InterfaceType;
+  typedef AssemblableFaceBaseTraits< ImpTraits > Traits;
+public:
+  typedef typename Traits::GridViewType GridViewType;
+  typedef typename Traits::SpaceType    SpaceType;
+  typedef typename Traits::VectorType   VectorType;
+  typedef typename Traits::ScalarType   ScalarType;
+
+  typedef typename GridViewType::template Codim< 0 >::Entity  EntityType;
+  typedef typename GridViewType::Intersection                 IntersectionType;
+
+private:
+  typedef typename ImpTraits::LocalFunctionalType             LocalFunctionalType;
+  typedef LocalAssembler::Codim1Vector< LocalFunctionalType > LocalAssemblerType;
+
+public:
+  AssemblableFaceBase(VectorType& vector, const SpaceType& space, const GridViewType& grid_view)
+    : vector_(vector)
+    , space_(space)
+    , grid_view_(grid_view)
+    , local_assembler_(nullptr)
+    , prepared_(false)
+    , assembled_(false)
+  {}
+
+  AssemblableFaceBase(VectorType& vector, const SpaceType& space)
+    : vector_(vector)
+    , space_(space)
+    , grid_view_(*(space.grid_view()))
+    , local_assembler_(nullptr)
+    , prepared_(false)
+    , assembled_(false)
+  {}
+
+  const GridViewType& grid_view() const
+  {
+    return grid_view_;
+  }
+
+  const SpaceType& space() const
+  {
+    return space_;
+  }
+
+  VectorType& vector()
+  {
+    assemble();
+    return vector_;
+  }
+
+  const VectorType& vector() const
+  {
+    assemble();
+    return vector_;
+  }
+
+private:
+  virtual const LocalFunctionalType& local_functional() const = 0;
+
+public:
+  virtual void prepare()
+  {
+    if (!assembled_ && !prepared_) {
+      local_assembler_ = std::unique_ptr< LocalAssemblerType >(new LocalAssemblerType(local_functional()));
+      const auto num_tmp_objects_required = local_assembler_->numTmpObjectsRequired();
+      const size_t max_local_size = space_.mapper().maxNumDofs();
+      tmp_local_vectors_
+          = { std::vector< DynamicVector< ScalarType > >(num_tmp_objects_required[0],
+                                                         DynamicVector< ScalarType >(max_local_size))
+            , std::vector< DynamicVector< ScalarType > >(num_tmp_objects_required[1],
+                                                         DynamicVector< ScalarType >(max_local_size))
+            };
+      tmp_indices_ = DynamicVector< size_t >(max_local_size, 0);
+      prepared_ = true;
+    }
+  } // ... prepare()
+
+  virtual void apply_local(const IntersectionType& intersection)
+  {
+    assert(prepared_);
+    local_assembler_->assembleLocal(space_, intersection, vector_, tmp_local_vectors_, tmp_indices_);
+  } // ... apply_local(...)
+
+  void assemble()
+  {
+    if (!assembled_) {
+      GridWalker< GridViewType > grid_walker(grid_view_);
+      grid_walker.add(*this);
+      grid_walker.walk();
+      assembled_ = true;
+    }
+  } // ... assemble()
+
+  template< class S >
+  ScalarType apply(const Stuff::LA::VectorInterface< S >& source) const
+  {
+    assemble();
+    return vector_.dot(source);
+  } // ... apply(...)
+
+private:
+  VectorType& vector_;
+  const SpaceType& space_;
+  const GridViewType& grid_view_;
+  std::unique_ptr< LocalAssemblerType > local_assembler_;
+  bool prepared_;
+  bool assembled_;
+  std::vector< std::vector< DynamicVector< ScalarType > > > tmp_local_vectors_;
+  DynamicVector< size_t > tmp_indices_;
+}; // class AssemblableFaceBase
+
+
+} // namespace Functional
+} // namespace GDT
+} // namespace Dune
+
+#endif // DUNE_GDT_FUNCTIONAL_BASE_HH
