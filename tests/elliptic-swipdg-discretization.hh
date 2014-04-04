b0c7ebf082511689c53c75d11a2a86f6d3e0f045
Felix Schindler <felix@schindlerfamily.de>
2014-04-04 18:14:30 +0200

    [tests.elliptic-swipdg...] don't be final in the EOC study * we need to override those in the estimator study

--- tests/elliptic-swipdg-discretization.hh
+++ tests/elliptic-swipdg-discretization.hh
@@ -243,12 +243,12 @@ public:
   {
   }
 
-  virtual std::string identifier() const DS_OVERRIDE DS_FINAL
+  virtual std::string identifier() const DS_OVERRIDE
   {
     return "SWIP discontinuous galerkin discretization, polOrder " + Dune::Stuff::Common::toString(polOrder);
   }
 
-  virtual size_t num_refinements() const DS_OVERRIDE DS_FINAL
+  virtual size_t num_refinements() const DS_OVERRIDE
   {
     if (test_.num_levels() == 0)
       return test_.num_levels();
@@ -256,12 +256,12 @@ public:
       return test_.num_levels() - 1;
   }
 
-  virtual std::vector<std::string> provided_norms() const DS_OVERRIDE DS_FINAL
+  virtual std::vector<std::string> provided_norms() const DS_OVERRIDE
   {
     return {"L2", "H1_semi"};
   }
 
-  virtual size_t expected_rate(const std::string type) const DS_OVERRIDE DS_FINAL
+  virtual size_t expected_rate(const std::string type) const DS_OVERRIDE
   {
     if (type.compare("L2") == 0)
       return polOrder + 1;
@@ -271,7 +271,7 @@ public:
       DUNE_THROW(Dune::RangeError, "Wrong type '" << type << "' requested!");
   } // ... expected_rate(...)
 
-  virtual double norm_reference_solution(const std::string type) DS_OVERRIDE DS_FINAL
+  virtual double norm_reference_solution(const std::string type) DS_OVERRIDE
   {
     if (test_.provides_exact_solution()) {
       return compute_norm(*(test_.reference_grid_view()), test_.exact_solution(), type);
@@ -286,21 +286,21 @@ public:
     }
   } // ... norm_reference_solution(...)
 
-  virtual size_t current_grid_size() const DS_OVERRIDE DS_FINAL
+  virtual size_t current_grid_size() const DS_OVERRIDE
   {
     assert(current_level_ < test_.num_levels());
     const auto grid_part = test_.level_grid_part(current_level_);
     return grid_part->grid().size(grid_part->level(), 0);
   } // ... current_grid_size(...)
 
-  virtual double current_grid_width() const DS_OVERRIDE DS_FINAL
+  virtual double current_grid_width() const DS_OVERRIDE
   {
     assert(current_level_ < test_.num_levels());
     const auto grid_part = test_.level_grid_part(current_level_);
     return Dune::Fem::GridWidth::calcGridWidth(*grid_part);
   } // ... current_grid_width(...)
 
-  virtual double compute_on_current_refinement() DS_OVERRIDE DS_FINAL
+  virtual double compute_on_current_refinement() DS_OVERRIDE
   {
     using namespace Dune;
     using namespace Dune::GDT;
@@ -335,7 +335,7 @@ public:
     return timer.elapsed() + elapsed;
   } // ... compute_on_current_refinement(...)
 
-  virtual double current_error_norm(const std::string type) DS_OVERRIDE DS_FINAL
+  virtual double current_error_norm(const std::string type) DS_OVERRIDE
   {
     // get current solution
     assert(current_level_ < test_.num_levels());
@@ -367,7 +367,7 @@ public:
     }
   } // ... current_error_norm(...)
 
-  virtual void refine() DS_OVERRIDE DS_FINAL
+  virtual void refine() DS_OVERRIDE
   {
     if (current_level_ < test_.num_levels())
       ++current_level_;
@@ -469,7 +469,7 @@ public:
     return BaseType::run(true, out);
   }
 
-private:
+protected:
   void compute_reference_solution()
   {
     if (!reference_solution_computed_) {
@@ -502,7 +502,6 @@ private:
       DUNE_THROW(Dune::RangeError, "Wrong type '" << type << "' requested!");
   } // ... compute_norm(...)
 
-protected:
   const TestCase& test_;
   size_t current_level_;
   size_t last_computed_level_;
