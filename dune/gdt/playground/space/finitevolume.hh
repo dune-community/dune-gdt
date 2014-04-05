d5fce1c6fdf15a862a8f8314ebb24c3c671c1d84
Felix Schindler <felix@schindlerfamily.de>
2014-04-05 15:49:01 +0200

    [space.finitevolume] added first draft * seems to work

---
+++ dune/gdt/playground/space/finitevolume.hh
@@ -0,0 +1,128 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Albrecht
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_GDT_SPACE_FINITEVOLUME_HH
+#define DUNE_GDT_SPACE_FINITEVOLUME_HH
+
+#include "../mapper/finitevolume.hh"
+#include "../basefunctionset/finitevolume.hh"
+#include "../../space/interface.hh"
+
+namespace Dune {
+namespace GDT {
+namespace FiniteVolumeSpace {
+
+
+// forward, to be used in the traits and to allow for specialization
+template <class GridViewImp, class RangeFieldImp, int rangeDim, int rangeDimCols = 1>
+class Default
+{
+  static_assert(Dune::AlwaysFalse<GridViewImp>::value, "Untested for these dimensions!");
+};
+
+
+/**
+ *  \brief Traits class for ContinuousLagrangeSpace::FemWrapper.
+ */
+template <class GridViewImp, class RangeFieldImp, int rangeDim, int rangeDimCols>
+class DefaultTraits
+{
+public:
+  typedef Default<GridViewImp, RangeFieldImp, rangeDim, rangeDimCols> derived_type;
+  static const int polOrder = 0;
+  typedef double BackendType;
+  typedef GridViewImp GridViewType;
+  typedef typename GridViewType::template Codim<0>::Entity EntityType;
+  typedef RangeFieldImp RangeFieldType;
+  static const unsigned int dimRange     = rangeDim;
+  static const unsigned int dimRangeCols = rangeDimCols;
+  typedef Mapper::FiniteVolume<GridViewType> MapperType;
+  typedef BaseFunctionSet::FiniteVolume<typename GridViewType::template Codim<0>::Entity, typename GridViewType::ctype,
+                                        GridViewType::dimension, RangeFieldType, dimRange,
+                                        dimRangeCols> BaseFunctionSetType;
+  static const bool needs_grid_view = true;
+}; // class DefaultTraits
+
+
+template <class GridViewImp, class RangeFieldImp>
+class Default<GridViewImp, RangeFieldImp, 1, 1> : public SpaceInterface<DefaultTraits<GridViewImp, RangeFieldImp, 1, 1>>
+{
+  typedef SpaceInterface<DefaultTraits<GridViewImp, RangeFieldImp, 1, 1>> BaseType;
+  typedef Default<GridViewImp, RangeFieldImp, 1, 1> ThisType;
+
+public:
+  typedef DefaultTraits<GridViewImp, RangeFieldImp, 1, 1> Traits;
+
+  typedef typename Traits::GridViewType GridViewType;
+  static const int polOrder = Traits::polOrder;
+  typedef typename GridViewType::ctype DomainFieldType;
+  static const unsigned int dimDomain = GridViewType::dimension;
+  typedef typename Traits::RangeFieldType RangeFieldType;
+  static const unsigned int dimRange     = Traits::dimRange;
+  static const unsigned int dimRangeCols = Traits::dimRangeCols;
+
+  typedef typename Traits::BackendType BackendType;
+  typedef typename Traits::MapperType MapperType;
+  typedef typename Traits::BaseFunctionSetType BaseFunctionSetType;
+  typedef typename Traits::EntityType EntityType;
+
+  typedef Dune::Stuff::LA::SparsityPatternDefault PatternType;
+
+  Default(const std::shared_ptr<const GridViewType>& gv)
+    : grid_view_(gv)
+    , mapper_(std::make_shared<MapperType>(*grid_view_))
+  {
+  }
+
+  Default(const ThisType& other)
+    : grid_view_(other.grid_view_)
+    , mapper_(other.mapper_)
+  {
+  }
+
+  Default& operator=(const ThisType& other)
+  {
+    if (this != &other) {
+      grid_view_ = other.grid_view_;
+      mapper_    = other.mapper_;
+    }
+    return *this;
+  }
+
+  ~Default()
+  {
+  }
+
+  const std::shared_ptr<const GridViewType>& grid_view() const
+  {
+    return grid_view_;
+  }
+
+  const BackendType& backend() const
+  {
+    return 1.0;
+  }
+
+  const MapperType& mapper() const
+  {
+    return *mapper_;
+  }
+
+  BaseFunctionSetType base_function_set(const EntityType& entity) const
+  {
+    return BaseFunctionSetType(entity);
+  }
+
+private:
+  std::shared_ptr<const GridViewType> grid_view_;
+  std::shared_ptr<const MapperType> mapper_;
+}; // class Default< ..., 1, 1 >
+
+
+} // namespace FiniteVolumeSpace
+} // namespace GDT
+} // namespace Dune
+
+#endif // DUNE_GDT_SPACE_FINITEVOLUME_HH
