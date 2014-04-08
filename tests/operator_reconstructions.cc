b4ad9877c8d2cea9d94c10f58d3e604e4ed4d03b
Felix Schindler <felix@schindlerfamily.de>
2014-04-08 15:31:43 +0200

    [tests.operator_reconstructions] added RTN test for Darcy operator

--- tests/operator_reconstructions.cc
+++ tests/operator_reconstructions.cc
@@ -24,8 +24,11 @@
 
 #include <dune/gdt/space/tools.hh>
 #include <dune/gdt/space/continuouslagrange/fem.hh>
+//#include <dune/gdt/playground/space/raviartthomas/pdelab.hh>
+#include <dune/gdt/playground/space/finitevolume.hh>
 #include <dune/gdt/discretefunction/default.hh>
 #include <dune/gdt/operator/darcy.hh>
+#include <dune/gdt/operator/projections.hh>
 #include <dune/gdt/product/l2.hh>
 #include <dune/gdt/product/h1.hh>
 
@@ -38,24 +41,28 @@ class errors_are_not_as_expected : public Dune::Exception
 // +----------------------------------------------------------------------------+
 
 template <class SpaceTypes>
-struct Darcy_Operator : public ::testing::Test
+class Darcy_Operator : public ::testing::Test
 {
-#if HAVE_EIGEN
-  typedef Dune::Stuff::LA::EigenDenseVector<double> VectorType;
-#elif HAVE_DUNE_ISTL
-  typedef Dune::Stuff::LA::IstlDenseVector<double> VectorType;
-#else
-  typedef Dune::Stuff::LA::CommonDenseVector<double> VectorType;
-#endif
-
   typedef typename SpaceTypes::first_type SourceSpaceType;
   typedef typename SpaceTypes::second_type RangeSpaceType;
 
   typedef typename RangeSpaceType::GridViewType GridViewType;
   typedef typename GridViewType::Grid GridType;
   typedef Dune::Stuff::GridProviderCube<GridType> GridProviderType;
+  typedef typename GridViewType::template Codim<0>::Entity EntityType;
+  typedef typename GridViewType::ctype DomainFieldType;
   static const unsigned int dimDomain = SourceSpaceType::dimDomain;
+  typedef double RangeFieldType;
 
+#if HAVE_EIGEN
+  typedef Dune::Stuff::LA::EigenDenseVector<RangeFieldType> VectorType;
+#elif HAVE_DUNE_ISTL
+  typedef Dune::Stuff::LA::IstlDenseVector<RangeFieldType> VectorType;
+#else
+  typedef Dune::Stuff::LA::CommonDenseVector<RangeFieldType> VectorType;
+#endif
+
+public:
   void produces_correct_results() const
   {
     using namespace Dune;
@@ -65,12 +72,7 @@ struct Darcy_Operator : public ::testing::Test
     auto grid = grid_provider.grid();
     grid->globalRefine(1);
 
-    typedef Stuff::Function::Expression<typename GridViewType::template Codim<0>::Entity,
-                                        typename GridViewType::ctype,
-                                        GridViewType::dimension,
-                                        double,
-                                        1,
-                                        1> FunctionType;
+    typedef Stuff::Function::Expression<EntityType, DomainFieldType, dimDomain, RangeFieldType, 1> FunctionType;
     const FunctionType source("x", "x[0] * x[1]", 2, "source", {{"x[1]", "x[0]"}});
 
     const RangeSpaceType range_space(SpaceTools::GridPartView<RangeSpaceType>::create_leaf(*grid));
@@ -82,46 +84,75 @@ struct Darcy_Operator : public ::testing::Test
                                                                                         function);
     darcy_operator.apply(source, range);
 
-    const Stuff::Function::Expression<typename GridViewType::template Codim<0>::Entity,
-                                      typename GridViewType::ctype,
-                                      GridViewType::dimension,
-                                      double,
-                                      2,
-                                      1> desired_output("x",
-                                                        std::vector<std::string>({"x[1]", "x[0]"}),
-                                                        1,
-                                                        "desired output",
-                                                        {{"0.0", "1.0"}, {"1.0", "0.0"}});
-    const auto difference = desired_output - range;
+    const Stuff::Function::Expression<EntityType, DomainFieldType, dimDomain, RangeFieldType, dimDomain> desired_output(
+        "x", std::vector<std::string>({"x[1]", "x[0]"}), 1, "desired output", {{"0.0", "1.0"}, {"1.0", "0.0"}});
 
     const Product::L2Generic<GridViewType> l2_product(*(range_space.grid_view()));
-    const double l2_error = std::sqrt(l2_product.apply2(difference, difference));
-    if (l2_error > 1e-15)
-      DUNE_THROW_COLORFULLY(errors_are_not_as_expected, l2_error);
+    const RangeFieldType l2_error          = l2_product.induced_norm(desired_output - range);
+    const RangeFieldType l2_error_expected = expected_result_("l2", desired_output, range_space.grid_view());
+    if (l2_error > l2_error_expected)
+      DUNE_THROW_COLORFULLY(errors_are_not_as_expected, l2_error << " vs. " << l2_error_expected);
 
     const Product::H1SemiGeneric<GridViewType> h1_semi_product(*(range_space.grid_view()));
-    const double h1_semi_error = std::sqrt(h1_semi_product.apply2(difference, difference));
-    if (h1_semi_error > 1e-14)
-      DUNE_THROW_COLORFULLY(errors_are_not_as_expected, h1_semi_error);
+    const RangeFieldType h1_error          = h1_semi_product.induced_norm(desired_output - range);
+    const RangeFieldType h1_error_expected = expected_result_("h1", desired_output, range_space.grid_view());
+    if (h1_error > h1_error_expected)
+      DUNE_THROW_COLORFULLY(errors_are_not_as_expected, h1_error << " vs. " << h1_error_expected);
   } // ... produces_correct_results()
-}; // struct Darcy_Operator
+
+private:
+  template <class FunctionType, class GV>
+  RangeFieldType expected_result_(const std::string type, const FunctionType& desired_output,
+                                  const std::shared_ptr<const GV>& grid_view_ptr) const
+  {
+    typedef typename Dune::GDT::SpaceTools::LeafGridPartView<GridType, RangeSpaceType::needs_grid_view>::Type GPV;
+    if (std::is_base_of<Dune::GDT::ContinuousLagrangeSpace::FemWrapper<GPV, 1, RangeFieldType, dimDomain>,
+                        RangeSpaceType>::value) {
+      if (type == "l2")
+        return 2.18e-16;
+      else if (type == "h1")
+        return 3.12e-15;
+      else
+        DUNE_THROW_COLORFULLY(Dune::Stuff::Exceptions::internal_error, type);
+    } else if (std::is_base_of<Dune::GDT::RaviartThomasSpace::PdelabBased<GPV, 0, RangeFieldType, dimDomain>,
+                               RangeSpaceType>::value) {
+      typedef Dune::GDT::FiniteVolumeSpace::Default<GV, RangeFieldType, dimDomain> FvSpaceType;
+      const FvSpaceType fv_space(grid_view_ptr);
+      VectorType fv_desired_output_vector(fv_space.mapper().size());
+      Dune::GDT::DiscreteFunction<FvSpaceType, VectorType> fv_desired_output(fv_space, fv_desired_output_vector);
+      const Dune::GDT::ProjectionOperator::L2<GV> l2_projection(*grid_view_ptr);
+      l2_projection.apply(desired_output, fv_desired_output);
+      const Dune::GDT::Product::L2Generic<GV> l2_product(*grid_view_ptr);
+      const Dune::GDT::Product::H1SemiGeneric<GV> h1_semi_product(*grid_view_ptr);
+      if (type == "l2")
+        return 2.0 * l2_product.induced_norm(desired_output - fv_desired_output);
+      else if (type == "h1")
+        return h1_semi_product.induced_norm(desired_output - fv_desired_output);
+      else
+        DUNE_THROW_COLORFULLY(Dune::Stuff::Exceptions::internal_error, type);
+    } else
+      DUNE_THROW_COLORFULLY(Dune::Stuff::Exceptions::internal_error, type);
+  }
+}; // class Darcy_Operator
 
 // +----------------------------------------------------------------------------+
 // | 2nd we define all arguments the above test structs are to be compiled with |
 // +----------------------------------------------------------------------------+
 
 typedef Dune::ALUConformGrid<2, 2> AluConform2dGridType;
+typedef typename Dune::GDT::SpaceTools::LeafGridPartView<AluConform2dGridType, true>::Type AluConform2dLeafGridViewType;
 typedef
     typename Dune::GDT::SpaceTools::LeafGridPartView<AluConform2dGridType, false>::Type AluConform2dLeafGridPartType;
 
 typedef testing::
     Types<std::pair<Dune::GDT::ContinuousLagrangeSpace::FemWrapper<AluConform2dLeafGridPartType, 1, double, 1>,
-                    Dune::GDT::ContinuousLagrangeSpace::FemWrapper<AluConform2dLeafGridPartType, 1, double, 2>>>
-        SpaceTypes;
+                    Dune::GDT::ContinuousLagrangeSpace::FemWrapper<AluConform2dLeafGridPartType, 1, double, 2>>,
+          std::pair<Dune::GDT::ContinuousLagrangeSpace::FemWrapper<AluConform2dLeafGridPartType, 1, double, 1>,
+                    Dune::GDT::RaviartThomasSpace::PdelabBased<AluConform2dLeafGridViewType, 0, double, 2>>> SpaceTypes;
 
 // +--------------------------------------------------------------------------------------+
 // | 3rd we combine all test structs with their appropriate arguments to create the tests |
-// | (comment out the following lines if you do not want a test to be run)                |
+// | (comment out the following lines if you do not want a test to run)                   |
 // +--------------------------------------------------------------------------------------+
 
 TYPED_TEST_CASE(Darcy_Operator, SpaceTypes);
