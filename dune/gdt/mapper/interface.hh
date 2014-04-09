3641357618196ce9f2a745e9cbc68d9f6a9c269c
Felix Schindler <felix@schindlerfamily.de>
2014-04-09 11:30:29 +0200

    [mapper.interface] base on dune-stuff CRTP

--- dune/gdt/mapper/interface.hh
+++ dune/gdt/mapper/interface.hh
@@ -6,50 +6,50 @@
 #ifndef DUNE_GDT_MAPPER_INTERFACE_HH
 #define DUNE_GDT_MAPPER_INTERFACE_HH
 
-#include <dune/common/bartonnackmanifcheck.hh>
 #include <dune/common/dynvector.hh>
+
+#include <dune/stuff/common/crtp.hh>
 
 namespace Dune {
 namespace GDT {
 
 
 template <class Traits>
-class MapperInterface
+class MapperInterface : public Stuff::CRTPInterface<MapperInterface<Traits>, Traits>
 {
 public:
   typedef typename Traits::derived_type derived_type;
-
   typedef typename Traits::BackendType BackendType;
 
   const BackendType& backend() const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().backend());
-    return asImp().backend();
+    CHECK_CRTP(this->as_imp(*this).backend());
+    return this->as_imp(*this).backend();
   }
 
   size_t size() const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().size());
-    return asImp().size();
+    CHECK_CRTP(this->as_imp(*this).size());
+    return this->as_imp(*this).size();
   }
 
   size_t maxNumDofs() const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().maxNumDofs());
-    return asImp().maxNumDofs();
+    CHECK_CRTP(this->as_imp(*this).maxNumDofs());
+    return this->as_imp(*this).maxNumDofs();
   }
 
   template <class EntityType>
   size_t numDofs(const EntityType& entity) const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().numDofs(entity));
-    return asImp().numDofs(entity);
+    CHECK_CRTP(this->as_imp(*this).numDofs(entity));
+    return this->as_imp(*this).numDofs(entity);
   }
 
   template <class EntityType>
   void globalIndices(const EntityType& entity, Dune::DynamicVector<size_t>& ret) const
   {
-    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().globalIndices(entity, ret));
+    CHECK_AND_CALL_CRTP(this->as_imp(*this).globalIndices(entity, ret));
   }
 
   template <class EntityType>
@@ -63,18 +63,8 @@ public:
   template <class EntityType>
   size_t mapToGlobal(const EntityType& entity, const size_t& localIndex) const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().mapToGlobal(entity, localIndex));
-    return asImp().mapToGlobal(entity, localIndex);
-  }
-
-  derived_type& asImp()
-  {
-    return static_cast<derived_type&>(*this);
-  }
-
-  const derived_type& asImp() const
-  {
-    return static_cast<const derived_type&>(*this);
+    CHECK_CRTP(this->as_imp(*this).mapToGlobal(entity, localIndex));
+    return this->as_imp(*this).mapToGlobal(entity, localIndex);
   }
 }; // class MapperInterface
 
