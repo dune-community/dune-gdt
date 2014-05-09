4e9ae93ca3476848eeb75aac857b7183b11a9da0
Felix Schindler <felix@schindlerfamily.de>
2014-05-09 09:46:50 +0200

    [spaces.cg.base] fixed header guard

--- dune/gdt/spaces/continuouslagrange/base.hh
+++ dune/gdt/spaces/continuouslagrange/base.hh
@@ -3,8 +3,8 @@
 // Copyright holders: Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-#ifndef DUNE_GDT_SPACES_CONTINUOUSLAGRANGE_HH
-#define DUNE_GDT_SPACES_CONTINUOUSLAGRANGE_HH
+#ifndef DUNE_GDT_SPACES_CONTINUOUSLAGRANGE_BASE_HH
+#define DUNE_GDT_SPACES_CONTINUOUSLAGRANGE_BASE_HH
 
 #include <type_traits>
 
@@ -232,4 +232,4 @@ protected:
 } // namespace GDT
 } // namespace Dune
 
-#endif // DUNE_GDT_SPACES_CONTINUOUSLAGRANGE_HH
+#endif // DUNE_GDT_SPACES_CONTINUOUSLAGRANGE_BASE_HH
