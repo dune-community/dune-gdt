5186b7f5fc1b962bdfbdea3f53cf0f96df46cf16
Felix Schindler <felix@schindlerfamily.de>
2014-05-11 15:38:29 +0200

    [functionals.base] AssemblableVolumeBase -> VectorBased

--- dune/gdt/functionals/base.hh
+++ dune/gdt/functionals/base.hh
@@ -6,13 +6,7 @@
 #ifndef DUNE_GDT_FUNCTIONALS_BASE_HH
 #define DUNE_GDT_FUNCTIONALS_BASE_HH
 
-#include <type_traits>
-
-#include <dune/stuff/la/container/interfaces.hh>
-
-#include <dune/gdt/assembler/system.hh>
-#include <dune/gdt/assembler/local/codim0.hh>
-#include <dune/gdt/assembler/local/codim1.hh>
+#include <dune/stuff/la/solver.hh>
 
 #include "interfaces.hh"
 
@@ -21,67 +15,32 @@ namespace GDT {
 namespace Functionals {
 
 
-// forward, to be used in the traits
-template <class ImpTraits>
-class AssemblableVolumeBase;
-
-
-template <class ImpTraits>
-class AssemblableVolumeBaseTraits
+template <class Traits>
+class VectorBased : public AssemblableFunctionalInterface<Traits>
 {
-public:
-  typedef typename ImpTraits::derived_type derived_type;
-  typedef typename ImpTraits::GridViewType GridViewType;
-  typedef typename ImpTraits::SpaceType SpaceType;
-  typedef typename ImpTraits::VectorType VectorType;
-  typedef typename ImpTraits::ScalarType ScalarType;
-
-private:
-  static_assert(std::is_base_of<Stuff::LA::VectorInterface<typename VectorType::Traits>, VectorType>::value,
-                "VectorType has to be derived from Stuff::LA::VectorInterface!");
-}; // class AssemblableVolumeBaseTraits
-
-
-template <class ImpTraits>
-class AssemblableVolumeBase : public AssemblableFunctionalInterface<ImpTraits>,
-                              public Functor::Codim0<typename ImpTraits::GridViewType>
-{
-  typedef AssemblableFunctionalInterface<ImpTraits> InterfaceType;
-  typedef AssemblableVolumeBaseTraits<ImpTraits> Traits;
-  typedef TmpStorageProvider::Vectors<typename ImpTraits::ScalarType> TmpStorageProviderType;
+  typedef AssemblableFunctionalInterface<Traits> BaseType;
 
 public:
-  typedef typename Traits::GridViewType GridViewType;
-  typedef typename Traits::SpaceType SpaceType;
-  typedef typename Traits::VectorType VectorType;
-  typedef typename Traits::ScalarType ScalarType;
+  using typename BaseType::GridViewType;
+  using typename BaseType::SpaceType;
+  using typename BaseType::VectorType;
+  using typename BaseType::ScalarType;
 
-  typedef typename GridViewType::template Codim<0>::Entity EntityType;
-
-private:
-  typedef typename ImpTraits::LocalFunctionalType LocalFunctionalType;
-  typedef LocalAssembler::Codim0Vector<LocalFunctionalType> LocalAssemblerType;
-
-public:
-  AssemblableVolumeBase(VectorType& vector, const SpaceType& space, const GridViewType& grid_view)
+  VectorBased(VectorType& vector, const SpaceType& space, const GridViewType& grid_view)
     : vector_(vector)
     , space_(space)
     , grid_view_(grid_view)
-    , local_assembler_(nullptr)
-    , tmp_storage_provider_(nullptr)
-    , prepared_(false)
-    , assembled_(false)
   {
   }
 
-  AssemblableVolumeBase(VectorType& vector, const SpaceType& space)
+  VectorBased(VectorType& vector, const SpaceType& space)
     : vector_(vector)
     , space_(space)
     , grid_view_(*(space.grid_view()))
-    , local_assembler_(nullptr)
-    , tmp_storage_provider_(nullptr)
-    , prepared_(false)
-    , assembled_(false)
+  {
+  }
+
+  virtual ~VectorBased()
   {
   }
 
@@ -105,56 +64,20 @@ public:
     return vector_;
   }
 
-private:
-  virtual const LocalFunctionalType& local_functional() const = 0;
-
-public:
-  virtual void prepare() DS_OVERRIDE
-  {
-    if (!assembled_ && !prepared_) {
-      local_assembler_      = std::unique_ptr<LocalAssemblerType>(new LocalAssemblerType(local_functional()));
-      tmp_storage_provider_ = std::unique_ptr<TmpStorageProviderType>(
-          new TmpStorageProviderType(local_assembler_->numTmpObjectsRequired(), space_.mapper().maxNumDofs()));
-      prepared_ = true;
-    }
-  } // ... prepare()
-
-  virtual void apply_local(const EntityType& entity) DS_OVERRIDE
-  {
-    assert(prepared_);
-    assert(local_assembler_);
-    assert(tmp_storage_provider_);
-    local_assembler_->assembleLocal(
-        space_, entity, vector_, tmp_storage_provider_->vectors(), tmp_storage_provider_->indices());
-  } // ... apply_local(...)
-
-  void assemble()
-  {
-    if (!assembled_) {
-      GridWalker<GridViewType> grid_walker(grid_view_);
-      grid_walker.add(*this);
-      grid_walker.walk();
-      assembled_ = true;
-    }
-  } // ... assemble()
+  virtual void assemble() = 0;
 
   template <class S>
   ScalarType apply(const Stuff::LA::VectorInterface<S>& source) const
   {
-    typedef typename S::derived_type SourceType;
     assemble();
-    return vector_.dot(static_cast<const SourceType&>(source));
+    return vector_.dot(source.as_imp());
   } // ... apply(...)
 
 private:
   VectorType& vector_;
   const SpaceType& space_;
   const GridViewType& grid_view_;
-  std::unique_ptr<LocalAssemblerType> local_assembler_;
-  std::unique_ptr<TmpStorageProviderType> tmp_storage_provider_;
-  bool prepared_;
-  bool assembled_;
-}; // class AssemblableVolumeBase
+}; // class VectorBased
 
 
 // forward, to be used in the traits
