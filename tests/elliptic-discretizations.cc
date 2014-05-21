b4738aabd182aabb735d9b74f23699f69ac4224c
Tobias Leibner <tobias.leibner@googlemail.com>
2014-05-21 15:16:03 +0200

    [tests.elliptic-discretizations] silence warnings and add HAVE_DUNE_FEM_LOCALFUNCTIONS-guard

--- tests/elliptic-discretizations.cc
+++ tests/elliptic-discretizations.cc
@@ -6,7 +6,9 @@
 #define DUNE_STUFF_FUNCTIONS_DISABLE_CHECKS
 
 // This one has to come first (includes the config.h)!
-#include <dune/stuff/test/test_common.hh>
+#include <dune/stuff/common/disable_warnings.hh>
+# include <dune/stuff/test/test_common.hh>
+#include <dune/stuff/common/reenable_warnings.hh>
 
 #ifdef HAVE_FASP
 # undef HAVE_FASP
@@ -16,7 +18,9 @@
 
 #if HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
 # define ENABLE_ALUGRID 1
-# include <dune/grid/alugrid.hh>
+# include <dune/stuff/common/disable_warnings.hh>
+#   include <dune/grid/alugrid.hh>
+# include <dune/stuff/common/reenable_warnings.hh>
 #else
 # error This test requires ALUGrid!
 #endif
@@ -85,6 +89,7 @@ struct EllipticCGDiscretization
   }
 }; // EllipticCGDiscretization
 
+#if HAVE_DUNE_FEM_LOCALFUNCTIONS
 template< class TestCase >
 struct EllipticSIPDGDiscretization
   : public ::testing::Test
@@ -163,11 +168,6 @@ struct EllipticSWIPDGDiscretization
 };
 
 
-TYPED_TEST_CASE(EllipticCGDiscretization, AluConform2dTestCases);
-TYPED_TEST(EllipticCGDiscretization, produces_correct_results) {
-  this->produces_correct_results();
-}
-
 TYPED_TEST_CASE(EllipticSIPDGDiscretization, AluConform2dTestCases);
 TYPED_TEST(EllipticSIPDGDiscretization, produces_correct_results) {
   this->produces_correct_results();
@@ -175,6 +175,12 @@ TYPED_TEST(EllipticSIPDGDiscretization, produces_correct_results) {
 
 TYPED_TEST_CASE(EllipticSWIPDGDiscretization, AluConform2dTestCases);
 TYPED_TEST(EllipticSWIPDGDiscretization, produces_correct_results) {
+  this->produces_correct_results();
+}
+#endif //HAVE_DUNE_FEM_LOCALFUNCTIONS
+
+TYPED_TEST_CASE(EllipticCGDiscretization, AluConform2dTestCases);
+TYPED_TEST(EllipticCGDiscretization, produces_correct_results) {
   this->produces_correct_results();
 }
 
