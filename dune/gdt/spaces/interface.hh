dune-community/dune-gdt

commit 6c85d54554a759f1a3a4ba06f84142f13ea10c79

[spaces.interface] be less strict in GridView enforcing * these methods also work for grid parts

dune/gdt/spaces/interface.hh
@@ -131,12 +131,6 @@ public:
     return this->as_imp(*this).base_function_set(entity);
   }
 
-  template< class ConstraintsType >
-  void local_constraints(const EntityType& entity, ConstraintsType& ret) const
-  {
-    local_constraints(*this, entity, ret);
-  }
-
   template< class ConstraintsType, class T >
   void local_constraints(const SpaceInterface< T >& ansatz_space, const EntityType& entity, ConstraintsType& ret) const
   {
@@ -160,6 +154,12 @@ public:
    * \defgroup provided ´´These methods are provided by the interface for convenience.''
    * @{
    **/
+
+  template< class ConstraintsType >
+  void local_constraints(const EntityType& entity, ConstraintsType& ret) const
+  {
+    local_constraints(*this, entity, ret);
+  }
 
   PatternType compute_pattern() const
   {
@@ -301,7 +301,7 @@ public:
   }
 
   template< class G >
-  PatternType compute_face_pattern(const GridView< G >& local_grid_view) const
+  PatternType compute_face_pattern(const /*GridView<*/ G /*>*/& local_grid_view) const
   {
     return compute_face_pattern(local_grid_view, *this);
   }
@@ -313,7 +313,8 @@ public:
   }
 
   template< class G, class S >
-  PatternType compute_face_pattern(const GridView< G >& local_grid_view, const SpaceInterface< S >& ansatz_space) const
+  PatternType compute_face_pattern(const /*GridView<*/ G /*>*/& local_grid_view,
+                                   const SpaceInterface< S >& ansatz_space) const
   {
     // prepare
     PatternType pattern(mapper().size());

commit 60118a029079677b12b41f0d08fc9c9bd53031f1

[spaces.interfaces] fix an issue introduced in 6c85d54554

dune/gdt/spaces/interface.hh
@@ -301,7 +301,7 @@ public:
   }
 
   template< class G >
-  PatternType compute_face_pattern(const /*GridView<*/ G /*>*/& local_grid_view) const
+  PatternType compute_face_pattern(const GridView< G >& local_grid_view) const
   {
     return compute_face_pattern(local_grid_view, *this);
   }