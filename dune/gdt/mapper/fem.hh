96fdaf96450448f3616a8cc7b3356e5d1a53f5f0
Felix Schindler <felix@schindlerfamily.de>
2014-05-28 14:24:17 +0200

    [mapper.fem] added support for block size > 1
    This mapper worked until now becaus it was only used in the CG space, where
    the block size of the block mapper was always 1. For larger block sizes we
    need to wrap the block mapper in a non block mapper which flattens out
    everything.

--- dune/gdt/mapper/fem.hh
+++ dune/gdt/mapper/fem.hh
@@ -11,6 +11,10 @@
 #include <dune/common/dynvector.hh>
 #include <dune/common/typetraits.hh>
 
+#if HAVE_DUNE_FEM
+#include <dune/fem/space/mapper/nonblockmapper.hh>
+#endif
+
 #include "interface.hh"
 
 namespace Dune {
@@ -21,26 +25,122 @@ namespace Mapper {
 
 
 // forward, to be used in the traits and to allow for specialization
-template <class FemDofMapperImp>
+template <class FemDofMapperImp, int block_size = 1>
 class FemDofWrapper;
 
 
-template <class FemDofMapperImp>
+template <class FemDofMapperImp, int block_size>
 class FemDofWrapperTraits
 {
 public:
-  typedef FemDofWrapper<FemDofMapperImp> derived_type;
-  typedef FemDofMapperImp BackendType;
+  typedef FemDofWrapper<FemDofMapperImp, block_size> derived_type;
+  typedef Fem::NonBlockMapper<FemDofMapperImp, block_size> BackendType;
 };
 
 
 template <class FemDofMapperImp>
-class FemDofWrapper : public MapperInterface<FemDofWrapperTraits<FemDofMapperImp>>
+class FemDofWrapperTraits<FemDofMapperImp, 1>
 {
-  typedef MapperInterface<FemDofWrapperTraits<FemDofMapperImp>> InterfaceType;
+public:
+  typedef FemDofWrapper<FemDofMapperImp, 1> derived_type;
+  typedef FemDofMapperImp BackendType;
+};
+
+
+template <class FemDofMapperImp, int block_size>
+class FemDofWrapper : public MapperInterface<FemDofWrapperTraits<FemDofMapperImp, block_size>>
+{
+  typedef MapperInterface<FemDofWrapperTraits<FemDofMapperImp, block_size>> InterfaceType;
 
 public:
-  typedef FemDofWrapperTraits<FemDofMapperImp> Traits;
+  typedef FemDofWrapperTraits<FemDofMapperImp, block_size> Traits;
+  typedef typename Traits::BackendType BackendType;
+
+  FemDofWrapper(FemDofMapperImp& femNonBlockMapper)
+    : backend_(femNonBlockMapper)
+  {
+  }
+
+  const BackendType& backend() const
+  {
+    return backend_;
+  }
+
+  size_t size() const
+  {
+    return backend_.size();
+  }
+
+  template <class EntityType>
+  size_t numDofs(const EntityType& entity) const
+  {
+    return backend_.numDofs(entity);
+  }
+
+  size_t maxNumDofs() const
+  {
+    return backend_.maxNumDofs();
+  }
+
+private:
+  class Functor
+  {
+  public:
+    Functor(Dune::DynamicVector<size_t>& globalIndices)
+      : globalIndices_(globalIndices)
+    {
+    }
+
+    void operator()(int localDoF, int globalDoF)
+    {
+      assert(localDoF < int(globalIndices_.size()));
+      globalIndices_[localDoF] = globalDoF;
+    }
+
+  private:
+    Dune::DynamicVector<size_t>& globalIndices_;
+  };
+
+public:
+  template <class EntityType>
+  void globalIndices(const EntityType& entity, Dune::DynamicVector<size_t>& ret) const
+  {
+    // some checks
+    const size_t numLocalDofs = numDofs(entity);
+    if (ret.size() < numLocalDofs)
+      ret.resize(numLocalDofs);
+    // compute
+    Functor functor(ret);
+    backend_.mapEach(entity, functor);
+  }
+
+  using InterfaceType::globalIndices;
+
+  /**
+   *  \attention  This method is implemented using globalIndices() and thus not optimal!
+   */
+  template <class EntityType>
+  size_t mapToGlobal(const EntityType& entity, const size_t& localIndex) const
+  {
+    const size_t numLocalDofs = numDofs(entity);
+    assert(localIndex < numLocalDofs);
+    Dune::DynamicVector<size_t> tmpGlobalIndices(numLocalDofs);
+    globalIndices(entity, tmpGlobalIndices);
+    return tmpGlobalIndices[localIndex];
+  }
+
+private:
+  const BackendType backend_;
+}; // class FemDofWrapper
+
+
+template <class FemDofMapperImp>
+class FemDofWrapper<FemDofMapperImp, 1> : public MapperInterface<FemDofWrapperTraits<FemDofMapperImp, 1>>
+{
+  typedef MapperInterface<FemDofWrapperTraits<FemDofMapperImp, 1>> InterfaceType;
+
+public:
+  typedef FemDofWrapperTraits<FemDofMapperImp, 1> Traits;
   typedef typename Traits::BackendType BackendType;
 
   FemDofWrapper(const BackendType& femMapper)
@@ -118,7 +218,7 @@ public:
 
 private:
   const BackendType& backend_;
-}; // class FemDofWrapper
+}; // class FemDofWrapper< ..., 1 >
 
 
 #else // HAVE_DUNE_FEM
