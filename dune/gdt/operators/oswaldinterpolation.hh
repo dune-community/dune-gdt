5ffab68b32d08445070d55d0c7e78eaf69439ea6
Felix Schindler <felix@schindlerfamily.de>
2014-05-30 23:32:20 +0200

    [operators.oswaldinterpolation] added apply() for block space

--- dune/gdt/operators/oswaldinterpolation.hh
+++ dune/gdt/operators/oswaldinterpolation.hh
@@ -15,6 +15,7 @@
 #include <dune/gdt/assembler/gridwalker.hh>
 #include <dune/gdt/discretefunction/default.hh>
 #include <dune/gdt/spaces/discontinuouslagrange/fem-localfunctions.hh>
+#include <dune/gdt/playground/spaces/block.hh>
 
 #include "interfaces.hh"
 
@@ -371,7 +372,7 @@ public:
     //   given a vertex, one obtains a set of all global DoF ids, which are associated with this vertex
     typedef std::vector<std::set<size_t>> VertexToEntitiesMapType;
     VertexToEntitiesMapType vertex_to_dof_id_map(grid_view_.indexSet().size(dimDomain));
-    // * a set to hold the global id off all boundary vertices
+    // * a set to hold the global id of all boundary vertices
     std::set<size_t> boundary_vertices;
 
     // walk the grid for the first time
@@ -444,6 +445,139 @@ public:
       // * for the oswald projection
       typedef typename SpaceType::Traits::ContinuousFiniteElementType FiniteElementType;
       const auto dg_finite_element = range.space().backend().finiteElement(entity);
+      const FiniteElementType cg_finite_element(entity.geometry().type(), polOrder);
+      const auto& dg_local_coefficients = dg_finite_element.localCoefficients();
+      const auto& cg_local_coefficients = cg_finite_element.localCoefficients();
+      assert(dg_local_coefficients.size() == cg_local_coefficients.size() && "Wrong finite element given!");
+      // to compute the oswald projection
+      // * loop over all local DoFs
+      for (size_t ii = 0; ii < dg_local_coefficients.size(); ++ii) {
+        assert(ii < std::numeric_limits<int>::max());
+        const auto& entity_dg_local_key = dg_local_coefficients.localKey(int(ii));
+        const auto& entity_cg_local_key = cg_local_coefficients.localKey(int(ii));
+        assert(entity_cg_local_key.codim() == dimDomain && "Wrong finite element given!");
+        const int local_vertex_id     = entity_cg_local_key.subEntity();
+        const size_t local_DoF_id     = entity_dg_local_key.index();
+        const auto vertexPtr          = entity.template subEntity<dimDomain>(local_vertex_id);
+        const auto& vertex            = *vertexPtr;
+        const size_t global_vertex_id = grid_view_.indexSet().index(vertex);
+        // if we are on the domain boundary
+        if (zero_boundary_ && boundary_vertices.count(global_vertex_id)) {
+          // get global DoF id
+          const size_t global_DoF_id = range.space().mapper().mapToGlobal(entity, local_DoF_id);
+          // set the dof to zero (we have dirichlet zero)
+          range.vector().set_entry(global_DoF_id, FieldType(0));
+        } else {
+          // do the oswald projection
+          const size_t num_DoFS_per_vertex = vertex_to_dof_id_map[global_vertex_id].size();
+          // * get the source Dof
+          const FieldType source_Dof_value = local_source_DoF_vector.get(local_DoF_id);
+          // * and add it to all target DoFs
+          for (size_t target_global_DoF_id : vertex_to_dof_id_map[global_vertex_id])
+            range.vector().add_to_entry(target_global_DoF_id, source_Dof_value / num_DoFS_per_vertex);
+        } // if (boundary_vertices.find(global_vertex_id))
+      } // loop over all local DoFs
+    } // walk the grid for the second time
+  } // ... apply(...)
+
+  template <class SGP, class SV, class RGP, class RV>
+  void apply(
+      const ConstDiscreteFunction<Spaces::Block<Spaces::DiscontinuousLagrange::FemLocalfunctionsBased<SGP, 1, FieldType,
+                                                                                                      1, 1>>,
+                                  SV>& source,
+      DiscreteFunction<Spaces::Block<Spaces::DiscontinuousLagrange::FemLocalfunctionsBased<RGP, 1, FieldType, 1, 1>>,
+                       RV>& range) const
+  {
+    typedef Spaces::DiscontinuousLagrange::FemLocalfunctionsBased<RGP, 1, FieldType, 1, 1> LocalSpaceType;
+    static const unsigned int polOrder = 1;
+
+    // data structures we need
+    // * a map from a global vertex id to global DoF ids
+    //   given a vertex, one obtains a set of all global DoF ids, which are associated with this vertex
+    typedef std::vector<std::set<size_t>> VertexToEntitiesMapType;
+    VertexToEntitiesMapType vertex_to_dof_id_map(grid_view_.indexSet().size(dimDomain));
+    // * a set to hold the global id of all boundary vertices
+    std::set<size_t> boundary_vertices;
+
+    const auto& ms_grid     = *range.space().ms_grid();
+    const auto local_spaces = range.space().local_spaces();
+
+    // walk the grid for the first time
+    const auto entity_it_end = grid_view_.template end<0>();
+    for (auto entity_it = grid_view_.template begin<0>(); entity_it != entity_it_end; ++entity_it) {
+      const auto& entity     = *entity_it;
+      const size_t subdomain = ms_grid.subdomainOf(entity);
+      assert(subdomain < ms_grid.size());
+      assert(subdomain < local_spaces.size());
+      // get the local finite elements
+      typedef typename LocalSpaceType::Traits::ContinuousFiniteElementType FiniteElementType;
+      const auto dg_finite_element = local_spaces[subdomain]->backend().finiteElement(entity);
+      const FiniteElementType cg_finite_element(entity.geometry().type(), polOrder);
+      const auto& dg_local_coefficients = dg_finite_element.localCoefficients();
+      const auto& cg_local_coefficients = cg_finite_element.localCoefficients();
+      assert(dg_local_coefficients.size() == cg_local_coefficients.size() && "Wrong finite element given!");
+      // loop over all vertices
+      std::vector<DomainType> global_vertices(entity.template count<dimDomain>(), DomainType(0));
+      std::vector<size_t> global_vertex_ids(global_vertices.size(), 0);
+      for (size_t local_vertex_id = 0; local_vertex_id < global_vertices.size(); ++local_vertex_id) {
+        // get global vertex id
+        const auto vertexPtr               = entity.template subEntity<dimDomain>(int(local_vertex_id));
+        const auto& vertex                 = *vertexPtr;
+        global_vertex_ids[local_vertex_id] = grid_view_.indexSet().index(vertex);
+        global_vertices[local_vertex_id]   = vertex.geometry().center();
+        // find the global DoF id to this vertex, therefore
+        // loop over all local DoFs
+        for (size_t ii = 0; ii < dg_local_coefficients.size(); ++ii) {
+          assert(ii < std::numeric_limits<int>::max());
+          const auto& entity_cg_local_key = cg_local_coefficients.localKey(int(ii));
+          if (entity_cg_local_key.subEntity() == local_vertex_id) {
+            const auto& entity_dg_local_key = dg_local_coefficients.localKey(int(ii));
+            assert(entity_cg_local_key.codim() == dimDomain && "Wrong finite element given!");
+            const size_t local_DOF_id  = entity_dg_local_key.index();
+            const size_t global_DOF_id = range.space().mapper().mapToGlobal(entity, local_DOF_id);
+            // add this global DoF to this vertex
+            vertex_to_dof_id_map[global_vertex_ids[local_vertex_id]].insert(global_DOF_id);
+            // there must be one and only one for a polorder 1 lagrange basis
+            break;
+          }
+        } // loop over all local DoFs
+      } // loop over all vertices
+      if (zero_boundary_) {
+        // in order to determine the boundary vertices, we need to
+        // loop over all intersections
+        const auto intersectionEndIt = grid_view_.iend(entity);
+        for (auto intersectionIt = grid_view_.ibegin(entity); intersectionIt != intersectionEndIt; ++intersectionIt) {
+          const auto& intersection = *intersectionIt;
+          if (intersection.boundary() && !intersection.neighbor()) {
+            const auto& intersection_geometry = intersection.geometry();
+            for (int local_intersection_corner_id = 0; local_intersection_corner_id < intersection_geometry.corners();
+                 ++local_intersection_corner_id) {
+              const auto global_intersection_corner = intersection_geometry.corner(local_intersection_corner_id);
+              // now, we need to find the entity's vertex this intersection's corner point equals to, so we
+              // loop over all vertices of the entity
+              for (size_t local_vertex_id = 0; local_vertex_id < global_vertices.size(); ++local_vertex_id) {
+                if (Stuff::Common::FloatCmp::eq(global_intersection_corner, global_vertices[local_vertex_id]))
+                  boundary_vertices.insert(global_vertex_ids[local_vertex_id]);
+              } // loop over all vertices of the entity
+            } // if (intersection.boundary() && !intersection.neighbor())
+          } // loop over all intersections
+        } // loop over all intersections
+      }
+    } // walk the grid for the first time
+
+    // walk the grid for the second time
+    for (auto entity_it = grid_view_.template begin<0>(); entity_it != entity_it_end; ++entity_it) {
+      const auto& entity     = *entity_it;
+      const size_t subdomain = ms_grid.subdomainOf(entity);
+      assert(subdomain < ms_grid.size());
+      assert(subdomain < local_spaces.size());
+      // get the local functions
+      const auto local_source             = source.local_discrete_function(entity);
+      const auto& local_source_DoF_vector = local_source.vector();
+      // get the local finite elements
+      // * for the oswald projection
+      typedef typename LocalSpaceType::Traits::ContinuousFiniteElementType FiniteElementType;
+      const auto dg_finite_element = local_spaces[subdomain]->backend().finiteElement(entity);
       const FiniteElementType cg_finite_element(entity.geometry().type(), polOrder);
       const auto& dg_local_coefficients = dg_finite_element.localCoefficients();
       const auto& cg_local_coefficients = cg_finite_element.localCoefficients();
