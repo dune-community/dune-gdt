9a186ffbfa2e989541bc3e259e06ce752de8e43c
Felix Schindler <felix@schindlerfamily.de>
2014-05-30 17:45:58 +0200

    [operators.fluxreconstruction] extend signature

--- dune/gdt/operators/fluxreconstruction.hh
+++ dune/gdt/operators/fluxreconstruction.hh
@@ -25,12 +25,16 @@ namespace GDT {
 namespace Operators {
 
 
+template< class GridViewType, class DiffusionFactorType, class DiffusionTensorType = void >
+class DiffusiveFluxReconstruction;
+
+
 /**
  *  \todo Add more static checks that GridViewType and LocalizableFunctionType match.
- *  \todo Derived from operator interfaces.
+ *  \todo Derive from operator interfaces.
  */
 template< class GridViewType, class LocalizableFunctionType >
-class DiffusiveFluxReconstruction
+class DiffusiveFluxReconstruction< GridViewType, LocalizableFunctionType, void >
 {
   static_assert(GridViewType::dimension == 2, "Only implemented for dimDomain 2 at the moment!");
   static_assert(std::is_base_of< Stuff::IsLocalizableFunction, LocalizableFunctionType >::value,
@@ -46,8 +50,7 @@ private:
   static_assert(dimDomain == 2, "Not implemented!");
 
 public:
-  DiffusiveFluxReconstruction(const GridViewType& grid_view,
-                const LocalizableFunctionType& diffusion)
+  DiffusiveFluxReconstruction(const GridViewType& grid_view, const LocalizableFunctionType& diffusion)
     : grid_view_(grid_view)
     , diffusion_(diffusion)
   {}
