953ffcdc25db11dd9c1851da1e394422993f1f3c
Tobias Leibner <tobias.leibner@googlemail.com>
2014-06-04 15:45:22 +0200

    [operators.oswaldinterpolation] make the operator work with FemBased spaces

--- dune/gdt/operators/oswaldinterpolation.hh
+++ dune/gdt/operators/oswaldinterpolation.hh
@@ -11,6 +11,8 @@
 #include <limits>
 
 #include <dune/stuff/common/vector.hh>
+#include <dune/stuff/common/float_cmp.hh>
+#include <dune/stuff/common/print.hh>
 
 #include <dune/gdt/assembler/gridwalker.hh>
 #include <dune/gdt/discretefunction/default.hh>
@@ -255,11 +257,11 @@ public:
           } else {
             // do the oswald projection
             const size_t num_DoFS_per_vertex = vertex_to_dof_id_map_[global_vertex_id].size();
-            // * get the source Dof
-            const FieldType source_Dof_value = local_source_DoF_vector.get(local_DoF_id);
+            // * get the source DoF
+            const FieldType source_DoF_value = local_source_DoF_vector.get(local_DoF_id);
             // * and add it to all target DoFs
             for (size_t target_global_DoF_id : vertex_to_dof_id_map_[global_vertex_id])
-              range_.vector().add_to_entry(target_global_DoF_id, source_Dof_value / num_DoFS_per_vertex);
+              range_.vector().add_to_entry(target_global_DoF_id, source_DoF_value / num_DoFS_per_vertex);
           } // if (boundary_vertices.find(global_vertex_id))
         } // loop over all local DoFs
       } // walk the grid for the second time
@@ -367,51 +369,60 @@ public:
     static const unsigned int polOrder = 1;
 
     // data structures we need
-    // * a map from a global vertex id to global DoF ids
+    // * a map from a global vertex index to global DoF indices
     //   given a vertex, one obtains a set of all global DoF ids, which are associated with this vertex
-    typedef std::vector< std::set< size_t > > VertexToEntitiesMapType;
-    VertexToEntitiesMapType vertex_to_dof_id_map(grid_view_.indexSet().size(dimDomain));
+    std::map< size_t, std::set< size_t > > global_vertex_id_to_global_DoF_id_map;
+    // * a map from a global DoF index to the global index of its associated vertex
+    std::vector< size_t > global_DoF_id_to_global_vertex_id_map(source.space().mapper().size());
     // * a set to hold the global id off all boundary vertices
     std::set< size_t > boundary_vertices;
 
-    // walk the grid for the first time
-    const auto entity_it_end = grid_view_.template end< 0 >();
-    for (auto entity_it = grid_view_.template begin< 0 >(); entity_it != entity_it_end; ++entity_it) {
+    const auto entity_end_it = grid_view_.template end< 0 >();
+    //walk the grid to create the maps explained above and to find the boundary vertices
+    for (auto entity_it = grid_view_.template begin< 0 >(); entity_it != entity_end_it; ++entity_it) {
       const auto& entity = *entity_it;
-      // get the local finite elements
-      typedef typename SpaceType::Traits::ContinuousFiniteElementType FiniteElementType;
-      const auto dg_finite_element = range.space().backend().finiteElement(entity);
-      const FiniteElementType cg_finite_element(entity.geometry().type(), polOrder);
-      const auto& dg_local_coefficients = dg_finite_element.localCoefficients();
-      const auto& cg_local_coefficients = cg_finite_element.localCoefficients();
-      assert(dg_local_coefficients.size() == cg_local_coefficients.size()
-             && "Wrong finite element given!");
-      // loop over all vertices
-      std::vector< DomainType > global_vertices(entity.template count< dimDomain >(), DomainType(0));
-      std::vector< size_t > global_vertex_ids(global_vertices.size(), 0);
-      for (size_t local_vertex_id = 0; local_vertex_id < global_vertices.size(); ++local_vertex_id) {
-        // get global vertex id
-        const auto vertexPtr = entity.template subEntity< dimDomain >(int(local_vertex_id));
-        const auto& vertex = *vertexPtr;
-        global_vertex_ids[local_vertex_id] = grid_view_.indexSet().index(vertex);
-        global_vertices[local_vertex_id] = vertex.geometry().center();
-        // find the global DoF id to this vertex, therefore
-        // loop over all local DoFs
-        for (size_t ii = 0; ii < dg_local_coefficients.size(); ++ii) {
-          assert(ii < std::numeric_limits< int >::max());
-          const auto& entity_cg_local_key = cg_local_coefficients.localKey(int(ii));
-          if (entity_cg_local_key.subEntity() == local_vertex_id) {
-            const auto& entity_dg_local_key = dg_local_coefficients.localKey(int(ii));
-            assert(entity_cg_local_key.codim() == dimDomain && "Wrong finite element given!");
-            const size_t local_DOF_id = entity_dg_local_key.index();
-            const size_t global_DOF_id = range.space().mapper().mapToGlobal(entity, local_DOF_id);
-            // add this global DoF to this vertex
-            vertex_to_dof_id_map[global_vertex_ids[local_vertex_id]].insert(global_DOF_id);
-            // there must be one and only one for a polorder 1 lagrange basis
-            break;
-          }
-        } // loop over all local DoFs
-      } // loop over all vertices
+      const size_t num_vertices = entity.template count< dimDomain >();
+      const auto basis = source.space().base_function_set(entity);
+      if (basis.size() != size_t(num_vertices))
+        DUNE_THROW_COLORFULLY(Dune::Stuff::Exceptions::internal_error, "basis.size() = " << basis.size());
+
+      //loop over all vertices of the entitity, to find their associated global DoF indices
+      for (size_t local_vertex_id = 0; local_vertex_id < num_vertices; ++local_vertex_id) {
+        assert(local_vertex_id < std::numeric_limits< int >::max());
+        const auto vertex_ptr = entity.template subEntity< dimDomain >(int(local_vertex_id));
+        const size_t global_vertex_id = grid_view_.indexSet().index(*vertex_ptr);
+        const DomainType vertex = vertex_ptr->geometry().center();
+        // find the local basis function which corresponds to this vertex
+        const auto basis_values = basis.evaluate(entity.geometry().local(vertex));
+        if (basis_values.size() != size_t(num_vertices))
+          DUNE_THROW_COLORFULLY(Dune::Stuff::Exceptions::internal_error, "basis_values.size() = " << basis_values.size());
+        size_t ones = 0;
+        size_t zeros = 0;
+        size_t failures = 0;
+        size_t local_DoF_index = 0;
+        for (size_t ii = 0; ii < basis.size(); ++ii) {
+          if (Stuff::Common::FloatCmp::eq(basis_values[ii][0], typename SpaceType::RangeFieldType(1))) {
+            local_DoF_index = ii;
+            ++ones;
+          } else if (Stuff::Common::FloatCmp::eq(basis_values[ii][0], typename SpaceType::RangeFieldType(0)))
+            ++zeros;
+          else
+            ++failures;
+        }
+        if (ones != 1 || zeros != (basis.size() - 1) || failures > 0) {
+          std::stringstream ss;
+          ss << "ones = " << ones << ", zeros = " << zeros << ", failures = " << failures << ", num_vertices = "
+             << num_vertices << ", entity " << grid_view_.indexSet().index(entity)
+             << ", vertex " << local_vertex_id << ": [ " << vertex << "], ";
+          Stuff::Common::print(basis_values, "basis_values", ss);
+          DUNE_THROW_COLORFULLY(Dune::Stuff::Exceptions::internal_error, ss.str());
+        }
+        // now we know that the local DoF index of this vertex is ii
+        const size_t global_DoF_index = source.space().mapper().mapToGlobal(entity, local_DoF_index);
+        global_DoF_id_to_global_vertex_id_map[global_DoF_index] = global_vertex_id;
+        global_vertex_id_to_global_DoF_id_map[global_vertex_id].insert(global_DoF_index);
+      } //loop over all vertices
+
       if (zero_boundary_) {
         // in order to determine the boundary vertices, we need to
         // loop over all intersections
@@ -426,57 +437,44 @@ public:
               const auto global_intersection_corner = intersection_geometry.corner(local_intersection_corner_id);
               // now, we need to find the entity's vertex this intersection's corner point equals to, so we
               // loop over all vertices of the entity
-              for (size_t local_vertex_id = 0; local_vertex_id < global_vertices.size(); ++local_vertex_id) {
-                if (Stuff::Common::FloatCmp::eq(global_intersection_corner, global_vertices[local_vertex_id]))
-                  boundary_vertices.insert(global_vertex_ids[local_vertex_id]);
+              for (size_t local_vertex_id = 0; local_vertex_id < num_vertices; ++local_vertex_id) {
+                assert(local_vertex_id < std::numeric_limits< int >::max());
+                const auto vertex_ptr = entity.template subEntity< dimDomain >(int(local_vertex_id));
+                const size_t global_vertex_id = grid_view_.indexSet().index(*vertex_ptr);
+                const DomainType vertex = vertex_ptr->geometry().center();
+                if (Stuff::Common::FloatCmp::eq(global_intersection_corner, vertex))
+                  boundary_vertices.insert(global_vertex_id);
               } // loop over all vertices of the entity
-            } // if (intersection.boundary() && !intersection.neighbor())
-          } // loop over all intersections
+            } //loop over all intersection corners
+          } // if (intersection.boundary() && !intersection.neighbor())
         } // loop over all intersections
-      }
-    } // walk the grid for the first time
+      } // if(zero_boundary)
+    } //walk the grid for the first time
 
     // walk the grid for the second time
-    for (auto entity_it = grid_view_.template begin< 0 >(); entity_it != entity_it_end; ++entity_it) {
+    for (auto entity_it = grid_view_.template begin< 0 >(); entity_it != entity_end_it; ++entity_it) {
       const auto& entity = *entity_it;
+      const size_t num_vertices = entity.template count< dimDomain >();
       // get the local functions
       const auto local_source = source.local_discrete_function(entity);
       const auto& local_source_DoF_vector = local_source.vector();
-      // get the local finite elements
-      // * for the oswald projection
-      typedef typename SpaceType::Traits::ContinuousFiniteElementType FiniteElementType;
-      const auto dg_finite_element = range.space().backend().finiteElement(entity);
-      const FiniteElementType cg_finite_element(entity.geometry().type(), polOrder);
-      const auto& dg_local_coefficients = dg_finite_element.localCoefficients();
-      const auto& cg_local_coefficients = cg_finite_element.localCoefficients();
-      assert(dg_local_coefficients.size() == cg_local_coefficients.size()
-             && "Wrong finite element given!");
-      // to compute the oswald projection
+
       // * loop over all local DoFs
-      for (size_t ii = 0; ii < dg_local_coefficients.size(); ++ii) {
-        assert(ii < std::numeric_limits< int >::max());
-        const auto& entity_dg_local_key = dg_local_coefficients.localKey(int(ii));
-        const auto& entity_cg_local_key = cg_local_coefficients.localKey(int(ii));
-        assert(entity_cg_local_key.codim() == dimDomain && "Wrong finite element given!");
-        const int local_vertex_id = entity_cg_local_key.subEntity();
-        const size_t local_DoF_id = entity_dg_local_key.index();
-        const auto vertexPtr = entity.template subEntity< dimDomain >(local_vertex_id);
-        const auto& vertex = *vertexPtr;
-        const size_t global_vertex_id = grid_view_.indexSet().index(vertex);
+      for (size_t local_DoF_id = 0; local_DoF_id < num_vertices; ++local_DoF_id) {
+      const size_t global_DoF_index = source.space().mapper().mapToGlobal(entity, local_DoF_id);
+      const size_t global_vertex_id = global_DoF_id_to_global_vertex_id_map[global_DoF_index];
         // if we are on the domain boundary
         if (zero_boundary_ && boundary_vertices.count(global_vertex_id)) {
-          // get global DoF id
-          const size_t global_DoF_id = range.space().mapper().mapToGlobal(entity, local_DoF_id);
           // set the dof to zero (we have dirichlet zero)
-          range.vector().set_entry(global_DoF_id, FieldType(0));
+          range.vector().set_entry(global_DoF_index, FieldType(0));
         } else {
           // do the oswald projection
-          const size_t num_DoFS_per_vertex = vertex_to_dof_id_map[global_vertex_id].size();
-          // * get the source Dof
-          const FieldType source_Dof_value = local_source_DoF_vector.get(local_DoF_id);
+          const size_t num_DoFS_per_vertex = global_vertex_id_to_global_DoF_id_map[global_vertex_id].size();
+          // * get the source DoF
+          const FieldType source_DoF_value = local_source_DoF_vector.get(local_DoF_id);
           // * and add it to all target DoFs
-          for (size_t target_global_DoF_id : vertex_to_dof_id_map[global_vertex_id])
-            range.vector().add_to_entry(target_global_DoF_id, source_Dof_value / num_DoFS_per_vertex);
+          for (size_t target_global_DoF_id : global_vertex_id_to_global_DoF_id_map[global_vertex_id])
+            range.vector().add_to_entry(target_global_DoF_id, source_DoF_value / num_DoFS_per_vertex);
         } // if (boundary_vertices.find(global_vertex_id))
       } // loop over all local DoFs
     } // walk the grid for the second time
