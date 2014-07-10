3dca73318742fa67affdb627daf57906546c9ed3
Tobias Leibner <tobias.leibner@googlemail.com>
2014-07-10 09:11:25 +0200

    [spaces.continuouslagrange.pdelab and playground.spaces.discontinuouslagrange.pdelab] move CommunicationChooser to its own header

---
+++ dune/gdt/spaces/parallel.hh
@@ -0,0 +1,83 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_GDT_SPACES_PARALLEL_HH
+#define DUNE_GDT_SPACES_PARALLEL_HH
+
+#include <dune/stuff/common/disable_warnings.hh>
+#include <dune/common/parallel/communicator.hh>
+#include <dune/stuff/common/reenable_warnings.hh>
+
+#if HAVE_DUNE_ISTL
+#include <dune/stuff/common/disable_warnings.hh>
+#include <dune/istl/owneroverlapcopy.hh>
+#include <dune/stuff/common/reenable_warnings.hh>
+#endif
+
+#if HAVE_DUNE_PDELAB
+#include <dune/stuff/common/disable_warnings.hh>
+#include <dune/pdelab/backend/istl/parallelhelper.hh>
+#include <dune/stuff/common/reenable_warnings.hh>
+#endif
+
+#include <dune/stuff/la/container/istl.hh>
+#include <dune/stuff/common/parallel/helper.hh>
+
+namespace Dune {
+
+namespace GDT {
+namespace Spaces {
+
+template <class ViewImp,
+          bool is_parallel =
+              Dune::Stuff::UseParallelCommunication<typename ViewImp::Grid::CollectiveCommunication>::value>
+struct CommunicationChooser
+{
+  typedef Dune::Stuff::SequentialCommunication Type;
+
+  static std::shared_ptr<Type> create(const ViewImp& /*gridView*/)
+  {
+    return std::make_shared<Type>();
+  }
+
+  template <class SpaceBackend>
+  static bool prepare(const SpaceBackend& /*space_backend*/, Type& /*communicator*/)
+  {
+    return false;
+  }
+}; // struct CommunicationChooser
+
+
+#if HAVE_MPI
+
+
+template <class ViewImp>
+struct CommunicationChooser<ViewImp, true>
+{
+  typedef OwnerOverlapCopyCommunication<bigunsignedint<96>, int> Type;
+
+  static std::shared_ptr<Type> create(const ViewImp& gridView)
+  {
+    return std::make_shared<Type>(gridView.comm());
+  }
+
+  template <class Space>
+  static bool prepare(const Space& space, Type& communicator)
+  {
+    Stuff::LA::IstlRowMajorSparseMatrix<typename Space::RangeFieldType> matrix;
+    PDELab::istl::ParallelHelper<typename Space::BackendType>(space.backend(), 0)
+        .createIndexSetAndProjectForAMG(matrix.backend(), communicator);
+    return true;
+  }
+}; // struct CommunicationChooser< ..., true >
+
+
+#endif // HAVE_MPI
+
+} // namespace Spaces
+} // namespace GDT
+} // namespace Dune
+
+#endif // DUNE_GDT_SPACES_PARALLEL_HH
