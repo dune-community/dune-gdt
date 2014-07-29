0320d575b1c5a4943fc61e20f6b6c2a0a79c427a
Rene Milk <rene.milk@uni-muenster.de>
2014-07-29 14:42:16 +0200

    [tests] splits the insane grouping of operator tests over multiple files

--- tests/operators_products_localizable.cc
+++ tests/operators_products_localizable.cc
@@ -1,7 +1,74 @@
 #include "operators_products.hh"
 
+
+template< class SpaceType, class ProductType >
+struct LocalizableProduct
+{
+  typedef typename SpaceType::GridViewType          GridViewType;
+  typedef typename GridViewType::Grid               GridType;
+  typedef Dune::Stuff::Grid::Providers::Cube< GridType > GridProviderType;
+  typedef typename GridViewType::template Codim< 0 >::Entity EntityType;
+  typedef typename SpaceType::DomainFieldType DomainFieldType;
+  static const unsigned int                   dimDomain = SpaceType::dimDomain;
+  typedef typename SpaceType::RangeFieldType  RangeFieldType;
+  static const unsigned int                   dimRange = SpaceType::dimRange;
+  typedef Dune::Stuff::Functions::Expression
+      < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange > FunctionType;
+  typedef typename Dune::GDT::LocalizableProductInterface< typename ProductType::Traits > InterfaceType;
+
+  static void fulfills_interface()
+  {
+    // static tests
+    // * of the derived type
+#include <dune/stuff/common/disable_warnings.hh>
+    typedef typename ProductType::Traits        Traits;
+#include <dune/stuff/common/reenable_warnings.hh>
+    typedef typename ProductType::GridViewType  D_GridViewType;
+    typedef typename ProductType::RangeType     D_RangeType;
+    typedef typename ProductType::SourceType    D_SourceType;
+    typedef typename ProductType::FieldType     D_FieldType;
+    // * of the derived type as the interface
+    typedef typename InterfaceType::derived_type  derived_type;
+    typedef typename InterfaceType::GridViewType  I_GridViewType;
+    typedef typename InterfaceType::RangeType     I_RangeType;
+    typedef typename InterfaceType::SourceType    I_SourceType;
+    typedef typename InterfaceType::FieldType     I_FieldType;
+    static_assert(std::is_same< ProductType, derived_type >::value, "");
+    static_assert(std::is_same< D_GridViewType, I_GridViewType >::value, "");
+    static_assert(std::is_same< D_RangeType, I_RangeType >::value, "");
+    static_assert(std::is_same< D_SourceType, I_SourceType >::value, "");
+    static_assert(std::is_same< D_FieldType, D_FieldType >::value, "");
+    // prepare
+    GridProviderType grid_provider(0.0, 1.0, 4u);
+    auto& grid = grid_provider.grid();
+    const auto grid_part_view = Dune::GDT::SpaceTools::GridPartView< SpaceType >::create_leaf(grid);
+    const SpaceType space(grid_part_view);
+    const FunctionType function("x", "1.0", 0, "function", {{"1.0", "1.0", "1.0"}});
+    ProductType product(*(space.grid_view()), function, function);
+    // dynamic tests
+    // * of the derived type
+    const D_GridViewType& d_gp = product.grid_view();
+    if (&d_gp != &(*(space.grid_view()))) DUNE_THROW_COLORFULLY(Dune::Exception, "");
+    const D_RangeType& d_r = product.range();
+    if (&d_r != &function) DUNE_THROW_COLORFULLY(Dune::Exception, "");
+    const D_SourceType& d_s = product.source();
+    if (&d_s != &function) DUNE_THROW_COLORFULLY(Dune::Exception, "");
+    D_FieldType d_a = product.apply2();
+    // * of the derived type as the interface
+    InterfaceType& i_product = static_cast< InterfaceType& >(product);
+    const I_GridViewType& i_gp = i_product.grid_view();
+    if (&i_gp != &d_gp) DUNE_THROW_COLORFULLY(Dune::Exception, "");
+    const I_RangeType& i_r = i_product.range();
+    if (&i_r != &d_r) DUNE_THROW_COLORFULLY(Dune::Exception, "");
+    const I_SourceType& i_s = i_product.source();
+    if (&i_s != &d_s) DUNE_THROW_COLORFULLY(Dune::Exception, "");
+    I_FieldType i_a = i_product.apply2();
+    if (Dune::Stuff::Common::FloatCmp::ne(i_a, d_a)) DUNE_THROW_COLORFULLY(Dune::Exception, "");
+  }
+}; // struct LocalizableProduct
+
 template< class SpaceType >
-struct L2ProductOperator
+struct L2LocalizableProduct
   : public ::testing::Test
 {
   typedef typename SpaceType::GridViewType          GridViewType;
@@ -14,7 +81,7 @@ struct L2ProductOperator
   static const unsigned int                   dimRange = SpaceType::dimRange;
   typedef Dune::Stuff::Functions::Expression
       < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange > FunctionType;
-  typedef Dune::GDT::Products::L2< GridViewType > ProductType;
+  typedef Dune::GDT::Products::L2Localizable< GridViewType, FunctionType, FunctionType > ProductType;
 
   void produces_correct_results() const
   {
@@ -23,10 +90,10 @@ struct L2ProductOperator
     auto& grid = grid_provider.grid();
     const auto grid_part_view = Dune::GDT::SpaceTools::GridPartView< SpaceType >::create_leaf(grid);
     const SpaceType space(grid_part_view);
-    const ProductType l2_product_operator(*(space.grid_view()));
     // test 1 (constant)
     const FunctionType function_1("x", "1.0", 0);
-    auto l2_product = l2_product_operator.apply2(function_1, function_1);
+    ProductType l2_product_operator_1(*(space.grid_view()), function_1, function_1);
+    auto l2_product = l2_product_operator_1.apply2();
     RangeFieldType error = l2_product - RangeFieldType(1.0);
     if (error > RangeFieldType(1e-15))
       DUNE_THROW_COLORFULLY(errors_are_not_as_expected,
@@ -34,7 +101,8 @@ struct L2ProductOperator
                             << " (difference: " << std::scientific << error << ")");
     // test 2 (linear)
     const FunctionType function_2("x", "x[0] - 1.0", 1);
-    l2_product = l2_product_operator.apply2(function_2, function_2);
+    ProductType l2_product_operator_2(*(space.grid_view()), function_2, function_2);
+    l2_product = l2_product_operator_2.apply2();
     error = l2_product - RangeFieldType(1.0/3.0);
     if (error > RangeFieldType(1e-15))
       DUNE_THROW_COLORFULLY(errors_are_not_as_expected,
@@ -42,23 +110,25 @@ struct L2ProductOperator
                             << " (difference: " << std::scientific << error << ")");
     // test 3 (quadratic)
     const FunctionType function_3("x", "x[0]*x[0]", 2);
-    l2_product = l2_product_operator.apply2(function_3, function_3);
+    ProductType l2_product_operator_3(*(space.grid_view()), function_3, function_3);
+    l2_product = l2_product_operator_3.apply2();
     error = l2_product - RangeFieldType(1.0/5.0);
     if (error > RangeFieldType(1e-15))
       DUNE_THROW_COLORFULLY(errors_are_not_as_expected,
                             "They really ain't!\n" << l2_product << " vs. " << RangeFieldType(1.0/5.0)
                             << " (difference: " << std::scientific << error << ")");
-  }
+  } // ... produces_correct_results()
 
   void fulfills_interface() const
   {
-    ProductBase< SpaceType, ProductType >::fulfills_interface();
+    LocalizableProduct< SpaceType, ProductType >::fulfills_interface();
   }
-}; // L2ProductOperator
+}; // L2LocalizableProduct
+
 
 
 template< class SpaceType >
-struct H1SemiProductOperator
+struct H1SemiLocalizableProduct
   : public ::testing::Test
 {
   typedef typename SpaceType::GridViewType          GridViewType;
@@ -71,7 +141,7 @@ struct H1SemiProductOperator
   static const unsigned int                   dimRange = SpaceType::dimRange;
   typedef Dune::Stuff::Functions::Expression
       < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange > FunctionType;
-  typedef Dune::GDT::Products::H1SemiGeneric< GridViewType > ProductType;
+  typedef Dune::GDT::Products::H1SemiLocalizable< GridViewType, FunctionType, FunctionType > ProductType;
 
   void produces_correct_results() const
   {
@@ -80,10 +150,10 @@ struct H1SemiProductOperator
     auto& grid = grid_provider.grid();
     const auto grid_part_view = Dune::GDT::SpaceTools::GridPartView< SpaceType >::create_leaf(grid);
     const SpaceType space(grid_part_view);
-    const ProductType h1_semi_product_operator(*(space.grid_view()));
     // test 1 (constant)
     const FunctionType function_1("x", "fake_value", 1, "constant gradient", {{"1.0", "1.0", "1.0"}});
-    auto h1_semi_product = h1_semi_product_operator.apply2(function_1, function_1);
+    ProductType h1_semi_product_operator_1(*(space.grid_view()), function_1, function_1);
+    auto h1_semi_product = h1_semi_product_operator_1.apply2();
     RangeFieldType error = h1_semi_product - dimDomain * RangeFieldType(1.0);
     if (error > RangeFieldType(1e-15))
       DUNE_THROW_COLORFULLY(errors_are_not_as_expected,
@@ -92,7 +162,8 @@ struct H1SemiProductOperator
     // test 2 (linear)
     const FunctionType function_2("x", "fake_value", 2, "affine gradient",
                                   {{"x[0] - 1.0", "x[0] - 1.0", "x[0] - 1.0"}});
-    h1_semi_product = h1_semi_product_operator.apply2(function_2, function_2);
+    ProductType h1_semi_product_operator_2(*(space.grid_view()), function_2, function_2);
+    h1_semi_product = h1_semi_product_operator_2.apply2();
     error = h1_semi_product - dimDomain * RangeFieldType(1.0/3.0);
     if (error > RangeFieldType(1e-15))
       DUNE_THROW_COLORFULLY(errors_are_not_as_expected,
@@ -101,40 +172,43 @@ struct H1SemiProductOperator
     // test 3 (quadratic)
     const FunctionType function_3("x", "fake_value", 3, ", quadratic gradient",
                                   {{"x[0]*x[0]", "x[0]*x[0]", "x[0]*x[0]"}});
-    h1_semi_product = h1_semi_product_operator.apply2(function_3, function_3);
+    ProductType h1_semi_product_operator_3(*(space.grid_view()), function_3, function_3);
+    h1_semi_product = h1_semi_product_operator_3.apply2();
     error = h1_semi_product - dimDomain * RangeFieldType(1.0/5.0);
     if (error > RangeFieldType(1e-15))
       DUNE_THROW_COLORFULLY(errors_are_not_as_expected,
                             "They really ain't!\n" << h1_semi_product << " vs. " << dimDomain * RangeFieldType(1.0/5.0)
                             << " (difference: " << std::scientific << error << ")");
-  }
+  } // ... produces_correct_results()
 
   void fulfills_interface() const
   {
-    ProductBase< SpaceType, ProductType >::fulfills_interface();
+    LocalizableProduct< SpaceType, ProductType >::fulfills_interface();
   }
-}; // H1SemiProductOperator
+}; // H1SemiLocalizableProduct
 
 
-TYPED_TEST_CASE(L2ProductOperator, ProductOperatorSpaceTypes);
-TYPED_TEST(L2ProductOperator, fulfills_interface) {
+TYPED_TEST_CASE(L2LocalizableProduct, ProductOperatorSpaceTypes);
+TYPED_TEST(L2LocalizableProduct, fulfills_interface) {
   this->fulfills_interface();
 }
 
-TYPED_TEST_CASE(L2ProductOperator, ProductOperatorSpaceTypes);
-TYPED_TEST(L2ProductOperator, produces_correct_results) {
+TYPED_TEST_CASE(L2LocalizableProduct, ProductOperatorSpaceTypes);
+TYPED_TEST(L2LocalizableProduct, produces_correct_results) {
   this->produces_correct_results();
 }
 
-TYPED_TEST_CASE(H1SemiProductOperator, ProductOperatorSpaceTypes);
-TYPED_TEST(H1SemiProductOperator, fulfills_interface) {
+
+TYPED_TEST_CASE(H1SemiLocalizableProduct, ProductOperatorSpaceTypes);
+TYPED_TEST(H1SemiLocalizableProduct, fulfills_interface) {
   this->fulfills_interface();
 }
 
-TYPED_TEST_CASE(H1SemiProductOperator, ProductOperatorSpaceTypes);
-TYPED_TEST(H1SemiProductOperator, produces_correct_results) {
+TYPED_TEST_CASE(H1SemiLocalizableProduct, ProductOperatorSpaceTypes);
+TYPED_TEST(H1SemiLocalizableProduct, produces_correct_results) {
   this->produces_correct_results();
 }
+
 
 int main(int argc, char** argv)
 {
---
+++ tests/operators_products_projection.cc
@@ -0,0 +1,219 @@
+#include "operators_products.hh"
+
+// +-------------------------------------+
+// |  * to test the projection operators |
+// +-------------------------------------+
+
+template< class SpaceType, class ProjectionOperatorType >
+struct ProjectionOperatorBase
+{
+  typedef typename SpaceType::GridViewType          GridViewType;
+  typedef typename GridViewType::Grid               GridType;
+  typedef Dune::Stuff::Grid::Providers::Cube< GridType > GridProviderType;
+  typedef typename GridViewType::template Codim< 0 >::Entity EntityType;
+  typedef typename SpaceType::DomainFieldType DomainFieldType;
+  static const unsigned int                   dimDomain = SpaceType::dimDomain;
+  typedef typename SpaceType::RangeFieldType  RangeFieldType;
+  static const unsigned int                   dimRange = SpaceType::dimRange;
+  static const unsigned int polOrder = SpaceType::polOrder;
+  typedef Dune::Stuff::Functions::Expression
+      < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange > FunctionType;
+
+  void produces_correct_results() const
+  {
+    // prepare
+    GridProviderType grid_provider(0.0, 1.0, 4u);
+    auto& grid = grid_provider.grid();
+    const auto grid_part_view = Dune::GDT::SpaceTools::GridPartView< SpaceType >::create_leaf(grid);
+    const SpaceType space(grid_part_view);
+    const FunctionType function("x", "x[0]", 1, "function");
+    VectorType vector(space.mapper().size());
+    typedef Dune::GDT::DiscreteFunction< SpaceType, VectorType > DiscreteFunctionType;
+    DiscreteFunctionType discrete_function(space, vector, "discrete function");
+    // project
+    const ProjectionOperatorType projection_operator(*(space.grid_view()));
+    projection_operator.apply(function, discrete_function);
+    // measure error
+    const Dune::Stuff::Functions::Difference< FunctionType, DiscreteFunctionType > difference(function,
+                                                                                             discrete_function);
+    const Dune::GDT::Products::L2< GridViewType > l2_product_operator(*(space.grid_view()));
+    const auto l2_error = std::sqrt(l2_product_operator.apply2(difference, difference));
+    if (l2_error > RangeFieldType(1e-15))
+      DUNE_THROW_COLORFULLY(errors_are_not_as_expected,
+                            "They really ain't!\n" << l2_error << " vs. " << RangeFieldType(1e-15));
+  }
+}; // ProjectionOperatorType
+
+template< class SpaceType >
+struct L2ProjectionOperator
+  : public ProjectionOperatorBase< SpaceType, Dune::GDT::Operators::L2Projection< typename SpaceType::GridViewType > >
+  , public ::testing::Test
+{};
+
+template< class SpaceType >
+struct LagrangeProjectionOperator
+  : public ProjectionOperatorBase< SpaceType,
+                                   Dune::GDT::Operators::LagrangeProjection< typename SpaceType::GridViewType > >
+  , public ::testing::Test
+{};
+
+template< class SpaceType >
+struct ProjectionOperator
+  : public ProjectionOperatorBase< SpaceType,
+                                   Dune::GDT::Operators::Projection< typename SpaceType::GridViewType > >
+  , public ::testing::Test
+{};
+
+template< class SpaceType >
+struct DirichletProjectionOperator
+  : public ::testing::Test
+{
+  typedef typename SpaceType::GridViewType          GridViewType;
+  typedef typename GridViewType::Grid               GridType;
+  typedef Dune::Stuff::Grid::Providers::Cube< GridType > GridProviderType;
+  typedef typename GridViewType::template Codim< 0 >::Entity EntityType;
+  typedef typename SpaceType::DomainFieldType DomainFieldType;
+  static const unsigned int                   dimDomain = SpaceType::dimDomain;
+  typedef Dune::FieldVector< DomainFieldType, dimDomain > DomainType;
+  typedef typename SpaceType::RangeFieldType  RangeFieldType;
+  static const unsigned int                   dimRange = SpaceType::dimRange;
+  static const unsigned int polOrder = SpaceType::polOrder;
+  typedef Dune::Stuff::Functions::Expression
+      < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange > FunctionType;
+
+  void produces_correct_results() const
+  {
+    // prepare
+    GridProviderType grid_provider(0.0, 1.0, 1u); // this has to be 1, otherwise the projection does not equal
+    auto& grid = grid_provider.grid();             // x[0] any more!
+    const auto grid_part_view = Dune::GDT::SpaceTools::GridPartView< SpaceType >::create_leaf(grid);
+    const SpaceType space(grid_part_view);
+    DomainType dirichlet_normal(0);
+    dirichlet_normal[0] = DomainFieldType(1);
+    const Dune::Stuff::Grid::BoundaryInfos::NormalBased< typename GridViewType::Intersection >
+        boundary_info(false, {dirichlet_normal});
+    const FunctionType function("x", "x[0]", 1, "function");
+    VectorType vector(space.mapper().size());
+    typedef Dune::GDT::DiscreteFunction< SpaceType, VectorType > DiscreteFunctionType;
+    DiscreteFunctionType discrete_function(space, vector, "discrete function");
+    // project
+    const Dune::GDT::Operators::DirichletProjection< GridViewType > projection_operator(*(space.grid_view()),
+                                                                                       boundary_info);
+    projection_operator.apply(function, discrete_function);
+    // measure error
+    const Dune::Stuff::Functions::Difference< FunctionType, DiscreteFunctionType > difference(function,
+                                                                                             discrete_function);
+    const Dune::GDT::Products::L2< GridViewType > l2_product_operator(*(space.grid_view()));
+    const auto l2_error = std::sqrt(l2_product_operator.apply2(difference, difference));
+    if (l2_error > RangeFieldType(1e-15))
+      DUNE_THROW_COLORFULLY(errors_are_not_as_expected,
+                            "They really ain't!\n" << l2_error << " vs. " << RangeFieldType(1e-15));
+  }
+}; // DirichletProjectionOperator
+
+
+// +-------------------------------------------------------+
+// |  * arguments for the projection operator test structs |
+// +-------------------------------------------------------+
+
+#define L2_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID \
+    Dune::GDT::Spaces::DiscontinuousLagrange::FemBased< AluConform2dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::DiscontinuousLagrange::FemBased< AluSimplex2dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::DiscontinuousLagrange::FemBased< AluSimplex3dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::DiscontinuousLagrange::FemBased< AluConform2dLeafGridPartType, 2, double, 1 > \
+  , Dune::GDT::Spaces::DiscontinuousLagrange::FemBased< AluSimplex2dLeafGridPartType, 2, double, 1 > \
+  , Dune::GDT::Spaces::DiscontinuousLagrange::FemBased< AluSimplex3dLeafGridPartType, 2, double, 1 >
+
+#if HAVE_ALUGRID
+typedef testing::Types<
+                        L2_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID
+                      > L2ProjectionOperatorSpaceTypes;
+#endif // HAVE_ALUGRID
+
+#define LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES \
+    Dune::GDT::Spaces::ContinuousLagrange::FemBased< S1dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< S2dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< S3dLeafGridPartType, 1, double, 1 > \
+  \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< Yasp1dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< Yasp2dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< Yasp3dLeafGridPartType, 1, double, 1 >
+
+#define LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID \
+    Dune::GDT::Spaces::ContinuousLagrange::FemBased< AluConform2dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< AluSimplex2dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< AluSimplex3dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< AluCube3dLeafGridPartType, 1, double, 1 > \
+  \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< AluConform2dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< AluSimplex2dLeafGridPartType, 1, double, 1 > \
+  , Dune::GDT::Spaces::ContinuousLagrange::FemBased< AluSimplex3dLeafGridPartType, 1, double, 1 >
+
+
+typedef testing::Types<
+                        LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES
+#if HAVE_ALUGRID
+                      , LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID
+#endif
+                      > LagrangeProjectionOperatorSpaceTypes;
+
+typedef testing::Types<
+                        LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES
+#if HAVE_ALUGRID
+                      , LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID
+                      , L2_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID
+#endif
+                      > ProjectionOperatorSpaceTypes;
+
+typedef testing::Types<
+                        LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES
+#if HAVE_ALUGRID
+                      , LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID
+#endif
+                      > DirichletProjectionOperatorSpaceTypes;
+
+#undef L2_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID
+#undef LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES
+#undef LAGRANGE_PROJECTION_OPERATOR_SPACE_TYPES_ALUGRID
+
+
+
+#if HAVE_ALUGRID
+TYPED_TEST_CASE(L2ProjectionOperator, L2ProjectionOperatorSpaceTypes);
+TYPED_TEST(L2ProjectionOperator, produces_correct_results) {
+ this->produces_correct_results();
+}
+#endif // HAVE_ALUGRID
+
+TYPED_TEST_CASE(LagrangeProjectionOperator, LagrangeProjectionOperatorSpaceTypes);
+TYPED_TEST(LagrangeProjectionOperator, produces_correct_results) {
+ this->produces_correct_results();
+}
+
+TYPED_TEST_CASE(ProjectionOperator, ProjectionOperatorSpaceTypes);
+TYPED_TEST(ProjectionOperator, produces_correct_results) {
+ this->produces_correct_results();
+}
+
+TYPED_TEST_CASE(DirichletProjectionOperator, DirichletProjectionOperatorSpaceTypes);
+TYPED_TEST(DirichletProjectionOperator, produces_correct_results) {
+ this->produces_correct_results();
+}
+
+
+int main(int argc, char** argv)
+{
+  try {
+    test_init(argc, argv);
+    return RUN_ALL_TESTS();
+  } catch (Dune::Exception& e) {
+    std::cerr << "\nDune reported error: " << e.what() << std::endl;
+    std::abort();
+  } catch (std::exception& e) {
+    std::cerr << "\n" << e.what() << std::endl;
+    std::abort();
+  } catch (...) {
+    std::cerr << "Unknown exception thrown!" << std::endl;
+    std::abort();
+  } // try
+}
