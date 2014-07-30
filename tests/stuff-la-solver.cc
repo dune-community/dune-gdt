3d331c06b24656f1c3ac4df9c8de377d93d49f50
Rene Milk <rene.milk@uni-muenster.de>
2014-07-30 15:15:29 +0200

    [tests] removes useless/harmful main code

--- tests/stuff-la-solver.cc
+++ tests/stuff-la-solver.cc
@@ -387,19 +387,8 @@ TYPED_TEST(LargeEllipticSystems, produces_correct_results) {
 
 int main(int argc, char** argv)
 {
-  try {
-    test_init(argc, argv);
-    return RUN_ALL_TESTS();
-  } catch (Dune::Exception& e) {
-    std::cerr << "\nDune reported error: " << e.what() << std::endl;
-    std::abort();
-  } catch (std::exception& e) {
-    std::cerr << "\n" << e.what() << std::endl;
-    std::abort();
-  } catch (...) {
-    std::cerr << "Unknown exception thrown!" << std::endl;
-    std::abort();
-  } // try
+  test_init(argc, argv);
+  return RUN_ALL_TESTS();
 }
 
 #else // ENABLE_ALUGRID
