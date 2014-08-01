4e4c0f68b6050dfb6c97ef9f20df45cf1dbe8d4c
Tobias Leibner <tobias.leibner@googlemail.com>
2014-08-01 13:08:59 +0200

    replace ConfigTree with ConfigContainer

--- dune/gdt/operators/base.hh
+++ dune/gdt/operators/base.hh
@@ -97,7 +97,7 @@ public:
     return LinearSolverType::options();
   }
 
-  static Stuff::Common::ConfigTree invert_options(const std::string& type)
+  static Stuff::Common::ConfigContainer invert_options(const std::string& type)
   {
     return LinearSolverType::options(type);
   }
@@ -105,7 +105,7 @@ public:
   template< class R, class S >
   void apply_inverse(const Stuff::LA::VectorInterface< R >& range,
                      Stuff::LA::VectorInterface< S >& source,
-                     const Stuff::Common::ConfigTree& opts)
+                     const Stuff::Common::ConfigContainer& opts)
   {
     assemble();
     LinearSolverType(matrix, source_space_.communicator()).apply(range.as_imp(), source.as_imp(), opts);
