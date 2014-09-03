87cebe7a15084f685e07918ce7d7acae2b480fea
Tobias Leibner <tobias.leibner@googlemail.com>
2014-09-03 13:49:34 +0200

    [localevaluation.interface] several changes - remove template parameters LocalfunctionTuple, E, D, d and get appropiate types from Traits - require implementation of method localfunctions from users - update to use stuff/common/crtp.hh

--- dune/gdt/localevaluation/interface.hh
+++ dune/gdt/localevaluation/interface.hh
@@ -12,9 +12,9 @@
 #include <dune/common/dynmatrix.hh>
 #include <dune/common/fvector.hh>
 #include <dune/stuff/common/reenable_warnings.hh>
-#include <dune/common/bartonnackmanifcheck.hh>
 
 #include <dune/stuff/functions/interfaces.hh>
+#include <dune/stuff/common/crtp.hh>
 
 namespace Dune {
 namespace GDT {
@@ -25,33 +25,11 @@ namespace LocalEvaluation {
  *  \brief  Interface for local evaluations that depend on a codim 0 entity.
  *  \tparam numArguments  The number of local bases.
  *  \note   All evaluations have to be copyable!
- *
- *  A derived class has to provide a method
-\code
-template< class EntityType >
-typename LocalfunctionTuple< EntityType >::Type localFunctions(const EntityType& entity) const
-{
-  ...
-}
-\endcode
- *  which returns the inducing local functions which are needed for one entity
- * and a class
-\code
-  template< class EntityType >
-  class LocalfunctionTuple
-  {
-  public:
-    typedef ... Type;
-  };
-\endcode
- * which defines the return type of this method. Whatever localFunctions() returns will be given to order() and
- * evaluate().
  */
 template <class Traits, int numArguments>
 class Codim0Interface
 {
-public:
-  Codim0Interface() = delete;
+  static_assert(AlwaysFalse<Traits>::value, "There is no interface for this numArguments!");
 };
 
 
@@ -59,51 +37,48 @@ public:
  *  \brief  Interface for unary codim 0 evaluations.
  */
 template <class Traits>
-class Codim0Interface<Traits, 1>
+class Codim0Interface<Traits, 1> : public Stuff::CRTPInterface<Codim0Interface<Traits, 1>, Traits>
 {
 public:
   typedef typename Traits::derived_type derived_type;
+  typedef typename Traits::EntityType EntityType;
+  typedef typename Traits::LocalfunctionTupleType LocalfunctionTupleType;
+  typedef typename Traits::DomainFieldType DomainFieldType;
+  static const unsigned int dimDomain = Traits::dimDomain;
+
+  LocalfunctionTupleType localFunctions(const EntityType& entity) const
+  {
+    CHECK_CRTP(this->as_imp(*this).localFunctions(entity));
+    return this->as_imp(*this).localFunctions(entity);
+  }
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
-   *  \tparam E   EntityType
-   *  \tparam D   DomainFieldType
-   *  \tparam d   dimDomain
    *  \tparam R   RangeFieldType
    *  \tparam r   dimRange of the testBase
    *  \tparam rC  dimRangeRows of the testBase
    */
-  template <class LocalFunctionTuple, class E, class D, int d, class R, int r, int rC>
-  size_t order(const LocalFunctionTuple& localFunctions,
-               const Stuff::LocalfunctionSetInterface<E, D, d, R, r, rC>& testBase) const
+  template <class R, int r, int rC>
+  size_t order(const LocalfunctionTupleType& localFunctions,
+               const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, r, rC>& testBase) const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctions, testBase));
-    return asImp().order(localFunctions, testBase);
+    CHECK_CRTP(this->as_imp(*this).order(localFunctions, testBase));
+    return this->as_imp(*this).order(localFunctions, testBase);
   }
 
   /**
    *  \brief  Computes a unary codim 0 evaluation.
-   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
-   *  \tparam E   EntityType
-   *  \tparam D   DomainFieldType
-   *  \tparam d   dimDomain
    *  \tparam R   RangeFieldType
    *  \tparam r   dimRange of the testBase
    *  \tparam rC  dimRangeRows of the testBase
    *  \attention ret is assumed to be zero!
    */
-  template <class LocalFunctionTuple, class E, class D, int d, class R, int r, int rC>
-  void evaluate(const LocalFunctionTuple& localFunctions,
-                const Stuff::LocalfunctionSetInterface<E, D, d, R, r, rC>& testBase,
-                const Dune::FieldVector<D, d>& localPoint, Dune::DynamicVector<R>& ret) const
+  template <class R, int r, int rC>
+  void evaluate(const LocalfunctionTupleType& localFunctions,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, r, rC>& testBase,
+                const Dune::FieldVector<DomainFieldType, dimDomain>& localPoint, Dune::DynamicVector<R>& ret) const
   {
-    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().evaluate(localFunctions, testBase, localPoint, ret));
-  }
-
-  const derived_type& asImp() const
-  {
-    return static_cast<const derived_type&>(*this);
+    CHECK_AND_CALL_CRTP(this->as_imp(*this).evaluate(localFunctions, testBase, localPoint, ret));
   }
 }; // class Codim0Interface< Traits, 1 >
 
@@ -112,53 +87,45 @@ public:
  *  \brief  Interface for binary codim 0 evaluations.
  **/
 template <class Traits>
-class Codim0Interface<Traits, 2>
+class Codim0Interface<Traits, 2> : public Stuff::CRTPInterface<Codim0Interface<Traits, 2>, Traits>
 {
 public:
   typedef typename Traits::derived_type derived_type;
+  typedef typename Traits::EntityType EntityType;
+  typedef typename Traits::LocalfunctionTupleType LocalfunctionTupleType;
+  typedef typename Traits::DomainFieldType DomainFieldType;
+  static const unsigned int dimDomain = Traits::dimDomain;
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
-   *  \tparam E       EntityType
-   *  \tparam D       DomainFieldType
-   *  \tparam d       dimDomain
    *  \tparam R       RangeFieldType
    *  \tparam r{T,A}  dimRange of the {testBase,ansatzBase}
    *  \tparam rC{T,A} dimRangeRows of the {testBase,ansatzBase}
    */
-  template <class LocalFunctionTuple, class E, class D, int d, class R, int rT, int rCT, int rA, int rCA>
-  size_t order(const LocalFunctionTuple& localFunctions,
-               const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBase,
-               const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBase) const
+  template <class R, int rT, int rCT, int rA, int rCA>
+  size_t
+  order(const LocalfunctionTupleType& localFunctions,
+        const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rT, rCT>& testBase,
+        const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rA, rCA>& ansatzBase) const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctions, testBase, ansatzBase));
-    return asImp().order(localFunctions, testBase, ansatzBase);
+    CHECK_CRTP(this->as_imp(*this).order(localFunctions, testBase, ansatzBase));
+    return this->as_imp(*this).order(localFunctions, testBase, ansatzBase);
   }
 
   /**
    *  \brief  Computes a binary codim 0 evaluation.
-   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
-   *  \tparam E         EntityType
-   *  \tparam D         DomainFieldType
-   *  \tparam d         dimDomain
    *  \tparam R         RangeFieldType
    *  \tparam r{L,T,A}  dimRange of the {localFunction,testBase,ansatzBase}
    *  \tparam rC{L,T,A} dimRangeRows of the {localFunction,testBase,ansatzBase}
    *  \attention ret is assumed to be zero!
    */
-  template <class LocalFunctionTuple, class E, class D, int d, class R, int rT, int rCT, int rA, int rCA>
-  void evaluate(const LocalFunctionTuple& localFunctions,
-                const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBase,
-                const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBase,
-                const Dune::FieldVector<D, d>& localPoint, Dune::DynamicMatrix<R>& ret) const
+  template <class R, int rT, int rCT, int rA, int rCA>
+  void evaluate(const LocalfunctionTupleType& localFunctions,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rT, rCT>& testBase,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rA, rCA>& ansatzBase,
+                const Dune::FieldVector<DomainFieldType, dimDomain>& localPoint, Dune::DynamicMatrix<R>& ret) const
   {
-    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().evaluate(localFunctions, testBase, ansatzBase, localPoint, ret));
-  }
-
-  const derived_type& asImp() const
-  {
-    return static_cast<const derived_type&>(*this);
+    CHECK_AND_CALL_CRTP(this->as_imp(*this).evaluate(localFunctions, testBase, ansatzBase, localPoint, ret));
   }
 }; // class Codim0Interface< Traits, 2 >
 
@@ -167,23 +134,11 @@ public:
  *  \brief  Interface for local evaluations that depend on an intersection.
  *  \tparam numArguments  The number of local bases.
  *  \note   All evaluations have to be copyable!
- *
- *  A derived class has to provide a method
-\code
-template< class EntityType >
-std::tuple< ... > localFunctions(const EntityType& entity) const
-{
-  ...
-}
-\endcode
- *  which returns the inducing local functions which are needed for one entity. Whatever this method returns will be
- *  given to order() and evaluate();
  */
 template <class Traits, int numArguments>
 class Codim1Interface
 {
-public:
-  Codim1Interface() = delete;
+  static_assert(AlwaysFalse<Traits>::value, "There is no interface for this numArguments!");
 };
 
 
@@ -191,53 +146,44 @@ public:
  *  \brief  Interface for unary codim 1 evaluations.
  */
 template <class Traits>
-class Codim1Interface<Traits, 1>
+class Codim1Interface<Traits, 1> : public Stuff::CRTPInterface<Codim1Interface<Traits, 1>, Traits>
 {
 public:
   typedef typename Traits::derived_type derived_type;
+  typedef typename Traits::EntityType EntityType;
+  typedef typename Traits::LocalfunctionTupleType LocalfunctionTupleType;
+  typedef typename Traits::DomainFieldType DomainFieldType;
+  static const unsigned int dimDomain = Traits::dimDomain;
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
-   *  \tparam E   EntityType
-   *  \tparam D   DomainFieldType
-   *  \tparam d   dimDomain
    *  \tparam R   RangeFieldType
    *  \tparam r   dimRange of the testBase
    *  \tparam rC  dimRangeRows of the testBase
    */
-  template <class LocalFunctionTuple, class E, class D, int d, class R, int r, int rC>
-  size_t order(const LocalFunctionTuple& localFunctions,
-               const Stuff::LocalfunctionSetInterface<E, D, d, R, r, rC>& testBase) const
+  template <class R, int r, int rC>
+  size_t order(const LocalfunctionTupleType& localFunctions,
+               const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, r, rC>& testBase) const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctions, testBase));
-    return asImp().order(localFunctions, testBase);
+    CHECK_CRTP(this->as_imp(*this).order(localFunctions, testBase));
+    return this->as_imp(*this).order(localFunctions, testBase);
   }
 
   /**
    *  \brief  Computes a binary codim 1 evaluation.
-   *  \tparam LocalFunctionTuple Type of the localFunction container that is returned by localFunctions()
-   *  \tparam E                   EntityType
    *  \tparam IntersectionType    A model of Dune::Intersection< ... >
-   *  \tparam D                   DomainFieldType
-   *  \tparam d                   dimDomain
    *  \tparam R                   RangeFieldType
    *  \tparam r                   dimRange of the testBase
    *  \tparam rC                  dimRangeRows of the testBase
    *  \attention ret is assumed to be zero!
    */
-  template <class LocalFunctionTuple, class E, class IntersectionType, class D, int d, class R, int r, int rC>
-  void evaluate(const LocalFunctionTuple& localFunctions,
-                const Stuff::LocalfunctionSetInterface<E, D, d, R, r, rC>& testBase,
-                const IntersectionType& intersection, const Dune::FieldVector<D, d - 1>& localPoint,
-                Dune::DynamicVector<R>& ret) const
+  template <class IntersectionType, class R, int r, int rC>
+  void evaluate(const LocalfunctionTupleType& localFunctions,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, r, rC>& testBase,
+                const IntersectionType& intersection,
+                const Dune::FieldVector<DomainFieldType, dimDomain - 1>& localPoint, Dune::DynamicVector<R>& ret) const
   {
-    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().evaluate(localFunctions, testBase, intersection, localPoint, ret));
-  }
-
-  const derived_type& asImp() const
-  {
-    return static_cast<const derived_type&>(*this);
+    CHECK_AND_CALL_CRTP(this->as_imp(*this).evaluate(localFunctions, testBase, intersection, localPoint, ret));
   }
 }; // class class Codim1Interface< Traits, 1 >
 
@@ -246,57 +192,48 @@ public:
  *  \brief  Interface for binary codim 1 evaluations.
  */
 template <class Traits>
-class Codim1Interface<Traits, 2>
+class Codim1Interface<Traits, 2> : public Stuff::CRTPInterface<Codim1Interface<Traits, 2>, Traits>
 {
 public:
   typedef typename Traits::derived_type derived_type;
+  typedef typename Traits::EntityType EntityType;
+  typedef typename Traits::LocalfunctionTupleType LocalfunctionTupleType;
+  typedef typename Traits::DomainFieldType DomainFieldType;
+  static const unsigned int dimDomain = Traits::dimDomain;
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTuple  Type of the container that is returned by localFunctions()
-   *  \tparam E                   EntityType
-   *  \tparam D                   DomainFieldType
-   *  \tparam d                   dimDomain
    *  \tparam R                   RangeFieldType
    *  \tparam r{T,A}              dimRange of the {testBase,ansatzBase}
    *  \tparam rC{T,A}             dimRangeRows of the {testBase,ansatzBase}
    */
-  template <class LocalFunctionTuple, class E, class D, int d, class R, int rT, int rCT, int rA, int rCA>
-  size_t order(const LocalFunctionTuple& localFunctions,
-               const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBase,
-               const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBase) const
+  template <class R, int rT, int rCT, int rA, int rCA>
+  size_t
+  order(const LocalfunctionTupleType& localFunctions,
+        const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rT, rCT>& testBase,
+        const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rA, rCA>& ansatzBase) const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().order(localFunctions, testBase, ansatzBase));
-    return asImp().order(localFunctions, testBase, ansatzBase);
+    CHECK_CRTP(this->as_imp(*this).order(localFunctions, testBase, ansatzBase));
+    return this->as_imp(*this).order(localFunctions, testBase, ansatzBase);
   }
 
   /**
    *  \brief  Computes a binary codim 1 evaluation.
-   *  \tparam LocalFunctionTuple  Type of the container that is returned by localFunctions()
-   *  \tparam E                   EntityType
    *  \tparam IntersectionType    A model of Dune::Intersection< ... >
-   *  \tparam D                   DomainFieldType
-   *  \tparam d                   dimDomain
    *  \tparam R                   RangeFieldType
    *  \tparam r{T,A}              dimRange of the {testBase*,ansatzBase*}
    *  \tparam rC{T,A}             dimRangeRows of the {testBase*,ansatzBase*}
    *  \attention ret is assumed to be zero!
    */
-  template <class LocalFunctionTuple, class E, class IntersectionType, class D, int d, class R, int rT, int rCT, int rA,
-            int rCA>
-  void evaluate(const LocalFunctionTuple& localFunctions,
-                const Stuff::LocalfunctionSetInterface<E, D, d, R, rT, rCT>& testBase,
-                const Stuff::LocalfunctionSetInterface<E, D, d, R, rA, rCA>& ansatzBase,
-                const IntersectionType& intersection, const Dune::FieldVector<D, d - 1>& localPoint,
-                Dune::DynamicMatrix<R>& ret) const
+  template <class IntersectionType, class R, int rT, int rCT, int rA, int rCA>
+  void evaluate(const LocalfunctionTupleType& localFunctions,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rT, rCT>& testBase,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rA, rCA>& ansatzBase,
+                const IntersectionType& intersection,
+                const Dune::FieldVector<DomainFieldType, dimDomain - 1>& localPoint, Dune::DynamicMatrix<R>& ret) const
   {
-    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(
-        asImp().evaluate(localFunctions, testBase, ansatzBase, intersection, localPoint, ret));
-  }
-
-  const derived_type& asImp() const
-  {
-    return static_cast<const derived_type&>(*this);
+    CHECK_AND_CALL_CRTP(
+        this->as_imp(*this).evaluate(localFunctions, testBase, ansatzBase, intersection, localPoint, ret));
   }
 }; // class class Codim1Interface< Traits, 2 >
 
