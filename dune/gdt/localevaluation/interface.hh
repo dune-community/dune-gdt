33b4d26635db2e36fd547868d82c7c18ab73b4e4
Tobias Leibner <tobias.leibner@googlemail.com>
2014-09-04 18:43:26 +0200

    [localevaluation.interface] update codim1_4 interface, refs #3 - assume EntityType and NeighbourEntityType are the same - same for LocalfunctionTuples - remove some template parameters and get types from Localfunction instead

--- dune/gdt/localevaluation/interface.hh
+++ dune/gdt/localevaluation/interface.hh
@@ -248,79 +248,77 @@ public:
  */
 template< class Traits >
 class Codim1Interface< Traits, 4 >
-    : public Stuff::CRTPInterface< Codim1Interface< Traits, 4 >, Traits >
+  : public Stuff::CRTPInterface< Codim1Interface< Traits, 4 >, Traits >
 {
 public:
-  typedef typename Traits::derived_type derived_type;
+  typedef typename Traits::derived_type           derived_type;
+  typedef typename Traits::EntityType             EntityType;
+  typedef typename Traits::LocalfunctionTupleType LocalfunctionTupleType;
+  typedef typename Traits::DomainFieldType        DomainFieldType;
+  static const unsigned int dimDomain = Traits::dimDomain;
 
   /**
    *  \brief  Computes the needed integration order.
-   *  \tparam LocalFunctionTupleEn  Type of the container that is returned by localFunctions(entity)
-   *  \tparam LocalFunctionTupleNe  Type of the container that is returned by localFunctions(neighbour)
-   *  \tparam E                     EntityType
-   *  \tparam N                     NeighbourEntityType
-   *  \tparam D                     DomainFieldType
-   *  \tparam d                     dimDomain
    *  \tparam R                     RangeFieldType
    *  \tparam r{T,A}                dimRange of the {testBase*,ansatzBase*}
    *  \tparam rC{T,A}               dimRangeRows of the {testBase*,ansatzBase*}
    */
-  template< class LocalFunctionTupleEn, class LocalFunctionTupleNe, class E, class N,
-            class D, int d, class R, int rT, int rCT, int rA, int rCA >
-  size_t order(const LocalFunctionTupleEn& localFunctionsEntity,
-               const LocalFunctionTupleNe& localFunctionsNeighbor,
-               const Stuff::LocalfunctionSetInterface< E, D, d, R, rT, rCT >& testBaseEntity,
-               const Stuff::LocalfunctionSetInterface< E, D, d, R, rA, rCA >& ansatzBaseEntity,
-               const Stuff::LocalfunctionSetInterface< N, D, d, R, rT, rCT >& testBaseNeighbor,
-               const Stuff::LocalfunctionSetInterface< N, D, d, R, rA, rCA >& ansatzBaseNeighbor) const
+  template< class R, int rT, int rCT, int rA, int rCA >
+  size_t order(const LocalfunctionTupleType localFunctionsEntity,
+               const LocalfunctionTupleType localFunctionsNeighbor,
+               const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rT,
+                                                       rCT >& testBaseEntity,
+               const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rA,
+                                                       rCA >& ansatzBaseEntity,
+               const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rT,
+                                                       rCT >& testBaseNeighbor,
+               const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rA,
+                                                       rCA >& ansatzBaseNeighbor) const
   {
     CHECK_CRTP(this->as_imp(*this).order(localFunctionsEntity, localFunctionsNeighbor,
-                                                 testBaseEntity, ansatzBaseEntity,
-                                                 testBaseNeighbor, ansatzBaseNeighbor));
+                                         testBaseEntity, ansatzBaseEntity,
+                                         testBaseNeighbor, ansatzBaseNeighbor));
     return this->as_imp(*this).order(localFunctionsEntity, localFunctionsNeighbor,
-                         testBaseEntity, ansatzBaseEntity,
-                         testBaseNeighbor, ansatzBaseNeighbor);
+                                     testBaseEntity, ansatzBaseEntity,
+                                     testBaseNeighbor, ansatzBaseNeighbor);
   }
 
   /**
    *  \brief  Computes a quaternary codim 1 evaluation.
-   *  \tparam LocalFunctionTupleEn  Type of the container that is returned by localFunctions(entity)
-   *  \tparam LocalFunctionTupleNe  Type of the container that is returned by localFunctions(neighbour)
-   *  \tparam E                     EntityType
-   *  \tparam N                     NeighbourEntityType
    *  \tparam IntersectionType      A model of Dune::Intersection< ... >
-   *  \tparam D                     DomainFieldType
-   *  \tparam d                     dimDomain
    *  \tparam R                     RangeFieldType
    *  \tparam r{T,A}                dimRange of the {testBase*,ansatzBase*}
    *  \tparam rC{T,A}               dimRangeRows of the {testBase*,ansatzBase*}
    *  \attention entityEntityRet, entityEntityRet, entityEntityRet and neighborEntityRet are assumed to be zero!
    */
-  template< class LocalFunctionTupleEn, class LocalFunctionTupleNe, class E, class N,
-            class IntersectionType, class D, int d, class R, int rT, int rCT, int rA, int rCA >
-  void evaluate(const LocalFunctionTupleEn& localFunctionsEntity,
-                const LocalFunctionTupleNe& localFunctionsNeighbor,
-                const Stuff::LocalfunctionSetInterface< E, D, d, R, rT, rCT >& testBaseEntity,
-                const Stuff::LocalfunctionSetInterface< E, D, d, R, rA, rCA >& ansatzBaseEntity,
-                const Stuff::LocalfunctionSetInterface< N, D, d, R, rT, rCT >& testBaseNeighbor,
-                const Stuff::LocalfunctionSetInterface< N, D, d, R, rA, rCA >& ansatzBaseNeighbor,
+  template< class IntersectionType, class R, int rT, int rCT, int rA, int rCA >
+  void evaluate(const LocalfunctionTupleType& localFunctionsEntity,
+                const LocalfunctionTupleType& localFunctionsNeighbor,
+                const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rT,
+                                                        rCT >& testBaseEntity,
+                const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rA,
+                                                        rCA >& ansatzBaseEntity,
+                const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rT,
+                                                        rCT >& testBaseNeighbor,
+                const Stuff::LocalfunctionSetInterface< EntityType, DomainFieldType, dimDomain, R, rA,
+                                                        rCA >& ansatzBaseNeighbor,
                 const IntersectionType& intersection,
-                const Dune::FieldVector< D, d - 1 >& localPoint,
+                const Dune::FieldVector< DomainFieldType, dimDomain - 1 >& localPoint,
                 Dune::DynamicMatrix< R >& entityEntityRet,
                 Dune::DynamicMatrix< R >& neighborNeighborRet,
                 Dune::DynamicMatrix< R >& entityNeighborRet,
                 Dune::DynamicMatrix< R >& neighborEntityRet) const
   {
     CHECK_AND_CALL_CRTP(this->as_imp(*this).evaluate(localFunctionsEntity,
-                                                             localFunctionsNeighbor,
-                                                             testBaseEntity, ansatzBaseEntity,
-                                                             testBaseNeighbor, ansatzBaseNeighbor,
-                                                             intersection,
-                                                             localPoint,
-                                                             entityEntityRet, neighborNeighborRet,
-                                                             entityNeighborRet, neighborEntityRet));
+                                                     localFunctionsNeighbor,
+                                                     testBaseEntity, ansatzBaseEntity,
+                                                     testBaseNeighbor, ansatzBaseNeighbor,
+                                                     intersection,
+                                                     localPoint,
+                                                     entityEntityRet, neighborNeighborRet,
+                                                     entityNeighborRet, neighborEntityRet));
   }
-}; // class class Codim1Interface< Traits, 4 >
+}; // class Codim1Interface< Traits, 4 >
 
 
 } // namespace LocalEvaluation
