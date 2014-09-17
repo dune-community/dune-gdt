57bb5fe1c7f062315ac05a84e7339877c62f77dc
Felix Schindler <felix.schindler@wwu.de>
2014-09-17 22:30:58 +0200

    [assembler] put ApplyOn back to functors

--- dune/gdt/assembler/functors.hh
+++ dune/gdt/assembler/functors.hh
@@ -3,10 +3,12 @@
 
 #include <dune/stuff/grid/entity.hh>
 #include <dune/stuff/grid/intersection.hh>
+#include <dune/stuff/grid/boundaryinfo.hh>
 
 namespace Dune {
 namespace GDT {
 namespace Functor {
+
 
 template <class GridViewImp>
 class Codim0
@@ -84,6 +86,232 @@ public:
 
 
 } // namespace Functor
+namespace ApplyOn {
+
+
+/**
+ *  \brief Interface for functors to tell on which entity to apply.
+ *
+ *  The derived class has to provide a method with the following signature:
+ *  \code
+virtual bool apply_on(const GridViewType& grid_view, const EntityType& entity) const
+{
+  ...
+}
+\endcode
+ */
+template <class GridViewImp>
+class WhichEntity
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Entity<GridViewType>::Type EntityType;
+
+  virtual ~WhichEntity()
+  {
+  }
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/, const EntityType& /*entity*/) const = 0;
+}; // class WhichEntity
+
+
+/**
+ *  \brief Selects all entities.
+ */
+template <class GridViewImp>
+class AllEntities : public WhichEntity<GridViewImp>
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Entity<GridViewType>::Type EntityType;
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/, const EntityType& /*entity*/) const DS_OVERRIDE DS_FINAL
+  {
+    return true;
+  }
+}; // class AllEntities
+
+
+/**
+ *  \brief Selects entities which have a boundary intersection.
+ */
+template <class GridViewImp>
+class BoundaryEntities : public WhichEntity<GridViewImp>
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Entity<GridViewType>::Type EntityType;
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/, const EntityType& entity) const DS_OVERRIDE DS_FINAL
+  {
+    return entity.hasBoundaryIntersections();
+  }
+}; // class BoundaryEntities
+
+
+/**
+ *  \brief Interface for functors to tell on which intersection to apply.
+ *
+ *  The derived class has to provide a method with the following signature:
+ *  \code
+virtual bool apply_on(const GridViewType& grid_view, const IntersectionType& intersection) const
+{
+  ...
+}
+\endcode
+ */
+template <class GridViewImp>
+class WhichIntersection
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Intersection<GridViewType>::Type IntersectionType;
+
+  virtual ~WhichIntersection<GridViewImp>()
+  {
+  }
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/, const IntersectionType& /*intersection*/) const = 0;
+}; // class WhichIntersection< GridViewImp >
+
+
+/**
+ *  \brief Selects all intersections.
+ */
+template <class GridViewImp>
+class AllIntersections : public WhichIntersection<GridViewImp>
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Intersection<GridViewType>::Type IntersectionType;
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/,
+                        const IntersectionType& /*intersection*/) const DS_OVERRIDE DS_FINAL
+  {
+    return true;
+  }
+}; // class AllIntersections
+
+
+/**
+ *  \brief Selects each inner intersection.
+ *
+ *  To decide if this in an inner intersection,
+\code
+intersection.neighbor() && !intersection.boundary()
+\endcode
+ *  is used.
+ */
+template <class GridViewImp>
+class InnerIntersections : public WhichIntersection<GridViewImp>
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Intersection<GridViewType>::Type IntersectionType;
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/,
+                        const IntersectionType& intersection) const DS_OVERRIDE DS_FINAL
+  {
+    return intersection.neighbor() && !intersection.boundary();
+  }
+}; // class InnerIntersections
+
+
+/**
+ *  \brief Selects each inner intersection only once.
+ *
+ *  To decide if this in an inner intersection,
+\code
+intersection.neighbor() && !intersection.boundary()
+\endcode
+ *  is used, and true is returned, if the index of the inside() entity is smaller than the index of the outside()
+ *  entity.
+ */
+template <class GridViewImp>
+class InnerIntersectionsPrimally : public WhichIntersection<GridViewImp>
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Intersection<GridViewType>::Type IntersectionType;
+
+  virtual bool apply_on(const GridViewType& grid_view, const IntersectionType& intersection) const DS_OVERRIDE DS_FINAL
+  {
+    if (intersection.neighbor() && !intersection.boundary()) {
+      const auto insideEntityPtr    = intersection.inside();
+      const auto& insideEntity      = *insideEntityPtr;
+      const auto outsideNeighborPtr = intersection.outside();
+      const auto& outsideNeighbor = *outsideNeighborPtr;
+      return grid_view.indexSet().index(insideEntity) < grid_view.indexSet().index(outsideNeighbor);
+    } else
+      return false;
+  }
+}; // class InnerIntersections
+
+
+template <class GridViewImp>
+class BoundaryIntersections : public WhichIntersection<GridViewImp>
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Intersection<GridViewType>::Type IntersectionType;
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/,
+                        const IntersectionType& intersection) const DS_OVERRIDE DS_FINAL
+  {
+    return intersection.boundary();
+  }
+}; // class BoundaryIntersections
+
+
+template <class GridViewImp>
+class DirichletIntersections : public WhichIntersection<GridViewImp>
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Intersection<GridViewType>::Type IntersectionType;
+  typedef Stuff::Grid::BoundaryInfoInterface<IntersectionType> BoundaryInfoType;
+
+  DirichletIntersections(const BoundaryInfoType& boundary_info)
+    : boundary_info_(boundary_info)
+  {
+  }
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/,
+                        const IntersectionType& intersection) const DS_OVERRIDE DS_FINAL
+  {
+    return boundary_info_.dirichlet(intersection);
+  }
+
+private:
+  const BoundaryInfoType& boundary_info_;
+}; // class DirichletIntersections
+
+
+template <class GridViewImp>
+class NeumannIntersections : public WhichIntersection<GridViewImp>
+{
+public:
+  typedef GridViewImp GridViewType;
+  typedef typename Stuff::Grid::Intersection<GridViewType>::Type IntersectionType;
+  typedef Stuff::Grid::BoundaryInfoInterface<IntersectionType> BoundaryInfoType;
+
+  NeumannIntersections(const BoundaryInfoType& boundary_info)
+    : boundary_info_(boundary_info)
+  {
+  }
+
+  virtual bool apply_on(const GridViewType& /*grid_view*/,
+                        const IntersectionType& intersection) const DS_OVERRIDE DS_FINAL
+  {
+    return boundary_info_.neumann(intersection);
+  }
+
+private:
+  const BoundaryInfoType& boundary_info_;
+}; // class NeumannIntersections
+
+
+} // namespace ApplyOn
 } // namespace GDT
 } // namespace Dune
 
