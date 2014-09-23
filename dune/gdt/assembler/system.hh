217f3ed4166f2382f7b7dc0c3c4b29556ef46932
Rene Milk <rene.milk@uni-muenster.de>
2014-09-23 13:15:10 +0200

    [assembler] use ns aliases + as_imp for more concise code

--- dune/gdt/assembler/system.hh
+++ dune/gdt/assembler/system.hh
@@ -31,7 +31,7 @@ template< class TestSpaceImp,
           class GridViewImp = typename TestSpaceImp::GridViewType,
           class AnsatzSpaceImp = TestSpaceImp >
 class SystemAssembler
-  : public Stuff::Grid::Walker< GridViewImp >
+  : public DSG::Walker< GridViewImp >
 {
   static_assert(std::is_base_of< SpaceInterface< typename TestSpaceImp::Traits >, TestSpaceImp >::value,
                 "TestSpaceImp has to be derived from SpaceInterface!");
@@ -39,7 +39,7 @@ class SystemAssembler
                 "AnsatzSpaceImp has to be derived from SpaceInterface!");
   static_assert(std::is_same< typename TestSpaceImp::RangeFieldType, typename AnsatzSpaceImp::RangeFieldType >::value,
                 "Types do not match!");
-  typedef Stuff::Grid::Walker< GridViewImp >                          BaseType;
+  typedef DSG::Walker< GridViewImp >                          BaseType;
   typedef SystemAssembler<TestSpaceImp, GridViewImp, AnsatzSpaceImp > ThisType;
 public:
   typedef TestSpaceImp                           TestSpaceType;
@@ -50,8 +50,8 @@ public:
   typedef typename BaseType::EntityType       EntityType;
   typedef typename BaseType::IntersectionType IntersectionType;
 
-  typedef Stuff::Grid::ApplyOn::WhichEntity< GridViewType >       ApplyOnWhichEntity;
-  typedef Stuff::Grid::ApplyOn::WhichIntersection< GridViewType > ApplyOnWhichIntersection;
+  typedef DSG::ApplyOn::WhichEntity< GridViewType >       ApplyOnWhichEntity;
+  typedef DSG::ApplyOn::WhichIntersection< GridViewType > ApplyOnWhichIntersection;
 
   SystemAssembler(const TestSpaceType& test, const AnsatzSpaceType& ansatz, const GridViewType& grid_view)
     : BaseType(grid_view)
@@ -92,7 +92,7 @@ public:
   template< class C, class M >
   void add(Spaces::ConstraintsInterface< C, RangeFieldType >& constraints,
            Stuff::LA::MatrixInterface< M >& matrix,
-           const ApplyOnWhichEntity* where = new Stuff::Grid::ApplyOn::AllEntities< GridViewType >())
+           const ApplyOnWhichEntity* where = new DSG::ApplyOn::AllEntities< GridViewType >())
   {
     assert(matrix.rows() == test_space_.mapper().size());
     assert(matrix.cols() == ansatz_space_.mapper().size());
@@ -111,10 +111,10 @@ public:
   template< class ConstraintsType, class V >
   void add(ConstraintsType& constraints,
            Stuff::LA::VectorInterface< V >& vector,
-           const ApplyOnWhichEntity* where = new Stuff::Grid::ApplyOn::AllEntities< GridViewType >())
+           const ApplyOnWhichEntity* where = new DSG::ApplyOn::AllEntities< GridViewType >())
   {
     typedef typename V::derived_type VectorType;
-    VectorType& vector_imp = static_cast< VectorType& >(vector);
+    VectorType& vector_imp = vector.as_imp();
     assert(vector_imp.size() == test_space_.mapper().size());
     typedef internal::LocalVectorConstraintsWrapper< ThisType, ConstraintsType, VectorType > WrapperType;
     this->codim0_functors_.emplace_back(new WrapperType(test_space_, where, constraints, vector_imp));
@@ -123,10 +123,10 @@ public:
   template< class L, class M >
   void add(const LocalAssembler::Codim0Matrix< L >& local_assembler,
            Stuff::LA::MatrixInterface< M >& matrix,
-           const ApplyOnWhichEntity* where = new Stuff::Grid::ApplyOn::AllEntities< GridViewType >())
+           const ApplyOnWhichEntity* where = new DSG::ApplyOn::AllEntities< GridViewType >())
   {
     typedef typename M::derived_type MatrixType;
-    MatrixType& matrix_imp = static_cast< MatrixType& >(matrix);
+    MatrixType& matrix_imp = matrix.as_imp();
     assert(matrix_imp.rows() == test_space_.mapper().size());
     assert(matrix_imp.cols() == ansatz_space_.mapper().size());
     typedef internal::LocalVolumeMatrixAssemblerWrapper< ThisType, LocalAssembler::Codim0Matrix< L >, MatrixType >
@@ -138,10 +138,10 @@ public:
   template< class Codim0Assembler, class M >
   void add_codim0_assembler(const Codim0Assembler& local_assembler,
                             Stuff::LA::MatrixInterface< M >& matrix,
-                            const ApplyOnWhichEntity* where = new Stuff::Grid::ApplyOn::AllEntities< GridViewType >())
+                            const ApplyOnWhichEntity* where = new DSG::ApplyOn::AllEntities< GridViewType >())
   {
     typedef typename M::derived_type MatrixType;
-    MatrixType& matrix_imp = static_cast< MatrixType& >(matrix);
+    MatrixType& matrix_imp = matrix.as_imp();
     assert(matrix_imp.rows() == test_space_.mapper().size());
     assert(matrix_imp.cols() == ansatz_space_.mapper().size());
     typedef internal::LocalVolumeMatrixAssemblerWrapper< ThisType, Codim0Assembler, MatrixType > WrapperType;
@@ -152,10 +152,10 @@ public:
   template< class Codim0Assembler, class V >
   void add_codim0_assembler(const Codim0Assembler& local_assembler,
                             Stuff::LA::VectorInterface< V >& vector,
-                            const ApplyOnWhichEntity* where = new Stuff::Grid::ApplyOn::AllEntities< GridViewType >())
+                            const ApplyOnWhichEntity* where = new DSG::ApplyOn::AllEntities< GridViewType >())
   {
     typedef typename V::derived_type VectorType;
-    VectorType& vector_imp = static_cast< VectorType& >(vector);
+    VectorType& vector_imp = vector.as_imp();
     assert(vector_imp.size() == test_space_.mapper().size());
     typedef internal::LocalVolumeVectorAssemblerWrapper< ThisType, Codim0Assembler, VectorType > WrapperType;
     this->codim0_functors_.emplace_back(new WrapperType(test_space_, where, local_assembler, vector_imp));
@@ -165,10 +165,10 @@ public:
   void add(const LocalAssembler::Codim1CouplingMatrix< L >& local_assembler,
            Stuff::LA::MatrixInterface< M >& matrix,
            const ApplyOnWhichIntersection* where
-              = new Stuff::Grid::ApplyOn::AllIntersections< GridViewType >())
+              = new DSG::ApplyOn::AllIntersections< GridViewType >())
   {
     typedef typename M::derived_type MatrixType;
-    MatrixType& matrix_imp = static_cast< MatrixType& >(matrix);
+    MatrixType& matrix_imp = matrix.as_imp();
     assert(matrix_imp.rows() == test_space_.mapper().size());
     assert(matrix_imp.cols() == ansatz_space_.mapper().size());
     typedef internal::LocalFaceMatrixAssemblerWrapper< ThisType, LocalAssembler::Codim1CouplingMatrix< L >, MatrixType >
@@ -181,10 +181,10 @@ public:
   void add(const LocalAssembler::Codim1BoundaryMatrix< L >& local_assembler,
            Stuff::LA::MatrixInterface< M >& matrix,
            const ApplyOnWhichIntersection* where
-              = new Stuff::Grid::ApplyOn::AllIntersections< GridViewType >())
+              = new DSG::ApplyOn::AllIntersections< GridViewType >())
   {
     typedef typename M::derived_type MatrixType;
-    MatrixType& matrix_imp = static_cast< MatrixType& >(matrix);
+    MatrixType& matrix_imp = matrix.as_imp();
     assert(matrix_imp.rows() == test_space_.mapper().size());
     assert(matrix_imp.cols() == ansatz_space_.mapper().size());
     typedef internal::LocalFaceMatrixAssemblerWrapper< ThisType, LocalAssembler::Codim1BoundaryMatrix< L >, MatrixType >
@@ -197,10 +197,10 @@ public:
   void add(const LocalAssembler::Codim0Vector< L >& local_assembler,
            Stuff::LA::VectorInterface< V >& vector,
            const ApplyOnWhichEntity* where
-              = new Stuff::Grid::ApplyOn::AllEntities< GridViewType >())
+              = new DSG::ApplyOn::AllEntities< GridViewType >())
   {
     typedef typename V::derived_type VectorType;
-    VectorType& vector_imp = static_cast< VectorType& >(vector);
+    VectorType& vector_imp = vector.as_imp();
     assert(vector_imp.size() == test_space_.mapper().size());
     typedef internal::LocalVolumeVectorAssemblerWrapper< ThisType, LocalAssembler::Codim0Vector< L >, VectorType >
         WrapperType;
@@ -211,7 +211,7 @@ public:
   void add(const LocalAssembler::Codim1Vector< L >& local_assembler,
            Stuff::LA::VectorInterface< V >& vector,
            const ApplyOnWhichIntersection* where
-              = new Stuff::Grid::ApplyOn::AllIntersections< GridViewType >())
+              = new DSG::ApplyOn::AllIntersections< GridViewType >())
   {
     typedef typename V::derived_type VectorType;
     VectorType& vector_imp = static_cast< VectorType& >(vector);
