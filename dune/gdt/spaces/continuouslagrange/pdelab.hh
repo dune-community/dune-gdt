7aa39a08c070517155fad3a1951971cba85b6bdb
Felix Schindler <felix.schindler@wwu.de>
2014-09-25 00:00:35 +0200

    [spaces.cg.pdelab] added manual copy ctor
    The default one does not compile bc of the mutex.

--- dune/gdt/spaces/continuouslagrange/pdelab.hh
+++ dune/gdt/spaces/continuouslagrange/pdelab.hh
@@ -162,7 +162,15 @@ public:
   {
   }
 
-  PdelabBased(const ThisType& other) = default;
+  PdelabBased(const ThisType& other)
+    : gridView_(other.gridView_)
+    , fe_map_(other.fe_map_)
+    , backend_(other.backend_)
+    , mapper_(other.mapper_)
+    , communicator_(other.communicator_)
+    , communicator_prepared_(other.communicator_prepared_)
+  {
+  }
 
   ThisType& operator=(const ThisType& other)
   {
