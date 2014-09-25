8d485a73f272304b02cbad12750f5ad1ad6c763e
Rene Milk <rene.milk@uni-muenster.de>
2014-09-25 16:53:41 +0200

    [spaces.cg.pdelab] simplify member
    since its not necessary for those memeber to be shared ptrs, there's no
    need to make them PerThreadValues either

--- dune/gdt/spaces/continuouslagrange/pdelab.hh
+++ dune/gdt/spaces/continuouslagrange/pdelab.hh
@@ -154,9 +154,9 @@ public:
 
   PdelabBased(const std::shared_ptr<const GridViewType>& gV)
     : gridView_(gV)
-    , fe_map_(std::make_shared<FEMapType>(*(gridView_)))
-    , backend_(std::make_shared<BackendType>(const_cast<GridViewType&>(*gridView_), *(*fe_map_)))
-    , mapper_(std::make_shared<MapperType>(*(*backend_)))
+    , fe_map_(*(gridView_))
+    , backend_(const_cast<GridViewType&>(*gridView_), fe_map_)
+    , mapper_(backend_)
     , communicator_(CommunicationChooser<GridViewImp>::create(*gridView_))
     , communicator_prepared_(false)
   {
@@ -196,17 +196,17 @@ public:
 
   const BackendType& backend() const
   {
-    return *(*backend_);
+    return backend_;
   }
 
   const MapperType& mapper() const
   {
-    return *(*mapper_);
+    return mapper_;
   }
 
   BaseFunctionSetType base_function_set(const EntityType& entity) const
   {
-    return BaseFunctionSetType(*(*backend_), entity);
+    return BaseFunctionSetType(backend_, entity);
   }
 
   CommunicatorType& communicator() const
@@ -220,9 +220,9 @@ public:
 
 private:
   std::shared_ptr<const GridViewType> gridView_;
-  DS::PerThreadValue<std::shared_ptr<const FEMapType>> fe_map_;
-  DS::PerThreadValue<std::shared_ptr<const BackendType>> backend_;
-  DS::PerThreadValue<std::shared_ptr<const MapperType>> mapper_;
+  const FEMapType fe_map_;
+  const BackendType backend_;
+  const MapperType mapper_;
   mutable std::shared_ptr<CommunicatorType> communicator_;
   mutable bool communicator_prepared_;
   mutable std::mutex communicator_mutex_;
