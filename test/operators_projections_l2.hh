cdf5139c5e60e9f8028877e5f5177557104a42a9
Felix Schindler <felix.schindler@wwu.de>
2014-09-30 15:17:00 +0200

    [test.operators_projection] added
    This commit introduces detailed tests of the projection operators. A base
    class is provided in test/operators_projections.hh that checks the
    projection of a linear function for correctness. There exists another
    header for each projection operator type (checking nothing additionally
    atm) and a test source for each projection operator/space type/space
    backend type combination.

---
+++ test/operators_projections_l2.hh
@@ -0,0 +1,20 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_GDT_OPERATORS_PROJECTIONS_L2_HH
+#define DUNE_GDT_OPERATORS_PROJECTIONS_L2_HH
+
+#include <dune/gdt/operators/projections.hh>
+
+#include "operators_projections.hh"
+
+
+template< class SpaceType >
+struct L2ProjectionOperator
+  : public ProjectionOperatorBase< SpaceType, Operators::L2Projection< typename SpaceType::GridViewType > >
+{};
+
+
+#endif // DUNE_GDT_OPERATORS_PROJECTIONS_L2_HH
---
+++ test/operators_projections_lagrange.hh
@@ -0,0 +1,20 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_GDT_OPERATORS_PROJECTIONS_LAGRANGE_HH
+#define DUNE_GDT_OPERATORS_PROJECTIONS_LAGRANGE_HH
+
+#include <dune/gdt/operators/projections.hh>
+
+#include "operators_projections.hh"
+
+
+template< class SpaceType >
+struct LagrangeProjectionOperator
+  : public ProjectionOperatorBase< SpaceType, Operators::LagrangeProjection< typename SpaceType::GridViewType > >
+{};
+
+
+#endif // DUNE_GDT_OPERATORS_PROJECTIONS_LAGRANGE_HH
