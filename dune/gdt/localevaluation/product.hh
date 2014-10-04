d644d2e6c781ed94e288263bd03e223b6822dd04
Felix Schindler <felix.schindler@wwu.de>
2014-10-04 16:23:00 +0200

    [localevaluation.product] derive from Codim1Interface< ..., 2 >
    This can be used e.g. to compute an L2 product over the boundary of a
    domain.

--- dune/gdt/localevaluation/product.hh
+++ dune/gdt/localevaluation/product.hh
@@ -51,7 +51,8 @@ public:
 template <class LocalizableFunctionImp>
 class Product : public LocalEvaluation::Codim0Interface<ProductTraits<LocalizableFunctionImp>, 1>,
                 public LocalEvaluation::Codim0Interface<ProductTraits<LocalizableFunctionImp>, 2>,
-                public LocalEvaluation::Codim1Interface<ProductTraits<LocalizableFunctionImp>, 1>
+                public LocalEvaluation::Codim1Interface<ProductTraits<LocalizableFunctionImp>, 1>,
+                public LocalEvaluation::Codim1Interface<ProductTraits<LocalizableFunctionImp>, 2>
 {
 public:
   typedef ProductTraits<LocalizableFunctionImp> Traits;
@@ -106,6 +107,7 @@ public:
    * \brief extracts the local function and calls the correct order() method
    * \note  required by
    *        - `LocalEvaluation::Codim0Interface< ..., 2 >`
+   *        - `LocalEvaluation::Codim1Interface< ..., 2 >`
    */
   template <class R, int rT, int rCT, int rA, int rCA>
   size_t
@@ -143,6 +145,21 @@ public:
   {
     const auto localFunction = std::get<0>(localFuncs);
     evaluate(*localFunction, testBase, intersection, localPoint, ret);
+  }
+
+  /**
+   * \brief extracts the local function and calls the correct evaluate() method
+   * \note  required by `LocalEvaluation::Codim1Interface< ..., 2 >`
+   */
+  template <class IntersectionType, class R, int rT, int rCT, int rA, int rCA>
+  void evaluate(const LocalfunctionTupleType& localFunctions,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rT, rCT>& testBase,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, rA, rCA>& ansatzBase,
+                const IntersectionType& intersection,
+                const Dune::FieldVector<DomainFieldType, dimDomain - 1>& localPoint, Dune::DynamicMatrix<R>& ret) const
+  {
+    const auto localFunction = std::get<0>(localFunctions);
+    evaluate(*localFunction, testBase, ansatzBase, intersection, localPoint, ret);
   }
 
   /**
@@ -258,6 +275,40 @@ public:
     assert(ret.size() >= size);
     for (size_t ii = 0; ii < size; ++ii) {
       ret[ii] = functionValue * testValues[ii];
+    }
+  } // ... evaluate(...)
+
+  /**
+   * \brief Computes a product evaluation for a scalar local function and scalar or vector valued basefunctionsets.
+   * \note  implementation for `LocalEvaluation::Codim1Interface< ..., 2 >`
+   */
+  template <class IntersectionType, class R, int r>
+  void evaluate(const Stuff::LocalfunctionInterface<EntityType, DomainFieldType, dimDomain, R, 1, 1>& localFunction,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, r, 1>& testBase,
+                const Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, r, 1>& ansatzBase,
+                const IntersectionType& intersection,
+                const Dune::FieldVector<DomainFieldType, dimDomain - 1>& localPoint, Dune::DynamicMatrix<R>& ret) const
+  {
+    const auto localPointEntity = intersection.geometryInInside().global(localPoint);
+    // evaluate local function
+    const auto functionValue = localFunction.evaluate(localPointEntity);
+    // evaluate bases
+    const size_t rows = testBase.size();
+    const size_t cols = ansatzBase.size();
+    typedef
+        typename Stuff::LocalfunctionSetInterface<EntityType, DomainFieldType, dimDomain, R, r, 1>::RangeType RangeType;
+    std::vector<RangeType> testValues(rows, RangeType(0));
+    std::vector<RangeType> ansatzValues(cols, RangeType(0));
+    testBase.evaluate(localPointEntity, testValues);
+    ansatzBase.evaluate(localPointEntity, ansatzValues);
+    // compute product
+    assert(ret.rows() >= rows);
+    assert(ret.cols() >= cols);
+    for (size_t ii = 0; ii < rows; ++ii) {
+      auto& retRow = ret[ii];
+      for (size_t jj = 0; jj < cols; ++jj) {
+        retRow[jj] = functionValue * (testValues[ii] * ansatzValues[jj]);
+      }
     }
   } // ... evaluate(...)
 
