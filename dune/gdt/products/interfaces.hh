95f332e7fd9b7bafdfca26b50d03bd48147b6347
Tobias Leibner <tobias.leibner@googlemail.com>
2014-10-20 15:51:32 +0200

    [products.interfaces] fix ScalarType for VectorInterface

--- dune/gdt/products/interfaces.hh
+++ dune/gdt/products/interfaces.hh
@@ -205,7 +205,7 @@ public:
 
   template <class R, class S>
   FieldType apply2(const Stuff::LA::VectorInterface<R, FieldType>& range,
-                   const Stuff::LA::VectorInterface<S, DomainFieldType>& source)
+                   const Stuff::LA::VectorInterface<S, FieldType>& source)
   {
     assemble();
     assert(range.size() == matrix().rows());
