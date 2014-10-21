7e8c6f502b2fcfb6b3d27c8fab9cc5386cb8e3aa
Rene Milk <rene.milk@uni-muenster.de>
2014-10-21 11:52:13 +0200

    [spaces] fixes cg provider

--- dune/gdt/spaces/continuouslagrange.hh
+++ dune/gdt/spaces/continuouslagrange.hh
@@ -64,13 +64,13 @@ public:
 
   static Type create(const GridProviderType& grid_provider, const int level = 0)
   {
-    return Type(*grid_provider.template layer<layer_type, part_view_type>(level));
+    return Type(grid_provider.template layer<layer_type, part_view_type>(level));
   }
 
 #if HAVE_DUNE_GRID_MULTISCALE
   static Type create(const MsGridProviderType& grid_provider, const int level_or_subdomain = 0)
   {
-    return Type(*grid_provider.template layer<layer_type, part_view_type>(level_or_subdomain));
+    return Type(grid_provider.template layer<layer_type, part_view_type>(level_or_subdomain));
   }
 #endif // HAVE_DUNE_GRID_MULTISCALE
 }; // class ContinuousLagrangeProvider
