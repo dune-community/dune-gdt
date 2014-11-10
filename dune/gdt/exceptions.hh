b07d7c16d253332ae106425e764aa118fd44026e
Felix Schindler <felix.schindler@wwu.de>
2014-11-10 16:46:43 +0100

    [exceptions] add prolongation_error

---
+++ dune/gdt/exceptions.hh
@@ -0,0 +1,23 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_GDT_EXCEPTIONS_HH
+#define DUNE_GDT_EXCEPTIONS_HH
+
+#include <dune/stuff/common/exceptions.hh>
+
+namespace Dune {
+namespace GDT {
+namespace Exceptions {
+
+
+class prolongation_error : public Dune::Exception {};
+
+
+} // namespace Exceptions
+} // namespace GDT
+} // namespace Dune
+
+#endif // DUNE_GDT_EXCEPTIONS_HH
