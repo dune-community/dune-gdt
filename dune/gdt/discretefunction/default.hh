7644b0a416848237d3592fc230d44ebf8296fce9
Felix Schindler <felix.schindler@wwu.de>
2014-12-04 20:59:15 +0100

    [discretefunction.default] add ctor which moves in a vector

--- dune/gdt/discretefunction/default.hh
+++ dune/gdt/discretefunction/default.hh
@@ -158,6 +158,11 @@ public:
     , BaseType(sp, VectorProviderBaseType::storage_access(), nm)
   {}
 
+  DiscreteFunction(const SpaceType& sp, VectorType&& vec, const std::string nm = "gdt.discretefunction")
+    : VectorProviderBaseType(vec)
+    , BaseType(sp, VectorProviderBaseType::storage_access(), nm)
+  {}
+
   DiscreteFunction(const SpaceType& sp, const std::string nm = "gdt.discretefunction")
     : VectorProviderBaseType(new VectorType(sp.mapper().size()))
     , BaseType(sp, VectorProviderBaseType::storage_access(), nm)
