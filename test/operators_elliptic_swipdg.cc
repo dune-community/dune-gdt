ae60396ac1c33f7a7f859c737443833af870390c
Felix Schindler <felix.schindler@wwu.de>
2015-02-02 16:03:00 +0100

    [test.operators_elliptic_swipdg] remove unused define

--- test/operators_elliptic_swipdg.cc
+++ test/operators_elliptic_swipdg.cc
@@ -3,10 +3,6 @@
 // Copyright holders: Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-#ifndef DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING
-# define DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING 1
-#endif
-
 // This one has to come first (includes the config.h)!
 #include <dune/stuff/test/main.hxx>
 
