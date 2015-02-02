51c61974231de2e898368963de6c0807c099f00a
Felix Schindler <felix.schindler@wwu.de>
2015-02-02 16:48:22 +0100

    [test.spaces_rt] add signature and 2d simplicial check

--- test/spaces_rt.hh
+++ test/spaces_rt.hh
@@ -6,13 +6,49 @@
 #ifndef DUNE_GDT_TEST_SPACES_RT_HH
 #define DUNE_GDT_TEST_SPACES_RT_HH
 
+#include <dune/common/unused.hh>
+
+#include <dune/stuff/common/ranges.hh>
+
+#include <dune/gdt/spaces/rt/interface.hh>
+
 #include "spaces.hh"
 
 
 template< class SpaceType >
 class RT_Space
   : public SpaceBase< SpaceType >
-{};
+{
+  template< class T, int d, class R, int r, int rC >
+  void matches_signature(const Dune::GDT::Spaces::RTInterface< T, d, R, r, rC >& /*space*/)
+  {
+    static_assert(Dune::GDT::is_rt_space< SpaceType >::value, "");
+    static_assert(std::is_same< typename SpaceType::Traits, T >::value, "");
+    static_assert(std::is_same< typename SpaceType::RangeFieldType, R >::value, "");
+    static_assert(d == SpaceType::dimDomain, "");
+    static_assert(r == SpaceType::dimRange, "");
+    static_assert(rC == SpaceType::dimRangeCols, "");
+  }
+
+public:
+  void matches_raviart_thomas_signature()
+  {
+    matches_signature(this->space_);
+  }
+};
+
+
+template< class SpaceType >
+class RT_2d_simplicial_Space
+  : public RT_Space< SpaceType >
+{
+public:
+  void fulfills_raviart_thomas_2d_simplicial_interface()
+  {
+    for (const auto& entity : DSC::entityRange(this->space_.grid_view()))
+      auto DUNE_UNUSED(local_DoF_indices) = this->space_.local_DoF_indices(entity);
+  }
+}; // class RT_2d_simplicial_Space
 
 
 #endif // DUNE_GDT_TEST_SPACES_RT_HH
