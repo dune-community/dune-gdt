8bda8d8ce0e59ed610ec6cced8204420c22b15ee
Felix Schindler <felix.schindler@wwu.de>
2015-02-02 16:49:28 +0100

    [test.spaces_rt_pdelab] add tests
    * matches_raviart_thomas_signature
    * fulfills_raviart_thomas_2d_simplicial_interface

--- test/spaces_rt_pdelab.cc
+++ test/spaces_rt_pdelab.cc
@@ -31,3 +31,24 @@ TYPED_TEST(RT_Space, basefunctionset_fulfills_interface) {
 TYPED_TEST(RT_Space, check_for_correct_copy) {
   this->check_for_correct_copy();
 }
+TYPED_TEST(RT_Space, matches_raviart_thomas_signature) {
+  this->matches_raviart_thomas_signature();
+}
+
+
+#if HAVE_ALUGRID
+
+typedef testing::Types<
+                        SPACE_RT_PDELAB_ALUCONFORMGRID(2) \
+                      > RT_2d_Spaces;
+
+TYPED_TEST_CASE(RT_2d_simplicial_Space, RT_2d_Spaces);
+TYPED_TEST(RT_2d_simplicial_Space, fulfills_raviart_thomas_2d_simplicial_interface) {
+  this->fulfills_raviart_thomas_2d_simplicial_interface();
+}
+
+#else // HAVE_ALUGRID
+
+TYPED_TEST(DISABLED_RT_2d_simplicial_Space, fulfills_raviart_thomas_2d_simplicial_interface) {}
+
+#endif // HAVE_ALUGRID
