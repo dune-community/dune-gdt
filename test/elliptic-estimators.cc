a162d239678d35dcfa6cb25a8006a97318037a47
Felix Schindler <felix.schindler@wwu.de>
2015-02-03 16:29:53 +0100

    [test.elliptic_estimators] do not fail if files are missing

--- test/elliptic-estimators.cc
+++ test/elliptic-estimators.cc
@@ -22,11 +22,15 @@ struct EllipticSWIPDGDiscretization : public ::testing::Test
 {
   void estimator_study() const
   {
-    const TestCase test_case;
-    test_case.print_header(DSC_LOG_INFO);
-    DSC_LOG_INFO << std::endl;
-    EllipticSWIPDG::EstimatorStudy<TestCase> estimator_study(test_case);
-    Dune::Stuff::Test::check_eoc_study_for_success(estimator_study, estimator_study.run(DSC_LOG_INFO));
+    try {
+      const TestCase test_case;
+      test_case.print_header(DSC_LOG_INFO);
+      DSC_LOG_INFO << std::endl;
+      EllipticSWIPDG::EstimatorStudy<TestCase> estimator_study(test_case);
+      Dune::Stuff::Test::check_eoc_study_for_success(estimator_study, estimator_study.run(DSC_LOG_INFO));
+    } catch (Dune::Stuff::Exceptions::spe10_data_file_missing& ee) {
+      std::cerr << ee.what() << std::endl;
+    }
   } // ... estimator_study()
 }; // struct EllipticSWIPDGDiscretization
 
