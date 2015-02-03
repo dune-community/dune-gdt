4ad3a4d55a217f4dd9a0f11201a3579b15675ab9
Felix Schindler <felix.schindler@wwu.de>
2015-02-03 21:34:13 +0100

    [localevaluation.interface] do not guard dune includes

--- dune/gdt/localevaluation/interface.hh
+++ dune/gdt/localevaluation/interface.hh
@@ -8,10 +8,8 @@
 
 #include <memory>
 
-#include <dune/stuff/common/disable_warnings.hh>
-# include <dune/common/dynmatrix.hh>
-# include <dune/common/fvector.hh>
-#include <dune/stuff/common/reenable_warnings.hh>
+#include <dune/common/dynmatrix.hh>
+#include <dune/common/fvector.hh>
 
 #include <dune/stuff/common/crtp.hh>
 #include <dune/stuff/common/type_utils.hh>
