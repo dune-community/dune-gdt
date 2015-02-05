b63a490090f2ce2c423aafbb1a85f49b57eed895
Felix Schindler <felix.schindler@wwu.de>
2015-02-05 19:28:29 +0100

    [localoperator.interface] do not guard dune includes

--- dune/gdt/localoperator/interface.hh
+++ dune/gdt/localoperator/interface.hh
@@ -8,10 +8,7 @@
 
 #include <vector>
 
-#include <dune/common/bartonnackmanifcheck.hh>
-#include <dune/stuff/common/disable_warnings.hh>
 #include <dune/common/dynmatrix.hh>
-#include <dune/stuff/common/reenable_warnings.hh>
 
 #include <dune/gdt/basefunctionset/interface.hh>
 
