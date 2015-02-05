e7c18c8cdb69e46ab183f388da6943fc6f0b1093
Felix Schindler <felix.schindler@wwu.de>
2015-02-05 19:28:47 +0100

    [localoperator.interface] update CRTP handling

--- dune/gdt/localoperator/interface.hh
+++ dune/gdt/localoperator/interface.hh
@@ -10,6 +10,8 @@
 
 #include <dune/common/dynmatrix.hh>
 
+#include <dune/stuff/common/crtp.hh>
+
 #include <dune/gdt/basefunctionset/interface.hh>
 
 namespace Dune {
@@ -18,15 +20,15 @@ namespace LocalOperator {
 
 
 template <class Traits>
-class Codim0Interface
+class Codim0Interface : public Stuff::CRTPInterface<Codim0Interface<Traits>, Traits>
 {
 public:
   typedef typename Traits::derived_type derived_type;
 
   size_t numTmpObjectsRequired() const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().numTmpObjectsRequired());
-    return asImp().numTmpObjectsRequired();
+    CHECK_CRTP(this->as_imp().numTmpObjectsRequired());
+    return this->as_imp().numTmpObjectsRequired();
   }
 
   /**
@@ -44,31 +46,21 @@ public:
              const BaseFunctionSetInterface<A, D, d, R, rA, rCA>& ansatzBase, Dune::DynamicMatrix<R>& ret,
              std::vector<Dune::DynamicMatrix<R>>& tmpLocalMatrices) const
   {
-    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().apply(testBase, ansatzBase, ret, tmpLocalMatrices));
-  }
-
-  derived_type& asImp()
-  {
-    return static_cast<derived_type&>(*this);
-  }
-
-  const derived_type& asImp() const
-  {
-    return static_cast<const derived_type&>(*this);
+    CHECK_AND_CALL_CRTP(this->as_imp().apply(testBase, ansatzBase, ret, tmpLocalMatrices));
   }
 }; // class Codim0Interface
 
 
 template <class Traits>
-class Codim1CouplingInterface
+class Codim1CouplingInterface : public Stuff::CRTPInterface<Codim1CouplingInterface<Traits>, Traits>
 {
 public:
   typedef typename Traits::derived_type derived_type;
 
   size_t numTmpObjectsRequired() const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().numTmpObjectsRequired());
-    return asImp().numTmpObjectsRequired();
+    CHECK_CRTP(this->as_imp().numTmpObjectsRequired());
+    return this->as_imp().numTmpObjectsRequired();
   }
 
   /**
@@ -94,40 +86,30 @@ public:
              Dune::DynamicMatrix<R>& neighborNeighborRet, Dune::DynamicMatrix<R>& entityNeighborRet,
              Dune::DynamicMatrix<R>& neighborEntityRet, std::vector<Dune::DynamicMatrix<R>>& tmpLocalMatrices) const
   {
-    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().apply(testBaseEntity,
-                                                          ansatzBaseEntity,
-                                                          testBaseNeighbor,
-                                                          ansatzBaseNeighbor,
-                                                          intersection,
-                                                          entityEntityRet,
-                                                          neighborNeighborRet,
-                                                          entityNeighborRet,
-                                                          neighborEntityRet,
-                                                          tmpLocalMatrices));
-  }
-
-  derived_type& asImp()
-  {
-    return static_cast<derived_type&>(*this);
-  }
-
-  const derived_type& asImp() const
-  {
-    return static_cast<const derived_type&>(*this);
+    CHECK_AND_CALL_CRTP(this->as_imp().apply(testBaseEntity,
+                                             ansatzBaseEntity,
+                                             testBaseNeighbor,
+                                             ansatzBaseNeighbor,
+                                             intersection,
+                                             entityEntityRet,
+                                             neighborNeighborRet,
+                                             entityNeighborRet,
+                                             neighborEntityRet,
+                                             tmpLocalMatrices));
   }
 }; // class Codim1CouplingInterface
 
 
 template <class Traits>
-class Codim1BoundaryInterface
+class Codim1BoundaryInterface : public Stuff::CRTPInterface<Codim1BoundaryInterface<Traits>, Traits>
 {
 public:
   typedef typename Traits::derived_type derived_type;
 
   size_t numTmpObjectsRequired() const
   {
-    CHECK_INTERFACE_IMPLEMENTATION(asImp().numTmpObjectsRequired());
-    return asImp().numTmpObjectsRequired();
+    CHECK_CRTP(this->as_imp().numTmpObjectsRequired());
+    return this->as_imp().numTmpObjectsRequired();
   }
 
   /**
@@ -146,17 +128,7 @@ public:
              const BaseFunctionSetInterface<A, D, d, R, rA, rCA>& ansatzBase, const IntersectionType& intersection,
              Dune::DynamicMatrix<R>& ret, std::vector<Dune::DynamicMatrix<R>>& tmpLocalMatrices) const
   {
-    CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(asImp().apply(testBase, ansatzBase, intersection, ret, tmpLocalMatrices));
-  }
-
-  derived_type& asImp()
-  {
-    return static_cast<derived_type&>(*this);
-  }
-
-  const derived_type& asImp() const
-  {
-    return static_cast<const derived_type&>(*this);
+    CHECK_AND_CALL_CRTP(this->as_imp().apply(testBase, ansatzBase, intersection, ret, tmpLocalMatrices));
   }
 }; // class Codim1BoundaryInterface
 
