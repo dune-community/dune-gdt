ef4cbd4e5a4efc951561f640c9ee2113dfa40087
Felix Schindler <felix.schindler@wwu.de>
2015-02-08 20:44:53 +0100

    [test.spaces_dg_pdelab] whitespace

--- test/spaces_dg_pdelab.cc
+++ test/spaces_dg_pdelab.cc
@@ -3,6 +3,8 @@
 // Copyright holders: Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
+#define DUNE_STUFF_TEST_MAIN_CATCH_EXCEPTIONS 1
+
 #include <dune/stuff/test/main.hxx>
 
 #include "spaces_dg.hh"
