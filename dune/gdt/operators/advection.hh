82f56b919c1c0172cca687595f046aedf4912e8a
Tobias Leibner <tobias.leibner@googlemail.com>
2015-03-26 13:20:09 +0100

    [operators.advection] make AdvectionLaxFriedrichsLocalizable usable on its own

--- dune/gdt/operators/advection.hh
+++ dune/gdt/operators/advection.hh
@@ -8,9 +8,11 @@
 
 #include <type_traits>
 
+#include <dune/stuff/aliases.hh>
+#include <dune/stuff/functions/interfaces.hh>
+#include <dune/stuff/grid/walker/apply-on.hh>
 #include <dune/stuff/la/container/common.hh>
 #include <dune/stuff/la/container/interfaces.hh>
-#include <dune/stuff/functions/interfaces.hh>
 
 #include <dune/gdt/spaces/interface.hh>
 #include <dune/gdt/localevaluation/laxfriedrichs.hh>
@@ -27,23 +29,38 @@ namespace Operators {
 
 
 // forward
-template< class InnerAssemblerImp, class SourceImp, class RangeImp >
+template< class AnalyticalFluxImp, class LocalizableFunctionImp, class SourceImp, class RangeImp >
 class AdvectionLaxFriedrichsLocalizable;
 
-template< class AnalyticalFluxImp, class LocalizableFunctionImp,class FVSpaceImp >
+template< class AnalyticalFluxImp, class LocalizableFunctionImp, class FVSpaceImp >
 class AdvectionLaxFriedrichs;
 
 
 namespace internal {
 
-template< class InnerAssemblerImp, class SourceImp, class RangeImp >
+template< class AnalyticalFluxImp, class LocalizableFunctionImp, class SourceImp, class RangeImp >
 class AdvectionLaxFriedrichsLocalizableTraits
 {
+  static_assert(std::is_base_of< Stuff::GlobalFunctionInterface< typename AnalyticalFluxImp::EntityType,
+                                                                 typename SourceImp::RangeFieldType,
+                                                                 SourceImp::dimRange,
+                                                                 typename SourceImp::DomainFieldType,
+                                                                 SourceImp::dimDomain >,
+                AnalyticalFluxImp >::value,
+                "AnalyticalFluxImp has to be derived from Stuff::GlobalFunctionInterface!");
+  static_assert(Stuff::is_localizable_function< LocalizableFunctionImp >::value,
+                "LocalizableFunctionImp has to be derived from Stuff::LocalizableFunctionInterface!");
+  static_assert(is_discrete_function< SourceImp >::value, "SourceImp has to be derived from DiscreteFunction!");
+  static_assert(is_discrete_function< RangeImp >::value, "RangeImp has to be derived from DiscreteFunction!");
 public:
-  typedef AdvectionLaxFriedrichsLocalizable< InnerAssemblerImp, SourceImp, RangeImp >           derived_type;
+  typedef AdvectionLaxFriedrichsLocalizable< AnalyticalFluxImp,
+                                             LocalizableFunctionImp,
+                                             SourceImp,
+                                             RangeImp >                                         derived_type;
+  typedef AnalyticalFluxImp                                                                     AnalyticalFluxType;
+  typedef LocalizableFunctionImp                                                                LocalizableFunctionType;
   typedef SourceImp                                                                             SourceType;
   typedef RangeImp                                                                              RangeType;
-  typedef InnerAssemblerImp                                                                     InnerAssemblerType;
   typedef typename RangeType::SpaceType::GridViewType                                           GridViewType;
   typedef typename GridViewType::ctype                                                          FieldType;
 }; // class AdvectionLaxFriedrichsLocalizableTraits
@@ -57,52 +74,61 @@ class AdvectionLaxFriedrichsTraits
                                                                  typename FVSpaceImp::DomainFieldType,
                                                                  FVSpaceImp::dimDomain >,
                 AnalyticalFluxImp >::value,
-                "AnalyticalFluxType has to be derived from Stuff::GlobalFunctionInterface!");
+                "AnalyticalFluxImp has to be derived from Stuff::GlobalFunctionInterface!");
   static_assert(Stuff::is_localizable_function< LocalizableFunctionImp >::value,
                 "LocalizableFunctionImp has to be derived from Stuff::LocalizableFunctionInterface!");
   static_assert(is_space< FVSpaceImp >::value,    "FVSpaceImp has to be derived from SpaceInterface!");
 public:
   typedef AdvectionLaxFriedrichs< AnalyticalFluxImp, LocalizableFunctionImp, FVSpaceImp >       derived_type;
-  typedef FVSpaceImp                                                                            FVSpaceType;
-  typedef typename FVSpaceType::GridViewType                                                    GridViewType;
-  typedef typename FVSpaceType::RangeFieldType                                                  RangeFieldType;
   typedef AnalyticalFluxImp                                                                     AnalyticalFluxType;
   typedef LocalizableFunctionImp                                                                LocalizableFunctionType;
+  typedef FVSpaceImp                                                                            FVSpaceType;
+  typedef typename FVSpaceType::GridViewType                                                    GridViewType;
   typedef typename FVSpaceType::DomainFieldType                                                 FieldType;
 }; // class LaxFriedrichsTraits
 
 } // namespace internal
 
 
-template< class InnerAssemblerImp, class SourceImp, class RangeImp >
+template< class AnalyticalFluxImp, class LocalizableFunctionImp, class SourceImp, class RangeImp >
 class AdvectionLaxFriedrichsLocalizable
   : public Dune::GDT::LocalizableOperatorInterface<
-                             internal::AdvectionLaxFriedrichsLocalizableTraits< InnerAssemblerImp,
-                                                                                 SourceImp,
-                                                                                 RangeImp > >
+                             internal::AdvectionLaxFriedrichsLocalizableTraits< AnalyticalFluxImp,
+                                                                                LocalizableFunctionImp,
+                                                                                SourceImp,
+                                                                                RangeImp > >
   , public SystemAssembler< typename RangeImp::SpaceType >
 {
   typedef Dune::GDT::LocalizableOperatorInterface<
-                             internal::AdvectionLaxFriedrichsLocalizableTraits< InnerAssemblerImp,
-                                                                                 SourceImp,
-                                                                                 RangeImp > > OperatorBaseType;
-  typedef SystemAssembler< typename RangeImp::SpaceType >     AssemblerBaseType;
-
+                             internal::AdvectionLaxFriedrichsLocalizableTraits< AnalyticalFluxImp,
+                                                                                LocalizableFunctionImp,
+                                                                                SourceImp,
+                                                                                RangeImp > >    OperatorBaseType;
+  typedef SystemAssembler< typename RangeImp::SpaceType >                                       AssemblerBaseType;
 public:
-  typedef internal::AdvectionLaxFriedrichsLocalizableTraits< InnerAssemblerImp,
-                                                              SourceImp,
-                                                              RangeImp >         Traits;
-  typedef typename Traits::GridViewType            GridViewType;
-  typedef typename Traits::SourceType              SourceType;
-  typedef typename Traits::RangeType               RangeType;
-  typedef typename Traits::InnerAssemblerType      InnerAssemblerType;
+  typedef internal::AdvectionLaxFriedrichsLocalizableTraits< AnalyticalFluxImp,
+                                                             LocalizableFunctionImp,
+                                                             SourceImp,
+                                                             RangeImp >                         Traits;
 
-  AdvectionLaxFriedrichsLocalizable(const InnerAssemblerType inner_assembler,
-                                     const SourceType& source,
-                                     RangeType& range)
+  typedef typename Traits::GridViewType                                                         GridViewType;
+  typedef typename Traits::SourceType                                                           SourceType;
+  typedef typename Traits::RangeType                                                            RangeType;
+  typedef typename Traits::AnalyticalFluxType                                                   AnalyticalFluxType;
+  typedef typename Traits::LocalizableFunctionType                                              LocalizableFunctionType;
+
+  typedef typename Dune::GDT::LocalEvaluation::LaxFriedrichs::Inner< LocalizableFunctionImp >   NumericalFluxType;
+  typedef typename Dune::GDT::LocalOperator::Codim1FV< NumericalFluxType >                      LocalOperatorType;
+  typedef typename LocalAssembler::Codim1CouplingFV< LocalOperatorType >                        InnerAssemblerType;
+
+  AdvectionLaxFriedrichsLocalizable(const AnalyticalFluxType& analytical_flux,
+                                    const LocalizableFunctionType& ratio_dt_dx,
+                                    const SourceType& source,
+                                    RangeType& range)
     : OperatorBaseType()
     , AssemblerBaseType(range.space())
-    , inner_assembler_(inner_assembler)
+    , local_operator_(analytical_flux, ratio_dt_dx)
+    , inner_assembler_(local_operator_)
     , source_(source)
     , range_(range)
   {}
@@ -132,11 +158,13 @@ using AssemblerBaseType::assemble;
 
   void apply()
   {
-    this->add(inner_assembler_, source_, range_);
+    this->add(inner_assembler_, source_, range_, new DSG::ApplyOn::InnerIntersections< GridViewType >());
+    this->add(inner_assembler_, source_, range_, new DSG::ApplyOn::PeriodicIntersections< GridViewType >());
     this->assemble();
   }
 
 private:
+  const LocalOperatorType local_operator_;
   const InnerAssemblerType inner_assembler_;
   const SourceType& source_;
   RangeType& range_;
@@ -154,24 +182,19 @@ class AdvectionLaxFriedrichs
                                                                                  LocalizableFunctionImp,
                                                                                  FVSpaceImp > > OperatorBaseType;
 
-  typedef typename Dune::GDT::LocalEvaluation::LaxFriedrichs::Inner< LocalizableFunctionImp >   NumericalFluxType;
-  typedef typename Dune::GDT::LocalOperator::Codim1FV< NumericalFluxType >                      LocalOperatorType;
-  typedef typename LocalAssembler::Codim1CouplingFV< LocalOperatorType >                        InnerAssemblerType;
-
 public:
   typedef internal::AdvectionLaxFriedrichsTraits< AnalyticalFluxImp, LocalizableFunctionImp, FVSpaceImp > Traits;
   typedef typename Traits::GridViewType            GridViewType;
-  typedef typename Traits::RangeFieldType          RangeFieldType;
   typedef typename Traits::AnalyticalFluxType      AnalyticalFluxType;
   typedef typename Traits::LocalizableFunctionType LocalizableFunctionType;
   typedef typename Traits::FVSpaceType             FVSpaceType;
 
   AdvectionLaxFriedrichs(const AnalyticalFluxType& analytical_flux,
-                          const LocalizableFunctionType& ratio_dt_dx,
-                          const FVSpaceType& fv_space)
+                         const LocalizableFunctionType& ratio_dt_dx,
+                         const FVSpaceType& fv_space)
     : OperatorBaseType()
-    , local_operator_(*analytical_flux, ratio_dt_dx)
-    , inner_assembler_(local_operator_)
+    , analytical_flux_(analytical_flux)
+    , ratio_dt_dx_(ratio_dt_dx)
     , fv_space_(fv_space)
   {}
 
@@ -183,17 +206,16 @@ public:
   template< class SourceType, class RangeType >
   void apply(const SourceType& source, RangeType& range) const
   {
-    AdvectionLaxFriedrichsLocalizable< InnerAssemblerType, SourceType, RangeType > localizable_operator(flux, dt, source, range);
+    AdvectionLaxFriedrichsLocalizable< AnalyticalFluxType,
+                                       LocalizableFunctionType,
+                                       SourceType,
+                                       RangeType > localizable_operator(analytical_flux_, ratio_dt_dx_, source, range);
     localizable_operator.apply();
   }
 
 private:
-  const AnalyticalFluxType& analytical_flux,_;
-                            const LocalizableFunctionType& ratio_dt_dx_;
-
-
-//  const LocalOperatorType local_operator_;
-//  const InnerAssemblerType inner_assembler_;
+  const AnalyticalFluxType& analytical_flux_;
+  const LocalizableFunctionType& ratio_dt_dx_;
   const FVSpaceType& fv_space_;
 }; // class AdvectionLaxFriedrichs
 
