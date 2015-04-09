55f2f31439f0ff3c2b32b42766135909b0b7b6af
Felix Schindler <felix.schindler@wwu.de>
2015-04-10 01:36:26 +0200

    [spaces.constraints] refactor, refs #51
    * rename Constraints::Dirichlet -> DirichletConstraints
      This is for @renemilk, only top level namespaces (below module) from now
      on...
    * DirichletConstraints is now just a container of dirichlet DoFs. On apply
      it calls clear_row or unit_row on the matrix. Each backend is supposed to
      implement this efficiently.
    
    The new constraints are used like this:
    
        Spaces::DirichletConstraints< IntersectionType >
             dirichlet_constraints(boundary_info, space.mapper().size());
        // create system assembler, add matrix and rhs assembly
        // ...
        assembler.add(dirichlet_constraints);
        assembler.assemble();
        // apply dirichlet correction on rhs if needed (for nonzero boundary
        // values) ...
        dirichlet_constraints.apply(matrix, rhs);

--- dune/gdt/spaces/constraints.hh
+++ dune/gdt/spaces/constraints.hh
@@ -8,206 +8,60 @@
 
 #include <ostream>
 
-#include <dune/common/dynvector.hh>
-#include <dune/common/dynmatrix.hh>
-
-#include <dune/stuff/grid/boundaryinfo.hh>
 #include <dune/stuff/common/crtp.hh>
+#include <dune/stuff/grid/boundaryinfo.hh>
+#include <dune/stuff/la/container/interfaces.hh>
 
 namespace Dune {
 namespace GDT {
 namespace Spaces {
 
 
-template< class Traits, class ValueImp = double >
+/**
+ * \brief CRTP interface for all implementations of constraints.
+ *
+ *        We need this interface for template matching in the SystemAssembler.
+ */
+template< class Traits >
 class ConstraintsInterface
-  : public Stuff::CRTPInterface< ConstraintsInterface< Traits, ValueImp >, Traits >
+  : public Stuff::CRTPInterface< ConstraintsInterface< Traits >, Traits >
 {
 public:
   typedef typename Traits::derived_type derived_type;
-  typedef ValueImp                      ValueType;
-
-  inline size_t rows() const
-  {
-    CHECK_CRTP(this->as_imp().rows());
-    return this->as_imp().rows();
-  }
-
-  inline size_t cols() const
-  {
-    CHECK_CRTP(this->as_imp().cols());
-    return this->as_imp().cols();
-  }
-
-  inline size_t global_row(const size_t ii) const
-  {
-    CHECK_CRTP(this->as_imp().global_row(ii));
-    return this->as_imp().global_row(ii);
-  }
-
-  inline size_t global_col(const size_t jj) const
-  {
-    CHECK_CRTP(this->as_imp().global_col(jj));
-    return this->as_imp().global_col(jj);
-  }
-
-  inline ValueType value(const size_t ii, const size_t jj) const
-  {
-    CHECK_CRTP(this->as_imp().value(ii, jj));
-    return this->as_imp().value(ii, jj);
-  }
-
-private:
-  template< class T, class V >
-  friend std::ostream& operator<<(std::ostream& /*out*/, const ConstraintsInterface< T, V >& /*constraints*/);
 }; // class ConstraintsInterface
 
 
-template< class T, class V >
-std::ostream& operator<<(std::ostream& out, const ConstraintsInterface< T, V >& constraints)
-{
-  out << "gdt.spaces.constraints of size " << constraints.rows() << " x " << constraints.cols() << ", containing\n";
-  for (size_t ii = 0; ii < constraints.rows(); ++ii)
-    for (size_t jj = 0; jj < constraints.cols(); ++jj)
-      out << "  " << constraints.global_row(ii) << ", " << constraints.global_col(jj) << ": "
-          << constraints.value(ii, jj) << "\n";
-  return out;
-} // ... operator<<(...)
-
-
-namespace Constraints {
-namespace internal {
-
-
-template< class DerivedTraits, class ValueImp >
-class Default
-  : public ConstraintsInterface< DerivedTraits, ValueImp >
-{
-  typedef ConstraintsInterface< DerivedTraits, ValueImp > BaseType;
-public:
-  typedef DerivedTraits                Traits;
-  typedef typename BaseType::ValueType ValueType;
-
-private:
-  typedef DynamicVector< size_t >    IndicesType;
-  typedef DynamicMatrix< ValueType > ValuesType;
-
-public:
-  Default(const size_t rws, const size_t cls)
-    : rows_(rws)
-    , cols_(cls)
-    , global_rows_(rows_)
-    , global_cols_(cols_)
-    , values_(rows_, cols_)
-  {}
-
-  size_t rows() const
-  {
-    return rows_;
-  }
-
-  size_t cols() const
-  {
-    return cols_;
-  }
-
-  void set_size(const size_t rr, const size_t cc)
-  {
-    rows_ = rr;
-    cols_ = cc;
-    bool changed = false;
-    if (rows_ > global_rows_.size()) {
-      global_rows_.resize(rows_, 0);
-      changed = true;
-    }
-    if (cols_ > global_cols_.size()) {
-      global_cols_.resize(cols_, 0);
-      changed = true;
-    }
-    if (changed)
-      values_.resize(global_rows_.size(), global_cols_.size(), 0);
-  } // ... set_size(...)
-
-  size_t& global_row(const size_t ii)
-  {
-    assert(ii < std::min(rows_, global_rows_.size()));
-    return global_rows_[ii];
-  }
-
-  size_t global_row(const size_t ii) const
-  {
-    assert(ii < std::min(rows_, global_rows_.size()));
-    return global_rows_[ii];
-  }
-
-  size_t& global_col(const size_t jj)
-  {
-    assert(jj < std::min(cols_, global_cols_.size()));
-    return global_cols_[jj];
-  }
-
-  size_t global_col(const size_t jj) const
-  {
-    assert(jj < std::min(cols_, global_cols_.size()));
-    return global_cols_[jj];
-  }
-
-  ValueType& value(const size_t ii, const size_t jj)
-  {
-    assert(ii < std::min(rows_, global_rows_.size()));
-    assert(jj < std::min(cols_, global_cols_.size()));
-    return values_[ii][jj];
-  }
-
-  ValueType value(const size_t ii, const size_t jj) const
-  {
-    assert(ii < std::min(rows_, global_rows_.size()));
-    assert(jj < std::min(cols_, global_cols_.size()));
-    return values_[ii][jj];
-  }
-
-private:
-  size_t rows_;
-  size_t cols_;
-  IndicesType global_rows_;
-  IndicesType global_cols_;
-  ValuesType values_;
-}; // class Default
-
-
-} // namespace internal
-
-
-template< class IntersectionType, class ValueImp = double >
-class Dirichlet;
+// forward
+template< class IntersectionType >
+class DirichletConstraints;
 
 
 namespace internal {
 
 
-template< class IntersectionType, class ValueImp >
-class DirichletTraits
+template< class IntersectionType >
+class DirichletConstraintsTraits
 {
 public:
-  typedef Dirichlet< IntersectionType, ValueImp > derived_type;
+  typedef DirichletConstraints< IntersectionType > derived_type;
 };
 
 
 } // namespace internal
 
 
-template< class IntersectionType, class ValueImp >
-class Dirichlet
-  : public internal::Default< internal::DirichletTraits< IntersectionType, ValueImp >, ValueImp >
+template< class IntersectionType >
+class DirichletConstraints
+  : public ConstraintsInterface< internal::DirichletConstraintsTraits< IntersectionType > >
 {
-  typedef internal::Default< internal::DirichletTraits< IntersectionType, ValueImp >, ValueImp > BaseType;
 public:
-  typedef Stuff::Grid::BoundaryInfoInterface< IntersectionType > BoundaryInfoType;
+  typedef internal::DirichletConstraintsTraits< IntersectionType > Traits;
+  typedef Stuff::Grid::BoundaryInfoInterface< IntersectionType >   BoundaryInfoType;
 
-  Dirichlet(const BoundaryInfoType& bnd_info, const size_t rws, const size_t cls, const bool set_rw = true)
-    : BaseType(rws, cls)
-    , boundary_info_(bnd_info)
-    , set_row_(set_rw)
+  DirichletConstraints(const BoundaryInfoType& bnd_info, const size_t sz, const bool set = true)
+    : boundary_info_(bnd_info)
+    , size_(sz)
+    , set_(set)
   {}
 
   const BoundaryInfoType& boundary_info() const
@@ -215,18 +69,59 @@ public:
     return boundary_info_;
   }
 
-  bool set_row() const
+  inline void insert(const size_t DoF)
   {
-    return set_row_;
+    assert(DoF < size_);
+    dirichlet_DoFs_.insert(DoF);
   }
+
+  template< class M >
+  void apply(Stuff::LA::MatrixInterface< M >& matrix) const
+  {
+    assert(matrix.rows() == size_);
+    if (set_) {
+      for (const auto& DoF : dirichlet_DoFs_)
+        matrix.unit_row(DoF);
+    } else {
+      for (const auto& DoF : dirichlet_DoFs_)
+        matrix.clear_row(DoF);
+    }
+  } // ... apply(...)
+
+  template< class V >
+  void apply(Stuff::LA::VectorInterface< V >& vector) const
+  {
+    assert(vector.size() == size_);
+    for (const auto& DoF : dirichlet_DoFs_)
+      vector[DoF] = 0.0;
+  }
+
+  template< class M, class V >
+  void apply(Stuff::LA::MatrixInterface< M >& matrix, Stuff::LA::VectorInterface< V >& vector) const
+  {
+    assert(matrix.rows() == size_);
+    assert(vector.size() == size_);
+    if (set_) {
+      for (const auto& DoF : dirichlet_DoFs_) {
+        matrix.unit_row(DoF);
+        vector[DoF] = 0.0;
+      }
+    } else {
+      for (const auto& DoF : dirichlet_DoFs_) {
+        matrix.clear_row(DoF);
+        vector[DoF] = 0.0;
+      }
+    }
+  } // ... apply(...)
 
 private:
   const BoundaryInfoType& boundary_info_;
-  const bool set_row_;
-}; // class Dirichlet
+  const size_t size_;
+  const bool set_;
+  std::set< size_t > dirichlet_DoFs_;
+}; // class DirichletConstraints
 
 
-} // namespace Constraints
 } // namespace Spaces
 } // namespace GDT
 } // namespace Dune
