79cbaf232588f98b459636cfd15a638bed6c6dc3
Felix Schindler <felix.schindler@wwu.de>
2015-04-15 14:17:45 +0200

    [discretefunction.default] minor update and a little docu

--- dune/gdt/discretefunction/default.hh
+++ dune/gdt/discretefunction/default.hh
@@ -109,7 +109,7 @@ public:
 
   void visualize(const std::string filename,
                  const bool subsampling = (SpaceType::polOrder > 1),
-                 VTK::OutputType vtk_output_type = VTK::appendedraw) const
+                 const VTK::OutputType vtk_output_type = VTK::appendedraw) const
   {
     BaseType::template visualize< typename SpaceType::GridViewType >(space().grid_view(),
                                                                      filename,
@@ -213,14 +213,19 @@ ConstDiscreteFunction< SpaceType,
 
 
 template< class SpaceType, class VectorType >
-DiscreteFunction< SpaceType, VectorType > make_discrete_function(const SpaceType& space,
-                                                                 VectorType& vector,
-                                                                 const std::string nm = "gdt.discretefunction")
+typename std::enable_if< is_space< SpaceType >::value && Stuff::LA::is_vector< VectorType >::value,
+                         DiscreteFunction< SpaceType, VectorType > >::type
+make_discrete_function(const SpaceType& space, VectorType& vector, const std::string nm = "gdt.discretefunction")
 {
   return DiscreteFunction< SpaceType, VectorType >(space, vector, nm);
 }
 
 
+/**
+ * This can be used like \code
+auto discrete_function = make_discrete_function< VectorType >(space);
+\endcode
+ */
 template< class VectorType, class SpaceType >
 DiscreteFunction< SpaceType, VectorType > make_discrete_function(const SpaceType& space,
                                                                  const std::string nm = "gdt.discretefunction")
