18ad0df411242abc48143a48a8acb3762a9e661c
Felix Schindler <felix.schindler@wwu.de>
2015-04-17 13:28:33 +0200

    [test.linearelliptic.eoc] update expected resulsts for buildbot builds

--- dune/gdt/tests/linearelliptic/eocexpectations-cg-ao2013-2dalugrid.cxx
+++ dune/gdt/tests/linearelliptic/eocexpectations-cg-ao2013-2dalugrid.cxx
@@ -28,15 +28,24 @@ class LinearEllipticEocExpectations< LinearElliptic::AO2013TestCase< ALUGrid< 2,
 {
   typedef LinearElliptic::AO2013TestCase< ALUGrid< 2, 2, simplex, conforming >, double, 1 > TestCaseType;
 public:
-  static std::vector< double > results(const TestCaseType& /*test_case*/, const std::string type)
+  static std::vector< double > results(const TestCaseType& test_case, const std::string type)
   {
-    if (type == "L2")
-      return {1.30e-02, 6.81e-03, 1.96e-03, 4.47e-04};
-    else if (type == "H1_semi")
-      return {4.11e-01, 3.53e-01, 1.90e-01, 8.78e-02};
-    else if (type == "energy")
-      return {9.76e-02, 5.97e-02, 3.20e-02, 1.54e-02};
-    else
+    if (type == "L2") {
+      if (test_case.num_refinements() == 1)
+        return {1.17e-02, 5.32e-03};
+      else
+        return {1.30e-02, 6.81e-03, 1.96e-03, 4.47e-04};
+    } else if (type == "H1_semi") {
+      if (test_case.num_refinements() == 1)
+        return {3.63e-01, 2.97e-01};
+      else
+        return {4.11e-01, 3.53e-01, 1.90e-01, 8.78e-02};
+    } else if (type == "energy") {
+      if (test_case.num_refinements() == 1)
+        return {9.22e-02, 5.04e-02};
+      else
+        return {9.76e-02, 5.97e-02, 3.20e-02, 1.54e-02};
+    } else
       EXPECT_TRUE(false) << "test results missing for type: " << type;
     return {};
   } // ... results(...)
@@ -51,15 +60,24 @@ class LinearEllipticEocExpectations< LinearElliptic::AO2013TestCase< ALUGrid< 2,
 {
   typedef LinearElliptic::AO2013TestCase< ALUGrid< 2, 2, simplex, nonconforming >, double, 1 > TestCaseType;
 public:
-  static std::vector< double > results(const TestCaseType& /*test_case*/, const std::string type)
+  static std::vector< double > results(const TestCaseType& test_case, const std::string type)
   {
-    if (type == "L2")
-      return {1.65e-02, 5.12e-03, 1.35e-03, 2.94e-04, };
-    else if (type == "H1_semi")
-      return {5.75e-01, 3.22e-01, 1.64e-01, 7.42e-02};
-    else if (type == "energy")
-      return {9.06e-02, 4.97e-02, 2.60e-02, 1.24e-02};
-    else
+    if (type == "L2") {
+      if (test_case.num_refinements() == 1)
+        return {1.53e-02, 3.91e-03};
+      else
+        return {1.65e-02, 5.12e-03, 1.35e-03, 2.94e-04, };
+    } else if (type == "H1_semi") {
+      if (test_case.num_refinements() == 1)
+        return {5.51e-01, 2.77e-01};
+      else
+        return {5.75e-01, 3.22e-01, 1.64e-01, 7.42e-02};
+    } else if (type == "energy") {
+      if (test_case.num_refinements() == 1)
+        return {8.68e-02, 4.24e-02};
+      else
+        return {9.06e-02, 4.97e-02, 2.60e-02, 1.24e-02};
+    } else
       EXPECT_TRUE(false) << "test results missing for type: " << type;
     return {};
   } // ... results(...)
--- dune/gdt/tests/linearelliptic/eocexpectations-cg-ao2013-2dsgrid.cxx
+++ dune/gdt/tests/linearelliptic/eocexpectations-cg-ao2013-2dsgrid.cxx
@@ -25,15 +25,24 @@ class LinearEllipticEocExpectations< LinearElliptic::AO2013TestCase< SGrid< 2, 2
 {
   typedef LinearElliptic::AO2013TestCase< SGrid< 2, 2 >, double, 1 > TestCaseType;
 public:
-  static std::vector< double > results(const TestCaseType& /*test_case*/, const std::string type)
+  static std::vector< double > results(const TestCaseType& test_case, const std::string type)
   {
-    if (type == "L2")
-      return {7.42e-03, 2.09e-03, 5.39e-04, 1.40e-04};
-    else if (type == "H1_semi")
-      return {4.91e-01, 2.42e-01, 1.23e-01, 6.11e-02};
-    else if (type == "energy")
-      return {9.00e-02, 5.90e-02, 4.15e-02, 2.81e-02};
-    else
+    if (type == "L2") {
+      if (test_case.num_refinements() == 1)
+        return {7.21e-03, 1.82e-03};
+      else
+        return {7.42e-03, 2.09e-03, 5.39e-04, 1.40e-04};
+    } else if (type == "H1_semi") {
+      if (test_case.num_refinements() == 1)
+        return {4.75e-01, 2.11e-01};
+      else
+        return {4.91e-01, 2.42e-01, 1.23e-01, 6.11e-02};
+    } else if (type == "energy") {
+      if (test_case.num_refinements() == 1)
+        return {8.73e-02, 5.14e-02};
+      else
+        return {9.00e-02, 5.90e-02, 4.15e-02, 2.81e-02};
+    } else
       EXPECT_TRUE(false) << "test results missing for type: " << type;
     return {};
   } // ... results(...)
