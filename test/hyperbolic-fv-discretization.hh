2bb208f0e078c0bec78b1fe9322208fc028c3c8f
Tobias Leibner <tobias.leibner@googlemail.com>
2015-07-24 17:09:00 +0200

    [test.hyperbolic] current version

--- test/hyperbolic-fv-discretization.hh
+++ test/hyperbolic-fv-discretization.hh
@@ -25,7 +25,7 @@ struct hyperbolic_FV_discretization
 #if THIS_IS_A_BUILDBOT_BUILD
     TestCaseType test_case(/*num_refs = */ 1);
 #else
-    TestCaseType test_case;
+    TestCaseType test_case(5);
 #endif
     test_case.print_header(DSC_LOG_INFO);
     DSC_LOG_INFO << std::endl;
--- test/hyperbolic-testcases.hh
+++ test/hyperbolic-testcases.hh
@@ -12,10 +12,10 @@
 #include <dune/gdt/tests/hyperbolic/problems/transport.hh>
 
 
-typedef testing::Types<   Dune::GDT::Hyperbolic::BurgersTestCase< Dune::YaspGrid< 1 > >
-//                        , Dune::GDT::Hyperbolic::BurgersTestCase< Dune::YaspGrid< 2 > >
+typedef testing::Types<  /* Dune::GDT::Hyperbolic::BurgersTestCase< Dune::YaspGrid< 1 > >
+                        , Dune::GDT::Hyperbolic::BurgersTestCase< Dune::YaspGrid< 2 > >
                         , Dune::GDT::Hyperbolic::TransportTestCase< Dune::YaspGrid< 1 > >
-//                        , Dune::GDT::Hyperbolic::TransportTestCase< Dune::YaspGrid< 2 > >
+                        ,*/  Dune::GDT::Hyperbolic::TransportTestCase< Dune::YaspGrid< 2 > >
                       > YaspGridTestCases;
 
 
