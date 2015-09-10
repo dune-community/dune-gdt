58c88581c3f99c8a8d0a496aafaa0cba55f21e3f
Felix Schindler <felix.schindler@wwu.de>
2015-09-10 16:32:03 +0200

    [test.operators_elliptic_operator] add

---
+++ test/operators_elliptic_operator.cc
@@ -0,0 +1,45 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#define DUNE_STUFF_TEST_MAIN_CATCH_EXCEPTIONS 1
+
+// this one has to come first
+#include <dune/stuff/test/main.hxx>
+
+#include "spaces_fv_default.hh"
+#include "spaces_dg_fem.hh"
+#include "spaces_cg_pdelab.hh"
+
+#include <dune/gdt/tests/operators/elliptic.hh>
+
+using namespace Dune::GDT::Tests;
+
+typedef testing::Types<SPACE_FV_SGRID(1, 1), SPACE_FV_SGRID(2, 1), SPACE_FV_SGRID(3, 1)> ConstantSpaces;
+
+TYPED_TEST_CASE(EllipticOperatorTest, ConstantSpaces);
+TYPED_TEST(EllipticOperatorTest, constructible_by_ctor)
+{
+  this->constructible_by_ctor();
+}
+TYPED_TEST(EllipticOperatorTest, constructible_by_factory)
+{
+  this->constructible_by_factory();
+}
+TYPED_TEST(EllipticOperatorTest, apply_is_callable)
+{
+  this->apply_is_callable();
+}
+TYPED_TEST(EllipticOperatorTest, apply2_correct_for_constant_arguments)
+{
+  this->correct_for_constant_arguments();
+}
+TYPED_TEST(EllipticOperatorTest, apply2_correct_for_linear_arguments)
+{
+  this->correct_for_linear_arguments();
+}
+TYPED_TEST(EllipticOperatorTest, apply2_correct_for_quadratic_arguments)
+{
+  this->correct_for_quadratic_arguments();
+}
