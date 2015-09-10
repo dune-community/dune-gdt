609ec368aedb47795e39f979c39de7a7b8be0510
Felix Schindler <felix.schindler@wwu.de>
2015-09-10 18:37:10 +0200

    [operators.projections.l2] add L2LocalProjectionLocalizableOperatorTest

---
+++ dune/gdt/tests/operators/projections/l2.hh
@@ -0,0 +1,31 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_GDT_TEST_OPERATORS_PROJECTIONS_L2_HH
+#define DUNE_GDT_TEST_OPERATORS_PROJECTIONS_L2_HH
+
+#include <dune/gdt/operators/projections/l2.hh>
+
+#include "../projections.hh"
+
+namespace Dune {
+namespace GDT {
+namespace Tests {
+
+
+template< class SpaceType >
+class L2LocalProjectionLocalizableOperatorTest
+  : public LocalizableProjectionOperatorBase< SpaceType, L2LocalProjectionLocalizableOperator<
+        typename SpaceType::GridViewType,
+        typename internal::OperatorBaseTraits< SpaceType >::FunctionType,
+        typename internal::OperatorBaseTraits< SpaceType >::DiscreteFunctionType > >
+{};
+
+
+} // namespace Tests
+} // namespace GDT
+} // namespace Dune
+
+#endif // DUNE_GDT_TEST_OPERATORS_PROJECTIONS_L2_HH
