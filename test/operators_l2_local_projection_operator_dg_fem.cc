0f3cdc75c6db5d18fb6f1b5b8cee5f0cccfdd048
Felix Schindler <felix.schindler@wwu.de>
2015-09-21 17:08:06 +0200

    [tests.operators_l2_local_projection_operator...] add

---
+++ test/operators_l2_local_projection_operator_dg_fem.cc
@@ -0,0 +1,41 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#define DUNE_STUFF_TEST_MAIN_ENABLE_TIMED_LOGGING 1
+
+#include <dune/stuff/test/main.hxx>
+
+#include <dune/gdt/tests/operators/projections/l2.hh>
+
+#include "spaces_dg_fem.hh"
+
+using namespace Dune::GDT::Tests;
+
+#if HAVE_DUNE_FEM
+
+
+typedef testing::Types< SPACES_DG_FEM(1)
+#if HAVE_ALUGRID
+                      , SPACES_DG_FEM_ALUGRID(1)
+#endif
+                      > SpaceTypes;
+
+TYPED_TEST_CASE(L2LocalProjectionOperatorTest, SpaceTypes);
+TYPED_TEST(L2LocalProjectionOperatorTest, constructible_by_ctor) {
+ this->constructible_by_ctor();
+}
+TYPED_TEST(L2LocalProjectionOperatorTest, produces_correct_results) {
+ this->produces_correct_results();
+}
+
+
+#else // HAVE_DUNE_FEM
+
+
+TEST(DISABLED_L2LocalProjectionOperatorTest, constructible_by_ctor) {}
+TEST(DISABLED_L2LocalProjectionOperatorTest, produces_correct_results) {}
+
+
+#endif // HAVE_DUNE_FEM
