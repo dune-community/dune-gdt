84e87f99fdd7f879160bcd689ea9b4d6dc815296
Felix Schindler <felix.schindler@wwu.de>
2015-10-01 15:28:03 +0200

    [test.operators_l2_local_prolongation_localizable...] add

---
+++ test/operators_l2_local_prolongation_localizable_operator_rt_pdelab.cc
@@ -0,0 +1,35 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#include <dune/stuff/test/main.hxx>
+
+#include <dune/gdt/tests/operators/prolongations/l2.hh>
+
+#include "spaces_rt_pdelab.hh"
+
+using namespace Dune::GDT::Tests;
+
+#if HAVE_DUNE_PDELAB
+
+
+typedef testing::Types< SPACES_RT_PDELAB_LEVEL
+#if HAVE_ALUGRID
+                      , SPACES_RT_PDELAB_ALUGRID_LEVEL
+#endif
+                      > SpaceTypes;
+
+TYPED_TEST_CASE(L2LocalProlongationLocalizableOperatorTest, SpaceTypes);
+TYPED_TEST(L2LocalProlongationLocalizableOperatorTest, produces_correct_results) {
+  this->produces_correct_results(0.204125);
+}
+
+
+#else // HAVE_DUNE_PDELAB
+
+
+TEST(DISABLED_L2LocalProlongationLocalizableOperatorTest, produces_correct_results) {}
+
+
+#endif // HAVE_DUNE_PDELAB
