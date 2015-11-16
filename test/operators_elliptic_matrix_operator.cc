a3a46ddcab91d3a23ba57ff8c6211e9671e930c4
Felix Schindler <felix.schindler@wwu.de>
2015-11-16 14:18:11 +0100

    [test.operators_elliptic_matrix_operator] refactor

--- test/operators_elliptic_matrix_operator.cc
+++ test/operators_elliptic_matrix_operator.cc
@@ -14,12 +14,27 @@
 
 using namespace Dune::GDT::Tests;
 
+
 #if HAVE_DUNE_FEM
 
 typedef testing::Types<SPACE_DG_FEM_SGRID(1, 1, 3), SPACE_DG_FEM_SGRID(2, 1, 3), SPACE_DG_FEM_SGRID(3, 1, 3)>
     CubicSpaces;
-
 TYPED_TEST_CASE(EllipticMatrixOperatorTest, CubicSpaces);
+
+#elif HAVE_DUNE_PDELAB // HAVE_DUNE_FEM
+
+typedef testing::Types<SPACE_CG_PDELAB_SGRID(1, 1, 1), SPACE_CG_PDELAB_SGRID(2, 1, 1), SPACE_CG_PDELAB_SGRID(3, 1, 1)>
+    LinearSpaces;
+TYPED_TEST_CASE(EllipticMatrixOperatorTest, LinearSpaces);
+
+#else // HAVE_DUNE_FEM || HAVE_DUNE_PDELAB
+
+typedef testing::Types<SPACE_FV_SGRID(1, 1), SPACE_FV_SGRID(2, 1), SPACE_FV_SGRID(3, 1)> ConstantSpaces;
+TYPED_TEST_CASE(EllipticMatrixOperatorTest, ConstantSpaces);
+
+#endif // HAVE_DUNE_FEM || HAVE_DUNE_PDELAB
+
+
 TYPED_TEST(EllipticMatrixOperatorTest, constructible_by_ctor)
 {
   this->constructible_by_ctor();
@@ -32,10 +47,20 @@ TYPED_TEST(EllipticMatrixOperatorTest, is_matrix_operator)
 {
   this->is_matrix_operator();
 }
+
+#if HAVE_DUNE_FEM || HAVE_DUNE_PDELAB
 TYPED_TEST(EllipticMatrixOperatorTest, correct_for_constant_arguments)
 {
   this->correct_for_constant_arguments();
 }
+#else
+TEST(DISABLED_EllipticMatrixOperatorTest, correct_for_constant_arguments)
+{
+  std::cerr << DSC::colorStringRed("Missing dependencies!") << std::endl;
+}
+#endif
+
+#if HAVE_DUNE_FEM
 TYPED_TEST(EllipticMatrixOperatorTest, correct_for_linear_arguments)
 {
   this->correct_for_linear_arguments();
@@ -44,53 +69,13 @@ TYPED_TEST(EllipticMatrixOperatorTest, correct_for_quadratic_arguments)
 {
   this->correct_for_quadratic_arguments();
 }
-
-#elif HAVE_DUNE_PDELAB // HAVE_DUNE_FEM
-
-typedef testing::Types<SPACE_CG_PDELAB_SGRID(1, 1, 1), SPACE_CG_PDELAB_SGRID(2, 1, 1), SPACE_CG_PDELAB_SGRID(3, 1, 1)>
-    LinearSpaces;
-
-TYPED_TEST_CASE(EllipticMatrixOperatorTest, LinearSpaces);
-TYPED_TEST(EllipticMatrixOperatorTest, constructible_by_ctor)
+#else // HAVE_DUNE_FEM
+TEST(DISABLED_EllipticMatrixOperatorTest, correct_for_linear_arguments)
 {
-  this->constructible_by_ctor();
+  std::cerr << DSC::colorStringRed("Missing dependencies!") << std::endl;
 }
-TYPED_TEST(EllipticMatrixOperatorTest, constructible_by_factory)
+TEST(DISABLED_EllipticMatrixOperatorTest, correct_for_quadratic_arguments)
 {
-  this->constructible_by_factory();
+  std::cerr << DSC::colorStringRed("Missing dependencies!") << std::endl;
 }
-TYPED_TEST(EllipticMatrixOperatorTest, is_matrix_operator)
-{
-  this->is_matrix_operator();
-}
-TYPED_TEST(EllipticMatrixOperatorTest, correct_for_constant_arguments)
-{
-  this->correct_for_constant_arguments();
-}
-
-TEST(DISABLED_EllipticMatrixOperatorTest, correct_for_linear_arguments){};
-TEST(DISABLED_EllipticMatrixOperatorTest, correct_for_quadratic_arguments){};
-
-#else // HAVE_DUNE_PDELAB // HAVE_DUNE_FEM
-
-typedef testing::Types<SPACE_FV_SGRID(1, 1), SPACE_FV_SGRID(2, 1), SPACE_FV_SGRID(3, 1)> ConstantSpaces;
-
-TYPED_TEST_CASE(EllipticMatrixOperatorTest, ConstantSpaces);
-TYPED_TEST(EllipticMatrixOperatorTest, constructible_by_ctor)
-{
-  this->constructible_by_ctor();
-}
-TYPED_TEST(EllipticMatrixOperatorTest, constructible_by_factory)
-{
-  this->constructible_by_factory();
-}
-TYPED_TEST(EllipticMatrixOperatorTest, is_matrix_operator)
-{
-  this->is_matrix_operator();
-}
-
-TEST(DISABLED_EllipticMatrixOperatorTest, correct_for_constant_arguments){};
-TEST(DISABLED_EllipticMatrixOperatorTest, correct_for_linear_arguments){};
-TEST(DISABLED_EllipticMatrixOperatorTest, correct_for_quadratic_arguments){};
-
-#endif // HAVE_DUNE_PDELAB // HAVE_DUNE_FEM
+#endif // HAVE_DUNE_FEM
