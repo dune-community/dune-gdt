a90f8912c2fab111dce6cf7ce475e2ba22421aa8
Felix Schindler <felix.schindler@wwu.de>
2015-11-16 08:06:37 +0100

    [test.spaces_cg_pdelab] add missing namespaces

--- test/spaces_cg_pdelab.hh
+++ test/spaces_cg_pdelab.hh
@@ -13,9 +13,10 @@
 #if HAVE_DUNE_PDELAB
 
 
-#define SPACE_CG_PDELAB_SGRID(dd, rr, pp) Spaces::CG::PdelabBased<S##dd##dLeafGridViewType, pp, double, rr>
+#define SPACE_CG_PDELAB_SGRID(dd, rr, pp) Dune::GDT::Spaces::CG::PdelabBased<S##dd##dLeafGridViewType, pp, double, rr>
 
-#define SPACE_CG_PDELAB_YASPGRID(dd, rr, pp) Spaces::CG::PdelabBased<Yasp##dd##dLeafGridViewType, pp, double, rr>
+#define SPACE_CG_PDELAB_YASPGRID(dd, rr, pp)                                                                           \
+  Dune::GDT::Spaces::CG::PdelabBased<Yasp##dd##dLeafGridViewType, pp, double, rr>
 
 #define SPACES_CG_PDELAB(pp)                                                                                           \
   SPACE_CG_PDELAB_SGRID(1, pp, 1)                                                                                      \
@@ -27,9 +28,10 @@
 
 
 #define SPACE_CG_PDELAB_ALUCONFORMGRID(dd, rr, pp)                                                                     \
-  Spaces::CG::PdelabBased<AluConform##dd##dLeafGridViewType, pp, double, rr>
+  Dune::GDT::Spaces::CG::PdelabBased<AluConform##dd##dLeafGridViewType, pp, double, rr>
 
-#define SPACE_CG_PDELAB_ALUCUBEGRID(dd, rr, pp) Spaces::CG::PdelabBased<AluCube##dd##dLeafGridViewType, pp, double, rr>
+#define SPACE_CG_PDELAB_ALUCUBEGRID(dd, rr, pp)                                                                        \
+  Dune::GDT::Spaces::CG::PdelabBased<AluCube##dd##dLeafGridViewType, pp, double, rr>
 
 #define SPACES_CG_PDELAB_ALUGRID(pp)                                                                                   \
   SPACE_CG_PDELAB_ALUCONFORMGRID(2, 1, pp)                                                                             \
