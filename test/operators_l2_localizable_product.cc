20642c1aa39099b7c3f4f865f9aef1eac6a865f3
Felix Schindler <felix.schindler@wwu.de>
2015-11-16 20:14:25 +0100

    [test.operators_l2_localizbale_product] add

---
+++ test/operators_l2_localizable_product.cc
@@ -0,0 +1,39 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+// this one has to come first
+#include <dune/stuff/test/main.hxx>
+
+#include "spaces_fv_default.hh"
+
+#include <dune/gdt/tests/operators/l2.hh>
+
+using namespace Dune::GDT::Tests;
+
+
+typedef testing::Types< SPACE_FV_SGRID(1, 1)
+                      , SPACE_FV_SGRID(2, 1)
+                      , SPACE_FV_SGRID(3, 1)
+                      > ConstantSpaces;
+
+TYPED_TEST_CASE(L2LocalizableProductTest, ConstantSpaces);
+TYPED_TEST(L2LocalizableProductTest, constructible_by_ctor) {
+  this->constructible_by_ctor();
+}
+TYPED_TEST(L2LocalizableProductTest, constructible_by_factory) {
+  this->constructible_by_factory();
+}
+TYPED_TEST(L2LocalizableProductTest, is_localizable_product) {
+  this->is_localizable_product();
+}
+TYPED_TEST(L2LocalizableProductTest, correct_for_constant_arguments) {
+  this->correct_for_constant_arguments();
+}
+TYPED_TEST(L2LocalizableProductTest, correct_for_linear_arguments) {
+  this->correct_for_linear_arguments();
+}
+TYPED_TEST(L2LocalizableProductTest, correct_for_quadratic_arguments) {
+  this->correct_for_quadratic_arguments();
+}
