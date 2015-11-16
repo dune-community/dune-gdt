9cb479f401ec3b8be744bbf6052089d1fe976ac3
Felix Schindler <felix.schindler@wwu.de>
2015-11-16 14:37:38 +0100

    [operators.weighted-l2] add WeightedL2Operator and factory

--- dune/gdt/operators/weighted-l2.hh
+++ dune/gdt/operators/weighted-l2.hh
@@ -237,6 +237,110 @@ make_weighted_l2_matrix_operator(const WeightFunctionType& weight, MatrixType& m
 }
 
 
+// ////////////////// //
+// WeightedL2Operator //
+// ////////////////// //
+
+// forward, needed for the traits
+template <class WeightFunctionType, class GridView, class Field = typename WeightFunctionType::RangeFieldType>
+class WeightedL2Operator;
+
+
+namespace internal {
+
+
+template <class WeightFunctionType, class GridViewType, class Field>
+class WeightedL2OperatorTraits
+{
+public:
+  typedef WeightedL2Operator<WeightFunctionType, GridViewType, Field> derived_type;
+  typedef Field FieldType;
+};
+
+
+} // namespace internal
+
+
+template <class WeightFunctionType, class GridViewType, class Field>
+class WeightedL2Operator
+    : public OperatorInterface<internal::WeightedL2OperatorTraits<WeightFunctionType, GridViewType, Field>>
+{
+  typedef OperatorInterface<internal::WeightedL2OperatorTraits<WeightFunctionType, GridViewType, Field>> BaseType;
+
+public:
+  using typename BaseType::FieldType;
+
+  WeightedL2Operator(const WeightFunctionType& weight, GridViewType grid_view, const size_t over_integrate = 0)
+    : weight_(weight)
+    , grid_view_(grid_view)
+    , over_integrate_(over_integrate)
+  {
+  }
+
+  template <class SourceSpaceType, class VectorType, class RangeSpaceType>
+  void apply(const DiscreteFunction<SourceSpaceType, VectorType>& source,
+             DiscreteFunction<RangeSpaceType, VectorType>& range) const
+  {
+    typedef typename Stuff::LA::Container<typename VectorType::ScalarType, VectorType::sparse_matrix_type>::MatrixType
+        MatrixType;
+    auto op = make_weighted_l2_matrix_operator<MatrixType>(
+        weight_, source.space(), range.space(), grid_view_, over_integrate_);
+    op->apply(source, range);
+  }
+
+  template <class E, class D, size_t d, class R, size_t r, size_t rC>
+  FieldType apply2(const Stuff::LocalizableFunctionInterface<E, D, d, R, r, rC>& range,
+                   const Stuff::LocalizableFunctionInterface<E, D, d, R, r, rC>& source) const
+  {
+    auto product = make_weighted_l2_localizable_product(weight_, grid_view_, range, source, over_integrate_);
+    return product->apply2();
+  }
+
+  using BaseType::apply_inverse;
+
+  template <class RangeType, class SourceType>
+  void apply_inverse(const RangeType& /*range*/, SourceType& /*source*/,
+                     const Stuff::Common::Configuration& /*opts*/) const
+  {
+    DUNE_THROW(NotImplemented, "yet");
+  }
+
+  std::vector<std::string> invert_options() const
+  {
+    DUNE_THROW(NotImplemented, "yet");
+    return {"depends_on_the_vector_type_of_the_discrete_function"};
+  }
+
+  Stuff::Common::Configuration invert_options(const std::string& /*type*/) const
+  {
+    DUNE_THROW(NotImplemented, "yet");
+  }
+
+private:
+  const WeightFunctionType& weight_;
+  GridViewType grid_view_;
+  const size_t over_integrate_;
+}; // class WeightedL2Operator
+
+
+// ///////////////////////// //
+// make_weighted_l2_operator //
+// ///////////////////////// //
+
+template <class GridViewType, class WeightFunctionType>
+typename std::enable_if<Stuff::Grid::is_grid_layer<GridViewType>::value
+                            && Stuff::is_localizable_function<WeightFunctionType>::value,
+                        std::unique_ptr<WeightedL2Operator<WeightFunctionType, GridViewType,
+                                                           typename WeightFunctionType::RangeFieldType>>>::type
+make_weighted_l2_operator(const WeightFunctionType& weight, const GridViewType& grid_view,
+                          const size_t over_integrate = 0)
+{
+  return DSC::
+      make_unique<WeightedL2Operator<WeightFunctionType, GridViewType, typename WeightFunctionType::RangeFieldType>>(
+          weight, grid_view, over_integrate);
+}
+
+
 } // namespace GDT
 } // namespace Dune
 
