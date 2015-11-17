5717101fa6eb7b1a8f28dc64d2622d07900f624b
Felix Schindler <felix.schindler@wwu.de>
2015-11-17 16:46:55 +0100

    [test.operators_laplace_matrix_operator] add

---
+++ test/operators_laplace_matrix_operator.cc
@@ -0,0 +1,81 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+// this one has to come first
+#include <dune/stuff/test/main.hxx>
+
+#include "spaces_fv_default.hh"
+#include "spaces_dg_fem.hh"
+#include "spaces_cg_pdelab.hh"
+
+#include <dune/gdt/tests/operators/laplace.hh>
+
+using namespace Dune::GDT::Tests;
+
+
+#if HAVE_DUNE_FEM
+
+typedef testing::Types<SPACE_DG_FEM_SGRID(1, 1, 3), SPACE_DG_FEM_SGRID(2, 1, 3), SPACE_DG_FEM_SGRID(3, 1, 3)>
+    CubicSpaces;
+TYPED_TEST_CASE(LaplaceMatrixOperatorTest, CubicSpaces);
+
+#elif HAVE_DUNE_PDELAB // HAVE_DUNE_FEM
+
+typedef testing::Types<SPACE_CG_PDELAB_SGRID(1, 1, 1), SPACE_CG_PDELAB_SGRID(2, 1, 1), SPACE_CG_PDELAB_SGRID(3, 1, 1)>
+    LinearSpaces;
+TYPED_TEST_CASE(LaplaceMatrixOperatorTest, LinearSpaces);
+
+#else // HAVE_DUNE_FEM || HAVE_DUNE_PDELAB
+
+typedef testing::Types<SPACE_FV_SGRID(1, 1), SPACE_FV_SGRID(2, 1), SPACE_FV_SGRID(3, 1)> ConstantSpaces;
+TYPED_TEST_CASE(LaplaceMatrixOperatorTest, ConstantSpaces);
+
+#endif // HAVE_DUNE_FEM || HAVE_DUNE_PDELAB
+
+
+TYPED_TEST(LaplaceMatrixOperatorTest, constructible_by_ctor)
+{
+  this->constructible_by_ctor();
+}
+TYPED_TEST(LaplaceMatrixOperatorTest, constructible_by_factory)
+{
+  this->constructible_by_factory();
+}
+TYPED_TEST(LaplaceMatrixOperatorTest, is_matrix_operator)
+{
+  this->is_matrix_operator();
+}
+
+#if HAVE_DUNE_FEM || HAVE_DUNE_PDELAB
+TYPED_TEST(LaplaceMatrixOperatorTest, correct_for_constant_arguments)
+{
+  this->correct_for_constant_arguments();
+}
+#else
+TEST(DISABLED_LaplaceMatrixOperatorTest, correct_for_constant_arguments)
+{
+  std::cerr << DSC::colorStringRed("Missing dependencies!") << std::endl;
+}
+#endif
+
+#if HAVE_DUNE_FEM
+TYPED_TEST(LaplaceMatrixOperatorTest, correct_for_linear_arguments)
+{
+  this->correct_for_linear_arguments();
+}
+TYPED_TEST(LaplaceMatrixOperatorTest, correct_for_quadratic_arguments)
+{
+  this->correct_for_quadratic_arguments();
+}
+#else // HAVE_DUNE_FEM
+TEST(DISABLED_LaplaceMatrixOperatorTest, correct_for_linear_arguments)
+{
+  std::cerr << DSC::colorStringRed("Missing dependencies!") << std::endl;
+}
+TEST(DISABLED_LaplaceMatrixOperatorTest, correct_for_quadratic_arguments)
+{
+  std::cerr << DSC::colorStringRed("Missing dependencies!") << std::endl;
+}
+#endif // HAVE_DUNE_FEM
