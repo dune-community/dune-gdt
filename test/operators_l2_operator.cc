7494cac2bda535a0f8e6b2cb016840f4cacb9118
Felix Schindler <felix.schindler@wwu.de>
2015-11-17 12:37:03 +0100

    [test.operators_l2_operator] add

---
+++ test/operators_l2_operator.cc
@@ -0,0 +1,43 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+// this one has to come first
+#include <dune/stuff/test/main.hxx>
+
+#include "spaces_fv_default.hh"
+#include "spaces_dg_fem.hh"
+#include "spaces_cg_pdelab.hh"
+
+#include <dune/gdt/tests/operators/l2.hh>
+
+using namespace Dune::GDT::Tests;
+
+typedef testing::Types<SPACE_FV_SGRID(1, 1), SPACE_FV_SGRID(2, 1), SPACE_FV_SGRID(3, 1)> ConstantSpaces;
+
+TYPED_TEST_CASE(L2OperatorTest, ConstantSpaces);
+TYPED_TEST(L2OperatorTest, constructible_by_ctor)
+{
+  this->constructible_by_ctor();
+}
+TYPED_TEST(L2OperatorTest, constructible_by_factory)
+{
+  this->constructible_by_factory();
+}
+TYPED_TEST(L2OperatorTest, apply_is_callable)
+{
+  this->apply_is_callable();
+}
+TYPED_TEST(L2OperatorTest, apply2_correct_for_constant_arguments)
+{
+  this->correct_for_constant_arguments();
+}
+TYPED_TEST(L2OperatorTest, apply2_correct_for_linear_arguments)
+{
+  this->correct_for_linear_arguments();
+}
+TYPED_TEST(L2OperatorTest, apply2_correct_for_quadratic_arguments)
+{
+  this->correct_for_quadratic_arguments();
+}
