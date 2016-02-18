751f6423066e26f5bbe905ff8e178c75e620da63
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-02-18 10:32:27 +0100

    [localoperator.interfaces] add Local{Coupling,Boundary}OperatorInterface

--- dune/gdt/localoperator/interfaces.hh
+++ dune/gdt/localoperator/interfaces.hh
@@ -146,6 +146,32 @@ public:
 }; // class LocalBoundaryTwoFormInterface
 
 
+template <class Traits>
+class LocalCouplingOperatorInterface : Stuff::CRTPInterface<LocalCouplingOperatorInterface<Traits>, Traits>
+{
+public:
+  template <class SourceType, class IntersectionType, class SpaceType, class VectorType>
+  void apply(const SourceType& source, const IntersectionType& intersection,
+             LocalDiscreteFunction<SpaceType, VectorType>& local_range_entity,
+             LocalDiscreteFunction<SpaceType, VectorType>& local_range_neighbor)
+  {
+    this->as_imp().apply(source, intersection, local_range_entity, local_range_neighbor);
+  }
+};
+
+template <class Traits>
+class LocalBoundaryOperatorInterface : Stuff::CRTPInterface<LocalBoundaryOperatorInterface<Traits>, Traits>
+{
+public:
+  template <class SourceType, class IntersectionType, class SpaceType, class VectorType>
+  void apply(const SourceType& source, const IntersectionType& intersection,
+             LocalDiscreteFunction<SpaceType, VectorType>& local_range_entity)
+  {
+    this->as_imp().apply(source, intersection, local_range_entity);
+  }
+};
+
+
 namespace internal {
 
 
