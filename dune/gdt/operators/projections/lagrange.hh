32a049e5c286f6a2f79264863574630484e0cfaf
Felix Schindler <felix.schindler@wwu.de>
2016-02-22 20:35:05 +0100

    [operators.projections.lagrange] add operator, factory and free function

--- dune/gdt/operators/projections/lagrange.hh
+++ dune/gdt/operators/projections/lagrange.hh
@@ -76,6 +76,116 @@ typename std::
 }
 
 
+// forward
+template <class GridViewImp, class FieldImp = double>
+class LagrangeProjectionOperator;
+
+
+namespace internal {
+
+
+template <class GridViewImp, class FieldImp>
+class LagrangeProjectionOperatorTraits
+{
+public:
+  typedef LagrangeProjectionOperator<GridViewImp, FieldImp> derived_type;
+  typedef FieldImp FieldType;
+};
+
+
+} // namespace internal
+
+
+template <class GridViewImp, class FieldImp>
+class LagrangeProjectionOperator
+    : public OperatorInterface<internal::LagrangeProjectionOperatorTraits<GridViewImp, FieldImp>>
+{
+  typedef OperatorInterface<internal::LagrangeProjectionOperatorTraits<GridViewImp, FieldImp>> BaseType;
+
+public:
+  typedef internal::LagrangeProjectionOperatorTraits<GridViewImp, FieldImp> Traits;
+  typedef GridViewImp GridViewType;
+  using typename BaseType::FieldType;
+
+private:
+  typedef typename Stuff::Grid::Entity<GridViewType>::Type E;
+  typedef typename GridViewType::ctype D;
+  static const size_t d = GridViewType::dimension;
+
+public:
+  LagrangeProjectionOperator(GridViewType grid_view)
+    : grid_view_(grid_view)
+  {
+  }
+
+  template <class R, size_t r, size_t rC, class S, class V>
+  void apply(const Stuff::LocalizableFunctionInterface<E, D, d, R, r, rC>& source, DiscreteFunction<S, V>& range) const
+  {
+    make_lagrange_projection_localizable_operator(grid_view_, source, range)->apply();
+  }
+
+  template <class RangeType, class SourceType>
+  FieldType apply2(const RangeType& /*range*/, const SourceType& /*source*/) const
+  {
+    DUNE_THROW(NotImplemented, "Go ahead if you think this makes sense!");
+  }
+
+  template <class RangeType, class SourceType>
+  void apply_inverse(const RangeType& /*range*/, SourceType& /*source*/,
+                     const Stuff::Common::Configuration& /*opts*/) const
+  {
+    DUNE_THROW(NotImplemented, "Go ahead if you think this makes sense!");
+  }
+
+  std::vector<std::string> invert_options() const
+  {
+    DUNE_THROW(NotImplemented, "Go ahead if you think this makes sense!");
+  }
+
+  Stuff::Common::Configuration invert_options(const std::string& /*type*/) const
+  {
+    DUNE_THROW(NotImplemented, "Go ahead if you think this makes sense!");
+  }
+
+private:
+  GridViewType grid_view_;
+}; // class LagrangeProjectionOperator
+
+
+template <class GridViewType>
+typename std::enable_if<Stuff::Grid::is_grid_layer<GridViewType>::value,
+                        std::unique_ptr<LagrangeProjectionOperator<GridViewType>>>::type
+make_lagrange_projection_operator(const GridViewType& grid_view)
+{
+  return DSC::make_unique<LagrangeProjectionOperator<GridViewType>>(grid_view);
+}
+
+
+template <class GridViewType, class SourceType, class SpaceType, class VectorType>
+typename std::
+    enable_if<Stuff::Grid::is_grid_layer<GridViewType>::value && Stuff::is_localizable_function<SourceType>::value
+                  && is_space<SpaceType>::value && Stuff::LA::is_vector<VectorType>::value,
+              std::unique_ptr<LagrangeProjectionLocalizableOperator<GridViewType, SourceType,
+                                                                    DiscreteFunction<SpaceType, VectorType>>>>::type
+    project_lagrange(const GridViewType& grid_view, const SourceType& source,
+                     DiscreteFunction<SpaceType, VectorType>& range)
+{
+  make_lagrange_projection_operator(grid_view)->apply(source, range);
+}
+
+
+template <class SourceType, class SpaceType, class VectorType>
+typename std::
+    enable_if<Stuff::is_localizable_function<SourceType>::value && is_space<SpaceType>::value
+                  && Stuff::LA::is_vector<VectorType>::value,
+              std::unique_ptr<LagrangeProjectionLocalizableOperator<typename SpaceType::GridViewType, SourceType,
+                                                                    DiscreteFunction<SpaceType, VectorType>>>>::type
+    project_lagrange(const SourceType& source, DiscreteFunction<SpaceType, VectorType>& range)
+{
+  make_lagrange_projection_operator(range.space().grid_view())->apply(source, range);
+}
+
+
 } // namespace GDT
 } // namespace Dune
 
