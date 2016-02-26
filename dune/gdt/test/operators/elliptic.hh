2d6c8bc44a299dd7997b75fd6114d49835095c0d
Felix Schindler <felix.schindler@wwu.de>
2016-02-26 14:08:53 +0100

    fix ambiguous types for gcc

--- dune/gdt/test/operators/elliptic.hh
+++ dune/gdt/test/operators/elliptic.hh
@@ -177,12 +177,12 @@ struct EllipticMatrixOperatorTest
 {
   typedef EllipticProductBase< SpaceType > EllipticBaseType;
   typedef MatrixOperatorBase< SpaceType > MatrixBaseType;
-  using typename MatrixBaseType::GridViewType;
+  typedef typename MatrixBaseType::GridViewType GridViewType;
   using typename EllipticBaseType::ExpressionFunctionType;
   using typename MatrixBaseType::DiscreteFunctionType;
   using typename MatrixBaseType::ScalarFunctionType;
   using typename MatrixBaseType::TensorFunctionType;
-  using typename MatrixBaseType::RangeFieldType;
+  typedef typename MatrixBaseType::RangeFieldType RangeFieldType;
   using typename MatrixBaseType::MatrixType;
 
   EllipticMatrixOperatorTest(const double factor_value = 42.)
