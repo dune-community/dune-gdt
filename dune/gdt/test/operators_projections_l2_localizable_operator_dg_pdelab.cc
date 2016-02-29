45ad3e5a1f4933745c85f834d1902e5be5398c89
Felix Schindler <felix.schindler@wwu.de>
2016-02-29 22:30:31 +0100

    [operators.projections.l2] add tests for L2ProjectionLocalizableOperator

---
+++ dune/gdt/test/operators_projections_l2_localizable_operator_dg_pdelab.cc
@@ -0,0 +1,52 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#include <dune/stuff/test/main.hxx>
+
+#include "operators/projections/l2.hh"
+#include "spaces/dg/pdelab.hh"
+
+using namespace Dune::GDT::Test;
+
+#if HAVE_DUNE_PDELAB
+
+
+typedef testing::Types<SPACES_DG_PDELAB(1)
+#if HAVE_ALUGRID
+                           ,
+                       SPACES_DG_PDELAB_ALUGRID(1)
+#endif
+                       > SpaceTypes;
+
+TYPED_TEST_CASE(L2ProjectionLocalizableOperatorTest, SpaceTypes);
+TYPED_TEST(L2ProjectionLocalizableOperatorTest, constructible_by_ctor)
+{
+  this->constructible_by_ctor();
+}
+TYPED_TEST(L2ProjectionLocalizableOperatorTest, constructible_by_factory)
+{
+  this->constructible_by_factory();
+}
+TYPED_TEST(L2ProjectionLocalizableOperatorTest, produces_correct_results)
+{
+  this->produces_correct_results();
+}
+
+
+#else // HAVE_DUNE_PDELAB
+
+
+TEST(DISABLED_L2ProjectionLocalizableOperatorTest, constructible_by_ctor)
+{
+}
+TEST(DISABLED_L2ProjectionLocalizableOperatorTest, constructible_by_factory)
+{
+}
+TEST(DISABLED_L2ProjectionLocalizableOperatorTest, produces_correct_results)
+{
+}
+
+
+#endif // HAVE_DUNE_PDELAB
