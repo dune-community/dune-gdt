f66739edaa86344bd8566722618b74f366d4db33
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-03-01 17:24:08 +0100

    [test.hyperbolic.fv-discretization] dont visualize solution

--- dune/gdt/test/hyperbolic/fv-discretization.hh
+++ dune/gdt/test/hyperbolic/fv-discretization.hh
@@ -31,7 +31,7 @@ struct hyperbolic_FV_discretization : public ::testing::Test
     typedef Hyperbolic::
         FVDiscretizer<typename TestCaseType::GridType, double, TestCaseType::dimRange, TestCaseType::dimRangeCols, true>
             Discretizer;
-    Tests::HyperbolicEocStudy<TestCaseType, Discretizer> eoc_study(test_case, {}, "2dtransport");
+    Tests::HyperbolicEocStudy<TestCaseType, Discretizer> eoc_study(test_case, {});
     Stuff::Test::check_eoc_study_for_success(eoc_study, eoc_study.run(DSC_LOG_INFO));
   } // ... eoc_study()
 
