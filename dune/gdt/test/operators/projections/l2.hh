4bd3b1151c83aded2c68d3b237c8dacd4006ed1f
Felix Schindler <felix.schindler@wwu.de>
2016-03-01 09:52:50 +0100

    [operators.projections.l2] add tests for L2ProjectionOperator

--- dune/gdt/test/operators/projections/l2.hh
+++ dune/gdt/test/operators/projections/l2.hh
@@ -34,6 +34,29 @@ struct L2ProjectionLocalizableOperatorTest
 };
 
 
+template <class SpaceType>
+struct L2ProjectionOperatorTest
+    : public ProjectionOperatorBase<SpaceType, L2ProjectionOperator<typename SpaceType::GridViewType, double>>
+{
+  void constructible_by_factory()
+  {
+    const auto& grid_view = this->space_.grid_view();
+    auto DUNE_UNUSED(op_w_over_integrate) = make_l2_projection_operator(grid_view, 1);
+    auto DUNE_UNUSED(op_wo_over_integrate) = make_l2_projection_operator(grid_view);
+  } // ... constructible_by_factory(...)
+
+  void free_function_callable()
+  {
+    const auto& grid_view = this->space_.grid_view();
+    const auto& source    = this->function_;
+    auto& range           = this->discrete_function_;
+
+    Dune::GDT::project_l2(grid_view, source, range);
+    Dune::GDT::project_l2(source, range);
+  } // ... free_function_callable(...)
+};
+
+
 } // namespace Test
 } // namespace GDT
 } // namespace Dune
