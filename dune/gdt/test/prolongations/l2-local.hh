94330b6c88447a479e072b59b670809f7d5e4278
Felix Schindler <felix.schindler@wwu.de>
2016-03-03 10:01:45 +0100

    [prolongations.l2-local] add tests

--- dune/gdt/test/prolongations/l2-local.hh
+++ dune/gdt/test/prolongations/l2-local.hh
@@ -6,6 +6,8 @@
 #ifndef DUNE_GDT_TEST_PROLONGATIONS_L2_LOCAL_HH
 #define DUNE_GDT_TEST_PROLONGATIONS_L2_LOCAL_HH
 
+#include <dune/common/unused.hh>
+
 #include <dune/gdt/prolongations/l2-local.hh>
 
 #include "base.hh"
@@ -16,9 +18,38 @@ namespace Test {
 
 
 template< class SpaceType >
-class L2LocalProlongationLocalizableOperatorTest
+struct L2LocalProlongationLocalizableOperatorTest
   : public LocalizableProlongationOperatorBase< SpaceType, SpaceType, L2LocalProlongationLocalizableOperator >
-{};
+{
+  typedef LocalizableProlongationOperatorBase< SpaceType, SpaceType, L2LocalProlongationLocalizableOperator > BaseType;
+  using typename BaseType::ProlongationOperatorType;
+
+  void constructible_by_ctor(const double tolerance = 1e-15)
+  {
+    this->prepare(tolerance);
+
+    auto grid_view = this->fine_space_.grid_view();
+    const auto& source = this->coarse_discrete_function_;
+    auto& range = this->fine_discrete_function_;
+
+    ProlongationOperatorType DUNE_UNUSED(w_over_integrate)(0, grid_view, source, range);
+    ProlongationOperatorType DUNE_UNUSED(wo_over_integrate)(  grid_view, source, range);
+  } // ... constructible_by_ctor(...)
+
+  void constructible_by_factory(const double tolerance = 1e-15)
+  {
+    this->prepare(tolerance);
+
+    auto grid_view = this->fine_space_.grid_view();
+    const auto& source = this->coarse_discrete_function_;
+    auto& range = this->fine_discrete_function_;
+
+    auto DUNE_UNUSED(w_gv_w_oi)   = make_local_l2_prolongation_localizable_operator(grid_view, source, range, 1);
+    auto DUNE_UNUSED(w_gv_wo_oi)  = make_local_l2_prolongation_localizable_operator(grid_view, source, range);
+    auto DUNE_UNUSED(wo_gv_w_oi)  = make_local_l2_prolongation_localizable_operator(           source, range, 1);
+    auto DUNE_UNUSED(wo_gv_wo_oi) = make_local_l2_prolongation_localizable_operator(           source, range);
+  } // ... constructible_by_factory(...)
+};
 
 
 } // namespace Test
