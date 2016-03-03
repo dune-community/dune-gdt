a5a66276376573e3491487c0070ada19cbe46f75
Felix Schindler <felix.schindler@wwu.de>
2016-03-03 10:01:23 +0100

    [test.prolongations.base] refactor

--- dune/gdt/test/prolongations/base.hh
+++ dune/gdt/test/prolongations/base.hh
@@ -13,22 +13,41 @@
 
 #include <dune/gdt/discretefunction/default.hh>
 #include <dune/gdt/projections.hh>
-#include <dune/gdt/products/l2.hh>
+#include <dune/gdt/operators/l2.hh>
 
 namespace Dune {
 namespace GDT {
 namespace Test {
+namespace internal {
+
+template <class GridType>
+struct LocalizableProlongationOperatorBase
+{
+  typedef Dune::Stuff::Grid::Providers::Cube<GridType> GridProviderType;
+
+  LocalizableProlongationOperatorBase()
+    : grid_provider_(0.0, 1.0, 2u)
+  {
+    grid_provider_.grid().globalRefine(1);
+  }
+
+  GridProviderType grid_provider_;
+}; // struct LocalizableProlongationOperatorBase
+
+
+} // namespace internal
 
 
 /**
  * \note Assumes that project and Products::L2 does the right thing.
  */
-template <class CoarseSpaceType, class FineSpaceType, template <class, class, class> class ProlongationOperatorType>
-struct LocalizableProlongationOperatorBase : public ::testing::Test
+template <class CoarseSpaceType, class FineSpaceType, template <class, class, class> class ProlongationOperatorImp>
+struct LocalizableProlongationOperatorBase
+    : public ::testing::Test,
+      public internal::LocalizableProlongationOperatorBase<typename FineSpaceType::GridViewType::Grid>
 {
+  typedef internal::LocalizableProlongationOperatorBase<typename FineSpaceType::GridViewType::Grid> BaseType;
   typedef typename FineSpaceType::GridViewType GridViewType;
-  typedef typename GridViewType::Grid GridType;
-  typedef Dune::Stuff::Grid::Providers::Cube<GridType> GridProviderType;
   typedef typename GridViewType::template Codim<0>::Entity EntityType;
   typedef typename FineSpaceType::DomainFieldType DomainFieldType;
   static const size_t dimDomain = FineSpaceType::dimDomain;
@@ -38,41 +57,56 @@ struct LocalizableProlongationOperatorBase : public ::testing::Test
   typedef Dune::Stuff::Functions::Expression<EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange>
       FunctionType;
   typedef typename Dune::Stuff::LA::Container<RangeFieldType>::VectorType VectorType;
+  typedef Dune::GDT::DiscreteFunction<CoarseSpaceType, VectorType> CoarseDiscreteFunctionType;
+  typedef Dune::GDT::DiscreteFunction<FineSpaceType, VectorType> FineDiscreteFunctionType;
+  typedef ProlongationOperatorImp<GridViewType, CoarseDiscreteFunctionType, FineDiscreteFunctionType>
+      ProlongationOperatorType;
 
-  void produces_correct_results(const RangeFieldType& tolerance = 1e-15) const
+  LocalizableProlongationOperatorBase()
+    : function_("x", "x[0]", 1, "function")
+    , coarse_space_(grid_provider_.template level<CoarseSpaceType::part_view_type>(0))
+    , fine_space_(grid_provider_.template level<FineSpaceType::part_view_type>(grid_provider_.grid().maxLevel()))
+    , coarse_discrete_function_(coarse_space_, "coarse discrete function")
+    , fine_discrete_function_(fine_space_, "fine discrete function")
+    , prepared_(false)
   {
-    // prepare
-    GridProviderType grid_provider(0.0, 1.0, 2u);
-    grid_provider.grid().globalRefine(1);
-    auto coarse_grid_view = grid_provider.template level<CoarseSpaceType::part_view_type>(0);
-    auto fine_grid_view   = grid_provider.template level<FineSpaceType::part_view_type>(grid_provider.grid().maxLevel());
+  }
+
+  void prepare(const double tolerance = 1e-15)
+  {
+    if (prepared_)
+      return;
     // first, project an anlytical function onto the coarse grid
-    const FunctionType function("x", "x[0]", 1, "function");
-    const CoarseSpaceType coarse_space(coarse_grid_view);
-    typedef Dune::GDT::DiscreteFunction<CoarseSpaceType, VectorType> CoarseDiscreteFunctionType;
-    CoarseDiscreteFunctionType coarse_discrete_function(coarse_space, "coarse discrete function");
-    project(function, coarse_discrete_function);
+    project(function_, coarse_discrete_function_);
     // since the projection operator is tested elsewhere we are confident this worked, but we check anyway
-    const Dune::GDT::Products::L2<GridViewType> coarse_l2_product_operator(coarse_space.grid_view());
-    const auto coarse_l2_error = coarse_l2_product_operator.induced_norm(function - coarse_discrete_function);
+    const auto coarse_l2_error =
+        make_l2_operator(coarse_space_.grid_view(), 2)->induced_norm(function_ - coarse_discrete_function_);
     if (coarse_l2_error > tolerance)
       DUNE_THROW(Dune::Stuff::Exceptions::internal_error,
                  "This should not happen, the L2 product and projection operators are tested elsewhere!\n"
                      << coarse_l2_error
                      << " vs. "
-                     << RangeFieldType(1e-15));
-    // now we prolong the discrete function from the coarse to the fine grid part
-    const FineSpaceType fine_space(fine_grid_view);
-    typedef Dune::GDT::DiscreteFunction<FineSpaceType, VectorType> FineDiscreteFunctionType;
-    FineDiscreteFunctionType fine_discrete_function(fine_space, "fine discrete function");
-    ProlongationOperatorType<GridViewType, CoarseDiscreteFunctionType, FineDiscreteFunctionType> prolongation_operator(
-        fine_space.grid_view(), coarse_discrete_function, fine_discrete_function);
-    prolongation_operator.apply();
-    // and measure the error
-    const Dune::GDT::Products::L2<GridViewType> fine_l2_product_operator(fine_space.grid_view());
-    const auto fine_l2_error = fine_l2_product_operator.induced_norm(function - fine_discrete_function);
+                     << tolerance);
+    prepared_ = true;
+  } // ... prepare(...)
+
+  void produces_correct_results(const RangeFieldType& tolerance = 1e-15)
+  {
+    prepare(tolerance);
+    ProlongationOperatorType(fine_space_.grid_view(), coarse_discrete_function_, fine_discrete_function_).apply();
+    const auto fine_l2_error =
+        make_l2_operator(fine_space_.grid_view(), 2)->induced_norm(function_ - fine_discrete_function_);
     EXPECT_LE(fine_l2_error, tolerance);
-  }
+  } // ... (produces_correct_results(...)
+
+  using BaseType::grid_provider_;
+
+  const FunctionType function_;
+  const CoarseSpaceType coarse_space_;
+  const FineSpaceType fine_space_;
+  CoarseDiscreteFunctionType coarse_discrete_function_;
+  FineDiscreteFunctionType fine_discrete_function_;
+  bool prepared_;
 }; // LocalizableProlongationOperatorBase
 
 
