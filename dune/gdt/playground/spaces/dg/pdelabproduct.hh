fd7b90f5f2b2d8205b44f30915416ce885372331
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-03-04 15:44:36 +0100

    [playground.spaces.dg.pdelabproduct] fix

--- dune/gdt/playground/spaces/dg/pdelabproduct.hh
+++ dune/gdt/playground/spaces/dg/pdelabproduct.hh
@@ -73,6 +73,9 @@ public:
   typedef PdelabBasedProduct< GridViewImp, polynomialOrder, RangeFieldImp, rangeDim, rangeDimCols > derived_type;
   using typename BaseType::GridViewType;
   static const int polOrder = BaseType::polOrder;
+  static const size_t dimDomain = GridViewType::dimension;
+  static const size_t dimRange = rangeDim;
+  static const size_t dimRangeCols = rangeDimCols;
   using typename BaseType::BackendType;
   using typename BaseType::EntityType;
   using typename BaseType::RangeFieldType;
@@ -80,8 +83,8 @@ public:
   using BaseType::part_view_type;
   using BaseType::needs_grid_view;
 
-  typedef typename Dune::GDT::Spaces::DG::PdelabBased< GridViewType, polOrder, RangeFieldType, 1, rangeDimCols >  FactorSpaceType;
-  typedef typename make_identical_tuple< FactorSpaceType, rangeDim >::type                                        SpaceTupleType;
+  typedef typename Dune::GDT::Spaces::DG::PdelabBased< GridViewType, polOrder, RangeFieldType, 1, dimRangeCols >  FactorSpaceType;
+  typedef typename make_identical_tuple< FactorSpaceType, dimRange >::type                                        SpaceTupleType;
 };
 
 
@@ -91,40 +94,76 @@ public:
 template< class GridViewImp, int polynomialOrder, class RangeFieldImp, size_t rangeDim >
 class PdelabBasedProduct< GridViewImp, polynomialOrder, RangeFieldImp, rangeDim, 1 >
   : public Dune::GDT::ProductSpaceInterface< internal::PdelabBasedProductTraits< GridViewImp, polynomialOrder, RangeFieldImp, rangeDim, 1 >, GridViewImp::dimension, rangeDim, 1 >
-  , public PdelabBased< GridViewImp, polynomialOrder, RangeFieldImp, rangeDim, 1 >
 {
   typedef PdelabBasedProduct< GridViewImp, polynomialOrder, RangeFieldImp, rangeDim, 1 >                          ThisType;
-  typedef typename Dune::GDT::ProductSpaceInterface< internal::PdelabBasedProductTraits< GridViewImp, polynomialOrder, RangeFieldImp, rangeDim, 1 >, GridViewImp::dimension, rangeDim, 1 >  InterfaceType;
-  typedef PdelabBased< GridViewImp, polynomialOrder, RangeFieldImp, rangeDim, 1 > BaseType;
+  typedef typename Dune::GDT::ProductSpaceInterface< internal::PdelabBasedProductTraits< GridViewImp, polynomialOrder, RangeFieldImp, rangeDim, 1 >, GridViewImp::dimension, rangeDim, 1 >  BaseType;
 public:
-  using typename InterfaceType::Traits;
-  using typename InterfaceType::GridViewType;
-  using typename InterfaceType::EntityType;
-  using typename InterfaceType::BaseFunctionSetType;
-  using typename InterfaceType::MapperType;
-  using typename InterfaceType::CommunicatorType;
-  using typename InterfaceType::BackendType;
-  using InterfaceType::dimDomain;
-  using InterfaceType::dimRange;
-  using InterfaceType::dimRangeCols;
-  typedef typename Traits::SpaceTupleType    SpaceTupleType;
-  typedef typename Traits::FactorSpaceType   FactorSpaceType;
+  using typename BaseType::Traits;
+  using typename BaseType::GridViewType;
+  using typename BaseType::EntityType;
+  using typename BaseType::BaseFunctionSetType;
+  using typename BaseType::MapperType;
+  using typename BaseType::CommunicatorType;
+  using typename BaseType::BackendType;
+  using BaseType::dimDomain;
+  using BaseType::dimRange;
+  using BaseType::dimRangeCols;
+  typedef typename Traits::SpaceTupleType   SpaceTupleType;
+  typedef typename Traits::FactorSpaceType  FactorSpaceType;
 
   PdelabBasedProduct(GridViewType gv)
-    : BaseType(gv)
-    , factor_space_(gv)
+    : grid_view_(gv)
+    , factor_space_(grid_view_)
+    , factor_mapper_(factor_space_.backend())
+    , communicator_(CommunicationChooser<GridViewImp>::create(grid_view_))
+    , communicator_prepared_(false)
   {}
 
   PdelabBasedProduct(const ThisType& other)
-    : BaseType(other)
+    : grid_view_(other.grid_view_)
     , factor_space_(other.factor_space_)
-  {}
+    , factor_mapper_(other.factor_mapper_)
+    , communicator_(CommunicationChooser< GridViewImp >::create(grid_view_))
+    , communicator_prepared_(false)
+  {
+    // make sure our new communicator is prepared if other's was
+    if (other.communicator_prepared_)
+      const auto& DUNE_UNUSED(comm) = this->communicator();
+  }
 
   PdelabBasedProduct(ThisType&& source) = default;
 
   ThisType& operator=(const ThisType& other) = delete;
 
   ThisType& operator=(ThisType&& source) = delete;
+
+  const GridViewType& grid_view() const
+  {
+    return grid_view_;
+  }
+
+  const BackendType& backend() const
+  {
+    return factor_space_.backend();
+  }
+
+  const MapperType& mapper() const
+  {
+    return factor_mapper_;
+  }
+
+  BaseFunctionSetType base_function_set(const EntityType& entity) const
+  {
+    return BaseFunctionSetType(backend(), entity);
+  }
+
+  CommunicatorType& communicator() const
+  {
+    std::lock_guard< std::mutex > DUNE_UNUSED(gg)(communicator_mutex_);
+    if (!communicator_prepared_)
+      communicator_prepared_ = CommunicationChooser<GridViewType>::prepare(*this, *communicator_);
+    return *communicator_;
+  } // ... communicator(...)
 
   template< size_t ii >
   const FactorSpaceType& factor() const
@@ -133,7 +172,12 @@ public:
   }
 
 private:
+    const GridViewType grid_view_;
     const FactorSpaceType factor_space_;
+    const MapperType factor_mapper_;
+    mutable std::unique_ptr< CommunicatorType > communicator_;
+    mutable bool communicator_prepared_;
+    mutable std::mutex communicator_mutex_;
 }; // class DefaultProduct< ..., 1 >
 
 
