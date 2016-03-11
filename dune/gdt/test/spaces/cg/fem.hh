588806d23f2ec70787af33faee0e2baff9e1a5dc
Felix Schindler <felix.schindler@wwu.de>
2016-03-11 14:25:29 +0100

    [test.spaces.cg] add level defines

--- dune/gdt/test/spaces/cg/fem.hh
+++ dune/gdt/test/spaces/cg/fem.hh
@@ -28,6 +28,21 @@
   , SPACE_CG_FEM_YASPGRID(3, 1, pp)
 
 
+#define SPACE_CG_FEM_SGRID_LEVEL(dd, rr, pp) \
+  Dune::GDT::Spaces::CG::FemBased< S ## dd ## dLevelGridPartType, pp, double, rr >
+
+#define SPACE_CG_FEM_YASPGRID_LEVEL(dd, rr, pp) \
+  Dune::GDT::Spaces::CG::FemBased< Yasp ## dd ## dLevelGridPartType, pp, double, rr >
+
+#define SPACES_CG_FEM_LEVEL(pp) \
+    SPACE_CG_FEM_SGRID_LEVEL(1, 1, pp) \
+  , SPACE_CG_FEM_SGRID_LEVEL(2, 1, pp) \
+  , SPACE_CG_FEM_SGRID_LEVEL(3, 1, pp) \
+  , SPACE_CG_FEM_YASPGRID_LEVEL(1, 1, pp) \
+  , SPACE_CG_FEM_YASPGRID_LEVEL(2, 1, pp) \
+  , SPACE_CG_FEM_YASPGRID_LEVEL(3, 1, pp)
+
+
 # if HAVE_ALUGRID
 
 
@@ -42,6 +57,19 @@
   , SPACE_CG_FEM_ALUCONFORMGRID(3, 1, pp) \
   , SPACE_CG_FEM_ALUCUBEGRID(2, 1, pp) \
   , SPACE_CG_FEM_ALUCUBEGRID(3, 1, pp)
+
+
+#define SPACE_CG_FEM_ALUCONFORMGRID_LEVEL(dd, rr, pp) \
+  Dune::GDT::Spaces::CG::FemBased< AluConform ## dd ## dLevelGridPartType, pp, double, rr >
+
+#define SPACE_CG_FEM_ALUCUBEGRID_LEVEL(dd, rr, pp) \
+  Dune::GDT::Spaces::CG::FemBased< AluCube ## dd ## dLevelGridPartType, pp, double, rr >
+
+#define SPACES_CG_FEM_ALUGRID_LEVEL(pp) \
+    SPACE_CG_FEM_ALUCONFORMGRID_LEVEL(2, 1, pp) \
+  , SPACE_CG_FEM_ALUCONFORMGRID_LEVEL(3, 1, pp) \
+  , SPACE_CG_FEM_ALUCUBEGRID_LEVEL(2, 1, pp) \
+  , SPACE_CG_FEM_ALUCUBEGRID_LEVEL(3, 1, pp)
 
 
 # endif // HAVE_ALUGRID
--- dune/gdt/test/spaces/cg/pdelab.hh
+++ dune/gdt/test/spaces/cg/pdelab.hh
@@ -28,6 +28,21 @@
   , SPACE_CG_PDELAB_YASPGRID(3, 1, pp)
 
 
+#define SPACE_CG_PDELAB_SGRID_LEVEL(dd, rr, pp) \
+  Dune::GDT::Spaces::CG::PdelabBased< S ## dd ## dLevelGridViewType, pp, double, rr >
+
+#define SPACE_CG_PDELAB_YASPGRID_LEVEL(dd, rr, pp) \
+  Dune::GDT::Spaces::CG::PdelabBased< Yasp ## dd ## dLevelGridViewType, pp, double, rr >
+
+#define SPACES_CG_PDELAB_LEVEL(pp) \
+    SPACE_CG_PDELAB_SGRID_LEVEL(1, 1, pp) \
+  , SPACE_CG_PDELAB_SGRID_LEVEL(2, 1, pp) \
+  , SPACE_CG_PDELAB_SGRID_LEVEL(3, 1, pp) \
+  , SPACE_CG_PDELAB_YASPGRID_LEVEL(1, 1, pp) \
+  , SPACE_CG_PDELAB_YASPGRID_LEVEL(2, 1, pp) \
+  , SPACE_CG_PDELAB_YASPGRID_LEVEL(3, 1, pp)
+
+
 # if HAVE_ALUGRID
 
 
@@ -42,6 +57,14 @@
   , SPACE_CG_PDELAB_ALUCONFORMGRID(3, 1, pp) \
   , SPACE_CG_PDELAB_ALUCUBEGRID(2, 1, pp) \
   , SPACE_CG_PDELAB_ALUCUBEGRID(3, 1, pp)
+
+
+#define SPACE_CG_PDELAB_ALUCUBEGRID_LEVEL(dd, rr, pp) \
+  Dune::GDT::Spaces::CG::PdelabBased< AluCube ## dd ## dLevelGridViewType, pp, double, rr >
+
+#define SPACES_CG_PDELAB_ALUGRID_LEVEL(pp) \
+    SPACE_CG_PDELAB_ALUCUBEGRID_LEVEL(2, 1, pp) \
+  , SPACE_CG_PDELAB_ALUCUBEGRID_LEVEL(3, 1, pp)
 
 
 # endif // HAVE_ALUGRID
