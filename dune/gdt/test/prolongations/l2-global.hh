9096b4831679c04e57c1295fc41ad32f8a71762c
Felix Schindler <felix.schindler@wwu.de>
2016-03-11 15:27:49 +0100

    [prolongations.l2-global] add tests

--- dune/gdt/test/prolongations/l2-global.hh
+++ dune/gdt/test/prolongations/l2-global.hh
@@ -52,6 +52,37 @@ struct L2GlobalProlongationLocalizableOperatorTest
 };
 
 
+template< class SpaceType >
+struct L2GlobalProlongationOperatorTest
+  : public ProlongationOperatorBase< SpaceType, SpaceType, L2GlobalProlongationOperator >
+{
+  typedef ProlongationOperatorBase< SpaceType, SpaceType, L2GlobalProlongationOperator > BaseType;
+  using typename BaseType::ProlongationOperatorType;
+
+  void constructible_by_ctor(const double tolerance = 1e-15)
+  {
+    this->prepare(tolerance);
+
+    auto grid_view = this->fine_space_.grid_view();
+    const auto& source = this->coarse_discrete_function_;
+    auto& range = this->fine_discrete_function_;
+
+    ProlongationOperatorType DUNE_UNUSED(w_over_integrate)(0, grid_view);
+    ProlongationOperatorType DUNE_UNUSED(wo_over_integrate)(  grid_view);
+  } // ... constructible_by_ctor(...)
+
+  void constructible_by_factory(const double tolerance = 1e-15)
+  {
+    this->prepare(tolerance);
+
+    auto grid_view = this->fine_space_.grid_view();
+
+    auto DUNE_UNUSED(w_over_integrate)  = make_global_l2_prolongation_operator(grid_view, 1);
+    auto DUNE_UNUSED(wo_over_integrate) = make_global_l2_prolongation_operator(grid_view);
+  } // ... constructible_by_factory(...)
+};
+
+
 } // namespace Test
 } // namespace GDT
 } // namespace Dune
