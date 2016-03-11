2a74f8bc908e4259da74a747a9418f88827f6f3a
Felix Schindler <felix.schindler@wwu.de>
2016-03-11 23:32:56 +0100

    [prolongations.lagrange] add tests for LagrangeProlongationOperator

---
+++ dune/gdt/test/prolongations__lagrange__cg_pdelab.cc
@@ -0,0 +1,46 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#include <dune/stuff/test/main.hxx> // <- This one has to come first!
+
+#include "prolongations/lagrange.hh"
+#include "spaces/cg/pdelab.hh"
+
+using namespace Dune::GDT::Test;
+
+#if HAVE_DUNE_PDELAB
+
+
+typedef testing::Types< SPACES_CG_PDELAB_LEVEL(1)
+#if HAVE_ALUGRID && !defined(__GNUC__)
+                      , SPACES_CG_PDELAB_ALUGRID_LEVEL(1)
+#endif
+                      > SpaceTypes;
+
+TYPED_TEST_CASE(LagrangeProlongationOperatorTest, SpaceTypes);
+TYPED_TEST(LagrangeProlongationOperatorTest, constructible_by_ctor) {
+  this->constructible_by_ctor();
+}
+TYPED_TEST(LagrangeProlongationOperatorTest, constructible_by_factory) {
+  this->constructible_by_factory();
+}
+TYPED_TEST(LagrangeProlongationOperatorTest, free_function_callable) {
+  this->free_function_callable();
+}
+TYPED_TEST(LagrangeProlongationOperatorTest, produces_correct_results) {
+  this->produces_correct_results();
+}
+
+
+#else // HAVE_DUNE_PDELAB
+
+
+TEST(DISABLED_LagrangeProlongationOperatorTest, constructible_by_ctor) {}
+TEST(DISABLED_LagrangeProlongationOperatorTest, constructible_by_factory) {}
+TEST(DISABLED_LagrangeProlongationOperatorTest, free_function_callable) {}
+TEST(DISABLED_LagrangeProlongationOperatorTest, produces_correct_results) {}
+
+
+#endif // HAVE_DUNE_PDELAB
