b222a16099a5dfc887ccd8e3833eb03715ab8063
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-03-24 15:28:23 +0100

    [mapper.default] join productfv.hh and fv.hh

--- dune/gdt/mapper/default/fv.hh
+++ dune/gdt/mapper/default/fv.hh
@@ -27,6 +27,12 @@ class FiniteVolume
   static_assert(AlwaysFalse<GridViewImp>::value, "Not available for these dimensions!");
 };
 
+template <class GridViewImp, size_t rangeDim = 1, size_t rangeDimCols = 1>
+class ProductFiniteVolume
+{
+  static_assert(AlwaysFalse<GridViewImp>::value, "Not available for these dimensions!");
+};
+
 
 namespace internal {
 
@@ -42,6 +48,14 @@ public:
   typedef FiniteVolume<GridViewType, rangeDim, rangeDimCols> derived_type;
   typedef typename GridViewImp::IndexSet BackendType;
   typedef typename GridViewType::template Codim<0>::Entity EntityType;
+};
+
+template <class GridViewImp, size_t rangeDim, size_t rangeDimCols>
+class ProductFiniteVolumeTraits : public internal::FiniteVolumeTraits<GridViewImp, rangeDim, rangeDimCols>
+{
+public:
+  typedef ProductFiniteVolume<GridViewImp, rangeDim, rangeDimCols> derived_type;
+  static const size_t dimRange = rangeDim;
 };
 
 
@@ -162,6 +176,97 @@ public:
 private:
   const BackendType& backend_;
 }; // class FiniteVolume< ..., 1, 1 >
+
+
+template <class GridViewImp, size_t rangeDim>
+class ProductFiniteVolume<GridViewImp, rangeDim, 1>
+    : public ProductMapperInterface<internal::ProductFiniteVolumeTraits<GridViewImp, rangeDim, 1>>
+{
+  typedef ProductMapperInterface<internal::ProductFiniteVolumeTraits<GridViewImp, rangeDim, 1>> BaseType;
+  typedef FiniteVolume<GridViewImp, rangeDim, 1> FiniteVolumeMapperType;
+
+public:
+  typedef internal::ProductFiniteVolumeTraits<GridViewImp, rangeDim, 1> Traits;
+  typedef typename Traits::GridViewType GridViewType;
+  static const size_t dimRange = Traits::dimRange;
+  using typename BaseType::EntityType;
+  using typename BaseType::BackendType;
+
+  ProductFiniteVolume(const GridViewType& grid_view)
+    : fv_mapper_(grid_view)
+  {
+  }
+
+  // These methods are required by the ProductMapperInterface
+  size_t numDofs(const size_t /*factor_index*/, const EntityType& /*entity*/) const
+  {
+    return 1;
+  }
+
+  size_t maxNumDofs(const size_t /*factor_index*/) const
+  {
+    return 1;
+  }
+
+  void globalIndices(const size_t factor_index, const EntityType& entity, Dune::DynamicVector<size_t>& ret) const
+  {
+    if (ret.size() != 1)
+      ret.resize(1);
+    ret[0] = dimRange * (backend().index(entity)) + factor_index;
+  }
+
+  size_t mapToGlobal(const size_t factor_index, const EntityType& entity,
+                     const size_t& UNUSED_UNLESS_DEBUG(local_index_in_factor)) const
+  {
+    assert(local_index_in_factor == 0);
+    assert(factor_index < numDofs(entity));
+    return dimRange * (backend().index(entity)) + factor_index;
+  }
+
+  size_t mapToLocal(const size_t factor_index, const EntityType& entity,
+                    const size_t& UNUSED_UNLESS_DEBUG(local_index_in_factor)) const
+  {
+    assert(local_index_in_factor == 0);
+    assert(factor_index < numDofs(entity));
+    return factor_index;
+  }
+
+  // The remaining methods are just redirected to the usual finite volume mapper
+  const BackendType& backend() const
+  {
+    return fv_mapper_.backend();
+  }
+
+  size_t size() const
+  {
+    return fv_mapper_.size();
+  }
+
+  size_t numDofs(const EntityType& entity) const
+  {
+    return fv_mapper_.numDofs(entity);
+  }
+
+  size_t maxNumDofs() const
+  {
+    return fv_mapper_.maxNumDofs();
+  }
+
+  void globalIndices(const EntityType& entity, Dune::DynamicVector<size_t>& ret) const
+  {
+    return fv_mapper_.globalIndices(entity, ret);
+  } // ... globalIndices(...)
+
+  using BaseType::globalIndices;
+
+  size_t mapToGlobal(const EntityType& entity, const size_t& localIndex) const
+  {
+    return fv_mapper_.mapToGlobal(entity, localIndex);
+  }
+
+private:
+  const FiniteVolumeMapperType fv_mapper_;
+}; // class ProductFiniteVolume< ..., rangeDim, 1 >
 
 
 } // namespace Mapper
