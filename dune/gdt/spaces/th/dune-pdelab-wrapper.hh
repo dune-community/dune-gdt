1bd0fc25237fc14c9899f4c1a4281bed93c1e0a4
Felix Schindler <felix.schindler@wwu.de>
2016-04-12 16:11:27 +0200

    update names and include

--- dune/gdt/spaces/th/dune-pdelab-wrapper.hh
+++ dune/gdt/spaces/th/dune-pdelab-wrapper.hh
@@ -1,5 +1,21 @@
+// This file is part of the dune-gdt project:
+//   http://users.dune-project.org/projects/dune-gdt
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//
+// Contributors: Tobias Leibner
+
+#ifndef DUNE_GDT_SPACES_TH_DUNE_PDELAB_WRAPPER_HH
+#define DUNE_GDT_SPACES_TH_DUNE_PDELAB_WRAPPER_HH
+
+#include <dune/gdt/spaces/product.hh>
+
+namespace Dune {
+namespace GDT {
+
+
 template <class GridViewImp, int polynomialOrder, size_t domainDim, class RangeFieldImp>
-class TaylorHoodSpace
+class DunePdelabTaylorHoodSpaceWrapper
     : public DefaultProductSpace<DunePdelabCgSpaceWrapper<GridViewImp, polynomialOrder, RangeFieldImp, domainDim, 1>,
                                  DunePdelabCgSpaceWrapper<GridViewImp, polynomialOrder - 1, RangeFieldImp, 1, 1>>
 {
@@ -7,8 +23,14 @@ public:
   typedef DunePdelabCgSpaceWrapper<GridViewImp, polynomialOrder, RangeFieldImp, domainDim, 1> VelocitySpaceType;
   typedef DunePdelabCgSpaceWrapper<GridViewImp, polynomialOrder - 1, RangeFieldImp, 1, 1> PressureSpaceType;
   typedef DefaultProductSpace<VelocitySpaceType, PressureSpaceType> BaseType;
-  TaylorHoodSpace(const GridViewImp& grid_view)
+  DunePdelabTaylorHoodSpaceWrapper(const GridViewImp& grid_view)
     : BaseType(VelocitySpaceType(grid_view), PressureSpaceType(grid_view))
   {
   }
 };
+
+
+} // namespace GDT
+} // namespace Dune
+
+#endif // DUNE_GDT_SPACES_TH_DUNE_PDELAB_WRAPPER_HH
