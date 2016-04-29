0383ac0b0a531bfe3db4e125cce6068d0887083f
Rene Milk <rene.milk@wwu.de>
2016-04-29 16:34:18 +0200

    [tests] removes alugrid(3,simplex,conforming) from grid types
    refs #79

--- dune/gdt/test/projections__l2_localizable__fv_default.cc
+++ dune/gdt/test/projections__l2_localizable__fv_default.cc
@@ -17,7 +17,7 @@ typedef testing::Types<SPACE_FV_SGRID(1, 1), SPACE_FV_SGRID(2, 1), SPACE_FV_SGRI
                        SPACE_FV_YASPGRID(2, 1), SPACE_FV_YASPGRID(3, 1)
 #if HAVE_DUNE_ALUGRID
                                                     ,
-                       SPACE_FV_ALUCONFORMGRID(2, 1), SPACE_FV_ALUCONFORMGRID(3, 1), SPACE_FV_ALUCUBEGRID(2, 1),
+                       SPACE_FV_ALUCONFORMGRID(2, 1), SPACE_FV_ALUCUBEGRID(2, 1),
                        SPACE_FV_ALUCUBEGRID(3, 1)
 #endif // HAVE_DUNE_ALUGRID
                        > SpaceTypes;
