32ad5474333f4c8a8afa702f9fdcc7fa9c489858
Rene Milk <rene.milk@wwu.de>
2016-08-19 10:40:38 +0200

    [cmake] add config.h generation

---
+++ config.h.cmake
@@ -0,0 +1,30 @@
+// This file is part of the dune-gdt project:
+//   https://github.com/dune-community/dune-gdt
+// The copyright lies with the authors of this file (see below).
+// License: Dual licensed as  BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Rene Milk       (2016)
+
+/* begin dune-gdt */
+// NEVER delete/alter above comment, dune's cmake relies on it
+
+/* Define to the version of dune-xt-common */
+#define DUNE_GDT_VERSION ${DUNE_GDT_VERSION}
+
+/* Define to the major version of dune-xt-common */
+#define DUNE_GDT_VERSION_MAJOR ${DUNE_GDT_VERSION_MAJOR}
+
+/* Define to the minor version of dune-xt-common */
+#define DUNE_GDT_VERSION_MINOR ${DUNE_GDT_VERSION_MINOR}
+
+/* Define to the revision of dune-xt-common */
+#define DUNE_GDT_VERSION_REVISION ${DUNE_GDT_VERSION_REVISION}
+
+#ifndef DUNE_AVOID_SGRID_DEPRE_WARNING_BECAUSE_I_KNOW_WHAT_IM_DOING
+#define DUNE_AVOID_SGRID_DEPRE_WARNING_BECAUSE_I_KNOW_WHAT_IM_DOING 1
+#endif
+
+/* end dune-gdt */
+// NEVER delete/alter above comment, dune's cmake relies on it
