b45e3b7f089f5d984bb9bd83802da8ef2ba9efda
Felix Schindler <felix.schindler@wwu.de>
2016-10-26 21:26:20 +0200

    [test.linearelliptic.swipdg] fix ecpectations
    Those got broken in ba150336ddad528bdc7a91efe42e6e9eabf380c3.

--- dune/gdt/test/linearelliptic/eocexpectations/swipdg-er2007-2dyaspgrid.cxx
+++ dune/gdt/test/linearelliptic/eocexpectations/swipdg-er2007-2dyaspgrid.cxx
@@ -12,9 +12,9 @@ LinearEllipticEocExpectations<LinearElliptic::ER2007TestCase<Yasp2Grid, double, 
             const std::string type)
 {
   if (type == "L2")
-    return {2.05e-01, 1.66e-02, 1.91e-03};
+    return {2.30e-01, 1.60e-01, 4.88e-02};
   else if (type == "H1_semi" || type == "energy")
-    return {4.39e-01, 9.32e-02, 2.37e-02};
+    return {4.58e-01, 4.41e-01, 2.26e-01};
   else
     EXPECT_TRUE(false) << "test results missing for type: " << type;
   return {};
@@ -28,9 +28,9 @@ LinearEllipticEocExpectations<LinearElliptic::ER2007TestCase<Yasp2Grid, double, 
             const std::string type)
 {
   if (type == "L2")
-    return {2.30e-01, 1.60e-01, 4.88e-02};
+    return {2.05e-01, 1.66e-02, 1.91e-03};
   else if (type == "H1_semi" || type == "energy")
-    return {4.58e-01, 4.41e-01, 2.26e-01};
+    return {4.39e-01, 9.32e-02, 2.37e-02};
   else
     EXPECT_TRUE(false) << "test results missing for type: " << type;
   return {};
