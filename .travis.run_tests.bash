d9cc511186613fa8cb790df67e7b175ff15c3441
Rene Milk <rene.milk@wwu.de>
2016-11-03 17:41:06 +0100

    [travis] fixup test script

--- .travis.run_tests.bash
+++ .travis.run_tests.bash
@@ -3,8 +3,8 @@
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
 if [[ "x${TESTS}" != "xheadercheck" ]]; then
     ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
-    travis_wait 50 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v test_binaries_builder_${TESTS}
-    then travis_wait 50 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -j 2 -L "^builder_${TESTS}$"
+    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v test_binaries_builder_${TESTS}
+    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -j 2 -L "^builder_${TESTS}$"
     ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja headercheck
 fi
 
