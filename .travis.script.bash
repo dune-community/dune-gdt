4e876326d440b0b7811ed10cd53645cd5d462821
Rene Milk <rene.milk@wwu.de>
2016-11-04 10:43:48 +0100

    [travis] try building with -j1 to keep within memory limits

--- .travis.script.bash
+++ .travis.script.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
 if [[ "x${TESTS}" != "xheadercheck" ]]; then
     ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v test_binaries_builder_${TESTS}
+    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
     ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
 else
     ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja headercheck
