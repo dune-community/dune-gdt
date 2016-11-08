0d076ab313a2b28e8386ad0081c8e705f11cc81a
Rene Milk <rene.milk@wwu.de>
2016-11-08 15:54:47 +0100

    [test] linear elliptic is now testtools based

---
+++ dune/gdt/test/linearelliptic__swipdg_discretization.cc
@@ -0,0 +1,67 @@
+// This file is part of the dune-gdt project:
+//   https://github.com/dune-community/dune-gdt
+// Copyright 2010-2016 dune-gdt developers and contributors. All rights reserved.
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+// Authors:
+//   Felix Schindler (2015 - 2016)
+
+#ifndef DUNE_XT_COMMON_TEST_MAIN_ENABLE_INFO_LOGGING
+#define DUNE_XT_COMMON_TEST_MAIN_ENABLE_INFO_LOGGING 1
+#endif
+
+#include <dune/xt/common/test/main.hxx> // <- This one has to come first (includes the config.h)!
+
+#include <dune/xt/common/test/common.hh>
+#include <dune/xt/functions/spe10/model1.hh>
+#include <dune/xt/la/container.hh>
+
+#include <dune/gdt/spaces/interface.hh>
+
+#include "linearelliptic/eocstudy.hh"
+#include "linearelliptic/discretizers/ipdg.hh"
+
+
+struct linearelliptic_SWIPDG_discretization : public ::testing::Test
+{
+  typedef TESTCASETYPE TestCaseType;
+
+  template <int polOrder>
+  static void eoc_study()
+  {
+    using namespace Dune;
+    using namespace Dune::GDT;
+    TestCaseType test_case;
+    test_case.print_header(DXTC_LOG_INFO);
+    DXTC_LOG_INFO << std::endl;
+    typedef LinearElliptic::IpdgDiscretizer<typename TestCaseType::GridType,
+                                            XT::Grid::Layers::level,
+                                            ChooseSpaceBackend::SPACE_BACKEND,
+                                            XT::LA::Backends::LA_BACKEND,
+                                            polOrder,
+                                            typename TestCaseType::ProblemType::RangeFieldType,
+                                            1,
+                                            LocalEllipticIpdgIntegrands::Method::swipdg>
+        Discretizer;
+    Dune::GDT::Test::LinearEllipticEocStudy<TestCaseType, Discretizer> eoc_study(test_case);
+    try {
+      Dune::XT::Test::check_eoc_study_for_success(eoc_study, eoc_study.run(DXTC_LOG_INFO));
+    } catch (Dune::XT::Common::Exceptions::spe10_data_file_missing&) {
+      Dune::XT::Common::TimedLogger().get("gdt.test.linearelliptic.swipdg.discretization").warn()
+          << "missing SPE10 data file!" << std::endl;
+    }
+  } // ... eoc_study()
+}; // linearelliptic_SWIPDG_discretization
+
+
+using namespace Dune;
+using namespace Dune::GDT;
+
+
+TEST_F(linearelliptic_SWIPDG_discretization, eoc_study_order_1)
+{
+  this->template eoc_study<1>();
+}
+TEST_F(linearelliptic_SWIPDG_discretization, eoc_study_order_2)
+{
+  this->template eoc_study<2>();
+}
