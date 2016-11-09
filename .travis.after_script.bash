0173611ba95bf4a87dddce03e8905f38b46749be
Rene Milk <rene.milk@wwu.de>
2016-11-09 16:18:13 +0100

    [travis] scripts no longer need to special case for headercheck

--- .travis.after_script.bash
+++ .travis.after_script.bash
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [[ $TRAVIS_JOB_NUMBER == *.1 ]] ; then
-    git config --global hooks.clangformat ${CLANG_FORMAT}
-    PYTHONPATH=${SUPERDIR}/scripts/python/ python3 -c "import travis_report as tp; tp.clang_format_status(\"${TRAVIS_BUILD_DIR}\")"
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make doc
-    #${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-community.github.io
-    #${SUPERDIR}/.travis/deploy_docs.sh ${MY_MODULE} ${DUNE_BUILD_DIR}
-fi
+git config --global hooks.clangformat ${CLANG_FORMAT}
+PYTHONPATH=${SUPERDIR}/scripts/python/ python3 -c "import travis_report as tp; tp.clang_format_status(\"${TRAVIS_BUILD_DIR}\")"
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make doc
+#${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-community.github.io
+#${SUPERDIR}/.travis/deploy_docs.sh ${MY_MODULE} ${DUNE_BUILD_DIR}
--- .travis.script.bash
+++ .travis.script.bash
@@ -3,16 +3,12 @@
 set -e
 
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
-if [[ "x${TESTS}" != "xheadercheck" ]]; then
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
-    #${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-gdt-testlogs
-    #${SUPERDIR}/scripts/bash/travis_upload_test_logs.bash ${SUPERDIR}/${MY_MODULE}/${DUNE_BUILD_DIR}/dune/gdt/test/
 
-else
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja headercheck
-fi
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
+#${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-gdt-testlogs
+#${SUPERDIR}/scripts/bash/travis_upload_test_logs.bash ${SUPERDIR}/${MY_MODULE}/${DUNE_BUILD_DIR}/dune/gdt/test/
 
 unset GH_TOKEN
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make install | grep -v "Installing"
