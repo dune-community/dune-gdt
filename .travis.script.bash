9e11df1390c4cd2309c59ca93d6c41f85def62f9
Rene Milk <rene.milk@wwu.de>
2016-11-10 11:57:39 +0100

    [travis] safeguard against new tests not being run

--- .travis.script.bash
+++ .travis.script.bash
@@ -3,8 +3,12 @@
 set -e
 
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
-
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
+
+# this does nothing if all current tests are distributed already, but triggers full build if not
+# -> builder will timeout -> manually run refresh_test_timings -> push results
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 refresh_test_timings
+
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
 #${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-gdt-testlogs
