ae9065b716ebab68ed3c2048a11b21ed19a3bd8e
Rene Milk <rene.milk@wwu.de>
2016-11-11 16:44:12 +0100

    [test(tools)] fv projections added

--- dune/gdt/test/projections.cc
+++ dune/gdt/test/projections.cc
@@ -16,6 +16,8 @@
 #include "spaces/dg/fem.hh"
 #include "spaces/dg/pdelab.hh"
 
+#include "spaces/fv/default.hh"
+
 namespace Dune {
 namespace GDT {
 namespace Test {
@@ -40,47 +42,6 @@ struct ProjectionTest : public internal::ProjectionOperatorBase<SPACETYPE>
 } // namespace Dune
 
 using namespace Dune::GDT::Test;
-
-#if 0
-
-
- typedef testing::Types<SPACES_CG_FEM(1)
-#if HAVE_ALUGRID
-                           ,
-                       SPACES_CG_FEM_ALUGRID(1)
-#endif
-                       >
-    SpaceTypes;
- typedef testing::Types<SPACES_CG_PDELAB(1)
-#if HAVE_ALUGRID
-                           ,
-                       SPACES_CG_PDELAB_ALUGRID(1)
-#endif
-                       >
-    SpaceTypes;
- typedef testing::Types<SPACES_DG_FEM(1)
-#if HAVE_ALUGRID
-                           ,
-                       SPACES_DG_FEM_ALUGRID(1)
-#endif
-                       >
-    SpaceTypes;
- typedef testing::Types<SPACES_DG_PDELAB(1)
-#if HAVE_ALUGRID
-                           ,
-                       SPACES_DG_PDELAB_ALUGRID(1)
-#endif
-                       >
-    SpaceTypes;
- typedef testing::Types<SPACE_FV_YASPGRID(1, 1), SPACE_FV_YASPGRID(2, 1), SPACE_FV_YASPGRID(3, 1)
-#if HAVE_ALUGRID
-                                                                             ,
-                       SPACE_FV_ALUCONFORMGRID(2, 1), SPACE_FV_ALUCONFORMGRID(3, 1), SPACE_FV_ALUCUBEGRID(2, 1),
-                       SPACE_FV_ALUCUBEGRID(3, 1)
-#endif // HAVE_ALUGRID
-                       >
-    SpaceTypes;
-#endif // 0
 
 TEST_F(ProjectionTest, produces_correct_results)
 {
