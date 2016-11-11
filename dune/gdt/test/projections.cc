f5be8dcafbcb0b6f03afea4a3197083a72175d3c
Rene Milk <rene.milk@wwu.de>
2016-11-11 12:08:43 +0100

    [test(tools)] dg projections added

--- dune/gdt/test/projections.cc
+++ dune/gdt/test/projections.cc
@@ -7,7 +7,37 @@
 
 #include <dune/xt/common/test/main.hxx> // <- This one has to come first!
 
-#include "projections.hh"
+#include <dune/gdt/projections.hh>
+#include <dune/gdt/test/projections/base.hh>
+
+#include "spaces/cg/fem.hh"
+#include "spaces/cg/pdelab.hh"
+
+#include "spaces/dg/fem.hh"
+#include "spaces/dg/pdelab.hh"
+
+namespace Dune {
+namespace GDT {
+namespace Test {
+
+struct ProjectionTest : public internal::ProjectionOperatorBase<SPACETYPE>
+{
+  void produces_correct_results(const double& tolerance = 1e-15)
+  {
+    const auto& grid_view = this->space_.grid_view();
+    const auto& source    = this->function_;
+    auto& range           = this->discrete_function_;
+
+    project(grid_view, source, range);
+    project(source, range);
+
+    this->measure_error(tolerance);
+  } // ... produces_correct_results(...)
+}; // struct ProjectionTest
+
+} // namespace Test
+} // namespace GDT
+} // namespace Dune
 
 using namespace Dune::GDT::Test;
 
