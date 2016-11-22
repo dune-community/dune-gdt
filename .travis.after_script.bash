819f47208ef9ab0cf3466d330d5f78a2b8ab6c36
Rene Milk <rene.milk@wwu.de>
2016-11-22 12:05:52 +0100

    [travis] fix status setting

--- .travis.after_script.bash
+++ .travis.after_script.bash
@@ -5,4 +5,4 @@ PYTHONPATH=${SUPERDIR}/scripts/python/ python3 -c "import travis_report as tp; t
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make doc
 ${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-community.github.io
-${SUPERDIR}/.travis/deploy_docs.sh ${MY_MODULE} ${DUNE_BUILD_DIR}
+${SUPERDIR}/.travis/deploy_docs.sh ${MY_MODULE} "${DUNE_BUILD_DIR}"
