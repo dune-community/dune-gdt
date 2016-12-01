3db39e7440d7e890813cdbd269282813a2a7a4f8
Rene Milk <rene.milk@wwu.de>
2016-12-01 11:17:05 +0100

    [travis] try to re-enable new test safeguard

--- .travis.script.bash
+++ .travis.script.bash
@@ -7,7 +7,7 @@ ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
 
 # this does nothing if all current tests are distributed already, but triggers full build if not
 # -> builder will timeout -> manually run refresh_test_timings -> push results
-# ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 refresh_test_timings
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 refresh_test_timings
 
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
