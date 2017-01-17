e61079b4a2565db35142a1bd66aa5262e2d8f946
Rene Milk <rene.milk@wwu.de>
2017-01-17 19:37:36 +0100

    re-add retry for log upload

--- .travis.script.bash
+++ .travis.script.bash
@@ -22,7 +22,7 @@ ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
 ${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-gdt-testlogs
-${SUPERDIR}/scripts/bash/travis_upload_test_logs.bash ${DUNE_BUILD_DIR}/${MY_MODULE}/dune/gdt/test/
+travis_retry ${SUPERDIR}/scripts/bash/travis_upload_test_logs.bash ${DUNE_BUILD_DIR}/${MY_MODULE}/dune/gdt/test/ || echo Test upload failed
 
 unset GH_TOKEN
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make install | grep -v "Installing"
