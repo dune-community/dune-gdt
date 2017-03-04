9b1784d1a92d1badea54d7d9946a95079dc1df58
Felix Schindler <felix.schindler@wwu.de>
2017-03-04 18:56:17 +0100

    [config.h] drop useless include

--- config.h.cmake
+++ config.h.cmake
@@ -11,24 +11,17 @@
 /* begin dune-gdt */
 // NEVER delete/alter above comment, dune's cmake relies on it
 
-/* Define to the version of dune-xt-common */
+/* Define to the version of dune-gdt */
 #define DUNE_GDT_VERSION ${DUNE_GDT_VERSION}
 
-/* Define to the major version of dune-xt-common */
+/* Define to the major version of dune-gdt */
 #define DUNE_GDT_VERSION_MAJOR ${DUNE_GDT_VERSION_MAJOR}
 
-/* Define to the minor version of dune-xt-common */
+/* Define to the minor version of dune-gdt */
 #define DUNE_GDT_VERSION_MINOR ${DUNE_GDT_VERSION_MINOR}
 
-/* Define to the revision of dune-xt-common */
+/* Define to the revision of dune-gdt */
 #define DUNE_GDT_VERSION_REVISION ${DUNE_GDT_VERSION_REVISION}
-
-#ifndef DUNE_AVOID_SGRID_DEPRE_WARNING_BECAUSE_I_KNOW_WHAT_IM_DOING
-#define DUNE_AVOID_SGRID_DEPRE_WARNING_BECAUSE_I_KNOW_WHAT_IM_DOING 1
-#endif
-
-// is_alugrid<>
-#include <dune/xt/grid/information.hh>
 
 /* end dune-gdt */
 // NEVER delete/alter above comment, dune's cmake relies on it
