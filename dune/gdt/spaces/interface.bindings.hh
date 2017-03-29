45a8ea9b5bd0b386082ccd3fcb43f630ac8a105c
Felix Schindler <felix.schindler@wwu.de>
2017-03-29 14:47:15 +0200

    [spaces] use CamelCase for class name and add make_... in bindings

--- dune/gdt/spaces/interface.bindings.hh
+++ dune/gdt/spaces/interface.bindings.hh
@@ -16,6 +16,7 @@
 #include <dune/xt/common/string.hh>
 #include <dune/xt/grid/grids.bindings.hh>
 #include <dune/xt/grid/layers.bindings.hh>
+#include <dune/xt/grid/dd/subdomains/grid.hh>
 
 #include <dune/gdt/spaces.hh>
 
@@ -124,11 +125,17 @@ namespace internal {
 template <class G, XT::Grid::Layers layer, ChooseSpaceBackend backend, size_t r, size_t rC>
 struct space_name_base
 {
+  static std::string value_wo_grid()
+  {
+    using XT::Common::to_string;
+    return XT::Grid::bindings::layer_name<layer>::value() + "_to_" + to_string(r) + "x" + to_string(rC) + "_"
+           + backend_name<backend>::value();
+  }
+
   static std::string value()
   {
     using XT::Common::to_string;
-    return XT::Grid::bindings::grid_name<G>::value() + "_" + XT::Grid::bindings::layer_name<layer>::value() + "_to_"
-           + to_string(r) + "x" + to_string(rC) + "_" + backend_name<backend>::value();
+    return XT::Grid::bindings::grid_name<G>::value() + "_" + value_wo_grid();
   }
 };
 
@@ -155,6 +162,12 @@ struct space_name<CgSpaceProvider<G, layer, backend, p, double, r, rC>>
     return std::string("cg_") + internal::space_name_base<G, layer, backend, r, rC>::value() + "_p"
            + XT::Common::to_string(p) + "_space";
   }
+
+  static std::string value_wo_grid()
+  {
+    return std::string("cg_") + internal::space_name_base<G, layer, backend, r, rC>::value_wo_grid() + "_p"
+           + XT::Common::to_string(p) + "_space";
+  }
 };
 
 template <class G, XT::Grid::Layers layer, ChooseSpaceBackend backend, int p, size_t r, size_t rC>
@@ -163,6 +176,12 @@ struct space_name<DgSpaceProvider<G, layer, backend, p, double, r, rC>>
   static std::string value()
   {
     return std::string("dg_") + internal::space_name_base<G, layer, backend, r, rC>::value() + "_p"
+           + XT::Common::to_string(p) + "_space";
+  }
+
+  static std::string value_wo_grid()
+  {
+    return std::string("dg_") + internal::space_name_base<G, layer, backend, r, rC>::value_wo_grid() + "_p"
            + XT::Common::to_string(p) + "_space";
   }
 };
@@ -174,6 +193,11 @@ struct space_name<FvSpaceProvider<G, layer, backend, double, r, rC>>
   {
     return std::string("fv_") + internal::space_name_base<G, layer, backend, r, rC>::value() + "_space";
   }
+
+  static std::string value_wo_grid()
+  {
+    return std::string("fv_") + internal::space_name_base<G, layer, backend, r, rC>::value_wo_grid() + "_space";
+  }
 };
 
 template <class G, XT::Grid::Layers l, SpaceType tp, ChooseSpaceBackend backend, int p, class R, size_t r, size_t rC>
@@ -184,6 +208,12 @@ struct space_name<SpaceProvider<G, l, tp, backend, p, R, r, rC>>
     return space_type_name<tp>::value() + "_" + internal::space_name_base<G, l, backend, r, rC>::value() + "_p"
            + XT::Common::to_string(p) + "_space";
   }
+
+  static std::string value_wo_grid()
+  {
+    return space_type_name<tp>::value() + "_" + internal::space_name_base<G, l, backend, r, rC>::value_wo_grid() + "_p"
+           + XT::Common::to_string(p) + "_space";
+  }
 };
 
 
@@ -192,6 +222,45 @@ class SpaceInterface
 {
   typedef typename SP::type S;
   static_assert(is_space<S>::value, "");
+  typedef typename S::GridViewType::Grid G;
+
+  template <bool is_dd_subdomain_layer = (SP::grid_layer == XT::Grid::Layers::dd_subdomain), bool anything = true>
+  struct factory_methods
+  {
+    static void addbind(pybind11::module& m)
+    {
+      using namespace pybind11::literals;
+      const std::string factory_method_name = "make_" + space_name<SP>::value_wo_grid();
+
+      m.def(factory_method_name.c_str(),
+            [](XT::Grid::GridProvider<G, XT::Grid::DD::SubdomainGrid<G>>& grid_provider, int level) {
+              return SP::create(grid_provider, level);
+            },
+            "grid_provider"_a,
+            "level"_a = 0);
+    }
+  };
+
+  template <bool anything>
+  struct factory_methods<false, anything>
+  {
+    static void addbind(pybind11::module& m)
+    {
+      using namespace pybind11::literals;
+      const std::string factory_method_name = "make_" + space_name<SP>::value_wo_grid();
+
+      m.def(factory_method_name.c_str(),
+            [](XT::Grid::GridProvider<G>& grid_provider, int level) { return SP::create(grid_provider, level); },
+            "grid_provider"_a,
+            "level"_a = 0);
+      m.def(factory_method_name.c_str(),
+            [](XT::Grid::GridProvider<G, XT::Grid::DD::SubdomainGrid<G>>& grid_provider, int level) {
+              return SP::create(grid_provider, level);
+            },
+            "grid_provider"_a,
+            "level"_a = 0);
+    }
+  };
 
 public:
   typedef S type;
@@ -202,7 +271,7 @@ public:
     namespace py = pybind11;
     using namespace pybind11::literals;
 
-    const auto ClassName = space_name<SP>::value();
+    const auto ClassName = XT::Common::to_camel_case(space_name<SP>::value());
 
     bound_type c(m, ClassName.c_str(), ClassName.c_str(), py::metaclass());
 
@@ -219,6 +288,8 @@ public:
     c.def("compute_volume_pattern", [](const type& self) { return self.compute_volume_pattern(); });
     c.def("compute_face_pattern", [](const type& self) { return self.compute_face_pattern(); });
     c.def("compute_face_and_volume_pattern", [](const type& self) { return self.compute_face_and_volume_pattern(); });
+
+    factory_methods<>::addbind(m);
 
     return c;
   } // ... bind(...)
