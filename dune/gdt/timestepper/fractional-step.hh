0cbbdc9ed73145ac6c20ecc60487fef915374449
Tobias Leibner <tobias.leibner@googlemail.com>
2017-04-05 10:37:20 +0200

    [timestepper] add strang splitting

--- dune/gdt/timestepper/fractional-step.hh
+++ dune/gdt/timestepper/fractional-step.hh
@@ -29,7 +29,7 @@ namespace GDT {
 
 /**
  * Takes two time steppers and performs a simple fractional step scheme (see e.g. LeVeque, "Finite Volume Methods for
- * Hyperbolic Problems", 2002, Section 17.1. In each time step t, the first stepper is applied up to time t+dt and the
+ * Hyperbolic Problems", 2002, Section 17.1). In each time step t, the first stepper is applied up to time t+dt and the
  * result is taken as input for the second time stepper, which is also applied up to time t+dt. Initial values and time
  * are taken from the first time stepper.
  */
@@ -73,6 +73,63 @@ public:
     const auto dt_2 = second_stepper_.solve(t + actual_dt, dt_1, -1, false);
     t += actual_dt;
     return dt_2;
+  } // ... step(...)
+
+private:
+  FirstStepperType& first_stepper_;
+  SecondStepperType& second_stepper_;
+}; // class FractionalTimeStepper
+
+/**
+ * Takes two time steppers and performs a strang splitting fractional step scheme (see e.g. LeVeque, "Finite Volume
+ * Methods for
+ * Hyperbolic Problems", 2002, Section 17.1). In each time step t, the first stepper is applied up to time t+dt/2, then
+ * the
+ * second time stepper is applied up to time t+dt, then the first stepper is applied again up to time t+dt. Initial
+ * values and time
+ * are taken from the first time stepper.
+ */
+template <class FirstStepperImp, class SecondStepperImp>
+class StrangSplittingTimeStepper : public TimeStepperInterface<typename FirstStepperImp::DiscreteFunctionType,
+                                                               typename FirstStepperImp::TimeFieldType>
+{
+  typedef TimeStepperInterface<typename FirstStepperImp::DiscreteFunctionType, typename FirstStepperImp::TimeFieldType>
+      BaseType;
+
+public:
+  using typename BaseType::DiscreteFunctionType;
+  using typename BaseType::TimeFieldType;
+  using typename BaseType::DomainFieldType;
+  using typename BaseType::RangeFieldType;
+  using typename BaseType::SolutionType;
+
+  typedef FirstStepperImp FirstStepperType;
+  typedef SecondStepperImp SecondStepperType;
+
+  using BaseType::current_solution;
+  using BaseType::current_time;
+  using BaseType::solution;
+
+  StrangSplittingTimeStepper(FirstStepperType& first_stepper, SecondStepperType& second_stepper)
+    : BaseType(first_stepper.current_time(), first_stepper.current_solution())
+    , first_stepper_(first_stepper)
+    , second_stepper_(second_stepper)
+  {
+    first_stepper_.set_current_solution_pointer(current_solution());
+    first_stepper_.set_solution_pointer(solution());
+    second_stepper_.set_current_solution_pointer(current_solution());
+    second_stepper_.set_solution_pointer(solution());
+  } // constructor
+
+  TimeFieldType step(const TimeFieldType dt, const TimeFieldType max_dt) override final
+  {
+    auto& t = current_time();
+    const TimeFieldType actual_dt = std::min(dt, max_dt);
+    const auto dt_1 = first_stepper_.solve(t + actual_dt / 2, actual_dt / 2, -1, false);
+    const auto dt_2 = second_stepper_.solve(t + actual_dt, actual_dt, -1, false);
+    const auto dt_3 = first_stepper_.solve(t + actual_dt, actual_dt / 2, -1, false);
+    t += actual_dt;
+    return dt;
   } // ... step(...)
 
 private:
