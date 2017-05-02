95fe2da9c69a797c43b7df45390a26c2058a5813
Felix Schindler <felix.schindler@wwu.de>
2017-05-02 17:06:42 +0200

    [spaces.block] add first draft of bindings

---
+++ dune/gdt/playground/spaces/block.bindings.cc
@@ -0,0 +1,89 @@
+// This file is part of the dune-gdt project:
+//   https://github.com/dune-community/dune-gdt
+// Copyright 2010-2017 dune-gdt developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2017)
+
+#include "config.h"
+
+#if HAVE_DUNE_PYBINDXI
+
+#include <dune/xt/common/exceptions.hh>
+
+#include <dune/common/parallel/mpihelper.hh>
+
+#if HAVE_DUNE_FEM
+#include <dune/fem/misc/mpimanager.hh>
+#endif
+
+#include <dune/pybindxi/pybind11.h>
+#include <dune/pybindxi/stl.h>
+
+#include "block.bindings.hh"
+
+
+PYBIND11_PLUGIN(__spaces_block)
+{
+  namespace py = pybind11;
+  using namespace pybind11::literals;
+
+  py::module m("__spaces_block", "dune-gdt: Block spaces");
+
+  py::module::import("dune.xt.common");
+  py::module::import("dune.xt.grid");
+  py::module::import("dune.xt.functions");
+  py::module::import("dune.xt.la");
+
+  DUNE_GDT_SPACES_BLOCK_BIND(m);
+
+  m.def("_init_mpi",
+        [](const std::vector<std::string>& args) {
+          int argc = boost::numeric_cast<int>(args.size());
+          char** argv = Dune::XT::Common::vector_to_main_args(args);
+          Dune::MPIHelper::instance(argc, argv);
+#if HAVE_DUNE_FEM
+          Dune::Fem::MPIManager::initialize(argc, argv);
+#endif
+        },
+        "args"_a = std::vector<std::string>());
+
+  m.def("_init_logger",
+        [](const ssize_t max_info_level,
+           const ssize_t max_debug_level,
+           const bool enable_warnings,
+           const bool enable_colors,
+           const std::string& info_color,
+           const std::string& debug_color,
+           const std::string& warning_color) {
+          Dune::XT::Common::TimedLogger().create(
+              max_info_level, max_debug_level, enable_warnings, enable_colors, info_color, debug_color, warning_color);
+        },
+        "max_info_level"_a = std::numeric_limits<ssize_t>::max(),
+        "max_debug_level"_a = std::numeric_limits<ssize_t>::max(),
+        "enable_warnings"_a = true,
+        "enable_colors"_a = true,
+        "info_color"_a = "blue",
+        "debug_color"_a = "darkgray",
+        "warning_color"_a = "red");
+
+  m.def("_test_logger",
+        [](const bool info, const bool debug, const bool warning) {
+          auto logger = Dune::XT::Common::TimedLogger().get("dune.gdt.spaces.block");
+          if (info)
+            logger.info() << "info logging works!" << std::endl;
+          if (debug)
+            logger.debug() << "debug logging works!" << std::endl;
+          if (warning)
+            logger.warn() << "warning logging works!" << std::endl;
+        },
+        "info"_a = true,
+        "debug"_a = true,
+        "warning"_a = true);
+
+  return m.ptr();
+}
+
+#endif // HAVE_DUNE_PYBINDXI
---
+++ dune/gdt/playground/spaces/block.bindings.hh
@@ -0,0 +1,340 @@
+// This file is part of the dune-gdt project:
+//   https://github.com/dune-community/dune-gdt
+// Copyright 2010-2017 dune-gdt developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2017)
+
+#ifndef DUNE_GDT_PLAYGROUND_SPACES_BLOCK_BINDINGS_HH
+#define DUNE_GDT_PLAYGROUND_SPACES_BLOCK_BINDINGS_HH
+//#if HAVE_DUNE_PYBINDXI
+
+#include <dune/pybindxi/pybind11.h>
+
+#include <dune/xt/common/numeric_cast.hh>
+#include <dune/xt/common/string.hh>
+#include <dune/xt/la/container.hh>
+#include <dune/xt/grid/gridprovider/provider.hh>
+#include <dune/xt/grid/dd/subdomains/grid.hh>
+#include <dune/xt/grid/grids.bindings.hh>
+
+#include <dune/gdt/spaces.bindings.hh>
+
+#include "block.hh"
+
+namespace Dune {
+namespace GDT {
+namespace bindings {
+
+
+template <class SP>
+class BlockMapper
+{
+  typedef typename SP::type S;
+  static_assert(is_space<S>::value, "");
+  using G = XT::Grid::extract_grid_t<typename S::GridLayerType>;
+
+public:
+  typedef GDT::BlockMapper<S> type;
+  typedef pybind11::class_<type> bound_type;
+
+  template <XT::LA::Backends la>
+  static void addbind_matrix(bound_type& c)
+  {
+    namespace py = pybind11;
+    using namespace pybind11::literals;
+
+    typedef typename XT::LA::Container<double, la>::MatrixType M;
+
+    c.def("copy_local_to_global",
+          [](const type& self,
+             const M& local_matrix,
+             const XT::LA::SparsityPatternDefault& local_pattern,
+             const ssize_t block,
+             M& global_matrix) {
+            auto bb = XT::Common::numeric_cast<size_t>(block);
+            for (size_t local_ii = 0; local_ii < local_pattern.size(); ++local_ii) {
+              const size_t global_ii = self.mapToGlobal(bb, local_ii);
+              for (const size_t& local_jj : local_pattern.inner(local_ii)) {
+                const size_t global_jj = self.mapToGlobal(bb, local_jj);
+                global_matrix.add_to_entry(global_ii, global_jj, local_matrix.get_entry(local_ii, local_jj));
+              }
+            }
+          },
+          "local_matrix"_a,
+          "local_sparsity_pattern"_a,
+          "block"_a,
+          "global_matrix"_a);
+    c.def("copy_local_to_global",
+          [](const type& self,
+             const M& local_matrix,
+             const XT::LA::SparsityPatternDefault& local_pattern,
+             const ssize_t test_block,
+             const ssize_t ansatz_block,
+             M& global_matrix) {
+            auto tt = XT::Common::numeric_cast<size_t>(test_block);
+            auto aa = XT::Common::numeric_cast<size_t>(ansatz_block);
+            for (size_t local_ii = 0; local_ii < local_pattern.size(); ++local_ii) {
+              const size_t global_ii = self.mapToGlobal(tt, local_ii);
+              for (const size_t& local_jj : local_pattern.inner(local_ii)) {
+                const size_t global_jj = self.mapToGlobal(aa, local_jj);
+                global_matrix.add_to_entry(global_ii, global_jj, local_matrix.get_entry(local_ii, local_jj));
+              }
+            }
+          },
+          "local_matrix"_a,
+          "local_sparsity_pattern"_a,
+          "test_block"_a,
+          "ansatz_block"_a,
+          "global_matrix"_a);
+  } // ... addbind_matrix(...)
+
+  template <XT::LA::Backends la>
+  static void addbind_vector(bound_type& c)
+  {
+    namespace py = pybind11;
+    using namespace pybind11::literals;
+
+    typedef typename XT::LA::Container<double, la>::VectorType V;
+
+    c.def("copy_local_to_global",
+          [](const type& self, const V& local_vector, const ssize_t block, V& global_vector) {
+            auto bb = XT::Common::numeric_cast<size_t>(block);
+            for (size_t local_ii = 0; local_ii < local_vector.size(); ++local_ii) {
+              const size_t global_ii = self.mapToGlobal(bb, local_ii);
+              global_vector.add_to_entry(global_ii, local_vector.get_entry(local_ii));
+            }
+          },
+          "local_vector"_a,
+          "block"_a,
+          "global_vector"_a);
+  } // ... addbind_vector(...)
+
+  static bound_type bind(pybind11::module& m)
+  {
+    namespace py = pybind11;
+    using namespace pybind11::literals;
+
+    const auto ClassName = XT::Common::to_camel_case("block_" + space_name<SP>::value() + "_mappe");
+
+    bound_type c(m, ClassName.c_str());
+
+    c.def_property_readonly("size", [](const type& self) { return self.size(); });
+
+    // sparsity patterns
+    c.def("copy_local_to_global",
+          [](const type& self,
+             const XT::LA::SparsityPatternDefault& local_pattern,
+             const ssize_t block,
+             XT::LA::SparsityPatternDefault& global_pattern) {
+            auto bb = XT::Common::numeric_cast<size_t>(block);
+            for (size_t local_ii = 0; local_ii < local_pattern.size(); ++local_ii) {
+              const size_t global_ii = self.mapToGlobal(bb, local_ii);
+              const auto& local_rows = local_pattern.inner(local_ii);
+              for (const auto& local_jj : local_rows) {
+                const size_t global_jj = self.mapToGlobal(bb, local_jj);
+                global_pattern.insert(global_ii, global_jj);
+              }
+            }
+          },
+          "local_sparsity_pattern"_a,
+          "block"_a,
+          "global_sparsity_pattern"_a);
+    c.def("copy_local_to_global",
+          [](const type& self,
+             const XT::LA::SparsityPatternDefault& local_pattern,
+             const ssize_t test_block,
+             const ssize_t ansatz_block,
+             XT::LA::SparsityPatternDefault& global_pattern) {
+            auto tt = XT::Common::numeric_cast<size_t>(test_block);
+            auto aa = XT::Common::numeric_cast<size_t>(ansatz_block);
+            for (size_t local_ii = 0; local_ii < local_pattern.size(); ++local_ii) {
+              const size_t global_ii = self.mapToGlobal(tt, local_ii);
+              const auto& local_rows = local_pattern.inner(local_ii);
+              for (const auto& local_jj : local_rows) {
+                const size_t global_jj = self.mapToGlobal(aa, local_jj);
+                global_pattern.insert(global_ii, global_jj);
+              }
+            }
+          },
+          "local_sparsity_pattern"_a,
+          "test_block"_a,
+          "ansatz_block"_a,
+          "global_sparsity_pattern"_a);
+
+    // matrices
+    addbind_matrix<XT::LA::Backends::common_dense>(c);
+    addbind_matrix<XT::LA::Backends::common_sparse>(c);
+    addbind_matrix<XT::LA::Backends::eigen_dense>(c);
+    addbind_matrix<XT::LA::Backends::eigen_sparse>(c);
+    addbind_matrix<XT::LA::Backends::istl_sparse>(c);
+
+    // vectors
+    addbind_vector<XT::LA::Backends::common_dense>(c);
+    addbind_vector<XT::LA::Backends::eigen_dense>(c);
+    addbind_vector<XT::LA::Backends::istl_dense>(c);
+
+    return c;
+  } // ... bind(...)
+}; // class BlockMapper
+
+
+template <class SP>
+class BlockSpace
+{
+  typedef typename SP::type S;
+  static_assert(is_space<S>::value, "");
+  using G = XT::Grid::extract_grid_t<typename S::GridLayerType>;
+
+public:
+  typedef GDT::BlockSpace<S> type;
+  typedef pybind11::class_<type> bound_type;
+
+  static bound_type bind(pybind11::module& m)
+  {
+    BlockMapper<SP>::bind(m);
+
+    namespace py = pybind11;
+    using namespace pybind11::literals;
+
+    const auto ClassName = XT::Common::to_camel_case("block_" + space_name<SP>::value());
+
+    bound_type c(m, ClassName.c_str(), ClassName.c_str(), py::metaclass()); // metaclass required for static properties
+
+    c.def("__init__",
+          [](type& self, XT::Grid::GridProvider<G, XT::Grid::DD::SubdomainGrid<G>>& dd_grid_provider) {
+            const auto& dd_grid = dd_grid_provider.dd_grid();
+            auto local_spaces = std::make_shared<std::vector<S>>();
+            for (size_t ss = 0; ss < dd_grid.size(); ++ss)
+              local_spaces->emplace_back(SP::create(dd_grid_provider, boost::numeric_cast<int>(ss)));
+            try {
+              new (&self) type(dd_grid, local_spaces);
+            } catch (...) {
+              self.~type();
+              throw;
+            }
+          },
+          "dd_grid"_a,
+          py::keep_alive<1, 2>());
+    c.def_property_readonly("dimDomain", [](const type& /*self*/) { return S::dimDomain; });
+    c.def_property_readonly("dimRange", [](const type& /*self*/) { return S::dimRange; });
+    c.def_property_readonly("dimRangeCols", [](const type& /*self*/) { return S::dimRangeCols; });
+    c.def_property_readonly("polOrder", [](const type& /*self*/) { return S::polOrder; });
+    c.def_property_readonly("num_blocks", [](const type& self) { return self.num_blocks(); });
+    c.def_property_readonly("mapper",
+                            [](const type& self) {
+                              // we get a segfault without the explicit copy
+                              return std::decay_t<decltype(self.mapper())>(self.mapper());
+                            },
+                            py::keep_alive<0, 1>());
+    // these need to be defined *after* their non static counterparts
+    c.def_property_readonly_static("dimDomain", [](const type& /*self*/) { return S::dimDomain; });
+    c.def_property_readonly_static("dimRange", [](const type& /*self*/) { return S::dimRange; });
+    c.def_property_readonly_static("dimRangeCols", [](const type& /*self*/) { return S::dimRangeCols; });
+    c.def_property_readonly_static("polOrder", [](const type& /*self*/) { return S::polOrder; });
+    c.def("local_space",
+          [](const type& self, ssize_t block) { return self.local_space(XT::Common::numeric_cast<size_t>(block)); },
+          "block"_a);
+    c.def("compute_boundary_pattern",
+          [](const type& self, const ssize_t block, const std::string tp) {
+            auto bb = XT::Common::numeric_cast<size_t>(block);
+            if (tp == "volume")
+              return self.local_space(bb).compute_volume_pattern(self.dd_grid().boundaryGridPart(bb));
+            else if (tp == "face")
+              return self.local_space(bb).compute_face_pattern(self.dd_grid().boundaryGridPart(bb));
+            else if (tp == "face_and_volume")
+              return self.local_space(bb).compute_face_and_volume_pattern(self.dd_grid().boundaryGridPart(bb));
+            else
+              DUNE_THROW(XT::Common::Exceptions::wrong_input_given,
+                         "  type has to be one of ('volume', 'face', 'face_and_volume'), is '" << tp << "'!");
+            // we will never get here
+            return XT::LA::SparsityPatternDefault();
+          },
+          "block"_a,
+          "type"_a);
+    c.def("compute_coupling_pattern",
+          [](const type& self, const ssize_t subdomain, const ssize_t neighbor, const std::string tp) {
+            auto ss = XT::Common::numeric_cast<size_t>(subdomain);
+            auto nn = XT::Common::numeric_cast<size_t>(neighbor);
+            if (tp == "volume")
+              return self.local_space(ss).compute_volume_pattern(self.dd_grid().couplingGridPart(ss, nn),
+                                                                 self.local_space(nn));
+            else if (tp == "face")
+              return self.local_space(ss).compute_face_pattern(self.dd_grid().couplingGridPart(ss, nn),
+                                                               self.local_space(nn));
+            else if (tp == "face_and_volume")
+              return self.local_space(ss).compute_face_and_volume_pattern(self.dd_grid().couplingGridPart(ss, nn),
+                                                                          self.local_space(nn));
+            else
+              DUNE_THROW(XT::Common::Exceptions::wrong_input_given,
+                         "  type has to be one of ('volume', 'face', 'face_and_volume'), is '" << tp << "'!");
+            // we will never get here
+            return XT::LA::SparsityPatternDefault();
+          },
+          "block_subdomain"_a,
+          "neighboring_subdomain"_a,
+          "type"_a);
+
+    const std::string factory_method_name = "make_block_" + space_name<SP>::value_wo_grid();
+
+    m.def(factory_method_name.c_str(), [](XT::Grid::GridProvider<G, XT::Grid::DD::SubdomainGrid<G>>& dd_grid_provider) {
+      const auto& dd_grid = dd_grid_provider.dd_grid();
+      auto local_spaces = std::make_shared<std::vector<S>>();
+      for (size_t ss = 0; ss < dd_grid.size(); ++ss)
+        local_spaces->emplace_back(SP::create(dd_grid_provider, boost::numeric_cast<int>(ss)));
+      return type(dd_grid, local_spaces);
+    });
+
+    return c;
+  } // ... bind(...)
+}; // class BlockSpace
+
+
+} // namespace bindings
+} // namespace GDT
+} // namespace Dune
+
+
+// begin: this is what we need for the .so
+
+#define _DUNE_GDT_SPACES_BLOCK_BIND(_m, _GRID, _s_type, _s_backend, _p)                                                \
+  Dune::GDT::bindings::BlockSpace<Dune::GDT::SpaceProvider<_GRID,                                                      \
+                                                           Dune::XT::Grid::Layers::dd_subdomain,                       \
+                                                           Dune::GDT::SpaceType::_s_type,                              \
+                                                           Dune::GDT::Backends::_s_backend,                            \
+                                                           _p,                                                         \
+                                                           double,                                                     \
+                                                           1,                                                          \
+                                                           1>>::bind(_m)
+
+#if HAVE_DUNE_ALUGRID
+#define _DUNE_GDT_SPACES_BLOCK_BIND_ALU(_m, _s_type, _s_backend, _p)                                                   \
+  _DUNE_GDT_SPACES_BLOCK_BIND(_m, ALU_2D_SIMPLEX_CONFORMING, _s_type, _s_backend, _p)
+#else
+#define _DUNE_GDT_SPACES_BLOCK_BIND_ALU(_m, _s_type, _s_backend, _p)
+#endif
+
+#define _DUNE_GDT_SPACES_BLOCK_BIND_YASP(_m, _s_type, _s_backend, _p)                                                  \
+  _DUNE_GDT_SPACES_BLOCK_BIND(_m, YASP_1D_EQUIDISTANT_OFFSET, _s_type, _s_backend, _p);                                \
+  _DUNE_GDT_SPACES_BLOCK_BIND(_m, YASP_2D_EQUIDISTANT_OFFSET, _s_type, _s_backend, _p)
+
+#define _DUNE_GDT_SPACES_BLOCK_BIND_ALL_GRIDS(_m, _s_type, _s_backend, _p)                                             \
+  _DUNE_GDT_SPACES_BLOCK_BIND_ALU(_m, _s_type, _s_backend, _p);                                                        \
+  _DUNE_GDT_SPACES_BLOCK_BIND_YASP(_m, _s_type, _s_backend, _p)
+
+
+#if HAVE_DUNE_FEM
+#define _DUNE_GDT_SPACES_BLOCK_BIND_FEM(_m) _DUNE_GDT_SPACES_BLOCK_BIND_ALL_GRIDS(_m, dg, fem, 1)
+#else
+#define _DUNE_GDT_SPACES_BLOCK_BIND_FEM(_m)
+#endif
+
+#define DUNE_GDT_SPACES_BLOCK_BIND(_m) _DUNE_GDT_SPACES_BLOCK_BIND_FEM(_m)
+
+// end: this is what we need for the .so
+
+
+//#endif // HAVE_DUNE_PYBINDXI
+#endif // DUNE_GDT_PLAYGROUND_SPACES_BLOCK_BINDINGS_HH
