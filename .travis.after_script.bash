fe7053705b7f0fb11f1deb5d2e25d8ce8bf9aafa
Felix Schindler <felix.schindler@wwu.de>
2017-07-13 16:25:01 +0200

    [travis] do not fail prs which do not originate from us

--- .travis.after_script.bash
+++ .travis.after_script.bash
@@ -16,6 +16,8 @@ if [[ $TRAVIS_JOB_NUMBER == *.1 ]] ; then
     PYTHONPATH=${SUPERDIR}/scripts/python/ python3 -c "import travis_report as tp; tp.clang_format_status(\"${CHECK_DIR}\")"
     ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
     ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make doc
-    ${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-community.github.io
-    ${SUPERDIR}/.travis/deploy_docs.sh ${MY_MODULE} "${DUNE_BUILD_DIR}"
+    if [ "X${TRAVIS_PULL_REQUEST}" != "Xfalse" ] ; then
+        ${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-community.github.io
+        ${SUPERDIR}/.travis/deploy_docs.sh ${MY_MODULE} "${DUNE_BUILD_DIR}"
+    fi
 fi
