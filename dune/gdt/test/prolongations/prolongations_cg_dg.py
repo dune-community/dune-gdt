b5b4f367b2eb52479b47107b927cc2c3970914bb
Felix Schindler <felix.schindler@wwu.de>
2017-08-02 16:05:34 +0200

    [test] disable alugrid/levelview combinations

--- dune/gdt/test/prolongations/prolongations_cg_dg.py
+++ dune/gdt/test/prolongations/prolongations_cg_dg.py
@@ -13,8 +13,10 @@ from grids import LevelGrids
 
 cg = sp.CG(cache, base=LevelGrids)
 dg = sp.DG(cache, base=LevelGrids)
-fv = sp.FV(cache, base=LevelGrids, rdim=[1])
-rt = sp.RT(cache, base=LevelGrids)
-spaces = cg.spaces + dg.spaces + fv.spaces + rt.spaces
-names = cg.names + dg.names + fv.names + rt.names
+spaces = []
+names = []
+for sp, nm in zip(cg.spaces + dg.spaces, cg.names + dg.names):
+    if not ('AluConform2dLevelGrid' in sp):
+        spaces.append(sp)
+        names.append(nm)
 spaces_with_names = zip(spaces, names)
