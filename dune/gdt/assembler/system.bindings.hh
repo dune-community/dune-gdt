fdb4d88442865518945b999a5d30d49167d303f1
Felix Schindler <felix.schindler@wwu.de>
2017-11-30 15:01:03 +0100

    [bindings|assembler] split in two, allows for easier binding from elsewhere

--- dune/gdt/assembler/system.bindings.hh
+++ dune/gdt/assembler/system.bindings.hh
@@ -18,7 +18,7 @@
 #include <dune/xt/grid/grids.hh>
 #include <dune/xt/grid/grids.bindings.hh>
 #include <dune/xt/grid/layers.bindings.hh>
-#include <dune/xt/grid/walker.hh>
+#include <dune/xt/grid/walker.bindings.hh>
 
 #include <dune/gdt/spaces.bindings.hh>
 #include <dune/gdt/spaces/constraints.bindings.hh>
@@ -60,19 +60,22 @@ private:
 }; // class ResultStorage
 
 
-template <class TP, XT::Grid::Layers grid_layer, XT::Grid::Backends grid_backend>
+namespace internal {
+
+
+template <class T, class GL = typename T::GridLayerType, class A = T>
 class SystemAssembler
 {
-  typedef typename TP::type T;
   static_assert(is_space<T>::value, "");
-  typedef XT::Grid::extract_grid_t<typename T::GridLayerType> G;
-  typedef typename XT::Grid::Layer<G, grid_layer, grid_backend, XT::Grid::DD::SubdomainGrid<G>>::type GL;
+  static_assert(XT::Grid::is_layer<GL>::value, "");
+  static_assert(is_space<A>::value, "");
+  typedef XT::Grid::extract_grid_t<GL> G;
   typedef XT::Grid::extract_entity_t<GL> E;
   typedef typename G::ctype D;
   static const constexpr size_t d = G::dimension;
 
 public:
-  typedef GDT::SystemAssembler<T, GL> type;
+  typedef GDT::SystemAssembler<T, GL, A> type;
   typedef pybind11::class_<type, XT::Grid::Walker<GL>> bound_type;
 
 private:
@@ -80,53 +83,36 @@ private:
   typedef typename type::GridLayerType GridLayerType;
   typedef typename type::AnsatzSpaceType AnsatzSpaceType;
 
-  template <bool do_bind = (std::is_same<TestSpaceType, AnsatzSpaceType>::value
-                            && std::is_same<GridLayerType, typename TestSpaceType::GridLayerType>::value),
+  template <bool same_spaces = std::is_same<TestSpaceType, AnsatzSpaceType>::value,
+            bool same_layer = std::is_same<GridLayerType, typename TestSpaceType::GridLayerType>::value&&
+                std::is_same<GridLayerType, typename AnsatzSpaceType::GridLayerType>::value,
             bool anything = true>
-  struct addbind_ctor_single
+  struct addbind_switch
   {
-    void operator()(bound_type& c)
+    static void ctors(bound_type& /*c*/)
+    {
+    }
+
+    static void factory_methods(pybind11::module& /*m*/)
+    {
+    }
+  };
+
+  template <bool anything>
+  struct addbind_switch<true, true, anything>
+  {
+    static void ctors(bound_type& c)
     {
       namespace py = pybind11;
       using namespace pybind11::literals;
+
       c.def(py::init<T>(),
             "space"_a,
-            "Uses given space as test and ansatz space, and the grid layer of the given space as grid layer.",
+            "Uses space as test and ansatz space, and its grid layer as grid layer",
             py::keep_alive<1, 2>());
     }
-  }; // struct addbind_ctor_single
 
-  template <bool anything>
-  struct addbind_ctor_single<false, anything>
-  {
-    void operator()(bound_type& /*c*/)
-    {
-    }
-  }; // struct addbind_ctor_single
-
-  template <bool same = std::is_same<GL, typename T::GridLayerType>::value, bool anything = true>
-  struct space_and_layer_name
-  {
-    static std::string value()
-    {
-      return space_name<TP>::value();
-    }
-  };
-
-  template <bool anything>
-  struct space_and_layer_name<false, anything>
-  {
-    static std::string value()
-    {
-      return space_name<TP>::value() + "_" + XT::Grid::bindings::layer_name<grid_layer>::value() + "_"
-             + XT::Grid::bindings::backend_name<grid_backend>::value();
-    }
-  };
-
-  template <bool same = std::is_same<GL, typename T::GridLayerType>::value, bool anything = true>
-  struct addbind_factory_methods
-  {
-    void operator()(pybind11::module& m)
+    static void factory_methods(pybind11::module& m)
     {
       namespace py = pybind11;
       using namespace pybind11::literals;
@@ -134,17 +120,36 @@ private:
       m.def("make_system_assembler",
             [](const TestSpaceType& space) { return new type(space); },
             "space"_a,
+            "Uses space as test and ansatz space, and its grid layer as grid layer",
             py::keep_alive<0, 1>());
+
+      addbind_switch<false, true>::factory_methods(m);
     }
-  };
+  }; // struct factory_methods<true, true, ...>
 
   template <bool anything>
-  struct addbind_factory_methods<false, anything>
+  struct addbind_switch<false, true, anything>
   {
-    void operator()(pybind11::module& /*m*/)
+    static void ctors(bound_type& /*c*/)
     {
     }
-  };
+
+    static void factory_methods(pybind11::module& m)
+    {
+      namespace py = pybind11;
+      using namespace pybind11::literals;
+
+      m.def("make_system_assembler",
+            [](const TestSpaceType& test_space, const TestSpaceType& ansatz_space) {
+              return new type(test_space, ansatz_space);
+            },
+            "test_space"_a,
+            "ansatz_space"_a,
+            "Uses the grid layer of the test_space as grid layer.",
+            py::keep_alive<0, 1>(),
+            py::keep_alive<0, 2>());
+    }
+  }; // struct factory_methods<false, true, ...>
 
   template <XT::LA::Backends la>
   static void addaddbind_matrixatrix(bound_type& c)
@@ -210,17 +215,31 @@ private:
   } // ... addaddbind_matrixatrix(...)
 
 public:
-  static bound_type bind(pybind11::module& m)
+  static bound_type bind(pybind11::module& m,
+                         const std::string& test_space_name,
+                         const std::string& ansatz_space_name,
+                         const std::string& grid_layer_name)
   {
+    try {
+      XT::Grid::bindings::internal::Walker<GL>::bind(m,
+                                                     XT::Grid::bindings::grid_name<G>::value() + "_" + grid_layer_name);
+    } catch (std::runtime_error&) {
+      // no problem, someone added this walker already
+    }
+
     namespace py = pybind11;
     using namespace pybind11::literals;
 
-    const auto ClassName =
-        XT::Common::to_camel_case(std::string("system_assembler_") + space_and_layer_name<>::value());
-
-    bound_type c(m, ClassName.c_str(), ClassName.c_str());
-    addbind_ctor_single<>()(c);
-
+    // add class
+    std::string class_name = "system_assembler";
+    class_name += "_wrt_" + test_space_name;
+    if (ansatz_space_name != test_space_name)
+      class_name += "_and_" + ansatz_space_name;
+    class_name += "_on_" + grid_layer_name;
+    bound_type c(m, XT::Common::to_camel_case(class_name).c_str(), XT::Common::to_camel_case(class_name).c_str());
+    // add ctor
+    addbind_switch<>::ctors(c);
+    // add rest
     c.def("append",
           [](type& self, type& other, const XT::Grid::ApplyOn::WhichIntersection<GL>& which_intersections) {
             self.append(other, which_intersections.copy());
@@ -243,7 +262,7 @@ public:
             self.assemble(use_tbb);
           },
           "use_tbb"_a = false);
-
+    // add constraints
     bindings::DirichletConstraints<XT::Grid::extract_intersection_t<typename type::GridLayerType>,
                                    XT::Grid::extract_grid_t<typename type::GridLayerType>>::addbind(c);
 
@@ -270,27 +289,44 @@ public:
           py::keep_alive<0, 1>(),
           py::keep_alive<0, 2>(),
           py::keep_alive<0, 3>());
-
-    addbind_factory_methods<>()(m);
-
+    // add factory methods
+    addbind_switch<>::factory_methods(m);
+    // finished
     return c;
   } // ... bind(...)
 }; // class SystemAssembler
 
 
+} // namespace internal
+
+
+template <class TP, XT::Grid::Layers grid_layer, XT::Grid::Backends grid_backend, class AP>
+class SystemAssembler
+{
+  typedef typename TP::type T;
+  static_assert(is_space<T>::value, "");
+  typedef typename AP::type A;
+  static_assert(is_space<A>::value, "");
+  typedef XT::Grid::extract_grid_t<typename T::GridLayerType> G;
+  typedef typename XT::Grid::Layer<G, grid_layer, grid_backend, XT::Grid::DD::SubdomainGrid<G>>::type GL;
+  using binder = internal::SystemAssembler<T, GL, A>;
+
+public:
+  using type = typename binder::type;
+  using bound_type = typename binder::bound_type;
+
+  static bound_type bind(pybind11::module& m)
+  {
+    return binder::bind(m,
+                        space_name<TP>::value(),
+                        space_name<AP>::value(),
+                        XT::Grid::bindings::layer_name<grid_layer>::value() + "_"
+                            + XT::Grid::bindings::backend_name<grid_backend>::value());
+  }
+}; // class SystemAssembler
 } // namespace bindings
 } // namespace GDT
 } // namespace Dune
-
-
-
-
-
-
-  _DUNE_GDT_ASSEMBLER_SYSTEM_BIND_LIB_ALU_SPACE(_pre, leaf, view, _s_type, gdt, leaf, _p, 1, 1);                       \
-  _DUNE_GDT_ASSEMBLER_SYSTEM_BIND_LIB_ALU(_pre, fv, 0, 1, 1);                                                          \
-  _DUNE_GDT_ASSEMBLER_SYSTEM_BIND_LIB_ALU(_pre, cg, 1, 1, 1)
-  _DUNE_GDT_ASSEMBLER_SYSTEM_BIND_LIB_YASP(_pre, cg, 1, 1, 1)
 
 
 
