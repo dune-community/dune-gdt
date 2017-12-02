f26339591de000fab678cf338e2b0d1fcb0d5812
Felix Schindler <felix.schindler@wwu.de>
2017-12-02 09:53:43 +0100

    [prolongations] allow for different source/range, clean up header, simplify

--- dune/gdt/prolongations.bindings.cc
+++ dune/gdt/prolongations.bindings.cc
@@ -25,6 +25,29 @@
 #include <dune/gdt/prolongations.bindings.hh>
 
 
+#define DUNE_GDT_PROLONGATIONS_BIND(                                                                                   \
+    _m, _s_G, _s_gl, _s_backend, _s_type, _s_p, _r, _s_la, _r_G, _r_gl, _r_backend, _r_type, _r_p, _r_la)              \
+  Dune::GDT::bindings::prolong<                                                                                        \
+      typename Dune::GDT::SpaceProvider<_s_G,                                                                          \
+                                        Dune::XT::Grid::Layers::_s_gl,                                                 \
+                                        Dune::GDT::SpaceType::_s_type,                                                 \
+                                        Dune::GDT::Backends::_s_backend,                                               \
+                                        _s_p,                                                                          \
+                                        double,                                                                        \
+                                        _r,                                                                            \
+                                        1>::type,                                                                      \
+      typename Dune::XT::LA::Container<double, Dune::XT::LA::Backends::_s_la>::VectorType,                             \
+      typename Dune::GDT::SpaceProvider<_r_G,                                                                          \
+                                        Dune::XT::Grid::Layers::_r_gl,                                                 \
+                                        Dune::GDT::SpaceType::_r_type,                                                 \
+                                        Dune::GDT::Backends::_r_backend,                                               \
+                                        _r_p,                                                                          \
+                                        double,                                                                        \
+                                        _r,                                                                            \
+                                        1>::type,                                                                      \
+      typename Dune::XT::LA::Container<double, Dune::XT::LA::Backends::_r_la>::VectorType>::bind(_m)
+
+
 PYBIND11_PLUGIN(__prolongations)
 {
   namespace py = pybind11;
@@ -33,8 +56,12 @@ PYBIND11_PLUGIN(__prolongations)
   py::module m("__prolongations", "dune-gdt: prolongations");
   DUNE_XT_COMMON_BINDINGS_INITIALIZE(m, "dune.gdt.prolongations");
 
+  using G = ALU_2D_SIMPLEX_CONFORMING;
 
-  DUNE_GDT_PROLONGATIONS_BIND(m);
+  DUNE_GDT_PROLONGATIONS_BIND(m, G, leaf, fem, dg, 1, 1, istl_dense, G, leaf, fem, dg, 1, istl_dense);
+  DUNE_GDT_PROLONGATIONS_BIND(m, G, leaf, fem, dg, 1, 1, istl_dense, G, leaf, fem, dg, 2, istl_dense);
+  DUNE_GDT_PROLONGATIONS_BIND(m, G, leaf, fem, dg, 1, 1, istl_dense, G, leaf, fem, dg, 3, istl_dense);
+  DUNE_GDT_PROLONGATIONS_BIND(m, G, dd_subdomain, fem, block_dg, 1, 1, istl_dense, G, leaf, fem, dg, 1, istl_dense);
 
   return m.ptr();
 }
