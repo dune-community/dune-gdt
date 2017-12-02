a021c37e4114d549d33f0280723c1c82b034fc55
Felix Schindler <felix.schindler@wwu.de>
2017-12-02 09:50:40 +0100

    [bindings|discretefunction] clean header and simplify

--- dune/gdt/discretefunction.bindings.cc
+++ dune/gdt/discretefunction.bindings.cc
@@ -25,6 +25,19 @@
 #include <dune/gdt/discretefunction/default.bindings.hh>
 
 
+#define DUNE_GDT_DISCRETEFUNCTION_DEFAULT_BIND(_m, _G, _g_layer, _s_backend, _s_type, _p, _r, _la)                     \
+  Dune::GDT::bindings::                                                                                                \
+      DiscreteFunction<Dune::GDT::SpaceProvider<_G,                                                                    \
+                                                Dune::XT::Grid::Layers::_g_layer,                                      \
+                                                Dune::GDT::SpaceType::_s_type,                                         \
+                                                Dune::GDT::Backends::_s_backend,                                       \
+                                                _p,                                                                    \
+                                                double,                                                                \
+                                                _r,                                                                    \
+                                                1>,                                                                    \
+                       typename Dune::XT::LA::Container<double, Dune::XT::LA::Backends::_la>::VectorType>::bind(_m)
+
+
 PYBIND11_PLUGIN(__discretefunction)
 {
   namespace py = pybind11;
@@ -33,8 +46,13 @@ PYBIND11_PLUGIN(__discretefunction)
   py::module m("__discretefunction", "dune-gdt: discrete functions");
   DUNE_XT_COMMON_BINDINGS_INITIALIZE(m, "dune.gdt.discretefunction");
 
+  using G = ALU_2D_SIMPLEX_CONFORMING;
 
-  DUNE_GDT_DISCRETEFUNCTION_DEFAULT_BIND(m);
+  DUNE_GDT_DISCRETEFUNCTION_DEFAULT_BIND(m, G, leaf, fem, dg, 1, 1, istl_dense);
+  DUNE_GDT_DISCRETEFUNCTION_DEFAULT_BIND(m, G, leaf, fem, dg, 2, 1, istl_dense);
+  DUNE_GDT_DISCRETEFUNCTION_DEFAULT_BIND(m, G, leaf, fem, dg, 3, 1, istl_dense);
+  DUNE_GDT_DISCRETEFUNCTION_DEFAULT_BIND(m, G, dd_subdomain, fem, block_dg, 1, 1, istl_dense);
+  DUNE_GDT_DISCRETEFUNCTION_DEFAULT_BIND(m, G, leaf, pdelab, rt, 0, 2, istl_dense);
 
   return m.ptr();
 }
