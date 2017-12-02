3efe016756e9dc21b5f2e3fd13de84f78c7cc27b
Felix Schindler <felix.schindler@wwu.de>
2017-12-02 09:48:35 +0100

    [bindings] use module init macro

--- dune/gdt/operators/oswaldinterpolation.bindings.cc
+++ dune/gdt/operators/oswaldinterpolation.bindings.cc
@@ -34,8 +34,7 @@ PYBIND11_PLUGIN(__operators_oswaldinterpolation)
   using namespace pybind11::literals;
 
   py::module m("__operators_oswaldinterpolation", "dune-gdt: OswaldInterpolationOperator");
-
-  Dune::XT::Common::bindings::addbind_exceptions(m);
+  DUNE_XT_COMMON_BINDINGS_INITIALIZE(m, "dune.gdt.operators.oswald-interpolation");
 
 #if HAVE_DUNE_ALUGRID && HAVE_DUNE_FEM && HAVE_DUNE_ISTL
   Dune::GDT::bindings::OswaldInterpolationOperator<ALU_2D_SIMPLEX_CONFORMING,
@@ -48,50 +47,6 @@ PYBIND11_PLUGIN(__operators_oswaldinterpolation)
                                                    Dune::XT::LA::Backends::istl_dense,
                                                    Dune::XT::Grid::Layers::dd_subdomain_oversampled>::bind(m);
 #endif // HAVE_DUNE_ALUGRID && HAVE_DUNE_FEM && HAVE_DUNE_ISTL
-
-  m.def("_init_mpi",
-        [](const std::vector<std::string>& args) {
-          int argc = Dune::XT::Common::numeric_cast<int>(args.size());
-          char** argv = Dune::XT::Common::vector_to_main_args(args);
-          Dune::MPIHelper::instance(argc, argv);
-#if HAVE_DUNE_FEM
-          Dune::Fem::MPIManager::initialize(argc, argv);
-#endif
-        },
-        "args"_a = std::vector<std::string>());
-
-  m.def("_init_logger",
-        [](const ssize_t max_info_level,
-           const ssize_t max_debug_level,
-           const bool enable_warnings,
-           const bool enable_colors,
-           const std::string& info_color,
-           const std::string& debug_color,
-           const std::string& warning_color) {
-          Dune::XT::Common::TimedLogger().create(
-              max_info_level, max_debug_level, enable_warnings, enable_colors, info_color, debug_color, warning_color);
-        },
-        "max_info_level"_a = std::numeric_limits<ssize_t>::max(),
-        "max_debug_level"_a = std::numeric_limits<ssize_t>::max(),
-        "enable_warnings"_a = true,
-        "enable_colors"_a = true,
-        "info_color"_a = "blue",
-        "debug_color"_a = "darkgray",
-        "warning_color"_a = "red");
-
-  m.def("_test_logger",
-        [](const bool info, const bool debug, const bool warning) {
-          auto logger = Dune::XT::Common::TimedLogger().get("dune.gdt.operators.elliptic");
-          if (info)
-            logger.info() << "info logging works!" << std::endl;
-          if (debug)
-            logger.debug() << "debug logging works!" << std::endl;
-          if (warning)
-            logger.warn() << "warning logging works!" << std::endl;
-        },
-        "info"_a = true,
-        "debug"_a = true,
-        "warning"_a = true);
 
   return m.ptr();
 }
