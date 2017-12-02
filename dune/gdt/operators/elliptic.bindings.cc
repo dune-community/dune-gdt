943625fb1ec342d9998b3650997973c60862cfab
Felix Schindler <felix.schindler@wwu.de>
2017-12-02 09:51:31 +0100

    [bindings|operators.elliptic] clean header and simplify

--- dune/gdt/operators/elliptic.bindings.cc
+++ dune/gdt/operators/elliptic.bindings.cc
@@ -25,17 +25,33 @@
 #include <dune/gdt/operators/elliptic.bindings.hh>
 
 
+#define DUNE_GDT_OPERATORS_ELLIPTIC_BIND(_m, _G, _gl, _gl_backend, _dt, _la, _s_backend, _s_type, _s_layer, _s_p)      \
+  Dune::GDT::bindings::EllipticMatrixOperator<_G,                                                                      \
+                                              Dune::XT::Grid::Layers::_gl,                                             \
+                                              Dune::XT::Grid::Backends::_gl_backend,                                   \
+                                              _dt,                                                                     \
+                                              Dune::XT::LA::Backends::_la,                                             \
+                                              Dune::GDT::Backends::_s_backend,                                         \
+                                              Dune::GDT::SpaceType::_s_type,                                           \
+                                              Dune::XT::Grid::Layers::_s_layer,                                        \
+                                              _s_p>::bind(_m)
+
+
 PYBIND11_PLUGIN(__operators_elliptic)
 {
   namespace py = pybind11;
   using namespace pybind11::literals;
+  using namespace Dune;
 
   py::module m("__operators_elliptic", "dune-gdt: EllipticMatrixOperator");
   DUNE_XT_COMMON_BINDINGS_INITIALIZE(m, "dune.gdt.operators.elliptic");
 
+  using G = ALU_2D_SIMPLEX_CONFORMING;
 
-  DUNE_GDT_OPERATORS_ELLIPTIC_BIND_ISTL(m);
-
+  DUNE_GDT_OPERATORS_ELLIPTIC_BIND(m, G, leaf, part, true, istl_sparse, fem, dg, leaf, 1);
+  DUNE_GDT_OPERATORS_ELLIPTIC_BIND(m, G, leaf, part, true, istl_sparse, fem, dg, leaf, 2);
+  DUNE_GDT_OPERATORS_ELLIPTIC_BIND(m, G, leaf, part, true, istl_sparse, fem, dg, leaf, 3);
+  DUNE_GDT_OPERATORS_ELLIPTIC_BIND(m, G, dd_subdomain, part, true, istl_sparse, fem, dg, dd_subdomain, 1);
 
   return m.ptr();
 }
