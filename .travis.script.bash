29972769f438923211088d502cba1ef0ee0b4041
René Milk <rene.milk@wwu.de>
2017-12-05 12:21:18 +0100

    [ci] move travis wait inside container

--- .travis.script.bash
+++ .travis.script.bash
@@ -12,26 +12,29 @@
 
 set -e
 
+WAIT="${SUPERDIR}/scripts/bash/travis_wait_new.bash 45"
+source ${SUPERDIR}/scripts/bash/retry_command.bash
+
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make
 
 # this does nothing if all current tests are distributed already, but triggers full build if not
 # -> builder will timeout -> manually run refresh_test_timings -> push results
-# ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 refresh_test_timings
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 refresh_test_timings
 
 if [ x"${TESTS}" == x ] ; then
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v test_binaries
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2
+    ${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v test_binaries
+    ${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2
 else
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
+    ${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
+    ${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
 fi
 if [ "X${TRAVIS_PULL_REQUEST}" != "Xfalse" ] ; then
         ${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-gdt-testlogs
-        travis_retry ${SUPERDIR}/scripts/bash/travis_upload_test_logs.bash ${DUNE_BUILD_DIR}/${MY_MODULE}/dune/gdt/test/
+        retry_command ${SUPERDIR}/scripts/bash/travis_upload_test_logs.bash ${DUNE_BUILD_DIR}/${MY_MODULE}/dune/gdt/test/
 fi
 
-unset GH_TOKEN
-${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make install | grep -v "Installing"
-${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make package_source
+#unset GH_TOKEN
+#${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make install | grep -v "Installing"
+#${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make package_source
 
