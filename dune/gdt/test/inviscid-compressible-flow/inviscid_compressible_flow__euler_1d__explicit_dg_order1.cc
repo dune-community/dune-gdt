764e4b07ce6b50e418fe9da09ef3467706d8973f
Felix Schindler <felix.schindler@wwu.de>
2018-01-23 14:23:23 +0100

    [test...inviscid] add DG order 1, periodic BV

---
+++ dune/gdt/test/inviscid-compressible-flow/inviscid_compressible_flow__euler_1d__explicit_dg_order1.cc
@@ -0,0 +1,52 @@
+// This file is part of the dune-gdt project:
+//   https://github.com/dune-community/dune-gdt
+// Copyright 2010-2017 dune-gdt developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2017 - 2018)
+
+#define DUNE_XT_COMMON_TEST_MAIN_ENABLE_TIMED_LOGGING 1
+#define DUNE_XT_COMMON_TEST_MAIN_ENABLE_INFO_LOGGING 1
+
+#include <dune/xt/common/test/main.hxx> // <- this one has to come first (includes the config.h)!
+
+#include <dune/xt/functions/flattop.hh>
+
+#include <dune/gdt/operators/advection-dg.hh>
+#include <dune/gdt/spaces/dg/dune-fem-wrapper.hh>
+
+#include "inviscid_compressible_flow__euler_1d__explicit.hh"
+
+using namespace Dune;
+using namespace Dune::GDT;
+
+
+template <class GL, class R, size_t m>
+using DgOrder1SpaceType = DuneFemDgSpaceWrapper<GL, 1, R, m, 1>;
+
+template <class DF>
+using DgOperator = GDT::AdvectionDgOperator<DF, typename DF::SpaceType::GridLayerType>;
+
+using InviscidCompressibleFlowEuler1dExplicitDgOrder1 =
+    InviscidCompressibleFlowEuler1dExplicitTest<DgOrder1SpaceType, DgOperator, XT::Grid::Backends::part>;
+
+
+TEST_F(InviscidCompressibleFlowEuler1dExplicitDgOrder1, periodic_boundaries)
+{
+  // use smooth initial values to avoid the Gibbs phenomenon
+  ASSERT_NE(initial_values_, nullptr);
+  XT::Functions::FlatTopFunction<E, D, d, R, 1> density_minus_one({-0.5}, {0.}, 0.015625, 3.);
+  XT::Functions::FlatTopFunction<E, D, d, R, 1> pressure_minus_04({-0.5}, {0.}, 0.015625, 1.2);
+  U initial_values(
+      [&](const auto& xx, const auto& /*param*/) {
+        return euler_tools_.to_conservative(
+            XT::Common::hstack(density_minus_one.evaluate(xx) + 1., 0., pressure_minus_04.evaluate(xx) + 0.4));
+      },
+      1);
+  project(initial_values, *initial_values_);
+
+  const double CFL = 0.15;
+  this->periodic_boundaries(100 / CFL, CFL, {1e-15, 0.07254});
+}
