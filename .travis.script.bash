03948cc29290f48e6a46c7308be31121af45000e
René Milk <rene.milk@wwu.de>
2018-02-20 21:56:53 +0100

    [DO NOT MERGE] disable cpp tests

--- .travis.script.bash
+++ .travis.script.bash
@@ -24,17 +24,7 @@ ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 refresh_test_timings
 
 free -h
 
-if [ x"${TESTS}" == x ] ; then
-    ${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v test_binaries
-    ${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2
-else
-    ${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ninja -v -j 1 test_binaries_builder_${TESTS}
-    ${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ctest -V -j 2 -L "^builder_${TESTS}$"
-fi
-if [ "X${TRAVIS_PULL_REQUEST}" != "Xfalse" ] ; then
-        ${SUPERDIR}/.travis/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-gdt-testlogs
-        retry_command ${SUPERDIR}/scripts/bash/travis_upload_test_logs.bash ${DUNE_BUILD_DIR}/${MY_MODULE}/dune/gdt/test/
-fi
+
 
 #unset GH_TOKEN
 #${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make install | grep -v "Installing"
