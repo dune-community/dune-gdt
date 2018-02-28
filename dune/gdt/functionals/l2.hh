7f76b0d5fdd30846989a687b35c50a0b8b669e73
Felix Schindler <felix.schindler@wwu.de>
2018-02-28 13:56:53 +0100

    [functionals.l2] complete rewrite of L2VolumeVectorFunctional

--- dune/gdt/functionals/l2.hh
+++ dune/gdt/functionals/l2.hh
@@ -14,130 +14,145 @@
 
 #include <type_traits>
 
-#include <dune/xt/common/memory.hh>
-#include <dune/xt/functions/interfaces.hh>
-#include <dune/xt/grid/layers.hh>
-#include <dune/xt/la/container.hh>
-#include <dune/xt/la/container/interfaces.hh>
+#include <dune/xt/grid/type_traits.hh>
+#include <dune/xt/functions/interfaces/localizable-function.hh>
 
 #include <dune/gdt/local/functionals/integrals.hh>
+#include <dune/gdt/local/integrands/conversion.hh>
 #include <dune/gdt/local/integrands/product.hh>
-#include <dune/gdt/assembler/system.hh>
-#include <dune/gdt/type_traits.hh>
 
-#include "base.hh"
+#include "vector-based.hh"
 
 namespace Dune {
 namespace GDT {
 
-// //////////////////////// //
-// L2VolumeVectorFunctional //
-// //////////////////////// //
 
 /**
- * \todo Unit tests are missing for this class.
- * \note We could add additional ctors which accept XT::Grid::ApplyOn::WhichEntity, analogously to
- *       L2FaceVectorFunctional (see below), but we did not need this until now.
+ * See also VectorBasedFunctional and FunctionalInterface for a description of the tempate arguments.
+ *
+ * See also VectorBasedFunctional for the meaning of the different ctors. Some of these will not compile (if GV and
+ * AssemblyGridView do not coincide), which is intended.
+ *
+ * \sa VectorBasedFunctional
+ * \sa FunctionalInterface
  */
-template <class FunctionType,
-          class Space,
-          class Vector = typename XT::LA::Container<typename Space::RangeFieldType>::VectorType,
-          class GridLayer = typename Space::GridLayerType,
-          class Field = typename Space::RangeFieldType>
-class L2VolumeVectorFunctional : public VectorFunctionalBase<Vector, Space, GridLayer, Field>
+template <class V, class GV, size_t r = 1, size_t rC = 1, class F = double, class AssemblyGridView = GV>
+class L2VolumeVectorFunctional : public VectorBasedFunctional<V, GV, r, rC, F, AssemblyGridView>
 {
-  typedef L2VolumeVectorFunctional<FunctionType, Space, Vector, GridLayer, Field> ThisType;
-  typedef VectorFunctionalBase<Vector, Space, GridLayer, Field> BaseType;
+  using ThisType = L2VolumeVectorFunctional<V, GV, r, rC, F, AssemblyGridView>;
+  using BaseType = VectorBasedFunctional<V, GV, r, rC, F, AssemblyGridView>;
 
 public:
-  /// \sa VectorFunctionalBase
-  L2VolumeVectorFunctional(const ThisType& other) = delete;
-  L2VolumeVectorFunctional(ThisType&& source) = delete;
+  using typename BaseType::ElementType;
+  using typename BaseType::DofFieldType;
+  using typename BaseType::ElementFilterType;
+  using typename BaseType::ApplyOnAllElements;
+  using typename BaseType::SourceSpaceType;
+  using typename BaseType::SourceVectorType;
 
-  ThisType& operator=(const ThisType& other) = delete;
-  ThisType& operator=(ThisType&& source) = delete;
-
-  template <class... Args>
-  explicit L2VolumeVectorFunctional(const FunctionType& function, Args&&... args)
-    : BaseType(std::forward<Args>(args)...)
-    , local_l2_functional_(function)
-  {
-    this->append(local_l2_functional_);
-  }
-
-  template <class... Args>
-  explicit L2VolumeVectorFunctional(const size_t over_integrate, const FunctionType& function, Args&&... args)
-    : BaseType(std::forward<Args>(args)...)
-    , local_l2_functional_(over_integrate, function)
-  {
-    this->append(local_l2_functional_);
-  }
+  using InducingFunctionType = XT::Functions::LocalizableFunctionInterface<ElementType, r, rC, F>;
 
 private:
-  const LocalVolumeIntegralFunctional<LocalProductIntegrand<FunctionType>, typename Space::BaseFunctionSetType, Field>
-      local_l2_functional_;
+  using LocalFunctionalType = LocalElementIntegralFunctional<ElementType, r, rC, F, DofFieldType>;
+  using LocalIntegrandType = LocalElementProductIntegrand<ElementType, r, rC, F, DofFieldType>;
+
+public:
+  L2VolumeVectorFunctional(AssemblyGridView assembly_grid_view,
+                           const SourceSpaceType& source_spc,
+                           SourceVectorType& vec,
+                           const InducingFunctionType& inducing_function,
+                           const size_t over_integrate = 0,
+                           const XT::Common::Parameter& param = {},
+                           const ElementFilterType& filter = ApplyOnAllElements())
+    : BaseType(assembly_grid_view, source_spc, vec)
+  {
+    this->append(LocalFunctionalType(local_binary_to_unary_element_integrand(inducing_function, LocalIntegrandType(1)),
+                                     over_integrate),
+                 param,
+                 filter);
+  }
+
+  L2VolumeVectorFunctional(AssemblyGridView assembly_grid_view,
+                           const SourceSpaceType& source_spc,
+                           const InducingFunctionType& inducing_function,
+                           const size_t over_integrate = 0,
+                           const XT::Common::Parameter& param = {},
+                           const ElementFilterType& filter = ApplyOnAllElements())
+    : BaseType(assembly_grid_view, source_spc)
+  {
+    this->append(LocalFunctionalType(local_binary_to_unary_element_integrand(inducing_function, LocalIntegrandType(1)),
+                                     over_integrate),
+                 param,
+                 filter);
+  }
 }; // class L2VolumeVectorFunctional
 
 
-// //////////////////////////////// //
-// make_l2_volume_vector_functional //
-// //////////////////////////////// //
+/// \name Variants of make_l2_volume_vector_functional which accept an existing vector into which to assemble.
+/// \{
 
-template <class VectorType, class FunctionType, class SpaceType>
-typename std::enable_if<XT::LA::is_vector<VectorType>::value
-                            && XT::Functions::is_localizable_function<FunctionType>::value
-                            && is_space<SpaceType>::value,
-                        std::unique_ptr<L2VolumeVectorFunctional<FunctionType, SpaceType, VectorType>>>::type
-make_l2_volume_vector_functional(const FunctionType& function, const SpaceType& space, const size_t over_integrate = 0)
+template <class AGV, class GV, size_t r, size_t rC, class V, class F>
+L2VolumeVectorFunctional<typename XT::LA::VectorInterface<V>::derived_type, GV, r, rC, F, GridView<AGV>>
+make_l2_volume_vector_functional(
+    GridView<AGV> assembly_grid_view,
+    const SpaceInterface<GV, r, rC, F>& space,
+    XT::LA::VectorInterface<V>& vector,
+    const XT::Functions::LocalizableFunctionInterface<XT::Grid::extract_entity_t<GV>, r, rC, F>& inducing_function,
+    const size_t over_integrate = 0,
+    const XT::Common::Parameter& param = {},
+    const XT::Grid::ElementFilter<GridView<AGV>>& filter = XT::Grid::ApplyOn::AllElements<GridView<AGV>>())
 {
-  return Dune::XT::Common::make_unique<L2VolumeVectorFunctional<FunctionType, SpaceType, VectorType>>(
-      over_integrate, function, space);
+  return L2VolumeVectorFunctional<typename XT::LA::VectorInterface<V>::derived_type, GV, r, rC, F, GridView<AGV>>(
+      assembly_grid_view, space, vector.as_imp(), inducing_function, over_integrate, param, filter);
 }
 
-template <class VectorType, class FunctionType, class SpaceType, class GridLayerType>
-typename std::
-    enable_if<XT::LA::is_vector<VectorType>::value && XT::Functions::is_localizable_function<FunctionType>::value
-                  && is_space<SpaceType>::value
-                  && XT::Grid::is_layer<GridLayerType>::value,
-              std::unique_ptr<L2VolumeVectorFunctional<FunctionType, SpaceType, VectorType, GridLayerType>>>::type
-    make_l2_volume_vector_functional(const FunctionType& function,
-                                     const SpaceType& space,
-                                     const GridLayerType& grid_layer,
-                                     const size_t over_integrate = 0)
+template <class GV, size_t r, size_t rC, class F, class V>
+L2VolumeVectorFunctional<typename XT::LA::VectorInterface<V>::derived_type, GV, r, rC, F>
+make_l2_volume_vector_functional(
+    const SpaceInterface<GV, r, rC, F>& space,
+    XT::LA::VectorInterface<V>& vector,
+    const XT::Functions::LocalizableFunctionInterface<XT::Grid::extract_entity_t<GV>, r, rC, F>& inducing_function,
+    const size_t over_integrate = 0,
+    const XT::Common::Parameter& param = {},
+    const XT::Grid::ElementFilter<GV>& filter = XT::Grid::ApplyOn::AllElements<GV>())
 {
-  return Dune::XT::Common::make_unique<L2VolumeVectorFunctional<FunctionType, SpaceType, VectorType, GridLayerType>>(
-      over_integrate, function, space, grid_layer);
+  return L2VolumeVectorFunctional<typename XT::LA::VectorInterface<V>::derived_type, GV, r, rC, F>(
+      space.grid_view(), space, vector.as_imp(), inducing_function, over_integrate, param, filter);
 }
 
-template <class FunctionType, class VectorType, class SpaceType>
-typename std::enable_if<XT::Functions::is_localizable_function<FunctionType>::value
-                            && XT::LA::is_vector<VectorType>::value
-                            && is_space<SpaceType>::value,
-                        std::unique_ptr<L2VolumeVectorFunctional<FunctionType, SpaceType, VectorType>>>::type
-make_l2_volume_vector_functional(const FunctionType& function,
-                                 VectorType& vector,
-                                 const SpaceType& space,
-                                 const size_t over_integrate = 0)
+/// \}
+/// \name Variants of make_l2_volume_vector_functional which create an appropriate vector into which to assemble.
+/// \{
+
+template <class VectorType, class GV, size_t r, size_t rC, class F, class AGV>
+typename std::enable_if<XT::LA::is_vector<VectorType>::value,
+                        L2VolumeVectorFunctional<VectorType, GV, r, rC, F, AGV>>::type
+make_l2_volume_vector_functional(
+    GridView<AGV> assembly_grid_view,
+    const SpaceInterface<GV, r, rC, F>& space,
+    const XT::Functions::LocalizableFunctionInterface<XT::Grid::extract_entity_t<GV>, r, rC, F>& inducing_function,
+    const size_t over_integrate = 0,
+    const XT::Common::Parameter& param = {},
+    const XT::Grid::ElementFilter<AGV>& filter = XT::Grid::ApplyOn::AllElements<AGV>())
 {
-  return Dune::XT::Common::make_unique<L2VolumeVectorFunctional<FunctionType, SpaceType, VectorType>>(
-      over_integrate, function, vector, space);
+  return L2VolumeVectorFunctional<VectorType, GV, r, rC, F, AGV>(
+      assembly_grid_view, space, inducing_function, over_integrate, param, filter);
 }
 
-template <class FunctionType, class VectorType, class SpaceType, class GridLayerType>
-typename std::
-    enable_if<XT::Functions::is_localizable_function<FunctionType>::value && XT::LA::is_vector<VectorType>::value
-                  && is_space<SpaceType>::value
-                  && XT::Grid::is_layer<GridLayerType>::value,
-              std::unique_ptr<L2VolumeVectorFunctional<FunctionType, SpaceType, VectorType, GridLayerType>>>::type
-    make_l2_volume_vector_functional(const FunctionType& function,
-                                     VectorType& vector,
-                                     const SpaceType& space,
-                                     const GridLayerType& grid_layer,
-                                     const size_t over_integrate = 0)
+template <class VectorType, class GV, size_t r, size_t rC, class F>
+typename std::enable_if<XT::LA::is_vector<VectorType>::value, L2VolumeVectorFunctional<VectorType, GV, r, rC, F>>::type
+make_l2_volume_vector_functional(
+    const SpaceInterface<GV, r, rC, F>& space,
+    const XT::Functions::LocalizableFunctionInterface<XT::Grid::extract_entity_t<GV>, r, rC, F>& inducing_function,
+    const size_t over_integrate = 0,
+    const XT::Common::Parameter& param = {},
+    const XT::Grid::ElementFilter<GV>& filter = XT::Grid::ApplyOn::AllElements<GV>())
 {
-  return Dune::XT::Common::make_unique<L2VolumeVectorFunctional<FunctionType, SpaceType, VectorType, GridLayerType>>(
-      over_integrate, function, vector, space, grid_layer);
+  return L2VolumeVectorFunctional<VectorType, GV, r, rC, F>(
+      space.grid_view(), space, inducing_function, over_integrate, param, filter);
 }
+
+/// \}
 
 
 #if 0
@@ -153,9 +168,9 @@ template <class FunctionType,
           class Vector = typename XT::LA::Container<typename Space::RangeFieldType>::VectorType,
           class GridLayer = typename Space::GridLayerType,
           class Field = typename Space::RangeFieldType>
-class L2FaceVectorFunctional : public VectorFunctionalBase<Vector, Space, GridLayer, Field>
+class L2FaceVectorFunctional : public VectorBasedFunctional<Vector, Space, GridLayer, Field>
 {
-  typedef VectorFunctionalBase<Vector, Space, GridLayer, Field> BaseType;
+  typedef VectorBasedFunctional<Vector, Space, GridLayer, Field> BaseType;
 
 public:
   using typename BaseType::GridLayerType;
