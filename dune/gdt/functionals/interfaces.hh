2953db0393bc617b3159357e3d829d99539f5243
Felix Schindler <felix.schindler@wwu.de>
2018-02-28 13:52:56 +0100

    [functionals.interfaces] propose new interface

--- dune/gdt/functionals/interfaces.hh
+++ dune/gdt/functionals/interfaces.hh
@@ -12,24 +12,68 @@
 #ifndef DUNE_GDT_FUNCTIONALS_INTERFACES_HH
 #define DUNE_GDT_FUNCTIONALS_INTERFACES_HH
 
-#include <dune/xt/common/crtp.hh>
+#include <dune/xt/la/container/vector-interface.hh>
+
+#include <dune/gdt/discretefunction/default.hh>
+#include <dune/gdt/spaces/interface.hh>
 
 namespace Dune {
 namespace GDT {
 
 
-template <class Traits>
-class FunctionalInterface : public XT::CRTPInterface<FunctionalInterface<Traits>, Traits>
+/**
+ * \brief Interface for functionals.
+ *
+ * Consider a partition \tau_h (modelled by SourceGridView) of a physical domain, a (possibly vector- or matrix-values)
+ * vector space K^{r \times rC} (modelled by Field, source_dim and source_dim_cols), this interface modells functionals
+ *
+ *   f: V_h -> K
+ *
+ * where V_h is the (discrete) space
+ *
+ *   V_h := \{ v_h: \tau_h -> K^{r \times rC} \}
+ *
+ * modelled by a SpaceInterface of appropriate dimensions. The functions v_h \in V_h, to which the funtional can be
+ * applied, are modelled by ConstDiscreteFunction with an appropriate vector type derived from XT::LA::VectorInterface
+ * (modelled by SourceVector).
+ */
+template <class SourceVector,
+          class SourceGridView,
+          size_t source_dim = 1,
+          size_t source_dim_cols = 1,
+          class Field = double>
+class FunctionalInterface
 {
 public:
-  typedef typename Traits::derived_type derived_type;
-  typedef typename Traits::FieldType FieldType;
+  static const constexpr size_t r = source_dim;
+  static const constexpr size_t rC = source_dim_cols;
+  using F = Field;
 
-  template <class SourceType>
-  FieldType apply(const SourceType& source) const
+  using SourceSpaceType = SpaceInterface<SourceGridView, r, rC, F>;
+  using SourceVectorType = SourceVector;
+  using SourceType = ConstDiscreteFunction<SourceVectorType, SourceGridView, r, rC, F>;
+  using FieldType = Field;
+
+  virtual ~FunctionalInterface() = default;
+
+  /// \name These methods have to be implemented.
+  /// \{
+
+  virtual bool linear() const = 0;
+
+  virtual const SourceSpaceType& source_space() const = 0;
+
+  virtual FieldType apply(const SourceType& source) const = 0;
+
+  /// \}
+
+  /**
+   * Allows the implementation to do preparatory work (i.e., assemble the vector of a vector-based linear functional).
+   *
+   * \note In general, you have to call this method before calling apply!
+   */
+  virtual void assemble(const bool /*use_tbb*/ = 0)
   {
-    CHECK_CRTP(this->as_imp().apply(source));
-    return this->as_imp().apply(source);
   }
 }; // class FunctionalInterface
 
