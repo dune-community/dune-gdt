be86f92cca826cda5d91ca1fb533142013d72e2b
René Milk <rene.milk@wwu.de>
2018-04-12 14:28:04 +0200

    [test] tighten lin. cg. mixedboundarytest error exp.

--- dune/gdt/test/linearelliptic/eocexpectations/swipdg-mixedboundary-2dyaspgrid.cxx
+++ dune/gdt/test/linearelliptic/eocexpectations/swipdg-mixedboundary-2dyaspgrid.cxx
@@ -27,13 +27,13 @@ std::vector<double> LinearEllipticEocExpectations<LinearElliptic::MixedBoundaryT
 {
   if (type == "L2") {
 #if DXT_DISABLE_LARGE_TESTS
-    return {2.02e-02, 5.04e-03};
+    return {1.19e-02, 2.64e-03};
 #else
     return {7.56e-02, 2.06e-02, 5.56e-03, 1.36e-03};
 #endif
   } else if (type == "H1_semi" || type == "energy") {
 #if DXT_DISABLE_LARGE_TESTS
-    return {1.91e-01, 8.97e-02};
+    return {1.35e-01, 6.47e-02};
 #else
     return {3.76e-01, 1.95e-01, 9.89e-02, 4.67e-02};
 #endif
@@ -52,13 +52,13 @@ std::vector<double> LinearEllipticEocExpectations<LinearElliptic::MixedBoundaryT
 {
   if (type == "L2") {
 #if DXT_DISABLE_LARGE_TESTS
-    return {2.03e-03, 5.34e-04};
+    return {7.66e-04, 2.12e-04};
 #else
     return {8.82e-03, 2.02e-03, 5.24e-04, 1.42e-04};
 #endif
   } else if (type == "H1_semi" || type == "energy") {
 #if DXT_DISABLE_LARGE_TESTS
-    return {3.37e-02, 1.91e-02};
+    return {2.54e-02, 1.47e-02};
 #else
     return {6.61e-02, 3.27e-02, 1.81e-02, 1.03e-02};
 #endif
