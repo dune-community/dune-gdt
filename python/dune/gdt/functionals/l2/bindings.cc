25e319f39967db7cb66ade5d77debd624ea517d2
René Milk <rene.milk@wwu.de>
2018-05-04 12:49:53 +0200

    [py] l2 functionals use GDT_BINDINGS_GRID too now

--- python/dune/gdt/functionals/l2/bindings.cc
+++ python/dune/gdt/functionals/l2/bindings.cc
@@ -78,7 +78,7 @@ PYBIND11_MODULE(__functionals_l2, m)
 {
   namespace py = pybind11;
   using namespace pybind11::literals;
-  using G = YASP_2D_EQUIDISTANT_OFFSET;
+  using G = GDT_BINDINGS_GRID;
   DUNE_GDT_FUNCTIONALS_L2_BIND(m, 2, G, leaf, view, dg, gdt, 1, istl_sparse);
   DUNE_GDT_FUNCTIONALS_L2_BIND(m, 2, G, leaf, view, dg, gdt, 2, istl_sparse);
   DUNE_GDT_FUNCTIONALS_L2_BIND(m, 2, G, leaf, view, dg, gdt, 3, istl_sparse);
