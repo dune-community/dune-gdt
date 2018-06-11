80c6a2f858233ca276075416673229412bb14a53
Felix Schindler <felix.schindler@wwu.de>
2018-06-11 12:39:15 +0200

    [prolongations] implement for time-dependent functions

--- dune/gdt/prolongations.hh
+++ dune/gdt/prolongations.hh
@@ -12,16 +12,25 @@
 #ifndef DUNE_GDT_PROLONGATIONS_HH
 #define DUNE_GDT_PROLONGATIONS_HH
 
+#include <vector>
+
+#include <dune/common/dynvector.hh>
+
 #include <dune/xt/functions/reinterpret.hh>
 #include <dune/xt/la/container/vector-interface.hh>
 
+#include <dune/gdt/discretefunction/bochner.hh>
 #include <dune/gdt/discretefunction/default.hh>
 #include <dune/gdt/discretefunction/reinterpret.hh>
 #include <dune/gdt/interpolations.hh>
+#include <dune/gdt/spaces/bochner.hh>
 #include <dune/gdt/spaces/interface.hh>
 
 namespace Dune {
 namespace GDT {
+
+
+// ## Variants for a DiscreteFunction ##
 
 
 /**
@@ -66,7 +75,9 @@ auto target_function = prolong<TargetVectorType>(source, target_space, prolongat
 \endcode
  */
 template <class TargetVectorType, class SV, class SGV, size_t r, size_t rC, class SR, class TGV, class TR, class PGV>
-std::enable_if_t<std::is_same<XT::Grid::extract_entity_t<TGV>, typename PGV::Grid::template Codim<0>::Entity>::value,
+std::enable_if_t<XT::LA::is_vector<TargetVectorType>::value
+                     && std::is_same<XT::Grid::extract_entity_t<TGV>,
+                                     typename PGV::Grid::template Codim<0>::Entity>::value,
                  DiscreteFunction<TargetVectorType, TGV, r, rC, TR>>
 prolong(const DiscreteFunction<SV, SGV, r, rC, SR>& source,
         const SpaceInterface<TGV, r, rC, TR>& target_space,
@@ -89,8 +100,8 @@ auto target_function = prolong<TargetVectorType>(source, target_space);
 \endcode
  */
 template <class TargetVectorType, class SV, class SGV, size_t r, size_t rC, class SR, class TGV, class TR>
-DiscreteFunction<TargetVectorType, TGV, r, rC, TR> prolong(const DiscreteFunction<SV, SGV, r, rC, SR>& source,
-                                                           const SpaceInterface<TGV, r, rC, TR>& target_space)
+std::enable_if_t<XT::LA::is_vector<TargetVectorType>::value, DiscreteFunction<TargetVectorType, TGV, r, rC, TR>>
+prolong(const DiscreteFunction<SV, SGV, r, rC, SR>& source, const SpaceInterface<TGV, r, rC, TR>& target_space)
 {
   auto target_function = make_discrete_function<TargetVectorType>(target_space);
   prolong(source, target_function);
@@ -127,6 +138,101 @@ DiscreteFunction<V, TGV, r, rC, TR> prolong(const DiscreteFunction<V, SGV, r, rC
   auto target_function = make_discrete_function<V>(target_space);
   prolong(source, target_function);
   return target_function;
+}
+
+
+// ## Variants for a DiscreteBochnerFunction ##
+
+
+/**
+ * \brief Prolongs a DiscreteBochnerFunction from one (usually coarser) time grid onto another (usually finer) one [most
+ *        general variant].
+ *
+ * \note Uses prolong() in space.
+ *
+ * \sa prolong
+ * \sa interpolate
+ * \sa reinterpret
+ */
+template <class SV, class SGV, class TV, class TGV, class PGV>
+void prolong(const DiscreteBochnerFunction<SV, SGV>& source,
+             DiscreteBochnerFunction<TV, TGV>& target,
+             const GridView<PGV>& spatial_prolongation_grid_view)
+{
+  // prepare
+  target.dof_vectors().set_all(0.);
+  const auto& temporal_space = target.space().temporal_space();
+  DynamicVector<size_t> local_dof_indices(temporal_space.mapper().max_local_size());
+  std::vector<bool> dof_has_been_handled(temporal_space.mapper().size(), false);
+  // walk the time intervals
+  for (auto&& time_interval : elements(temporal_space.grid_view())) {
+    temporal_space.mapper().global_indices(time_interval, local_dof_indices);
+    const auto& lagrange_points_in_time =
+        temporal_space.finite_element(time_interval.geometry().type()).lagrange_points();
+    for (size_t ii = 0; ii < lagrange_points_in_time.size(); ++ii) {
+      const size_t global_dof_index = local_dof_indices[ii];
+      if (!dof_has_been_handled[global_dof_index]) {
+        const auto& point_in_time = time_interval.geometry().global(lagrange_points_in_time[ii]);
+        // evaluate in time
+        const auto coarse_spatial_function =
+            make_discrete_function(source.space().spatial_space(), source.evaluate(point_in_time));
+        // prolong in space
+        auto fine_spatial_function =
+            make_discrete_function(target.space().spatial_space(), target.dof_vectors()[global_dof_index]);
+        prolong(coarse_spatial_function, fine_spatial_function, spatial_prolongation_grid_view);
+        dof_has_been_handled[global_dof_index] = true;
+      }
+    }
+  }
+} // ... prolong(...)
+
+
+/**
+ * \brief Prolongs a DiscreteBochnerFunction from one (usually coarser) time grid onto another (usually finer) one [uses
+ *        target.space().spatial_space().grid_view() as spatial_prolongation_grid_view].
+ *
+ * \note Uses prolong() in space.
+ *
+ * \sa prolong
+ * \sa interpolate
+ * \sa reinterpret
+ */
+template <class SV, class SGV, class TV, class TGV>
+void prolong(const DiscreteBochnerFunction<SV, SGV>& source, DiscreteBochnerFunction<TV, TGV>& target)
+{
+  prolong(source, target, target.space().spatial_space().grid_view());
+}
+
+
+/**
+ * \brief Prolongs a DiscreteBochnerFunction from one (usually coarser) time grid onto another (usually finer) one
+ *        [creates suitable target_function, TargetVectorType has to be provided].
+ *
+ * Use as in
+\code
+auto target_function = prolong<TargetVectorType>(source, target_space, spatial_prolongation_grid_view);
+\endcode
+ */
+template <class TargetVectorType, class SV, class SGV, class TGV, size_t r, size_t rC, class R, class PGV>
+std::enable_if_t<XT::LA::is_vector<TargetVectorType>::value
+                     && std::is_same<XT::Grid::extract_entity_t<TGV>,
+                                     typename PGV::Grid::template Codim<0>::Entity>::value,
+                 DiscreteBochnerFunction<TargetVectorType, TGV, r, rC, R>>
+prolong(const DiscreteBochnerFunction<SV, SGV>& source,
+        const BochnerSpace<TGV, r, rC, R>& target_space,
+        const GridView<PGV>& spatial_prolongation_grid_view)
+{
+  auto target_function = make_discrete_bochner_function<TargetVectorType>(target_space);
+  prolong(source, target_function, spatial_prolongation_grid_view);
+  return target_function;
+}
+
+
+template <class TargetVectorType, class SV, class SGV, class TGV, size_t r, size_t rC, class R>
+std::enable_if_t<XT::LA::is_vector<TargetVectorType>::value, DiscreteBochnerFunction<TargetVectorType, TGV, r, rC, R>>
+prolong(const DiscreteBochnerFunction<SV, SGV>& source, const BochnerSpace<TGV, r, rC, R>& target_space)
+{
+  return prolong<TargetVectorType>(source, target_space, target_space.spatial_space().grid_view());
 }
 
 
