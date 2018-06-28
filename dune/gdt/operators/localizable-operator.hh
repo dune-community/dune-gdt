a6b8462f00888495c50298ee340f0187d805fae4
Felix Schindler <felix.schindler@wwu.de>
2018-06-28 06:48:16 +0200

    [operators.localizable] first draft of a reimplementation

--- dune/gdt/operators/localizable-operator.hh
+++ dune/gdt/operators/localizable-operator.hh
@@ -12,60 +12,82 @@
 #ifndef DUNE_GDT_OPERATORS_LOCALIZABLE_OPERATOR_HH
 #define DUNE_GDT_OPERATORS_LOCALIZABLE_OPERATOR_HH
 
-#include <dune/xt/common/exceptions.hh>
-#include <dune/xt/common/parameter.hh>
-#include <dune/xt/grid/walker/apply-on.hh>
-#include <dune/xt/grid/walker.hh>
+#include <dune/xt/la/type_traits.hh>
 #include <dune/xt/grid/type_traits.hh>
-#include <dune/xt/la/container/pattern.hh>
-#include <dune/xt/functions/interfaces.hh>
+#include <dune/xt/grid/walker.hh>
 
-#include <dune/gdt/local/assembler.hh>
-#include <dune/gdt/assembler/wrapper.hh>
-#include <dune/gdt/assembler/system.hh>
 #include <dune/gdt/discretefunction/default.hh>
+#include <dune/gdt/local/assembler/operator-applicators.hh>
 #include <dune/gdt/local/operators/interfaces.hh>
-#include <dune/gdt/spaces/interface.hh>
-#include <dune/gdt/type_traits.hh>
-
-#include "interfaces.hh"
 
 namespace Dune {
 namespace GDT {
 
 
-template <class GridLayerImp, class SourceImp, class RangeImp>
-class LocalizableOperatorBase : public XT::Grid::Walker<GridLayerImp>
+template <class AssemblyGridView,
+          class SourceVector,
+          size_t source_range_dim = 1,
+          size_t source_range_dim_cols = 1,
+          class SourceRangeField = double,
+          class SourceGridView = AssemblyGridView,
+          size_t range_range_dim = source_range_dim,
+          size_t range_range_dim_cols = source_range_dim_cols,
+          class RangeRangeField = SourceRangeField,
+          class RangeGridView = SourceGridView,
+          class RangeVector = SourceVector>
+class LocalizableOperatorBase : public XT::Grid::Walker<AssemblyGridView>
 {
-  typedef LocalizableOperatorBase<GridLayerImp, SourceImp, RangeImp> ThisType;
-  typedef XT::Grid::Walker<GridLayerImp> BaseType;
+  static_assert(XT::Grid::is_view<AssemblyGridView>::value, "");
+  static_assert(XT::LA::is_vector<SourceVector>::value, "");
+  static_assert(XT::Grid::is_view<SourceGridView>::value, "");
+  static_assert(XT::Grid::is_view<RangeGridView>::value, "");
+  static_assert(XT::LA::is_vector<RangeVector>::value, "");
+
+  using ThisType = LocalizableOperatorBase<AssemblyGridView,
+                                           SourceVector,
+                                           source_range_dim,
+                                           source_range_dim_cols,
+                                           SourceRangeField,
+                                           SourceGridView,
+                                           range_range_dim,
+                                           range_range_dim_cols,
+                                           RangeRangeField,
+                                           RangeGridView,
+                                           RangeVector>;
+  using BaseType = XT::Grid::Walker<AssemblyGridView>;
 
 public:
-  using typename BaseType::GridLayerType;
-  using typename BaseType::EntityType;
-  typedef SourceImp SourceType;
-  typedef RangeImp RangeType;
+  using AGV = AssemblyGridView;
+  using AssemblyGridViewType = AssemblyGridView;
 
-private:
-  static_assert(XT::Functions::is_localizable_function<SourceType>::value,
-                "SourceType has to be derived from XT::Functions::LocalizableFunctionInterface!");
-  static_assert(is_discrete_function<RangeType>::value, "RangeType has to be a DiscreteFunctionDefault!");
-  static_assert(std::is_same<typename SourceType::EntityType, EntityType>::value, "Have to match!");
-  static_assert(std::is_same<typename RangeType::EntityType, EntityType>::value, "Have to match!");
-  static_assert(std::is_same<typename SourceType::DomainFieldType, typename GridLayerType::ctype>::value,
-                "Have to match!");
-  static_assert(std::is_same<typename RangeType::DomainFieldType, typename GridLayerType::ctype>::value,
-                "Have to match!");
-  static_assert(SourceType::dimDomain == GridLayerType::dimension, "Have to match!");
-  static_assert(RangeType::dimDomain == GridLayerType::dimension, "Have to match!");
+  using SV = SourceVector;
+  using SGV = SourceGridView;
+  static const constexpr size_t s_r = source_range_dim;
+  static const constexpr size_t s_rC = source_range_dim_cols;
+  using SR = SourceRangeField;
+  using SourceType = ConstDiscreteFunction<SV, SGV, s_r, s_rC, SR>;
 
-public:
-  LocalizableOperatorBase(GridLayerType grd_layr, const SourceType& src, RangeType& rng)
-    : BaseType(grd_layr)
+  using RV = RangeVector;
+  using RGV = RangeGridView;
+  static const constexpr size_t r_r = range_range_dim;
+  static const constexpr size_t r_rC = range_range_dim_cols;
+  using RR = RangeRangeField;
+  using RangeType = DiscreteFunction<RV, RGV, r_r, r_rC, RR>;
+
+  using typename BaseType::I;
+  using ElementFilterType = XT::Grid::ElementFilter<AssemblyGridViewType>;
+  using IntersectionFilterType = XT::Grid::IntersectionFilter<AssemblyGridViewType>;
+  using ApplyOnAllElements = XT::Grid::ApplyOn::AllElements<AssemblyGridViewType>;
+  using ApplyOnAllIntersection = XT::Grid::ApplyOn::AllIntersections<AssemblyGridViewType>;
+
+  LocalizableOperatorBase(AssemblyGridViewType assembly_grid_view, const SourceType& src, RangeType& rng)
+    : BaseType(assembly_grid_view)
     , source_(src)
     , range_(rng)
-    , walked_(false)
+    , assembled_(false)
   {
+    // to detect assembly
+    this->append([&](const auto&) { assembled_ = true; });
   }
 
   const SourceType& source() const
@@ -83,79 +105,61 @@ public:
     return range_;
   }
 
-  using BaseType::grid_layer;
   using BaseType::append;
 
-  template <class L>
-  ThisType& append(
-      const LocalOperatorInterface<L>& local_operator,
-      const XT::Grid::ApplyOn::WhichEntity<GridLayerType>* where = new XT::Grid::ApplyOn::AllEntities<GridLayerType>())
+  ThisType& append(const LocalElementOperatorInterface<SV, SGV, s_r, s_rC, SR, r_r, r_rC, RR, RGV, RV>& local_operator,
+                   const XT::Common::Parameter& param = {},
+                   const ElementFilterType& filter = ApplyOnAllElements())
   {
-    typedef LocalOperatorApplicator<GridLayerType,
-                                    typename LocalOperatorInterface<L>::derived_type,
-                                    SourceType,
-                                    RangeType>
-        Applicator;
-    local_operators_codim_0.emplace_back(
-        new Applicator(grid_layer(), local_operator.as_imp(), source_, range_, *where));
-    BaseType::append(*local_operators_codim_0.back(), where);
+    this->append(make_local_element_operator_applicator(local_operator, source_, range_, param).release(), filter);
     return *this;
-  } // ... append(...)
-
-  template <class T>
-  ThisType& append(const LocalCouplingOperatorInterface<T>& local_operator,
-                   const XT::Grid::ApplyOn::WhichIntersection<GridLayerType>* where =
-                       new XT::Grid::ApplyOn::InnerIntersections<GridLayerType>(),
-                   const XT::Common::Parameter& mu = {})
-  {
-    typedef LocalCouplingOperatorApplicator<GridLayerType,
-                                            typename LocalCouplingOperatorInterface<T>::derived_type,
-                                            SourceType,
-                                            RangeType>
-        Applicator;
-    local_operators_codim_1.emplace_back(
-        new Applicator(grid_layer(), local_operator.as_imp(), source_, range_, *where, mu));
-    BaseType::append(*local_operators_codim_1.back(), where);
-    return *this;
-  } // ... append(...)
-
-  template <class T>
-  ThisType& append(const LocalCouplingOperatorInterface<T>& local_operator, const XT::Common::Parameter& mu)
-  {
-    return this->append(local_operator, new XT::Grid::ApplyOn::InnerIntersections<GridLayerType>(), mu);
   }
 
-  template <class T>
-  ThisType& append(const LocalBoundaryOperatorInterface<T>& local_operator,
-                   const XT::Grid::ApplyOn::WhichIntersection<GridLayerType>* where =
-                       new XT::Grid::ApplyOn::BoundaryIntersections<GridLayerType>())
+  ThisType&
+  append(const LocalIntersectionOperatorInterface<I, SV, SGV, s_r, s_rC, SR, r_r, r_rC, RR, RGV, RV>& local_operator,
+         const XT::Common::Parameter& param = {},
+         const IntersectionFilterType& filter = ApplyOnAllIntersection())
   {
-    typedef LocalBoundaryOperatorApplicator<GridLayerType,
-                                            typename LocalBoundaryOperatorInterface<T>::derived_type,
-                                            SourceType,
-                                            RangeType>
-        Applicator;
-    local_operators_codim_1.emplace_back(
-        new Applicator(grid_layer(), local_operator.as_imp(), source_, range_, *where));
-    BaseType::append(*local_operators_codim_1.back(), where);
+    this->append(make_local_intersection_operator_applicator(local_operator, source_, range_, param).release(), filter);
     return *this;
-  } // ... append(...)
+  }
 
-  void apply(const bool use_tbb = false)
+  void assemble(const bool use_tbb = false)
   {
-    if (!walked_) {
-      this->walk(use_tbb);
-      walked_ = true;
-    }
-  } // ... apply(...)
+    if (assembled_)
+      return;
+    // This clears all appended operators, which is ok, since we are done after assembling once!
+    this->walk(use_tbb);
+    assembled_ = true;
+  }
 
 protected:
   const SourceType& source_;
   RangeType& range_;
-  std::vector<std::unique_ptr<XT::Grid::internal::Codim0Object<GridLayerType>>> local_operators_codim_0;
-  std::vector<std::unique_ptr<XT::Grid::internal::Codim1Object<GridLayerType>>> local_operators_codim_1;
-  bool walked_;
+  bool assembled_;
 }; // class LocalizableOperatorBase
+
+
+template <class AGV,
+          class SV,
+          size_t s_r,
+          size_t s_rC,
+          class SF,
+          class SGV,
+          size_t r_r,
+          size_t r_rC,
+          class RF,
+          class RGV,
+          class RV>
+std::enable_if_t<XT::Grid::is_layer<AGV>::value,
+                 LocalizableOperatorBase<AGV, SV, s_r, s_rC, SF, SGV, r_r, r_rC, RF, RGV, RV>>
+make_localizable_operator(AGV assembly_grid_view,
+                          const ConstDiscreteFunction<SV, SGV, s_r, s_rC, SF>& source,
+                          DiscreteFunction<RV, RGV, r_r, r_rC, RF>& range)
+{
+  return LocalizableOperatorBase<AGV, SV, s_r, s_rC, SF, SGV, r_r, r_rC, RF, RGV, RV>(
+      assembly_grid_view, source, range);
+}
 
 
 } // namespace GDT
