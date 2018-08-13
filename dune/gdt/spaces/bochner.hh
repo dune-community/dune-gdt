400ac9a67dd532a9d8d894445f12697c0b42de18
Felix Schindler <felix.schindler@wwu.de>
2018-08-13 22:20:09 +0200

    [spaces.bochner] update use of CG space

--- dune/gdt/spaces/bochner.hh
+++ dune/gdt/spaces/bochner.hh
@@ -28,11 +28,11 @@ public:
   BochnerSpace(const SpaceInterface<GV, r, rC, R>& spatial_space, TemporalGridArgs&&... temporal_grid_args)
     : spatial_space_(spatial_space)
     , temporal_grid_(std::forward<TemporalGridArgs>(temporal_grid_args)...)
-    , temporal_space_(temporal_grid_.leafGridView())
+    , temporal_space_(temporal_grid_.leafGridView(), /*order=*/1)
   {
   }
 
-  const ContinuousLagrangeSpace<typename OneDGrid::LeafGridView, 1>& temporal_space() const
+  const ContinuousLagrangeSpace<typename OneDGrid::LeafGridView>& temporal_space() const
   {
     return temporal_space_;
   }
@@ -45,7 +45,7 @@ public:
 private:
   const SpaceInterface<GV, r, rC, R>& spatial_space_;
   const OneDGrid temporal_grid_;
-  const ContinuousLagrangeSpace<typename OneDGrid::LeafGridView, 1> temporal_space_;
+  const ContinuousLagrangeSpace<typename OneDGrid::LeafGridView> temporal_space_;
 }; // class BochnerSpace
 
 
