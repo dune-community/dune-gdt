fa19d87ddb5cbf9c98f4c5dfeb64f63e4927030a
Felix Schindler <felix.schindler@wwu.de>
2018-08-13 22:09:16 +0200

    [local.numerical-fluxes.vijayasundaram] fix memory error
    The lambda used to capture this, which is not accessible after copy.

--- dune/gdt/local/numerical-fluxes/vijayasundaram.hh
+++ dune/gdt/local/numerical-fluxes/vijayasundaram.hh
@@ -11,6 +11,7 @@
 #define DUNE_GDT_LOCAL_NUMERICAL_FLUXES_VIJAYASUNDARAM_HH
 
 #include <functional>
+#include <tuple>
 
 #include <dune/xt/common/matrix.hh>
 #include <dune/xt/common/math.hh>
@@ -37,13 +38,16 @@ public:
       std::function<std::tuple<std::vector<XT::Common::real_t<R>>,
                                XT::Common::FieldMatrix<XT::Common::real_t<R>, m, m>,
                                XT::Common::FieldMatrix<XT::Common::real_t<R>, m, m>>(
-          const FieldVector<R, m>&, const FieldVector<double, d>&, const XT::Common::Parameter& param)>;
+          const FluxType&,
+          const FieldVector<R, m>&,
+          const FieldVector<double, d>&,
+          const XT::Common::Parameter& param)>;
 
   NumericalVijayasundaramFlux(const FluxType& flx)
     : BaseType(flx)
-    , flux_eigen_decomposition_([&](const auto& w, const auto& n, const auto& param) {
+    , flux_eigen_decomposition_([](const auto& f, const auto& w, const auto& n, const auto& param) {
       // evaluate flux jacobian, compute P matrix [DF2016, p. 404, (8.17)]
-      const auto df = this->flux().jacobian(w, param);
+      const auto df = f.jacobian(w, param);
       const auto P = df * n;
       auto eigensolver = XT::LA::make_eigen_solver(
           P, {{"type", XT::LA::eigen_solver_types(P)[0]}, {"assert_real_eigendecomposition", "1e-10"}});
@@ -72,7 +76,7 @@ public:
                        const XT::Common::Parameter& param = {}) const override final
   {
     // compute decomposition
-    const auto eigendecomposition = flux_eigen_decomposition_(0.5 * (u + v), n, param);
+    const auto eigendecomposition = flux_eigen_decomposition_(this->flux(), 0.5 * (u + v), n, param);
     const auto& evs = std::get<0>(eigendecomposition);
     const auto& T = std::get<1>(eigendecomposition);
     const auto& T_inv = std::get<2>(eigendecomposition);
