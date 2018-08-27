cae7edfe61c22baabce90180595e0749c97183de
Tobias Leibner <tobias.leibner@googlemail.com>
2018-08-27 09:13:14 +0200

    [timestepper.interface] add option to store values on each intersection

--- dune/gdt/timestepper/interface.hh
+++ dune/gdt/timestepper/interface.hh
@@ -387,20 +387,31 @@ public:
                                 const std::string& prefix,
                                 const size_t step,
                                 const RangeFieldType t,
-                                const StringifierType& stringifier)
+                                const StringifierType& stringifier,
+                                const bool intersection_wise = false)
   {
     // write one file per MPI rank
     std::ofstream rankfile(rankfile_name(prefix, grid_layer.comm().rank(), step));
     for (const auto& entity : elements(grid_layer, Dune::Partitions::interiorBorder)) {
       const auto local_func = u_n.local_function(entity);
       const auto entity_center = entity.geometry().center();
-      for (const auto& intersection : Dune::intersections(grid_layer, entity)) {
-        auto position = intersection.geometry().center();
+      if (intersection_wise) {
+        for (const auto& intersection : Dune::intersections(grid_layer, entity)) {
+          auto position = intersection.geometry().center();
+          assert(position.size() == dimDomain);
+          // avoid ambiguity at interface
+          for (size_t ii = 0; ii < dimDomain; ++ii)
+            if (position[ii] < entity_center[ii])
+              position[ii] += 1e-6 * (entity_center[ii] - position[ii]);
+          const auto val = local_func->evaluate(entity.geometry().local(position), {"t", t});
+          for (size_t ii = 0; ii < dimDomain; ++ii)
+            rankfile << XT::Common::to_string(position[ii], 15) << " ";
+          rankfile << stringifier(val) << std::endl;
+        } // intersections
+      } else {
+        auto position = entity_center;
         assert(position.size() == dimDomain);
         // avoid ambiguity at interface
-        for (size_t ii = 0; ii < dimDomain; ++ii)
-          if (position[ii] < entity_center[ii])
-            position[ii] += 1e-6 * (entity_center[ii] - position[ii]);
         const auto val = local_func->evaluate(entity.geometry().local(position), {"t", t});
         for (size_t ii = 0; ii < dimDomain; ++ii)
           rankfile << XT::Common::to_string(position[ii], 15) << " ";
