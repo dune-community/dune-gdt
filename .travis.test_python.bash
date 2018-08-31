1fe7068764e6e738a9c5f08dfb6cf1842af8645c
René Milk <rene.milk@wwu.de>
2018-08-31 14:11:51 +0200

    [ci] simplify travis script

--- .travis.test_python.bash
+++ .travis.test_python.bash
@@ -3,13 +3,12 @@
 set -e
 set -x
 
-WAIT="${SUPERDIR}/scripts/bash/travis_wait_new.bash 45"
 source ${SUPERDIR}/scripts/bash/retry_command.bash
 
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
 ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ${BUILD_CMD}
-${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ${BUILD_CMD} bindings
-${WAIT} ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ${BUILD_CMD} test_python
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ${BUILD_CMD} bindings
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} bexec ${BUILD_CMD} test_python
 
 cd ${SUPERDIR}/${MY_MODULE}
 ${DUNE_BUILD_DIR}/${MY_MODULE}/dune-env pip install codecov
