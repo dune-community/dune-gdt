004c476666e533cd3b99bd492abdb4eea0051035
René Milk <rene.milk@wwu.de>
2018-08-31 14:11:51 +0200

    [ci] simplify travis script

--- .travis.after_script.bash
+++ .travis.after_script.bash
@@ -13,14 +13,13 @@
 #
 # ~~~
 
-if [[ $TRAVIS_JOB_NUMBER == *.1 ]] ; then
-    git config --global hooks.clangformat ${CLANG_FORMAT}
-    CHECK_DIR=${SUPERDIR}/${MY_MODULE}
-    PYTHONPATH=${SUPERDIR}/scripts/python/ python3 -c "import travis_report as tp; tp.clang_format_status(\"${CHECK_DIR}\")"
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
-    ${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make doc
-    if [ "X${TRAVIS_PULL_REQUEST}" != "Xfalse" ] ; then
+
+git config --global hooks.clangformat ${CLANG_FORMAT}
+CHECK_DIR=${SUPERDIR}/${MY_MODULE}
+PYTHONPATH=${SUPERDIR}/scripts/python/ python3 -c "import travis_report as tp; tp.clang_format_status(\"${CHECK_DIR}\")"
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} configure
+${SRC_DCTRL} ${BLD} --only=${MY_MODULE} make doc
+if [ "X${TRAVIS_PULL_REQUEST}" != "Xfalse" ] ; then
         ${SUPERDIR}/.ci/init_sshkey.sh ${encrypted_95fb78800815_key} ${encrypted_95fb78800815_iv} keys/dune-community/dune-community.github.io
         ${SUPERDIR}/.ci/deploy_docs.sh ${MY_MODULE} "${DUNE_BUILD_DIR}"
-    fi
 fi
