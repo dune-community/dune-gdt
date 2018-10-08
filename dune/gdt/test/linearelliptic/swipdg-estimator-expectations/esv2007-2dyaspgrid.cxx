4547178109df734eb91f21f93f63d821cfdac4e6
René Milk <rene.milk@wwu.de>
2018-10-08 09:01:24 +0200

    [test|swipdg-est] enable yaspgrid for esv07

--- dune/gdt/test/linearelliptic/swipdg-estimator-expectations/esv2007-2dyaspgrid.cxx
+++ dune/gdt/test/linearelliptic/swipdg-estimator-expectations/esv2007-2dyaspgrid.cxx
@@ -40,57 +40,57 @@ public:
   {
     if (type == "energy") {
 #if DXT_DISABLE_LARGE_TESTS
-      return {};
+      return {5.69e-1, 2.77e-1};
 #else
       return {2.77e-01, 1.39e-01, 6.98e-02, 3.50e-02};
 #endif
     } else if (type == LinearElliptic::SwipdgFluxreconstrutionEstimators::local_nonconformity_ESV2007_id()) {
 #if DXT_DISABLE_LARGE_TESTS
-      return {};
+      return {1.73e-1, 1.03e-1};
 #else
       return {1.03e-01, 5.69e-02, 2.99e-02, 1.53e-02};
 #endif
     } else if (type == LinearElliptic::SwipdgFluxreconstrutionEstimators::local_residual_ESV2007_id()) {
 #if DXT_DISABLE_LARGE_TESTS
-      return {};
+      return {3.48e-1, 8.85e-2};
 #else
       return {8.85e-02, 2.22e-02, 5.56e-03, 1.39e-03};
 #endif
     } else if (type == LinearElliptic::SwipdgFluxreconstrutionEstimators::local_diffusive_flux_ESV2007_id()) {
 #if DXT_DISABLE_LARGE_TESTS
-      return {};
+      return {8.97e-1, 4.42e-1};
 #else
       return {4.42e-01, 2.23e-01, 1.12e-01, 5.64e-02};
 #endif
     } else if (type == LinearElliptic::SwipdgFluxreconstrutionEstimators::ESV2007_id()) {
 #if DXT_DISABLE_LARGE_TESTS
-      return {};
+      return {2.16e-0, 1.91e-1};
 #else
-      return {};
+      return {0.0e-1, 0.0e-1};
 #endif
     } else if (type == "efficiency_" + LinearElliptic::SwipdgFluxreconstrutionEstimators::ESV2007_id()) {
 #if DXT_DISABLE_LARGE_TESTS
-      return {};
+      return {2.16e-0, 1.91e-0};
 #else
       return {1.91e+00, 1.79e+00, 1.73e+00, 1.70e+00};
 #endif
     } else if (type == LinearElliptic::SwipdgFluxreconstrutionEstimators::ESV2007_alternative_summation_id()) {
 #if DXT_DISABLE_LARGE_TESTS
-      return {};
+      return {0.0e-1, 0.0e-1};
 #else
-      return {};
+      return {0.0e-1, 0.0e-1};
 #endif
     } else if (type
                == "efficiency_"
                       + LinearElliptic::SwipdgFluxreconstrutionEstimators::ESV2007_alternative_summation_id()) {
 #if DXT_DISABLE_LARGE_TESTS
-      return {};
+      return {2.09e-0, 2.87e-0};
 #else
       return {2.87e+00, 3.96e+00, 5.51e+00, 7.72e+00};
 #endif
     } else
       EXPECT_TRUE(false) << "test results missing for type: " << type;
-    return {};
+    return {0.0e-1, 0.0e-1};
   } // ... results(...)
 }; // LinearEllipticSwipdgEstimatorExpectations
 
