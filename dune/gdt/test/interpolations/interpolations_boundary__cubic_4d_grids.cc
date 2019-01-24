c8288059c4f70b6b4daf5dbf1210a662809fd081
Felix Schindler <felix.schindler@wwu.de>
2019-01-24 21:49:43 +0100

    [test.interpolations] add boundary interpolation tests

---
+++ dune/gdt/test/interpolations/interpolations_boundary__cubic_4d_grids.cc
@@ -0,0 +1,26 @@
+// This file is part of the dune-gdt project:
+//   https://github.com/dune-community/dune-gdt
+// Copyright 2010-2018 dune-gdt developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2019)
+
+#include <dune/xt/common/test/main.hxx> // <- this one has to come first (includes the config.h)!
+
+#include <dune/xt/grid/grids.hh>
+
+#include "boundary.hh"
+
+
+using Cubic4dGrids = ::testing::Types<YASP_4D_EQUIDISTANT_OFFSET>;
+
+
+template <class G>
+using BoundaryInterpolation = Dune::GDT::Test::BoundaryInterpolationOnLeafViewTest<G>;
+TYPED_TEST_CASE(BoundaryInterpolation, Cubic4dGrids);
+TYPED_TEST(BoundaryInterpolation, interpolates_correctly)
+{
+  this->interpolates_correctly();
+}
