64f73eb3303564a7ecb624142e6773f4e4b69cb1
Felix Schindler <felix.schindler@wwu.de>
2019-01-25 15:19:21 +0100

    [test.linear-transport] actually check against expected results

--- dune/gdt/test/linear-transport/linear_transport__1d__implicit__fv.cc
+++ dune/gdt/test/linear-transport/linear_transport__1d__implicit__fv.cc
@@ -28,21 +28,24 @@ using LinearTransport1dExplicitFvTest = LinearTransportExplicitTest<YASP_1D_EQUI
 TEST_F(LinearTransport1dExplicitFvTest, dt_equals_h)
 {
   this->visualization_steps_ =
-      DXTC_CONFIG_GET("visualization_steps__explicit__dt_equals_h", 0); // <- Something like 100 to visualize!
+      DXTC_TEST_CONFIG_GET("setup.visualization_steps", 0); // <- Something like 100 to visualize!
   this->space_type_ = "fv";
   this->numerical_flux_type_ = "upwind";
-  this->run();
+  const auto actual_results = this->run();
+  const auto expected_results = DXTC_TEST_CONFIG_SUB("results");
+  XT::Test::check_eoc_study_for_success(expected_results, actual_results);
 }
 
 
 using LinearTransport1dImplicitFvTest = LinearTransportImplicitTest<YASP_1D_EQUIDISTANT_OFFSET>;
 TEST_F(LinearTransport1dImplicitFvTest, dt_equals_h)
 {
-  this->visualization_steps_ =
-      DXTC_CONFIG_GET("visualization_steps__implicit__dt_equals_h", 0); // <- Something like 100 to visualize!
+  this->visualization_steps_ = DXTC_TEST_CONFIG_GET("setup.visualization_steps", 0);
   this->space_type_ = "fv";
   this->numerical_flux_type_ = "upwind";
-  this->run();
+  const auto actual_results = this->run();
+  const auto expected_results = DXTC_TEST_CONFIG_SUB("results");
+  XT::Test::check_eoc_study_for_success(expected_results, actual_results);
 }
 
 
@@ -66,20 +69,22 @@ protected:
 
 TEST_F(LinearTransport1dExplicitWithAutomaticDtTest, automatic_dt)
 {
-  this->visualization_steps_ =
-      DXTC_CONFIG_GET("visualization_steps__explicit__automatic_dt", 0); // <- Something like 100 to visualize!
+  this->visualization_steps_ = DXTC_TEST_CONFIG_GET("setup.visualization_steps", 0);
   this->space_type_ = "fv";
   this->numerical_flux_type_ = "upwind";
-  this->run();
+  const auto actual_results = this->run();
+  const auto expected_results = DXTC_TEST_CONFIG_SUB("results");
+  XT::Test::check_eoc_study_for_success(expected_results, actual_results);
 }
 
 
 TEST_F(LinearTransport1dImplicitFvTest, larger_dt)
 {
   this->dt_factor_ = DXTC_CONFIG_GET("implicit_dt_factor", 10.);
-  this->visualization_steps_ =
-      DXTC_CONFIG_GET("visualization_steps__implicit__larger_dt", 0); // <- Something like 100 to visualize!
+  this->visualization_steps_ = DXTC_TEST_CONFIG_GET("setup.visualization_steps", 0);
   this->space_type_ = "fv";
   this->numerical_flux_type_ = "upwind";
-  this->run();
+  const auto actual_results = this->run();
+  const auto expected_results = DXTC_TEST_CONFIG_SUB("results");
+  XT::Test::check_eoc_study_for_success(expected_results, actual_results);
 }
