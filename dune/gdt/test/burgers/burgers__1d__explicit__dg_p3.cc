cbd792aa1a0b3febec9ea2138229a2a4c359dce3
Felix Schindler <felix.schindler@wwu.de>
2019-02-01 00:14:54 +0100

    [test.burgers] add DG P2 and P3

--- dune/gdt/test/burgers/burgers__1d__explicit__dg_p3.cc
+++ dune/gdt/test/burgers/burgers__1d__explicit__dg_p3.cc
@@ -7,10 +7,8 @@
 // Authors:
 //   Felix Schindler (2019)
 
-#define DUNE_XT_COMMON_TEST_MAIN_CATCH_EXCEPTIONS 1
 #define DUNE_XT_COMMON_TEST_MAIN_ENABLE_TIMED_LOGGING 1
 #define DUNE_XT_COMMON_TEST_MAIN_ENABLE_INFO_LOGGING 1
-#define DUNE_XT_COMMON_TEST_MAIN_ENABLE_DEBUG_LOGGING 1
 
 #include <dune/xt/common/test/main.hxx> // <- this one has to come first (includes the config.h)!
 
@@ -24,13 +22,13 @@ using namespace Dune;
 using namespace Dune::GDT;
 
 
-using Burgers1dExplicitDgP1Test = BurgersExplicitTest<YASP_1D_EQUIDISTANT_OFFSET>;
-TEST_F(Burgers1dExplicitDgP1Test, periodic_boundaries__numerical_engquist_osher_flux)
+using Burgers1dExplicitDgP3Test = BurgersExplicitTest<YASP_1D_EQUIDISTANT_OFFSET>;
+TEST_F(Burgers1dExplicitDgP3Test, periodic_boundaries__numerical_engquist_osher_flux)
 {
   this->visualization_steps_ = DXTC_TEST_CONFIG_GET("setup.visualization_steps", 0);
   this->num_refinements_ = 2;
   this->num_additional_refinements_for_reference_ = 3;
-  this->space_type_ = "dg_p1";
+  this->space_type_ = "dg_p3";
   this->numerical_flux_type_ = "engquist_osher";
   /*const auto actual_results =*/this->run();
   //  const auto expected_results = DXTC_TEST_CONFIG_SUB("results");
