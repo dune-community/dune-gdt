2fea4ca37128c9b203cb1ec54fe3bb1ad74f3153
Felix Schindler <felix.schindler@wwu.de>
2019-02-07 23:34:29 +0100

    [norms] add elliptic

--- dune/gdt/norms.hh
+++ dune/gdt/norms.hh
@@ -16,6 +16,7 @@
 #include <dune/xt/functions/interfaces/element-functions.hh>
 
 #include <dune/gdt/local/bilinear-forms/integrals.hh>
+#include <dune/gdt/local/integrands/elliptic.hh>
 #include <dune/gdt/local/integrands/product.hh>
 #include <dune/gdt/operators/localizable-bilinear-form.hh>
 
@@ -58,6 +59,57 @@ l2_norm(const GridViewType& grid_view,
         const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, r, rC, R>& func)
 {
   return std::sqrt(l2_product(grid_view, func, func));
+}
+
+
+template <class GridViewType, class F, size_t r>
+std::enable_if_t<XT::Grid::is_view<GridViewType>::value, LocalizableBilinearFormBase<GridViewType, r, 1, F, F, 1, 1, F>>
+make_localizable_elliptic_product(
+    const GridViewType& grid_view,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, 1, 1, F>& diffusion_factor,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>,
+                                               GridViewType::dimension,
+                                               GridViewType::dimension,
+                                               F>& diffusion_tensor,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, r, 1, F>& left,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, r, 1, F>& right)
+{
+  using E = XT::Grid::extract_entity_t<GridViewType>;
+  auto localizable_product = make_localizable_bilinear_form(grid_view, left, right);
+  localizable_product.append(LocalElementIntegralBilinearForm<E, r, 1, F, F, r, 1, F>(
+      LocalEllipticIntegrand<E, r, F>(diffusion_factor, diffusion_tensor)));
+  return localizable_product;
+}
+
+
+template <class GridViewType, class F, size_t r>
+std::enable_if_t<XT::Grid::is_view<GridViewType>::value, F> elliptic_product(
+    const GridViewType& grid_view,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, 1, 1, F>& diffusion_factor,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>,
+                                               GridViewType::dimension,
+                                               GridViewType::dimension,
+                                               F>& diffusion_tensor,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, r, 1, F>& left,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, r, 1, F>& right)
+{
+  auto product = make_localizable_elliptic_product(grid_view, diffusion_factor, diffusion_tensor, left, right);
+  product.assemble();
+  return product.result();
+}
+
+
+template <class GridViewType, class F, size_t r>
+std::enable_if_t<XT::Grid::is_view<GridViewType>::value, F> elliptic_norm(
+    const GridViewType& grid_view,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, 1, 1, F>& diffusion_factor,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>,
+                                               GridViewType::dimension,
+                                               GridViewType::dimension,
+                                               F>& diffusion_tensor,
+    const XT::Functions::GridFunctionInterface<XT::Grid::extract_entity_t<GridViewType>, r, 1, F>& func)
+{
+  return std::sqrt(elliptic_product(grid_view, diffusion_factor, diffusion_tensor, func, func));
 }
 
 
