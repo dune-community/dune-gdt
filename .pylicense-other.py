51f7bed87e4077492ef46c4126503f8dd76f6149
René Fritze <rene.fritze@wwu.de>
2019-03-28 11:21:11 +0100

    udpate pylicense to ignore .ci/shared submodule

--- .pylicense-other.py
+++ .pylicense-other.py
@@ -6,7 +6,8 @@
 #      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 #          with "runtime exception" (http://www.dune-project.org/license.html)
 # Authors:
-#   René Fritze (2018)
+#   René Fritze    (2018)
+#   Tobias Leibner (2019)
 # ~~~
 
 name = 'This file is part of the dune-gdt project:'
@@ -23,4 +24,4 @@ include_patterns = ('*.txt', '*.cmake', '*.py', '*.sh', '*.bash', '*.dgf', '*.ms
                     '*.gitignore', '*.mailmap', '*.gitattributes', '*gitignore-*', '*stamp-vc', '*dune.module',
                     '*Doxylocal', '*.clang-format', '*COPYING-CMAKE-SCRIPTS', '*README', '*LICENSE', '*mainpage',
                     '*switch-build_dir', '*dune-xt-common.pc.in', '*CMakeLists.txt')
-exclude_patterns = ('*config.h.cmake', '*.vcsetup*', '*builder_definitions.cmake')
+exclude_patterns = ('*config.h.cmake', '*.vcsetup*', '*builder_definitions.cmake', '*.ci/shared/*)
