32a982a23d24706786cfc2e82dd79bf7f4882506
Felix Schindler <felix.schindler@wwu.de>
2019-04-28 22:43:35 +0200

    [python|discretefunction] (re)add proper bindings

--- python/dune/gdt/discretefunction.cc
+++ python/dune/gdt/discretefunction.cc
@@ -12,33 +12,68 @@
 
 #if HAVE_DUNE_PYBINDXI
 
-#  include <dune/common/parallel/mpihelper.hh>
-
 #  include <dune/pybindxi/pybind11.h>
-#  include <dune/pybindxi/stl.h>
 
-#  include <python/dune/xt/common/bindings.hh>
+#  include <python/dune/xt/common/exceptions.bindings.hh>
+#  include <dune/xt/la/container.hh>
+#  include <dune/xt/grid/grids.hh>
 
-#  include <python/dune/gdt/discretefunction/bindings.hh>
-#  include <python/dune/gdt/shared.hh>
+#  include "discretefunction.hh"
 
-PYBIND11_MODULE(__discretefunction, m)
+
+template <class V, class Tuple = Dune::XT::Grid::AvailableGridTypes>
+struct binder_for_all_grids
+{
+  using G = typename Tuple::head_type;
+  using GV = typename G::LeafGridView;
+  static const constexpr size_t d = G::dimension;
+
+  static void bind(pybind11::module& m)
+  {
+    Dune::GDT::bindings::DiscreteFunction<V, GV>::bind(m);
+    if (d > 1)
+      Dune::GDT::bindings::DiscreteFunction<V, GV, d>::bind(m);
+    // add your extra dimensions here
+    // ...
+    binder_for_all_grids<V, typename Tuple::tail_type>::bind(m);
+  }
+};
+
+template <class V>
+struct binder_for_all_grids<V, boost::tuples::null_type>
+{
+  static void bind(pybind11::module& /*m*/) {}
+};
+
+
+template <class Tuple = Dune::XT::LA::AvailableVectorTypes<double>>
+void bind_for_all_vectors_and_grids(pybind11::module& m)
+{
+  using V = typename Tuple::head_type;
+  binder_for_all_grids<V>::bind(m);
+  bind_for_all_vectors_and_grids<typename Tuple::tail_type>(m);
+}
+
+template <>
+void bind_for_all_vectors_and_grids<boost::tuples::null_type>(pybind11::module& /*m*/)
+{}
+
+
+PYBIND11_MODULE(discretefunction, m)
 {
   namespace py = pybind11;
   using namespace pybind11::literals;
 
-  Dune::XT::Common::bindings::addbind_exceptions(m);
-
   py::module::import("dune.xt.common");
+  py::module::import("dune.xt.la");
   py::module::import("dune.xt.grid");
   py::module::import("dune.xt.functions");
-  py::module::import("dune.xt.la");
-  py::module::import("dune.gdt.__spaces");
-  py::module::import("dune.gdt.__spaces_block");
 
-  DUNE_GDT_DISCRETEFUNCTION_DEFAULT_BIND(m);
+  Dune::XT::Common::bindings::addbind_exceptions(m);
+  Dune::XT::Common::bindings::add_initialization(m, "dune.gdt");
 
-  add_initialization(m, "dune.gdt.discretefunction");
-}
+  bind_for_all_vectors_and_grids<>(m);
+} // PYBIND11_MODULE(discretefunction, ...)
+
 
 #endif // HAVE_DUNE_PYBINDXI
