09a8c11e230dcc7d14dc905dfb1cf2cf31eae4ea
Tim Keil <tim.keil@wwu.de>
2019-05-31 17:52:46 +0200

    [WIP|tools/dirichlet-constraints] Add ExtendedDirichletConstraints

--- dune/gdt/tools/dirichlet-constraints.hh
+++ dune/gdt/tools/dirichlet-constraints.hh
@@ -221,6 +221,188 @@ make_dirichlet_constraints(const SpaceInterface<GV, r, rC, R>& space,
   return DirichletConstraints<XT::Grid::extract_intersection_t<GV>, SpaceInterface<GV, r, rC, R>>(boundary_info, space);
 }
 
+/**
+ *
+ * This code realizes extented dirichlet constraint. It declares the inner dofs that share intersection dofs
+ * with the boundary.
+ */
+
+template <class IntersectionType, class SpaceType>
+class ExtendedDirichletConstraints
+  : public Dune::XT::Grid::ElementFunctor<typename SpaceType::GridViewType>
+  , public XT::Common::ThreadResultPropagator<ExtendedDirichletConstraints<IntersectionType, SpaceType>,
+                                              std::set<size_t>,
+                                              internal::setUnion<size_t>>
+{
+  using ThisType = ExtendedDirichletConstraints<IntersectionType, SpaceType>;
+  using BaseType = XT::Grid::ElementFunctor<typename SpaceType::GridViewType>;
+  using Propagator = XT::Common::ThreadResultPropagator<ThisType, std::set<size_t>, internal::setUnion<size_t>>;
+  friend Propagator;
+
+public:
+  using BoundaryInfoType = XT::Grid::BoundaryInfo<IntersectionType>;
+  using ElementType = typename ThisType::ElementType;
+  using GridView = typename SpaceType::GridViewType;
+  static const constexpr size_t d = SpaceType::d;
+  static const constexpr size_t r = SpaceType::r;
+  static const constexpr size_t rC = SpaceType::rC;
+  using R = typename SpaceType::R;
+
+  ExtendedDirichletConstraints(const BoundaryInfoType& bnd_info, const SpaceType& space)
+    : BaseType()
+    , Propagator(this)
+    , boundary_info_(bnd_info)
+    , space_(space)
+    , basis_(space_.basis().localize())
+  {}
+
+  ExtendedDirichletConstraints(const ThisType& other)
+    : BaseType(other)
+    , Propagator(this)
+    , boundary_info_(other.boundary_info_)
+    , space_(other.space_)
+    , basis_(space_.basis().localize())
+  {}
+
+  void apply_local(const ElementType& element) override final
+  {
+    std::set<size_t> local_DoFs;
+    basis_->bind(element);
+    const auto& reference_element = ReferenceElements<double, d>::general(element.geometry().type());
+    const auto local_key_indices = basis_->finite_element().coefficients().local_key_indices();
+    const auto intersection_it_end = space_.grid_view().iend(element);
+    for (auto intersection_it = space_.grid_view().ibegin(element); intersection_it != intersection_it_end;
+         ++intersection_it) {
+      const auto& intersection = *intersection_it;
+      const auto intersection_index = intersection.indexInInside();
+      for (const auto& local_DoF : local_key_indices[1][intersection_index]) {
+        local_DoFs.insert(local_DoF);
+      }
+      for (int cc = 2; cc <= XT::Common::numeric_cast<int>(d); ++cc) {
+        for (int ii = 0; ii < reference_element.size(intersection_index, 1, cc); ++ii) {
+          const auto subentity_id = reference_element.subEntity(intersection_index, 1, ii, cc);
+          for (const auto& local_DoF : local_key_indices[cc][subentity_id]) {
+            local_DoFs.insert(local_DoF);
+          }
+        }
+      }
+    }
+    for (const auto& local_DoF : local_DoFs) {
+      dirichlet_DoFs_.insert(space_.mapper().global_index(element, local_DoF));
+    }
+  } // ... apply_local(...)
+
+  const BoundaryInfoType& boundary_info() const
+  {
+    return boundary_info_;
+  }
+
+  const std::set<size_t>& dirichlet_DoFs() const
+  {
+    return dirichlet_DoFs_;
+  }
+
+  template <class M>
+  void apply(XT::LA::MatrixInterface<M>& matrix, const bool only_clear = false, const bool ensure_symmetry = true) const
+  {
+    if (only_clear) {
+      if (ensure_symmetry)
+        for (const auto& DoF : dirichlet_DoFs_) {
+          matrix.clear_col(DoF);
+          matrix.clear_row(DoF);
+        }
+      else
+        for (const auto& DoF : dirichlet_DoFs_)
+          matrix.clear_row(DoF);
+    } else {
+      if (ensure_symmetry)
+        for (const auto& DoF : dirichlet_DoFs_) {
+          matrix.unit_col(DoF);
+          matrix.unit_row(DoF);
+        }
+      else
+        for (const auto& DoF : dirichlet_DoFs_)
+          matrix.unit_row(DoF);
+    }
+  } // ... apply(...)
+
+  template <class V>
+  void apply(XT::LA::VectorInterface<V>& vector) const
+  {
+    for (const auto& DoF : dirichlet_DoFs_)
+      vector[DoF] = 0.0;
+  }
+
+  template <class M, class V>
+  void apply(XT::LA::MatrixInterface<M>& matrix,
+             XT::LA::VectorInterface<V>& vector,
+             const bool only_clear = false,
+             const bool ensure_symmetry = true) const
+  {
+    if (only_clear) {
+      if (ensure_symmetry)
+        for (const auto& DoF : dirichlet_DoFs_) {
+          matrix.clear_col(DoF);
+          matrix.clear_row(DoF);
+          vector[DoF] = 0.0;
+        }
+      else
+        for (const auto& DoF : dirichlet_DoFs_) {
+          matrix.clear_row(DoF);
+          vector[DoF] = 0.0;
+        }
+    } else {
+      if (ensure_symmetry)
+        for (const auto& DoF : dirichlet_DoFs_) {
+          matrix.unit_col(DoF);
+          matrix.unit_row(DoF);
+          vector[DoF] = 0.0;
+        }
+      else
+        for (const auto& DoF : dirichlet_DoFs_) {
+          matrix.unit_row(DoF);
+          vector[DoF] = 0.0;
+        }
+    }
+  } // ... apply(...)
+
+  void finalize() override final
+  {
+    this->finalize_imp();
+  }
+
+  BaseType* copy() override final
+  {
+    return Propagator::copy_imp();
+  }
+
+  std::set<size_t> result() const
+  {
+    return dirichlet_DoFs_;
+  }
+
+  void set_result(std::set<size_t> res)
+  {
+    dirichlet_DoFs_ = res;
+  }
+
+private:
+  const BoundaryInfoType& boundary_info_;
+  const SpaceType& space_;
+  mutable std::unique_ptr<typename SpaceType::GlobalBasisType::LocalizedType> basis_;
+  std::set<size_t> dirichlet_DoFs_;
+}; // class ExtendedDirichletConstraints
+
+
+template <class GV, size_t r, size_t rC, class R>
+ExtendedDirichletConstraints<XT::Grid::extract_intersection_t<GV>, SpaceInterface<GV, r, rC, R>>
+make_dirichlet_constraints(const SpaceInterface<GV, r, rC, R>& space,
+                           const XT::Grid::BoundaryInfo<XT::Grid::extract_intersection_t<GV>>& boundary_info)
+{
+  return ExtendedDirichletConstraints<XT::Grid::extract_intersection_t<GV>, SpaceInterface<GV, r, rC, R>>(boundary_info,
+                                                                                                          space);
+}
+
 
 } // namespace GDT
 } // namespace Dune
