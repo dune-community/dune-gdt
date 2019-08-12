e7ab25c48723a040d255c4729268c13edb67ffad
Felix Schindler <felix.schindler@wwu.de>
2019-08-12 13:43:42 +0200

    [python|discretefunction] split and improve

---
+++ python/dune/gdt/discretefunction/discretefunction_1d.cc
@@ -0,0 +1,34 @@
+// This file is part of the dune-gdt project:
+//   https://github.com/dune-community/dune-gdt
+// Copyright 2010-2018 dune-gdt developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2019)
+
+#include "config.h"
+
+#if HAVE_DUNE_PYBINDXI
+
+#  include "discretefunction.hh"
+
+
+PYBIND11_MODULE(_discretefunction_1d, m)
+{
+  namespace py = pybind11;
+  using namespace Dune;
+  using namespace Dune::XT;
+  using namespace Dune::GDT;
+
+  py::module::import("dune.xt.common");
+  py::module::import("dune.xt.la");
+  py::module::import("dune.xt.grid");
+  py::module::import("dune.xt.functions");
+
+  bindings::DiscreteFunction_for_all_vectors_and_grids<LA::AvailableVectorTypes<double>,
+                                                       XT::Grid::Available1dGridTypes>::bind(m);
+}
+
+
+#endif // HAVE_DUNE_PYBINDXI
