2389fbe73939d18c1e889b173ac4108d494b7c09
Felix Schindler <felix.schindler@wwu.de>
2020-07-09 21:58:38 +0200

    [print] pull XT::Common::print into GDT namespace

---
+++ dune/gdt/print.hh
@@ -0,0 +1,39 @@
+// This file is part of the dune-gdt project:
+//   https://github.com/dune-community/dune-gdt
+// Copyright 2010-2018 dune-gdt developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2020)
+
+#ifndef DUNE_GDT_PRINT_HH
+#define DUNE_GDT_PRINT_HH
+
+#include <dune/xt/common/print.hh>
+#include <dune/xt/grid/print.hh>
+
+namespace Dune {
+namespace GDT {
+
+
+/// \sa XT::Common::print
+template <class T>
+XT::Common::Printer<T, false> print(const T& value, const XT::Common::Configuration& param = {})
+{
+  return XT::Common::Printer<T, false>(value, param);
+}
+
+
+/// \sa XT::Common::repr
+template <class T>
+XT::Common::Printer<T, true> repr(const T& value, const XT::Common::Configuration& param = {})
+{
+  return XT::Common::Printer<T, true>(value, param);
+}
+
+
+} // namespace GDT
+} // namespace Dune
+
+#endif // DUNE_GDT_PRINT_HH
