acdaac84af13abac614841b3f11f84baa50c2dfc
Felix Schindler <felix.schindler@wwu.de>
2020-07-29 07:10:44 +0200

    [P] fix typo

--- python/dunegdtusercode.cc
+++ python/dunegdtusercode.cc
@@ -16,6 +16,11 @@
 #include <python/dune/xt/common/exceptions.bindings.hh>
 #include <python/dune/xt/common/parameter.hh>
 
+#include <dune/xt/grid/gridprovider/provider.hh>
+#include <dune/xt/grid/grids.hh>
+#include <dune/xt/functions/grid-function.hh>
+
+using namespace Dune::XT;
 
 PYBIND11_MODULE(dunegdtusercode, m)
 {
@@ -28,9 +33,16 @@ PYBIND11_MODULE(dunegdtusercode, m)
   py::module::import("dune.xt.functions");
   py::module::import("dune.gdt");
 
+  m.def("exp_generic", [](const Grid::GridProvider<ALU_2D_SIMPLEX_CONFORMING>&, const int order) {
+    return new Functions::GridFunction<Grid::extract_entity_t<ALU_2D_SIMPLEX_CONFORMING>>(
+        {order, [](const auto& x, const auto&) { return std::exp(x[0] * x[1]); }});
+  });
+  m.def("exp_generic", [](const Grid::GridProvider<YASP_2D_EQUIDISTANT_OFFSET>&, const int order) {
+    return new Functions::GridFunction<Grid::extract_entity_t<YASP_2D_EQUIDISTANT_OFFSET>>(
+        {order, [](const auto& x, const auto&) { return std::exp(x[0] * x[1]); }});
+  });
+
   // your code goes here ...
   m.def(
-      "example_function",
-      [](Dune::XT::Common::Parameter param) { std::cout << "param = " << param << std::endl; },
-      "param"_a);
+      "example_function", [](Common::Parameter param) { std::cout << "param = " << param << std::endl; }, "param"_a);
 } // PYBIND11_MODULE(dunegdtusercode, ...)
