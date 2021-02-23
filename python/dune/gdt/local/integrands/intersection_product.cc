b4518aa70558cbdbdfcc889945a3a327b9c840a1
Felix Schindler <felix.schindler@wwu.de>
2021-02-23 09:44:30 +0100

    [P|local.integrands] add LocalIntersectionNormalComponentProductIntegrand

--- python/dune/gdt/local/integrands/intersection_product.cc
+++ python/dune/gdt/local/integrands/intersection_product.cc
@@ -82,13 +82,67 @@ public:
 }; // class LocalIntersectionProductIntegrand
 
 
+template <class G, class I, size_t r = 1, class F = double>
+class LocalIntersectionNormalComponentProductIntegrand
+{
+  using GP = XT::Grid::GridProvider<G>;
+  using E = typename I::Entity;
+  static const size_t d = G::dimension;
+
+public:
+  using type = GDT::LocalIntersectionNormalComponentProductIntegrand<I, r, F>;
+  using base_type = GDT::LocalBinaryIntersectionIntegrandInterface<I, r, 1, F, F, r, 1, F>;
+  using bound_type = pybind11::class_<type, base_type>;
+
+  static bound_type bind(pybind11::module& m,
+                         const std::string& layer_id = "",
+                         const std::string& grid_id = XT::Grid::bindings::grid_name<G>::value(),
+                         const std::string& class_id = "local_intersection_normal_component_product_integrand")
+  {
+    namespace py = pybind11;
+    using namespace pybind11::literals;
+
+    std::string class_name = class_id;
+    class_name += "_" + grid_id;
+    if (!layer_id.empty())
+      class_name += "_" + layer_id;
+    class_name += "_" + XT::Common::to_string(r) + "d_bases";
+    class_name += "_to_scalar";
+    if (!std::is_same<F, double>::value)
+      class_name += "_" + XT::Common::Typename<F>::value(/*fail_wo_typeid=*/true);
+    const auto ClassName = XT::Common::to_camel_case(class_name);
+    bound_type c(m, ClassName.c_str(), ClassName.c_str());
+    c.def(
+        py::init([](const XT::Functions::GridFunctionInterface<E, d, 1, F>& weight,
+                    const bool use_inside_bases,
+                    const std::string& logging_prefix) { return new type(weight, use_inside_bases, logging_prefix); }),
+        "weight"_a,
+        "use_inside_bases"_a = true,
+        "logging_prefix"_a = "");
+
+    // factories
+    const auto FactoryName = XT::Common::to_camel_case(class_id);
+    m.def(
+        FactoryName.c_str(),
+        [](const XT::Functions::GridFunctionInterface<E, d, 1, F>& weight,
+           const bool use_inside_bases,
+           const std::string& logging_prefix) { return new type(weight, use_inside_bases, logging_prefix); },
+        "weight"_a,
+        "use_inside_bases"_a = true,
+        "logging_prefix"_a = "");
+
+    return c;
+  } // ... bind(...)
+}; // class LocalIntersectionNormalComponentProductIntegrand
+
+
 } // namespace bindings
 } // namespace GDT
 } // namespace Dune
 
 
 template <class GridTypes = Dune::XT::Grid::bindings::AvailableGridTypes>
-struct LocalIntersectionProductIntegrand_for_all_grids
+struct LocalIntersectionProductIntegrands_for_all_grids
 {
   using G = Dune::XT::Common::tuple_head_t<GridTypes>;
   using GV = typename G::LeafGridView;
@@ -98,18 +152,22 @@ struct LocalIntersectionProductIntegrand_for_all_grids
   static void bind(pybind11::module& m)
   {
     using Dune::GDT::bindings::LocalIntersectionProductIntegrand;
+    using Dune::GDT::bindings::LocalIntersectionNormalComponentProductIntegrand;
 
     LocalIntersectionProductIntegrand<G, I>::bind(m);
-    if (d > 1)
-      LocalIntersectionProductIntegrand<G, I, d>::bind(m);
-    // add your extra dimensions here
-    // ...
-    LocalIntersectionProductIntegrand_for_all_grids<Dune::XT::Common::tuple_tail_t<GridTypes>>::bind(m);
+    LocalIntersectionNormalComponentProductIntegrand<G, I>::bind(m);
+    if (d > 1) {
+        LocalIntersectionProductIntegrand<G, I, d>::bind(m);
+        LocalIntersectionNormalComponentProductIntegrand<G, I, d>::bind(m);
+      // add your extra dimensions here
+      // ...
+    }
+    LocalIntersectionProductIntegrands_for_all_grids<Dune::XT::Common::tuple_tail_t<GridTypes>>::bind(m);
   }
 };
 
 template <>
-struct LocalIntersectionProductIntegrand_for_all_grids<Dune::XT::Common::tuple_null_type>
+struct LocalIntersectionProductIntegrands_for_all_grids<Dune::XT::Common::tuple_null_type>
 {
   static void bind(pybind11::module& /*m*/) {}
 };
@@ -129,5 +187,5 @@ PYBIND11_MODULE(_local_integrands_intersection_product, m)
 
   py::module::import("dune.gdt._local_integrands_binary_intersection_interface");
 
-  LocalIntersectionProductIntegrand_for_all_grids<XT::Grid::bindings::AvailableGridTypes>::bind(m);
+  LocalIntersectionProductIntegrands_for_all_grids<XT::Grid::bindings::AvailableGridTypes>::bind(m);
 }
