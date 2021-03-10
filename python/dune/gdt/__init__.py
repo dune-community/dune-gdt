b784d97592c0944010118d71e5e61c6356fd5cde
Felix Schindler <felix.schindler@wwu.de>
2021-03-10 16:31:47 +0100

    [P|space.bochner] add bindings

--- python/dune/gdt/__init__.py
+++ python/dune/gdt/__init__.py
@@ -70,6 +70,7 @@ for mod_name in (     # order should not matter!
         '_operators_matrix_based_factory',
         '_operators_operator',
         '_prolongations',
+        '_spaces_bochner',
         '_spaces_h1_continuous_lagrange',
         '_spaces_hdiv_raviart_thomas',
         '_spaces_interface',
