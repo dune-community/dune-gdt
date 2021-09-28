561867db4de5bf7b9c421a0c4d2901e44afb97d6
Tobias Leibner <tobias.leibner@googlemail.com>
2021-09-28 10:47:38 +0200

    Fix some more licenses

--- dune/gdt/functionals/vector-based.hh
+++ dune/gdt/functionals/vector-based.hh
@@ -1,13 +1,14 @@
-﻿// This file is part of the dune-gdt project:
-//   https://github.com/dune-community/dune-gdt
-// Copyright 2010-2018 dune-gdt developers and contributors. All rights reserved.
+// This file is part of the dune-gdt project:
+//   https://zivgitlab.uni-muenster.de/ag-ohlberger/dune-community/dune-gdt
+// Copyright 2010-2021 dune-gdt developers and contributors. All rights reserved.
 // License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 //      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 //          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
-//   Felix Schindler (2015 - 2017)
-//   Rene Milk       (2016 - 2018)
+//   Felix Schindler (2015 - 2018, 2020)
+//   René Fritze     (2016 - 2018)
 //   Tim Keil        (2017)
+//   Tobias Leibner  (2019 - 2020)
 
 #ifndef DUNE_GDT_FUNCTIONALS_VECTOR_BASED_HH
 #define DUNE_GDT_FUNCTIONALS_VECTOR_BASED_HH
--- dune/gdt/operators/laplace-ipdg-flux-reconstruction.hh
+++ dune/gdt/operators/laplace-ipdg-flux-reconstruction.hh
@@ -1,11 +1,12 @@
-﻿// This file is part of the dune-gdt project:
-//   https://github.com/dune-community/dune-gdt
-// Copyright 2010-2018 dune-gdt developers and contributors. All rights reserved.
+// This file is part of the dune-gdt project:
+//   https://zivgitlab.uni-muenster.de/ag-ohlberger/dune-community/dune-gdt
+// Copyright 2010-2021 dune-gdt developers and contributors. All rights reserved.
 // License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 //      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 //          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
-//   Felix Schindler (2019)
+//   Felix Schindler (2019 - 2020)
+//   Tobias Leibner  (2019 - 2020)
 
 #ifndef DUNE_GDT_OPERATORS_LAPLACE_IPDG_FLUX_RECONSTRUCTION_HH
 #define DUNE_GDT_OPERATORS_LAPLACE_IPDG_FLUX_RECONSTRUCTION_HH
