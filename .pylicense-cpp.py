8c7a38a509af8c2397a4e3314041073c4aa8e782
Tobias Leibner <tobias.leibner@googlemail.com>
2021-09-28 10:25:54 +0200

    Update pylicense to use zivgitlab repo

--- .pylicense-cpp.py
+++ .pylicense-cpp.py
@@ -11,8 +11,8 @@
 # ~~~
 
 name = 'This file is part of the dune-gdt project:'
-url = 'https://github.com/dune-community/dune-gdt'
-copyright_statement = 'Copyright 2010-2018 dune-gdt developers and contributors. All rights reserved.'
+url = 'https://zivgitlab.uni-muenster.de/ag-ohlberger/dune-community/dune-gdt'
+copyright_statement = 'Copyright 2010-2021 dune-gdt developers and contributors. All rights reserved.'
 license = '''Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
       or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
           with "runtime exception" (http://www.dune-project.org/license.html)'''
