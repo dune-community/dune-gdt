38e04d330aa5c1a9dc185767dd9b6f5d75130970
Tobias Leibner <tobias.leibner@googlemail.com>
2021-09-28 10:47:38 +0200

    Fix some more licenses

--- dune/gdt/spaces/hdiv/raviart-thomas.hh
+++ dune/gdt/spaces/hdiv/raviart-thomas.hh
@@ -1,12 +1,13 @@
-﻿// This file is part of the dune-gdt project:
-//   https://github.com/dune-community/dune-gdt
-// Copyright 2010-2018 dune-gdt developers and contributors. All rights reserved.
+// This file is part of the dune-gdt project:
+//   https://zivgitlab.uni-muenster.de/ag-ohlberger/dune-community/dune-gdt
+// Copyright 2010-2021 dune-gdt developers and contributors. All rights reserved.
 // License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 //      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 //          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
-//   Felix Schindler (2017 - 2018)
+//   Felix Schindler (2017 - 2020)
 //   René Fritze     (2018)
+//   Tobias Leibner  (2019 - 2020)
 
 #ifndef DUNE_GDT_SPACES_HDIV_RAVIART_THOMAS_HH
 #define DUNE_GDT_SPACES_HDIV_RAVIART_THOMAS_HH
